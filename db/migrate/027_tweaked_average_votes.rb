aaecfe2327f10464ddf3af7a9b99e03214aff9de
Jason Hollinger <pellaea@gmail.com>
2008-03-21 04:21:28 +0000

    Forgot to add migration.

---
+++ db/migrate/027_tweaked_average_votes.rb
@@ -0,0 +1,38 @@
+class TweakedAverageVotes < ActiveRecord::Migration
+  def self.up
+    add_column :namings, "vote_cache", :float, :default => 0
+
+    Naming.connection.update %(
+      UPDATE namings
+      SET vote_cache=(
+        SELECT sum(votes.value)/(count(votes.value)+1)
+        FROM votes
+        WHERE namings.id = votes.naming_id
+      )
+    )
+
+    for match in Observation.find_by_sql %(
+        SELECT o.id
+        FROM observations o, namings n
+        WHERE o.id = n.observation_id and o.name_id != n.name_id
+      )
+      o = Observation.find(match.id)
+      print "Updating observation ##{o.id}\n"
+      o.calc_consensus
+    end
+  end
+
+  def self.down
+    remove_column :namings, "vote_cache"
+
+    for match in Observation.find_by_sql %(
+        SELECT o.id
+        FROM observations o, namings n
+        WHERE o.id = n.observation_id and o.name_id != n.name_id
+      )
+      o = Observation.find(match.id)
+      print "Updating observation ##{o.id}\n"
+      o.calc_consensus
+    end
+  end
+end
