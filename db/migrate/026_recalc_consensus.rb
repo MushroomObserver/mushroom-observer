edc399a3005309896e978625f0c3d5fb58e02362
Nathan Wilson <nathan@collectivesource.com>
2008-03-23 03:57:01 +0000

    Made 026 migration a no-op given the 027 migration.

--- db/migrate/026_recalc_consensus.rb
+++ db/migrate/026_recalc_consensus.rb
@@ -1,14 +1,6 @@
 class RecalcConsensus < ActiveRecord::Migration
   def self.up
-    for match in Observation.find_by_sql %(
-        SELECT o.id
-        FROM observations o, namings n
-        WHERE o.id = n.observation_id and o.name_id != n.name_id
-      )
-      o = Observation.find(match.id)
-      print "Updating observation ##{o.id}\n"
-      o.calc_consensus
-    end
+    # Now taken care of 027
   end
 
   def self.down
