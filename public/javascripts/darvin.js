bf83c6a4656a6e46961e0e5bda5445e0891af0b6
Jason Hollinger <pellaea@gmail.com>
2008-08-17 08:33:50 +0000

    Got create_darvin to work with IE finally.  Ugh. -JPH

--- public/javascripts/darvin.js
+++ public/javascripts/darvin.js
@@ -1,8 +1,9 @@
+
 function de(e, mode, name) {
 
   // Mouse moving over or off of an item.
   if (mode < 2) {
-    $(e).style.background = mode ? "#FF8" : "#FFF";
+    $(e).style.background = mode ? '#FF8' : '#FFF';
   }
 
   // Click on genus changes list of species.
@@ -13,7 +14,7 @@ function de(e, mode, name) {
       if (species.substr(0,name.length) == name)
         list.push(species);
     }
-    darvin_draw("species", list);
+    darvin_draw('species', list);
   }
 
   // Click on species adds name.
@@ -33,7 +34,7 @@ function de(e, mode, name) {
     if (last == null || last < name)
       new_list.push(name);
     NAMES = new_list;
-    darvin_draw("names", NAMES);
+    darvin_draw('names', NAMES);
     darvin_set_names();
   }
 
@@ -44,38 +45,48 @@ function de(e, mode, name) {
       if (NAMES[i] != name)
         new_list.push(NAMES[i]);
     NAMES = new_list;
-    darvin_draw("names", NAMES);
+    darvin_draw('names', NAMES);
     darvin_set_names();
   }
 }
 
-// Draw contents of one of the three columns.  Section is "genera", "species"
-// or "names"; list is GENERA, SPECIES or NAMES.
+var IEFIX = (navigator.appVersion.indexOf('MSIE') > 0 &&
+             navigator.userAgent.indexOf('Opera') < 0);
+
+// Draw contents of one of the three columns.  Section is 'genera', 'species'
+// or 'names'; list is GENERA, SPECIES or NAMES.
 function darvin_draw(section, list) {
   var s = section.charAt(0);
-  var html = ""
+  var html = '';
   for (var i=0; i<list.length; i++) {
-    html += "<li" +
-      " id=\"" + s + i + "\"" +
-      " onmouseout=\"de('"  + s + i + "',0,0)\"" +
-      " onmouseover=\"de('" + s + i + "',1,0)\"" +
-      " onclick=\"de('"     + s + i + "',2,'" + list[i] + "')\"" +
-      "><nobr>" + list[i] + "</nobr></li>"
+    html += '<li' +
+      ' id="' + s + i + '"' +
+      ' onmouseout="de(\''  + s + i + '\',0,0)"' +
+      ' onmouseover="de(\'' + s + i + '\',1,0)"' +
+      ' onclick="de(\''     + s + i + '\',2,\'' + list[i] + '\')"' +
+      '><nobr>' + list[i] + '</nobr></li>'
   }
-  $(section).innerHTML = "<ul>" + html + "</ul>"
+  html = '<ul>' + html + '</ul>';
+  var e = $(section);
+  if (IEFIX) {
+    e.innerHTML = "";
+    setTimeout(function() { e.innerHTML = html }, 50);
+  } else {
+    e.innerHTML = html;
+  }
 }
 
-// Concat names in NAMES and store in hidden "results" field.
+// Concat names in NAMES and store in hidden 'results' field.
 function darvin_set_names() {
-  var val = ""
+  var val = '';
   for (var i=0; i<NAMES.length; i++)
     val += NAMES[i] + "\n";
-  $("results").value = val;
+  $('results').value = val;
 }
 
-// Reverse of above: parse hidden "results" field, and populate NAMES.
+// Reverse of above: parse hidden 'results' field, and populate NAMES.
 function darvin_initialize_names() {
-  var str = $("results").value || "";
+  var str = $('results').value || '';
   str += "\n";
   var x;
   for (var i=0; (x = str.indexOf("\n")) >= 0; ) {
