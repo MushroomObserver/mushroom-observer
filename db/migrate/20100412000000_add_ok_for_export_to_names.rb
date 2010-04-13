1f82bc10629c45fffd125cdae3791695fb531460
Jason Hollinger <pellaea@gmail.com>
2010-04-13 00:34:14 +0000

    Damn, forgot the migration.

---
+++ db/migrate/20100412000000_add_ok_for_export_to_names.rb
@@ -0,0 +1,25 @@
+class AddOkForExportToNames < ActiveRecord::Migration
+  def self.up
+    add_column :names, :ok_for_export, :boolean, :null => false, :default => true
+    add_column :locations, :ok_for_export, :boolean, :null => false, :default => true
+    add_column :location_descriptions, :ok_for_export, :boolean, :null => false, :default => true
+    add_column :name_descriptions, :project_id, :integer
+    add_column :location_descriptions, :project_id, :integer
+
+    for desc in NameDescription.find_all_by_source_type(:project)
+      project = Project.find_by_title(desc.source_name)
+      Name.connection.update %(
+        UPDATE name_descriptions SET project_id = #{project.id}
+        WHERE id = #{desc.id}
+      )
+    end
+  end
+
+  def self.down
+    remove_column :names, :ok_for_export
+    remove_column :locations, :ok_for_export
+    remove_column :location_descriptions, :ok_for_export
+    remove_column :name_descriptions, :project_id
+    remove_column :location_descriptions, :project_id
+  end
+end
