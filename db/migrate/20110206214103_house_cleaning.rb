ecc8ed84a09209d86ca7e57e864d41ab61c9c1a1
Nathan Wilson <nathan@collectivesource.com>
2011-02-08 01:29:56 +0000

    Migration for getting rid of add_image_test_logs, t1 and t2

---
+++ db/migrate/20110206214103_house_cleaning.rb
@@ -0,0 +1,29 @@
+class HouseCleaning < ActiveRecord::Migration
+  def self.up
+    drop_table :add_image_test_logs
+    drop_table :t1
+    drop_table :t2
+  end
+
+  def self.down
+    create_table "t2", :id => false, :force => true do |t|
+      t.integer "j"
+      t.integer "k"
+    end
+
+    create_table "t1", :id => false, :force => true do |t|
+      t.integer "i"
+      t.integer "j"
+    end
+
+    create_table "add_image_test_logs", :force => true do |t|
+      t.integer  "user_id" # Who ran the test
+      t.datetime "created_at" # When was the test created by visiting test_add_image
+      t.datetime "upload_start" # When did the upload start
+      t.datetime "upload_data_start" # When did actual data transfer start
+      t.datetime "upload_end" # When did the upload complete
+      t.integer  "image_count" # How many images were uploaded
+      t.integer  "image_bytes" # How many bytes were uploaded
+    end
+  end
+end
