704d136a9fd24e9ce3ceb2b7cf08f29efdf89f70
Jason Hollinger <pellaea@gmail.com>
2012-06-16 08:52:35 +0000

    Fixed the problems with the unit test runner that popped up with the upgrade to ruby 1.9.3.

--- test/boot.rb
+++ test/boot.rb
@@ -106,3 +106,29 @@ end
 # Re-raise errors caught by the controller.
 class ApplicationController; def rescue_action(e) raise e end; end
 
+require 'test/unit/ui/console/testrunner'
+
+# Apparently bugs in the new version of Test::Unit?  Probably because we're using
+# old version of rails...
+module Test
+  module Unit
+    module UI
+      module Console
+        class TestRunner
+          # When running in "show_detail_immediately" and "need_detail_faults"
+          # mode it totally screws up the assertion message. 
+          def output_fault_message(fault)
+            output_single(fault.message, fault_color(fault))
+          end
+
+          # It no longer prints dots for successful tests.
+          alias old_attach_to_mediator attach_to_mediator
+          def attach_to_mediator
+            old_attach_to_mediator
+            @mediator.add_listener(TestResult::FINISHED, &method(:test_finished))
+          end
+        end
+      end
+    end
+  end
+end
