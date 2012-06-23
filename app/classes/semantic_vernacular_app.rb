05e8a425e6797d9f96fee602ef452508aeb22b89
Han Wang <wangh17@rpi.edu>
2012-06-23 16:41:36 +0000

    Organizing continued

--- app/classes/semantic_vernacular_app.rb
+++ app/classes/semantic_vernacular_app.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 
 class SemanticVernacularApp
-  require_dependency "semantic_vernacular_app/semantic_vernacular_data_source"
-  require_dependency "semantic_vernacular_app/semantic_vernacular_data_source_via_proxy"
+  require_dependency "semantic_vernacular_app/semantic_vernacular_app_data_source"
+  require_dependency "semantic_vernacular_app/semantic_vernacular_app_data_source_via_proxy"
   require_dependency "semantic_vernacular_app/semantic_vernacular"
   require_dependency "semantic_vernacular_app/semantic_species"
 end
