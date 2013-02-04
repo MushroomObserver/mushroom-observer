e49cc9175fb673927c98fe420782ed71ce73276d
Nathan Wilson <nathan@collectivesource.com>
2013-02-04 15:06:46 -0500

    Attempt to make it re-runnable for vagrant

--- db/initialize.sql
+++ db/initialize.sql
@@ -1,5 +1,24 @@
+drop database if exists mo_development;
 create database mo_development;
+drop database if exists mo_test;
 create database mo_test;
-create user 'mo'@'localhost' identified by 'mo';
+	
+drop procedure if exists createUser;
+delimiter $$
+create procedure createUser(username varchar(50), pw varchar(50))
+begin
+IF (SELECT EXISTS(SELECT 1 FROM `mysql`.`user` WHERE `user` = username)) = 0 THEN
+    begin
+    set @sql = CONCAT('CREATE USER ', username, '@\'localhost\' IDENTIFIED BY \'', pw, '\'');
+    prepare stmt from @sql;
+    execute stmt;
+    deallocate prepare stmt;
+    end;
+END IF;
+end $$
+delimiter ;
+
+call createUser('mo', 'mo')
+
 grant all privileges on mo_development.* to 'mo'@'localhost' with grant option;
 grant all privileges on mo_test.* to 'mo'@'localhost' with grant option;
