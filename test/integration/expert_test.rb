22c21f5f35fea14dd2e634a557584de96dace22f
Jason <pellaea@gmail.com>
2014-07-24 14:43:31 -0700

    Rewrote name_description integration tests. Still some work to do with expert integration tests.

--- test/integration/expert_test.rb
+++ test/integration/expert_test.rb
@@ -14,255 +14,32 @@ class ExpertTest < IntegrationTestCase
     return hash
   end
 
-################################################################################
-
-  # -----------------------------------------
-  #  Test standard creation of public desc.
-  # -----------------------------------------
-
-  def test_creating_public_description
-    dick.admin = true
-    dick.save
-
-    admin    = login!(dick)     # admin
-    reviewer = login!(rolf)     # reviewer
-    owner    = login!(mary)     # owner
-    user     = login!(katrina)  # random user
-    lurker   = open_session     # not logged in
-
-    declare_abilities(admin,    :can_edit => true, :can_destroy => true,  :source_type_field => :enabled,  :source_name_field => :enabled,  :permission_fields => :enabled)
-    declare_abilities(reviewer, :can_edit => true, :can_destroy => true,  :source_type_field => :hidden,   :source_name_field => :enabled,  :permission_fields => :disabled)
-    declare_abilities(owner,    :can_edit => true, :can_destroy => false, :source_type_field => :no_field, :source_name_field => :enabled,  :permission_fields => :disabled)
-    declare_abilities(user,     :can_edit => true, :can_destroy => false, :source_type_field => :no_field, :source_name_field => :no_field, :permission_fields => :no_field)
-    declare_abilities(lurker,   :can_edit => false, :can_destroy => false)
-
-    admin.click(:href => /turn_admin_on/)
-
-    owner.create_public_description
-    owner.check_public_description(NameDescription.last)
-
-    admin.   check_abilities
-    reviewer.check_abilities
-    owner.   check_abilities
-    user.    check_abilities
-    lurker.  check_abilities
-  end
-
-  def declare_abilities(user, abilities)
-    user.extend(Module.new do
-      attr_accessor :abilities
-
-      def name
-        @name ||= Name.find_by_text_name('Strobilurus diminutivus')
-      end
-
-      def show_name
-        "/name/show_name/#{name.id}"
-      end
-
-      def create_public_description
-        assert_equal([], name.descriptions)
-        get(show_name)
-        click(:href => /create_name_description/)
-        assert_template('name/create_name_description')
-        open_form do |form|
-          fill_in(form)
-          form.submit
-        end
-        assert_flash_success
-        assert_template('name/show_name_description')
-      end
-
-      def fill_in(form)
-        form.assert_value('source_type', 'public')
-        form.assert_value('source_name', '')
-        form.assert_value('public_write', true)
-        form.assert_value('public', true)
-        form.assert_enabled('source_type')
-        form.assert_enabled('source_name')
-        # (have to be enabled because user could switch to :source or :user,
-        # instead must use javascript to disable these when :public)
-        form.assert_enabled('public_write')
-        form.assert_enabled('public')
-        form.change('notes', 'I like this mushroom.')
-      end
-
-      def check_public_description(desc)
-        assert_obj_list_equal([UserGroup.reviewers], desc.admin_groups)
-        assert_obj_list_equal([UserGroup.all_users], desc.writer_groups)
-        assert_obj_list_equal([UserGroup.all_users], desc.reader_groups)
-        assert_user_list_equal([], desc.authors)
-        assert_user_list_equal([mary], desc.editors) # (owner = mary)
-        assert_equal('I like this mushroom.', desc.notes)
-      end
-
-      def check_abilities
-        get(show_name)
-        assert_link_exists('edit_name_description', true)
-        assert_link_exists('destroy_name_description', abilities[:can_destroy])
-        click(:href => /edit_name_description/)
-        if abilities[:can_edit]
-          assert_template('name/edit_name_description')
-          check_name_description_fields
-        else
-          assert_template('account/login')
-        end
-      end
-
-      def assert_link_exists(name, val)
-        if val
-          assert_select("a[href*=#{name}]")
-        else
-          assert_select("a[href*=#{name}]", 0)
-        end
-      end
-
-      def check_name_description_fields
-        open_form do |form|
-          form.send("assert_#{abilities[:source_type_field]}", 'source_type')
-          form.send("assert_#{abilities[:source_name_field]}", 'source_name')
-          form.send("assert_#{abilities[:permission_fields]}", 'public_write')
-          form.send("assert_#{abilities[:permission_fields]}", 'public')
-        end
-      end
-    end)
-
-    user.abilities = abilities
-  end
-
-#   # -------------------------------------------
-#   #  Test standard creation of personal desc.
-#   # -------------------------------------------
-# 
-#   def test_creating_user_description
-#     name = Name.find_by_text_name('Peltigera')
-#     assert_equal(4, name.descriptions.length)
-# 
-#     dick.admin = true
-#     dick.save
-# 
-#     show_name = "/name/show_name/#{name.id}"
-# 
-#     admin    = login!(dick)     # we'll make him admin
-#     reviewer = login!(rolf)     # reviewer
-#     owner    = login!(mary)     # random user
-#     user     = login!(katrina)  # another random user
-#     lurker   = open_session     # nobody
-# 
-#     # Make Dick an admin.
-#     admin.click(:href => /turn_admin_on/)
-# 
-#     # Have random user create a personal description.
-#     in_session(owner) do
-#       get(show_name)
-#       click(:href => /create_name_description/)
-#       assert_template('name/create_name_description')
-#       open_form do |form|
-#         form.assert_value('source_type', 'public')
-#         form.assert_value('source_name', '')
-#         form.assert_value('public_write', true)
-#         form.assert_value('public', true)
-#         form.assert_enabled('source_type')
-#         form.assert_enabled('source_name')
-#         form.assert_enabled('public_write')
-#         form.assert_enabled('public')
-#         form.select('source_type', /user/i)
-#         form.change('source_name', "Mary's Corner")
-#         form.uncheck('public_write')
-#         form.change('gen_desc', 'Leafy felt lichens.')
-#         form.change('diag_desc', 'Usually with veins and tomentum below.')
-#         form.change('look_alikes', '_Solorina_ maybe, but not much else.')
-#         form.submit
-#       end
-#       assert_flash_success
-#       assert_template('name/show_name_description')
-#     end
-# 
-#     desc = NameDescription.last
-#     assert_equal(:user, desc.source_type)
-#     assert_equal("Mary's Corner", desc.source_name)
-#     assert_equal(false, desc.public_write)
-#     assert_equal(true, desc.public)
-#     assert_obj_list_equal([UserGroup.one_user(mary)], desc.admin_groups)
-#     assert_obj_list_equal([UserGroup.one_user(mary)], desc.writer_groups)
-#     assert_obj_list_equal([UserGroup.all_users], desc.reader_groups)
-#     assert_user_list_equal([mary], desc.authors)
-#     assert_user_list_equal([], desc.editors)
-#     assert_equal(empty_notes.merge(
-#       :gen_desc => 'Leafy felt lichens.',
-#       :diag_desc => 'Usually with veins and tomentum below.',
-#       :look_alikes => '_Solorina_ maybe, but not much else.'
-#     ), desc.all_notes)
-# 
-#     edit_name    = "/name/edit_name_description/#{desc.id}"
-#     destroy_name = "/name/destroy_name_description/#{desc.id}"
-# 
-#     # Admin of course can do anything.
-#     in_session(admin) do
-#       admin.get(show_name)
-#       admin.assert_select("a[href*=#{edit_name}]")
-#       admin.assert_select("a[href*=#{destroy_name}]")
-#       admin.click(:href => edit_name)
-#     end
-# 
-#     # Reviewer is nothing in this case.
-#     in_session(reviewer) do
-#       get(show_name)
-#       assert_select("a[href*=#{edit_name}]", 0)
-#       assert_select("a[href*=#{destroy_name}]", 0)
-#     end
-# 
-#     # Owner, is an admin and can do anything.
-#     # destroy.  But can edit.
-#     in_session(owner) do
-#       get(show_name)
-#       assert_select("a[href*=#{edit_name}]")
-#       assert_select("a[href*=#{destroy_name}]")
-#       click(:href => edit_name)
-#       assert_template('name/edit_name_description')
-#     end
-# 
-#     # Other random users are also nobodies.
-#     in_session(user) do
-#       get(show_name)
-#       assert_select("a[href*=#{edit_name}]", 0)
-#       assert_select("a[href*=#{destroy_name}]", 0)
-#     end
-# 
-#     # The lurker is nobody.
-#     in_session(lurker) do
-#       get(show_name)
-#       assert_select("a[href*=#{edit_name}]", 0)
-#       assert_select("a[href*=#{destroy_name}]", 0)
-#     end
-#   end
-# 
 #   # --------------------------------------------------------
 #   #  Test passing of arguments around in bulk name editor.
 #   # --------------------------------------------------------
-# 
+#
 #   def test_bulk_name_editor
 #     name1 = "Caloplaca arnoldii"
 #     author1 = "(Wedd.) Zahlbr."
 #     full_name1 = "#{name1} #{author1}"
-# 
+#
 #     name2 = "Caloplaca arnoldii ssp. obliterate"
 #     author2 = "(Pers.) Gaya"
 #     full_name2 = "#{name1} #{author2}"
-# 
+#
 #     name3 = "Acarospora nodulosa var. reagens"
 #     author3 = "Zahlbr."
 #     full_name3 = "#{name1} #{author3}"
-# 
+#
 #     name4 = "Lactarius subalpinus"
 #     name5 = "Lactarius newname"
-# 
+#
 #     list =
 #       "#{name1} #{author1}\r\n" +
 #       "#{name2} #{author2}\r\n" +
 #       "#{name3} #{author3}\r\n" +
 #       "#{name4} = #{name5}"
-# 
+#
 #     login!(dick)
 #     get('name/bulk_name_edit')
 #     open_form do |form|
@@ -273,7 +50,7 @@ class ExpertTest < IntegrationTestCase
 #     assert_flash_error
 #     assert_response(:success)
 #     assert_template('name/bulk_name_edit')
-# 
+#
 #     # Don't mess around, just let it do whatever it does, and make sure it is
 #     # correct.  I don't want to make any assumptions about how the internals
 #     # work (e.g., I don't want to make any assertions about the hidden fields)
@@ -286,52 +63,52 @@ class ExpertTest < IntegrationTestCase
 #     end
 #     assert_flash_success
 #     assert_template('observer/list_rss_logs')
-# 
+#
 #     assert_not_nil(Name.find_by_text_name('Caloplaca'))
-# 
+#
 #     names = Name.find_all_by_text_name(name1)
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal(author1, names.first.author)
 #     assert_equal(false, names.first.deprecated)
-# 
+#
 #     names = Name.find_all_by_text_name(name2.sub(/ssp/, 'subsp'))
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal(author2, names.first.author)
 #     assert_equal(false, names.first.deprecated)
-# 
+#
 #     names = Name.find_all_by_text_name(name2.sub(/ssp/, 'subsp'))
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal(author2, names.first.author)
 #     assert_equal(false, names.first.deprecated)
-# 
+#
 #     assert_not_nil(Name.find_by_text_name('Acarospora'))
 #     assert_not_nil(Name.find_by_text_name('Acarospora nodulosa'))
-# 
+#
 #     names = Name.find_all_by_text_name(name3)
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal(author3, names.first.author)
 #     assert_equal(false, names.first.deprecated)
-# 
+#
 #     names = Name.find_all_by_text_name(name4)
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal(false, names.first.deprecated)
-# 
+#
 #     names = Name.find_all_by_text_name(name5)
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal('', names.first.author)
 #     assert_equal(true, names.first.deprecated)
-# 
+#
 #     # I guess this is left alone, even though you would probably
 #     # expect it to be deprecated.
 #     names = Name.find_all_by_text_name('Lactarius alpinus')
 #     assert_equal(1, names.length, names.map(&:search_name).inspect)
 #     assert_equal(false, names.first.deprecated)
 #   end
-# 
+#
 #   # ----------------------------------------------------------
 #   #  Test passing of arguments around in species list forms.
 #   # ----------------------------------------------------------
-# 
+#
 #   def test_species_list_forms
 #     names = [
 #       'Petigera',
@@ -341,24 +118,24 @@ class ExpertTest < IntegrationTestCase
 #       'Caloplaca arnoldii ssp. obliterate',
 #     ]
 #     list = names.join("\r\n")
-# 
+#
 #     amanita = Name.find_all_by_text_name('Amanita baccata')
 #     suillus = Name.find_all_by_text_name('Suillus')
-# 
+#
 #     albion = locations(:albion)
 #     albion_name = albion.name
 #     albion_name_reverse = Location.reverse_name(albion.name)
-# 
+#
 #     new_location = 'Somewhere New, California, USA'
 #     new_location_reverse = 'USA, California, Somewhere New'
-# 
+#
 #     newer_location = 'Somewhere Else, California, USA'
 #     newer_location_reverse = 'USA, California, Somewhere Else'
-# 
+#
 #     # Good opportunity to test scientific location notation!
 #     dick.location_format = :scientific
 #     dick.save
-# 
+#
 #     # First attempt at creating a list.
 #     login!(dick)
 #     get('species_list/create_species_list')
@@ -376,9 +153,9 @@ class ExpertTest < IntegrationTestCase
 #     assert_flash_error
 #     assert_response(:success)
 #     assert_template('species_list/create_species_list')
-# 
+#
 #     assert_select('div#missing_names', /Caloplaca arnoldii ssp. obliterate/)
-#     
+#
 #     begin
 #       assert_select('div#deprecated_names', /Lactarius alpigenes/)
 #       assert_select('div#deprecated_names', /Lactarius alpinus/)
@@ -393,12 +170,12 @@ class ExpertTest < IntegrationTestCase
 #       assert(/Petigera/ =~ body)
 #       assert(/Peltigera/ =~ body)
 #     end
-#     
+#
 #     assert_select('div#ambiguous_names', /Amanita baccata.*sensu Arora/)
 #     assert_select('div#ambiguous_names', /Amanita baccata.*sensu Borealis/)
 #     assert_select('div#ambiguous_names', /Suillus.*Gray/)
 #     assert_select('div#ambiguous_names', /Suillus.*White/)
-# 
+#
 #     # Fix the ambiguous names: should be good now.
 #     open_form do |form|
 #       assert_equal(list.split(/\r\n/).sort,
@@ -409,7 +186,7 @@ class ExpertTest < IntegrationTestCase
 #     end
 #     assert_flash_success
 #     assert_template('species_list/show_species_list')
-# 
+#
 #     spl = SpeciesList.last
 #     obs = spl.observations
 #     assert_equal(5, obs.length, obs.map(&:text_name).inspect)
@@ -427,7 +204,7 @@ class ExpertTest < IntegrationTestCase
 #     assert_equal('Member notes.', obs.last.notes)
 #     assert_true(obs.last.is_collection_location)
 #     assert_true(obs.last.specimen)
-# 
+#
 #     # Try making some edits, too.
 #     click(:href => /edit_species_list/)
 #     open_form do |form|
@@ -450,11 +227,11 @@ class ExpertTest < IntegrationTestCase
 #     assert_flash_error
 #     assert_response(:success)
 #     assert_template('species_list/edit_species_list')
-# 
+#
 #     assert_select('div#missing_names', /Agaricus nova/)
 #     assert_select('div#ambiguous_names', /Amanita baccata.*sensu Arora/)
 #     assert_select('div#ambiguous_names', /Amanita baccata.*sensu Borealis/)
-# 
+#
 #     # Fix the ambiguous name.
 #     open_form do |form|
 #       form.check(/chosen_multiple_names_\d+_#{amanita[1].id}/)
@@ -462,7 +239,7 @@ class ExpertTest < IntegrationTestCase
 #     end
 #     assert_flash_success
 #     assert_template('location/create_location')
-# 
+#
 #     spl.reload
 #     obs = spl.observations
 #     assert_equal(7, obs.length, obs.map(&:text_name).inspect)
@@ -485,7 +262,7 @@ class ExpertTest < IntegrationTestCase
 #     assert_equal('New member notes.', obs.last.notes)
 #     assert_false(obs.last.is_collection_location)
 #     assert_false(obs.last.specimen)
-# 
+#
 #     # Should have chained us into create_location.  Define this location
 #     # and make sure it updates both the observations and the list.
 #     open_form do |form|
@@ -501,7 +278,7 @@ class ExpertTest < IntegrationTestCase
 #     assert_template('species_list/show_species_list')
 #     assert_select('div#Title', :text => /#{spl.title}/)
 #     assert_select("a[href*=edit_species_list/#{spl.id}]", :text => /edit/i)
-# 
+#
 #     loc = Location.last
 #     assert_equal(newer_location, loc.name)
 #     assert_equal(dick, User.current)
@@ -512,7 +289,7 @@ class ExpertTest < IntegrationTestCase
 #     assert_equal(loc, spl.location)
 #     assert_equal(nil, obs.last.where)
 #     assert_equal(loc, obs.last.location)
-#     
+#
 #     # Try adding a comment, just for kicks.
 #     click(:href => /add_comment/)
 #     assert_template('comment/add_comment')
