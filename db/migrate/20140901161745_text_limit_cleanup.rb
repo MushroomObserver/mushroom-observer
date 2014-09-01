2baff2a0752b0d8e003f6be36d953daa80374e1d
Nathan Wilson <nathan@collectivesource.com>
2014-09-01 16:30:29 +0000

    Migration for the text limit issue

---
+++ db/migrate/20140901161745_text_limit_cleanup.rb
@@ -0,0 +1,10 @@
+class TextLimitCleanup < ActiveRecord::Migration
+  def up
+    change_column(:herbaria, :mailing_address, :text, :limit => nil)
+    change_column(:herbaria, :description, :text, :limit => nil)
+    change_column(:specimens, :notes, :text, :limit => nil)
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140804135800) do
+ActiveRecord::Schema.define(:version => 20140901161745) do
 
   create_table "api_keys", :force => true do |t|
     t.datetime "created_at"
