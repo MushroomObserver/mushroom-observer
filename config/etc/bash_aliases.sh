cb1acb1fecb8b2826b44a492c45f10416a17669e
Jason <pellaea@gmail.com>
2014-09-05 23:14:47 -0700

    Still working out environment kinks on production server.

--- config/etc/bash_aliases.sh
+++ config/etc/bash_aliases.sh
@@ -22,34 +22,10 @@ function pfind {
 
 alias mo='cd /var/web/mo'
 
-alias mosql="mysql -u mo -p'k498dnt4' mo_production"
-function mosqle {
-  mysql -u mo -p'k498dnt4' mo_production -e "source $1"
-}
+alias uni='service unicorn'
 
-function uni {
-  case $1 in
-  show)
-    ps -ef | grep unicorn ;;
-  start)
-    unicorn_rails -c /var/web/mo/config/unicorn.rb -D ;;
-  reload)
-    kill -HUP `cat /var/web/mo/tmp/unicorn.pid` ;;
-  stop)
-    kill -QUIT `cat /var/web/mo/tmp/unicorn.pid` ;;
-  kill)
-    kill -TERM `cat /var/web/mo/tmp/unicorn.pid` ;;
-  *)
-    echo
-    echo "USAGE: uni <command>"
-    echo
-    echo COMMANDS:
-    echo "  show     Show unicorn processes."
-    echo "  start    Start unicorn."
-    echo "  reload   Reload application."
-    echo "  stop     Shutdown unicorn and workers gracefully, waiting for them to"
-    echo "           finish serving their last request."
-    echo "  kill     Kill unicorn and workers immediately."
-    echo
-  esac
+alias mosql='mysql -u mo -p"k498dnt4" mo_production'
+
+function mosqle {
+  mysql -u mo -p"k498dnt4" mo_production -e "source $1"
 }
