1a2fdbd6961ef262b75cf00a76c098abfe082741
Nathan Wilson <nathan@collectivesource.com>
2014-10-16 18:51:39 +0000

    Increased unicorn and nginx timeouts

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ worker_processes 4
 working_directory APP_PATH
 listen APP_PATH + '/tmp/.unicorn.sock', :backlog => 64
 listen 8080, :tcp_nopush => true
-timeout 30
+timeout 300
 pid APP_PATH + '/tmp/unicorn.pid'
 stderr_path APP_PATH + '/log/unicorn.stderr.log'
 stdout_path APP_PATH + '/log/unicorn.stdout.log'
