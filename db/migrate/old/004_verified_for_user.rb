0bd9d3ac51cae60dd6d457de36c85fe9b588757c
Nathan Wilson <nathan@collectivesource.com>
2014-10-17 21:53:32 +0000

    Got rid of db/migrate/old

--- db/migrate/old/004_verified_for_user.rb
+++
@@ -1,9 +0,0 @@
-class VerifiedForUser < ActiveRecord::Migration
-  def self.up
-    add_column :users,  "verified", :datetime
-  end
-
-  def self.down
-    remove_column :users,  "verified"
-  end
-end
