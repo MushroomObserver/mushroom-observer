c2f830a7aad492a659fd156b8c9b303e82b7af94
Joe Cohen <jdcohenesq@gmail.com>
2014-11-19 15:23:08 -0800

    correct route to fix "ArgumentError: ':controller' is not a supported controller name" when running rake

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,31 @@ MushroomObserver::Application.routes.draw do
   # map.resources :publications
   get 'publications/:id/destroy' => 'publications#destroy'
   resources :publications
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
-  
+
   # Default page is /observer/index.
   # map.connect '', :controller => 'observer', :action => 'index'
   root :to => "observer#index"
-  
+
   # Route /123 to /observer/show_observation/123.
   # map.connect ':id', :controller => 'observer', :action => 'show_observation', :id => /\d+/
   get ':id' => "observer#show_observation", :constraints => { :id => /\d+/ }
-  
+
   # Short-hand notation for AJAX methods.
   # map.connect 'ajax/:action/:type/:id', :controller => 'ajax', :id => /\S.*/
   get 'ajax/:action/:type/:id' => "ajax", :constraints => { :id => /\S.*/ }
 
   # Default action for any controller is 'index'.
   # map.connect ':controller', :action => 'index'
-  get ':controller' => ':controller#index'
-  
+  get ':controller', :action => 'index'
+
   # Standard routes.
   # map.connect ':controller/:action'
   get ':controller/:action'
   # map.connect ':controller/:action/:id', :id => /\d+/
   match ':controller/:action/:id', :constraints => { :id => /\d+/ }, via: [:get, :post]
-  
+
   # Accept non-numeric ids for the lookup_xxx actions.
   # map.connect ':controller/:action/:id', :action => /lookup_\w+/
   get ':controller/:action/:id', :action => /lookup_\w+/
