7690a903045b2a3feeb553d91a1b22e5ea1d94f5
Joe Cohen <jdcohenesq@gmail.com>
2014-11-19 20:20:53 -0800

    Removed config.action_dispatch.best_standards_support per http://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#action-pack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,9 +66,6 @@ MushroomObserver::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
 
