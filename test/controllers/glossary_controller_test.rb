c31a9421a898951e86861914e28fc5f64c8877dd
Joe Cohen <jdcohenesq@gmail.com>
2014-12-16 17:26:37 -0800

    fix all Errors and Failures caused by glossary_controller_test * fix use of assert_template with unknown keys    * remove keys or use assert_redirected_to * fix mass assignment Errors * modernize query syntax * some style changes per [Ruby Style Guide](https://github.com/bbatsov/ruby-style-guide) * miscellaneous other fixes

--- test/controllers/glossary_controller_test.rb
+++ test/controllers/glossary_controller_test.rb
@@ -1,27 +1,29 @@
-require 'test_helper'
+require "test_helper"
 
+# tests of GlossaryController
 class GlossaryControllerTest < FunctionalTestCase
   def test_show_glossary_term
     glossary_term = glossary_terms(:plane_glossary_term)
-    get_with_dump(:show_glossary_term, :id => glossary_term.id)
-    assert_template(action: 'show_glossary_term')
+    get_with_dump(:show_glossary_term, id: glossary_term.id)
+    assert_template("show_glossary_term")
   end
 
   def test_show_past_glossary_term
     conic = glossary_terms(:conic_glossary_term)
-    get_with_dump(:show_past_glossary_term, :id => conic.id, :version => conic.version - 1)
-    assert_template(action: 'show_past_glossary_term', partial: "_glossary_term")
+    get_with_dump(:show_past_glossary_term, id: conic.id,
+                  version: conic.version - 1)
+    assert_template("show_past_glossary_term", partial: "_glossary_term")
   end
 
   def test_show_past_glossary_term_no_version
     conic = glossary_terms(:conic_glossary_term)
-    get_with_dump(:show_past_glossary_term, :id => conic.id)
+    get_with_dump(:show_past_glossary_term, id: conic.id)
     assert_response(:redirect)
   end
 
   def test_index
     get_with_dump(:index)
-    assert_template(action: 'index')
+    assert_template("index")
   end
 
   def test_create_glossary_term
@@ -30,22 +32,15 @@ class GlossaryControllerTest < FunctionalTestCase
 
     login
     get_with_dump(:create_glossary_term)
-    assert_template(action: 'create_glossary_term')
+    assert_template("create_glossary_term")
   end
 
   def create_glossary_term_params
     return {
-      :glossary_term => {
-        :name => 'Convex',
-        :description => 'Boring old convex',
-      },
-      :copyright_holder => "Insil Choi",
-      :date => {
-        :copyright_year => '2013'
-      },
-      :upload => {
-        :license_id => '1'
-      }
+      glossary_term: { name: "Convex", description: "Boring old convex" },
+      copyright_holder: "Insil Choi",
+      date: { copyright_year: "2013" },
+      upload: { license_id: "1" }
     }
   end
 
@@ -53,10 +48,11 @@ class GlossaryControllerTest < FunctionalTestCase
     user = login
     params = create_glossary_term_params
     post(:create_glossary_term, params)
-    # glossary_term = GlossaryTerm.find(:all, :order => "created_at DESC")[0] # Rails 3
-    glossary_term = GlossaryTerm.all.order("created_at DESC")[0]
+    glossary_term = GlossaryTerm.order(created_at: :desc).first
+
     assert_equal(params[:glossary_term][:name], glossary_term.name)
-    assert_equal(params[:glossary_term][:description], glossary_term.description)
+    assert_equal(params[:glossary_term][:description],
+                 glossary_term.description)
     assert_not_nil(glossary_term.rss_log)
     assert_equal(user.id, glossary_term.user_id)
     assert_response(:redirect)
@@ -64,12 +60,12 @@ class GlossaryControllerTest < FunctionalTestCase
 
   def test_edit_glossary_term
     conic = glossary_terms(:conic_glossary_term)
-    get_with_dump(:edit_glossary_term, :id => conic.id)
+    get_with_dump(:edit_glossary_term, id: conic.id)
     assert_response(:redirect)
 
     login
-    get_with_dump(:edit_glossary_term, :id => conic.id)
-    assert_template(action: 'edit_glossary_term')
+    get_with_dump(:edit_glossary_term, id: conic.id)
+    assert_template("edit_glossary_term")
   end
 
   def test_edit_glossary_term_post
@@ -81,6 +77,7 @@ class GlossaryControllerTest < FunctionalTestCase
     params[:id] = conic.id
     post(:edit_glossary_term, params)
     conic.reload
+
     assert_equal(params[:glossary_term][:name], conic.name)
     assert_equal(params[:glossary_term][:description], conic.description)
     assert_equal(count+1, GlossaryTerm::Version.count)
@@ -91,12 +88,12 @@ class GlossaryControllerTest < FunctionalTestCase
     login
     glossary_term = glossary_terms(:plane_glossary_term)
     old_count = glossary_term.versions.length
-    glossary_term.update(:description => 'Are we flying yet?')
+    glossary_term.update(description: "Are we flying yet?")
     glossary_term.reload
     new_count = glossary_term.versions.length
     assert_equal(1, new_count - old_count)
-    get_with_dump(:show_past_glossary_term, :id => glossary_term.id, :version => glossary_term.version - 1)
-    assert_template(action: 'show_past_glossary_term', partial: "_glossary_term")
+    get_with_dump(:show_past_glossary_term, id: glossary_term.id,
+                  version: glossary_term.version - 1)
+    assert_template("show_past_glossary_term", partial: "_glossary_term")
   end
-
 end
