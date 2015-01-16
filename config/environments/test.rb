c8254f41accb9d27f573142a44a70cf757e43fc9
Joe Cohen <jdcohenesq@gmail.com>
2015-01-15 17:53:34 -0800

    conform config/test.rb to Rails 4.0 default

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,24 +19,24 @@ MushroomObserver::Application.configure do
   # Keep thumbnails locally, and only copy thumbnails to second server.
   config.local_image_files = "#{config.root}/public/test_images"
   config.image_sources = {
-    :local => {
-      :test => "file://#{config.local_image_files}",
-      :read => "/local_images",
+    local: {
+      test: "file://#{config.local_image_files}",
+      read: "/local_images",
     },
-    :remote1 => {
-      :test  => :transferred_flag,
-      :read  => "/remote_images",
-      :write => "file://#{config.root}/public/test_server1"
+    remote1: {
+      test: :transferred_flag,
+      read: "/remote_images",
+      write: "file://#{config.root}/public/test_server1"
     },
-    :remote2 => {
-      :write => "file://#{config.root}/public/test_server2",
+    remote2: {
+      write: "file://#{config.root}/public/test_server2",
       # Having trouble getting this to work on vagrant machine...
-      # :write => "ssh://vagrant@localhost:#{config.root}/public/test_server2",
-      :sizes => [ :thumbnail, :small, :medium ]
+      # write: "ssh://vagrant@localhost:#{config.root}/public/test_server2",
+      sizes: [ :thumbnail, :small, :medium ]
     }
   }
   config.image_precedence = {
-    :default => [:local, :remote1 ]
+    default: [:local, :remote1 ]
   }
   config.image_fallback_source = :remote1
   config.keep_these_image_sizes_local = [ :thumbnail, :small ]
@@ -51,8 +51,14 @@ MushroomObserver::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil.
-  # config.whiny_nils = true # Rails 3 (Rails 4 removes this setting.)
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+
+  # Configure static asset server for tests with Cache-Control for performance.
+  config.serve_static_assets  = true
+  config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -77,9 +83,8 @@ MushroomObserver::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
-  config.static_cache_control = 'public, max-age=3600'
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 
   config.eager_load = false
 end
