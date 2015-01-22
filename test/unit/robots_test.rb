390ffdb1955f602e95ede3d941b05ebe46c8abb4
Jason <pellaea@gmail.com>
2015-01-22 09:28:03 -0800

    Arg.

--- test/unit/robots_test.rb
+++ test/unit/robots_test.rb
@@ -5,10 +5,9 @@ class RobotsTest < UnitTestCase
   def test_parse_robots_dot_text
     file = "#{::Rails.root}/test/fixtures/robots.txt"
     data = Robots.parse_robots_dot_text(file)
-    assert_equal(["glossary/show_term", "species_list/show_species_list"],
-                 data.keys.sort)
+    assert_equal(["glossary/show_term", "observer/intro"], data.keys.sort)
     assert_true(data["glossary/show_term"])
-    assert_true(data["species_list/show_species_list"])
-    assert_false(data["observer/really_long_query"])
+    assert_true(data["observer/intro"])
+    assert_false(data["name/really_long_query"])
   end
 end
