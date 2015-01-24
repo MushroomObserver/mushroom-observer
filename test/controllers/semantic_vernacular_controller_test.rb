f1953e6053c6a5955509ae8135df7cda523b4235
Joe Cohen <jdcohenesq@gmail.com>
2015-01-23 17:24:24 -0800

    fix Errors caused by semantic_vernacular_controller_test * make SVD model available to controller * fix assert_template issues in test * trivial style changes to test

--- test/controllers/semantic_vernacular_controller_test.rb
+++ test/controllers/semantic_vernacular_controller_test.rb
@@ -1,25 +1,26 @@
-require 'test_helper'
+
+require "test_helper"
 
 class SemanticVernacularControllerTest < FunctionalTestCase
-  
   def test_index
     begin
-      get(:index) 
+      get(:index)
       assert_response(:redirect)
       login
       get_with_dump(:index)
-      assert_template(action: 'index')
+      assert_template(:index)
     rescue Errno::EHOSTUNREACH => err
     end
   end
 
   def test_show
+  uri = "http://aquarius.tw.rpi.edu/ontology/svf.owl#SVD1"
     begin
-    	get(:show, :uri => "http://aquarius.tw.rpi.edu/ontology/svf.owl#SVD1")
+    	get(:show, uri: uri)
     	assert_response(:redirect)
     	login
-    	get_with_dump(:show, :uri => "http://aquarius.tw.rpi.edu/ontology/svf.owl#SVD1")
-      assert_template(action: 'show')
+    	get_with_dump(:show, uri: uri)
+      assert_template(:show)
     	assert_not_nil(assigns(:svd))
     rescue Errno::EHOSTUNREACH => err
     rescue ActionView::TemplateError => err
@@ -27,14 +28,14 @@ class SemanticVernacularControllerTest < FunctionalTestCase
   end
 
   # Need a real test framework to do anything meaningful.
-  # In the meantime, simply ensure that both logged out and logged in users get redirected
-  # since delete requires admin.
+  # In the meantime, simply ensure that both logged out and logged in users get
+  # redirected since delete requires admin.
   def test_delete
     begin
-      get(:delete) 
+      get(:delete)
       assert_response(:redirect)
       login
-      get(:delete) 
+      get(:delete)
       assert_response(:redirect)
     rescue Errno::EHOSTUNREACH => err
     end
