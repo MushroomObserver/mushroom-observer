51c2ce5f85594fdcd95c009d8eafecefdbc524c9
Joe Cohen <jdcohenesq@gmail.com>
2015-02-09 17:12:32 -0800

    Simplify extensions * remove MO methods which are now in Ruby or Rails * write some tests * minor refactoring, including Rubocopping

---
+++ test/models/array_extensions_test.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+require "test_helper"
+
+# test MO extensions to Ruby's Array class
+class ArrayExtensionsTest < UnitTestCase
+  def test_to_boolean_hash
+    assert_equal({}, [].to_boolean_hash)
+    assert_equal({ "a" => true, "b" => true, "c" => true },
+                 %w(a b c).to_boolean_hash)
+  end
+
+  def test_safe_join
+    array1 = ["<p>foo</p>".html_safe, "<p>bar</p>"]
+    array2 = ["<p>foo</p>".html_safe, "<p>bar</p>".html_safe]
+
+    assert_equal("<p>foo</p>&lt;br /&gt;&lt;p&gt;bar&lt;/p&gt;",
+                 array1.safe_join("<br />"),
+                 "safe_joiner returned incorrect value")
+    assert_equal("<p>foo</p><br /><p>bar</p>",
+                 array2.safe_join("<br />".html_safe),
+                 "safe_joiner returned incorrect value")
+  end
+end
