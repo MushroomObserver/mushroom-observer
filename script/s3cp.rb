bcfdc92ef610c461d6c1603d005dda3be664416d
Jason <pellaea@gmail.com>
2015-03-01 13:51:59 -0800

    Finished filling out and testing script/s3xxx.rb helper scripts.

--- script/s3cp.rb
+++ script/s3cp.rb
@@ -1,41 +1,74 @@
 #!/usr/bin/env ruby
-#
-#  USAGE::
-#
-#    script/s3cp.rb file server key [--update-cache]
-#
-#  DESCRIPTION::
-#
-#    Uploads a single local file to an S3 server.
-#
-#  PARAMETERS::
-#
-#    file            Source file.
-#    server          Primary key in MO.s3_credentials configuration hash.
-#    key             Key to store file under on S3 server.
-#    --update-cache  Update cached listing for this server if successful.
-#
-################################################################################
 
 app_root = File.expand_path("../..", __FILE__)
 require "#{app_root}/app/classes/image_s3.rb"
+require "fileutils"
 
-file, server, key, update = ARGV
+abort(<<"EOB") if ARGV.any? {|arg| arg == "-h" || arg == "--help" }
 
-if !File.exists? file
-  $stderr.puts("File doesn't exist: #{file.inspect}")
-  exit 1
+  USAGE::
+
+    script/s3cp.rb file server key [--cache]
+    script/s3cp.rb --delete server key [--cache]
+
+  DESCRIPTION::
+
+    The first form uploads a single local file to an S3 server.
+    The second form deletes a single key from an S3 server.
+
+  PARAMETERS::
+
+    file       Source file.
+    server     Primary key in MO.s3_credentials configuration hash.
+    key        Key to store file under on S3 server.
+    --cache    Check cache to see if the file has already been uploaded.
+               Update cached listing for this server if upload is successful.
+    --verbose  Print what's happening to stdout.
+    --help     Print this message.
+
+EOB
+
+delete  = true if ARGV.any? {|arg| arg == "-d" || arg == "--delete"  }
+cache   = true if ARGV.any? {|arg| arg == "-c" || arg == "--cache"   }
+verbose = true if ARGV.any? {|arg| arg == "-v" || arg == "--verbose" }
+copy = !delete
+flags = ARGV.select {|arg| arg.match(/^-/)}.
+             reject {|arg| arg.match(/^(-c|-d|-v|--cache|--delete|--verbose)$/)}
+words = ARGV.reject {|arg| arg.match(/^-/)}
+abort("Bad flag(s): #{flags.inspect}") if flags.length > 0
+if copy
+  abort("Missing file!") if words.length == 0
+  file = words.shift
+  abort("File doesn't exist: #{file.inspect}") if !File.exists?(file)
 end
-
-if update && update != "--update-cache"
-  $stderr.puts("Invalid parameter: #{update.inspect}")
-  exit 1
-end
+abort("Missing server!") if words.length == 0
+server = words.shift
+abort("Missing key!") if words.length == 0
+key = words.shift
+abort("Unexpected parameter(s): #{words.inspect}") if words.length > 0
 
 cache_file = "#{app_root}/public/images/#{server}.files"
-size = File.size(file)
-md5  = `md5sum #{file}`.split.first
-type = `file --mime-type #{file}`.split.last
+temp_file  = "#{app_root}/tmp/#{server}.files.#{Process.pid}"
+
+def log(msg)
+  $stdout.write(msg)
+  $stdout.flush
+end
+
+if copy
+  size = File.size(file)
+  md5  = `md5sum #{file}`.split.first
+  type = `file --mime-type #{file}`.split.last
+
+  # Trust cached listing: no need to resend a file if we've already logged it.
+  if cache
+    old_size, old_md5 = `grep ^#{key} #{cache_file} | tail -1`.split[1,2]
+    if old_size == size.to_s && old_md5 == md5
+      log("Already uploaded #{server}/#{key}\n") if verbose
+      exit 0
+    end
+  end
+end
 
 # Hacky way to grab MO.s3_credentials from config files using script/config.rb.
 cmd = File.expand_path("../../script/config.rb", __FILE__)
@@ -43,8 +76,10 @@ url               = `#{cmd} MO.s3_credentials[:#{server}][:server]`
 bucket            = `#{cmd} MO.s3_credentials[:#{server}][:bucket]`
 access_key_id     = `#{cmd} MO.s3_credentials[:#{server}][:access_key_id]`
 secret_access_key = `#{cmd} MO.s3_credentials[:#{server}][:secret_access_key]`
+abort("Bad server: #{server.inspect}") if !url
 
 begin
+  log("Connecting...\r") if verbose
   s3 = ImageS3.new(
     server: url,
     bucket: bucket,
@@ -52,25 +87,37 @@ begin
     secret_access_key: secret_access_key
   )
 rescue => e
-  $stderr.puts("Upload failed on #{server}/#{key}: couldn't connect: #{e}");
-  exit 1
+  abort("Upload failed on #{server}/#{key}: couldn't connect: #{e}");
 end
 
-begin
-  result = s3.upload(key, file, content_type: type)
-  if result.etag != "\"#{md5}\""
-    $stderr.puts("Upload failed on #{server}/#{key}: md5sum didn't match")
-    s3.delete(key) rescue nil
-    exit 1
+if copy
+  begin
+    log("Uploading... \r") if verbose
+    result = s3.upload(key, file, content_type: type)
+    if result.etag != "\"#{md5}\""
+      s3.delete(key) rescue nil
+      abort("Upload failed on #{server}/#{key}: md5sum didn't match")
+    end
+  rescue => e
+    abort("Upload failed on #{server}/#{key}: #{e}")
   end
-rescue => e
-  $stderr.puts("Upload failed on #{server}/#{key}: #{e}")
-  exit 1
+  log("Uploaded #{server}/#{key}\n") if verbose
+  if cache
+    File.open(cache_file, "a") do |fh|
+      fh.puts [key, size, md5].join("\t")
+    end
+  end
 end
 
-if update
-  File.open(cache_file, "a") do |fh|
-    fh.puts [key, size].join("\t")
+if delete
+  log( "Deleting...  \r") if verbose
+  s3.delete(key)
+  log("Deleted #{server}/#{key}\n") if verbose
+  if cache
+    FileUtils.mv(cache_file, temp_file)
+    FileUtils.touch(cache_file)
+    system("grep -v ^#{key} #{temp_file} >> #{cache_file}")
+    FileUtils.rm(temp_file)
   end
 end
 
---
+++ script/s3uniq.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+abort(<<"EOB") if ARGV.any? {|arg| arg == "-h" || arg == "--help" }
+
+  USAGE::
+
+    cat input_files | script/s3uniq.rb > output_file
+
+  DESCRIPTION::
+
+    Reads key/value pairs from stdout and prints only the last entry
+    corresponding to each key.  Input and outout are tab-delimited.
+
+EOB
+
+abort("Unexpected parameters: #{ARGV.inspect}") if ARGV.length > 0
+
+data = Hash.new
+$stdin.each_line do |line|
+  key, val = line.split("\t", 2)
+  data[key] = val
+end
+data.each do |key, val|
+  $stdout.write [key, val].join("\t")
+end
+
+exit 0
