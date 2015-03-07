66b7e1da8f1f63e30cea79967e34a5392132ca8e
Jason <pellaea@gmail.com>
2015-03-07 11:04:25 -0800

    Tweaked theater to only load medium-sized image if can't display larger. Moved theater JS out into new file, since it's gotten rather large. Thumbnail passes in image width and height if available, allowing JS to choose and position image very precisely in most cases.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,10 +3,11 @@
 //= require bootstrap
 //= require jstz
 //= require autocomplete
+//= require box_resizer
 //= require confirm
 //= require csrf_token
 //= require image_vote
 //= require image_export
+//= require theater
 //= require validate_file_input_fields
-//= require box_resizer
 //= require main
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,8 +2,9 @@
  * This should be included on every page.
  */
 jQuery(document).ready(function () {
-    // this works better than straing autofocus attribute in firefox
-    // normal autofocus causes it to scroll window hiding title etc.
+
+    // This works better than straight autofocus attribute in firefox.
+    // Normal autofocus causes it to scroll window hiding title etc.
     jQuery('[data-autofocus=true]').first().focus();
 
     jQuery('[data-role=link]').on('click', function() {
@@ -29,73 +30,6 @@ jQuery(document).ready(function () {
     jQuery('[data-dismiss="alert"]').click(function() {
         setCookie('hideBanner', banner_md5, 30);
     });
-
-    jQuery('body').delegate('[data-toggle="expand-icon"]', "mouseenter mouseleave", function (e){
-        var btn = jQuery(this).find('.theater-btn');
-        if(e.type == "mouseleave") {
-            return btn.hide();
-        }
-        var img = jQuery(this).find('img');
-        btn.css('right', img.position().left);
-        btn.show();
-    });
-
-    jQuery('body').delegate('[data-toggle="theater"]', 'click', function (e) {
-        e.preventDefault();
-        jQuery('body').addClass('theater-shown');
-        var win = jQuery(window),
-            doc = jQuery(document),
-            div = jQuery('.img-theater-div'),
-            w = win.width(),
-            h = win.height(),
-            img_orig = jQuery(this).data().orig,
-            img_src = w-40 > 1280 || h-40 > 1280 ? jQuery(this).data().full :
-                      w-40 > 960  || h-40 > 960  ? jQuery(this).data().huge :
-                                                   jQuery(this).data().large;
-        var img, img_w, img_h;
-        var reposition_image = function() {
-            var win_w = jQuery(window).width(),
-                win_h = jQuery(window).height();
-            if (img_w < win_w - 40 && img_h < win_h - 40) {
-                // image fits unscaled
-                pad = Math.round((win_h - img_h) / 2);
-            } else if ((win_h - 40) * img_w / img_h > win_w - 40) {
-                // scale up so takes up full width
-                pad = Math.round((win_h - (win_w - 40) * img_h / img_w) / 2);
-            } else {
-                // scale up so takes up full height
-                pad = 20;
-            }
-            // console.log("img: "+img_w+","+img_h + " win: "+win_w+","+win_h + " -> pad: " + pad);
-            div.css('padding', pad + 'px 20px');
-        };
-        img = new Image();
-        img.onload = function() {
-            img_w = this.width;
-            img_h = this.height;
-            reposition_image();
-            win.on('resize.theater', reposition_image);
-        };
-        img.src = img_src;
-        div.css('padding', '20px').html(img);
-        div.find('img').click(function() { window.location = img_orig });
-        jQuery('.img-theater').css('top', doc.scrollTop()).show();
-        doc.on('keyup.hideTheater', function (e) {
-            if (e.keyCode == 27)
-                hideTheater();
-        });
-    });
-
-    jQuery('[data-dismiss="theater"]').click(function (e){
-        hideTheater();
-    });
-
-    function hideTheater() {
-        jQuery('.img-theater').hide();
-        jQuery('body').removeClass('theater-shown');
-        jQuery(document).unbind('keyup.hideTheater');
-        jQuery(window).unbind('resize.theater');
-    }
 
     function setCookie(cname, cvalue, exdays) {
         var d = new Date();
@@ -134,5 +68,4 @@ jQuery(document).ready(function () {
         // If file field immediately followed by span, show selection there.
         if (next.is('span')) next.html(val);
     });
-
 });
