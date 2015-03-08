bc91c834cb7c906cf0db07ca5b1fc3009ee68477
Raymond Suelzer <rsuelzer@gmail.com>
2015-03-08 13:45:24 -0700

    A few more tweaks for mobile.

--- app/assets/javascripts/box_resizer.js
+++ app/assets/javascripts/box_resizer.js
@@ -1,15 +1,40 @@
 $(window).load(function () {
     var boxes = jQuery('.rss-box-details');
-    if (boxes) {
 
-        for (var i = 0; i < boxes.length; i = i + 3) {
-            [jQuery(boxes[i]), jQuery(boxes[i + 1]), jQuery(boxes[i + 2])]
-                .sort(function (a, b) {
-                    return b.height() - a.height()
-                })
-                .forEach(function (box, i, ary) {
-                    box.height(ary[0].height())
-                });
+    if (boxes) {
+        var dataBootstrapSize = jQuery("[data-bootstrap-size]:visible").data('bootstrap-size');
+        if (dataBootstrapSize != "xs") {
+            var arrayedObjects = boxes.toArray().map(function (el) {
+                return jQuery(el)
+            });
+            switch (dataBootstrapSize) {
+                case "xs":
+                    return;
+                case "sm":
+                    forEveryNth(arrayedObjects, 2);
+                    break;
+                case "md":
+                    forEveryNth(arrayedObjects, 3);
+                    break;
+                case "lg":
+                    forEveryNth(arrayedObjects, 4);
+                    break;
+            }
         }
+    }
+
+    function forEveryNth(array, nth) {
+        var splicedAndSorted = array.splice(0, nth)
+            .sort(function (a, b) {
+                return b.height() - a.height(
+                    )
+            });
+
+        if (splicedAndSorted.length <= 1)
+            return;
+        splicedAndSorted.forEach(function (item) {
+            item.height(splicedAndSorted[0].height());
+        });
+        forEveryNth(array, nth);
     }
 });
