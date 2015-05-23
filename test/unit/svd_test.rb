bb23b76b2bce48fc2cd14c6284f58e1afa2bef1b
Jason <pellaea@gmail.com>
2015-05-23 14:18:52 -0700

    Disabled SVD tests on master branch.

--- test/unit/svd_test.rb
+++ test/unit/svd_test.rb
@@ -2,28 +2,28 @@
 require 'test_helper'
 
 class SvdTest < UnitTestCase
-  def name_counts(svds)
-    with_name = 0
-    without_name = 0
-    svds.each {|svd|
-      if svd.name
-        with_name += 1
-      else
-        without_name += 1
-      end
-    }
-    [with_name, without_name]
-  end
-  
-  def test_all_svds
-    begin
-      results = Svd.all_svds
-      with_name, without_name = name_counts(results)
-      assert(with_name > 0)
-      assert(without_name > 0)
-      svd = results[0]
-      assert(svd.uri)
-    rescue Errno::EHOSTUNREACH => err
-    end
-  end
+#   def name_counts(svds)
+#     with_name = 0
+#     without_name = 0
+#     svds.each {|svd|
+#       if svd.name
+#         with_name += 1
+#       else
+#         without_name += 1
+#       end
+#     }
+#     [with_name, without_name]
+#   end
+#   
+#   def test_all_svds
+#     begin
+#       results = Svd.all_svds
+#       with_name, without_name = name_counts(results)
+#       assert(with_name > 0)
+#       assert(without_name > 0)
+#       svd = results[0]
+#       assert(svd.uri)
+#     rescue Errno::EHOSTUNREACH => err
+#     end
+#   end
 end
