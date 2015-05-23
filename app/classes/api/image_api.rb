8e831f208c9200af963c1e19acabdd76564f926a
Jason <pellaea@gmail.com>
2015-05-23 14:29:03 -0700

    Wasn't calling API parser correctly for params with float range values.
    Conflicts:
    	app/classes/api/image_api.rb
    	app/classes/api/observation_api.rb

--- app/classes/api/image_api.rb
+++ app/classes/api/image_api.rb
@@ -36,9 +36,9 @@ class API
         license:              parse_license(:license),
         has_votes:            parse_boolean(:has_votes),
         quality:              parse_float_range(:quality,
-                                limit: [Image.minimum_vote..Image.maximum_vote]),
+                                limit: Range.new(Image.minimum_vote, Image.maximum_vote)),
         confidence:           parse_float_range(:confidence,
-                                limit:  [Vote.minimum_vote..Vote.maximum_vote]),
+                                limit: Range.new(Vote.minimum_vote, Vote.maximum_vote)),
         ok_for_export:        parse_boolean(:ok_for_export),
       }
     end
