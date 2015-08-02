704942368b7b713a7318fa802b38865890cfbe6f
Joe Cohen <jdcohenesq@gmail.com>
2015-08-02 10:00:08 -0700

    Some trivial RuboCop-type changes to object_link_helper_test

--- test/helpers/object_link_helper_test.rb
+++ test/helpers/object_link_helper_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
+# test helper for links in views
 class ObjectLinkHelperTest < ActionView::TestCase
-# help for links in views
-
   def test_mycobank_language_suffix
     # language exists in MO and in Mycobank translation
     assert_equal("&Lang=Fra", mycobank_language_suffix(:fr))
@@ -29,7 +28,8 @@ class ObjectLinkHelperTest < ActionView::TestCase
 
     name = Name.find(52)  # Amanita subgenus Lepidella
     assert_equal("Amanita", mycobank_taxon(name),
-      "MycoBank taxon name for Ranks between Genus and Species should == genus")
+                 "MycoBank taxon name for Ranks between Genus and Species" \
+                 "should == genus")
 
     name = Name.find(2)   # Coprinus comatus
     assert_equal("Coprinus%20comatus", mycobank_taxon(name))
