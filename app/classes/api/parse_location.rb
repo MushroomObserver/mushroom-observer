1662a7399df319b4390260f1ff279937dfc8e63a
Nathan Wilson <nathan@collectivesource.com>
2015-10-11 23:52:17 +0000

    CodeClimate duplication removal

--- app/classes/api/parse_location.rb
+++ app/classes/api/parse_location.rb
@@ -2,55 +2,54 @@
 
 # Manages the Mushroom Observer Application Programming Interface
 class API
-  def parse_latitude(key, args = {})
-    declare_parameter(key, :latitude, args)
+  def location_component(key, what, args)
+    declare_parameter(key, what, args)
     str = get_param(key)
     return args[:default] unless str
-    val = Location.parse_latitude(str)
-    fail BadParameterValue.new(str, :latitude) unless val
+    case what
+    when :altitude then val = Location.parse_altitude(str)
+    when :latitude then val = Location.parse_latitude(str)
+    when :longitude then val = Location.parse_longitude(str)
+    end
+    fail BadParameterValue.new(str, what) unless val
     val
+  end
+
+  def parse_altitude(key, args = {})
+    location_component(key, :altitude, args)
+  end
+
+  def parse_latitude(key, args = {})
+    location_component(key, :latitude, args)
+  end
+
+  def parse_longitude(key, args = {})
+    location_component(key, :longitude, args)
   end
 
   def parse_longitude_range(key, args = {})
     do_parse_range(:parse_longitude, key, args.merge(leave_order: true))
   end
 
-  def parse_longitude(key, args = {})
-    declare_parameter(key, :longitude, args)
+  def location_val(key, what, args)
+    declare_parameter(key, what, args)
     str = get_param(key)
     return args[:default] unless str
-    val = Location.parse_longitude(str)
-    fail BadParameterValue.new(str, :longitude) unless val
-    val
-  end
-
-  def parse_altitude(key, args = {})
-    declare_parameter(key, :altitude, args)
-    str = get_param(key)
-    return args[:default] unless str
-    val = Location.parse_altitude(str)
-    fail BadParameterValue.new(str, :altitude) unless val
-    val
+    fail BadParameterValue.new(str, what) if str.blank?
+    val = try_parsing_id(str, Location)
+    val ||= Location.find_by_name_or_reverse_name(str)
+    return val if val
+    fail ObjectNotFoundByString.new(get_param(key), Location)
   end
 
   def parse_location(key, args = {})
-    declare_parameter(key, :location, args)
-    str = get_param(key)
-    return args[:default] unless str
-    fail BadParameterValue.new(str, :location) if str.blank?
-    val = try_parsing_id(str, Location)
-    val ||= Location.find_by_name_or_reverse_name(str)
-    fail ObjectNotFoundByString.new(str, Location) unless val
-    val
+    location_val(key, :location, args)
   end
 
   def parse_place_name(key, args = {})
-    declare_parameter(key, :place_name, args)
-    str = get_param(key)
-    return args[:default] unless str
-    fail BadParameterValue.new(str, :location) if str.blank?
-    val = try_parsing_id(str, Location)
-    val ||= Location.find_by_name_or_reverse_name(str)
-    val ? val.display_name : str
+    val = location_val(key, :place_name, args)
+    val.is_a?(Location) ? val.display_name : val
+  rescue ObjectNotFoundByString
+    get_param(key)
   end
 end
