c5dc371726b6f412685a38e99b1021cb6ae70d0e
Joe Cohen <jdcohenesq@gmail.com>
2015-10-15 12:48:04 -0700

    Removed some comments in application_helper per code review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,15 +41,12 @@ module ApplicationHelper
     "<span style='margin-left:#{w}px'>&nbsp;</span>".html_safe
   end
 
-  # Wrap content in tag if condition is true, else nil
-  # 1st arg is condition, which wraps content_tag
   def content_tag_if(condition, name, content_or_options_with_block = nil,
                      options = nil, escape = true, &block)
     return unless condition
     content_tag(name, content_or_options_with_block, options, escape, &block)
   end
 
-  # Wrap content in tag unless condition is true, else nil
   def content_tag_unless(condition, name, content_or_options_with_block = nil,
                          options = nil, escape = true, &block)
     content_tag_if(!condition, name, content_or_options_with_block,
