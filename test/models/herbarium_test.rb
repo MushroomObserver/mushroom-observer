1cddcb0ab890410b1d08edacb96e0922d58a8751
Joe Cohen <jdcohenesq@gmail.com>
2016-03-19 20:28:01 -0700

    Update reference to re-name herbarium fixture
    Fixes errors in herbarium_test

--- test/models/herbarium_test.rb
+++ test/models/herbarium_test.rb
@@ -9,22 +9,22 @@ class HerbariumTest < UnitTestCase
 
   def test_mailing_address
     assert(herbaria(:nybg_herbarium).mailing_address)
-    assert_nil(herbaria(:rolf).mailing_address)
+    assert_nil(herbaria(:rolf_herbarium).mailing_address)
   end
 
   def test_location
     assert(herbaria(:nybg_herbarium).location)
-    assert_nil(herbaria(:rolf).location)
+    assert_nil(herbaria(:rolf_herbarium).location)
   end
 
   def test_email
     assert(herbaria(:nybg_herbarium).email)
-    assert(herbaria(:rolf).email)
+    assert(herbaria(:rolf_herbarium).email)
   end
 
   def test_curators
     assert(herbaria(:nybg_herbarium).curators.length > 1)
-    assert_equal(1, herbaria(:rolf).curators.length)
+    assert_equal(1, herbaria(:rolf_herbarium).curators.length)
   end
 
   def test_fields
