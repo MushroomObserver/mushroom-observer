3932b146f4113c5ad2db69e29df09d449bfc9bb0
Joe Cohen <jdcohenesq@gmail.com>
2016-03-27 08:35:20 -0400

    Improve clarity and intent of project_test
      Use text instead of magic numbers
      Sort expected object IDs when comparing to sorted expectations

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,86 +4,93 @@ require "test_helper"
 class ProjectTest < UnitTestCase
   def test_add_and_remove_observations
     proj = projects(:eol_project)
-    obs1 = observations(:minimal_unknown_obs)
-    obs2 = observations(:detailed_unknown_obs)
-    imgs = obs2.images.sort_by(&:id)
+    minimal_unknown_obs = observations(:minimal_unknown_obs)
+    detailed_unknown_obs = observations(:detailed_unknown_obs)
+    imgs = detailed_unknown_obs.images.sort_by(&:id)
     assert_obj_list_equal([], proj.observations)
     assert_obj_list_equal([], proj.images)
-    assert_obj_list_equal([], obs1.images)
+    assert_obj_list_equal([], minimal_unknown_obs.images)
     assert(imgs.any?)
 
-    proj.add_observation(obs1)
-    assert_obj_list_equal([obs1], proj.observations)
+    proj.add_observation(minimal_unknown_obs)
+    assert_obj_list_equal([minimal_unknown_obs], proj.observations)
     assert_obj_list_equal([], proj.images)
 
-    proj.add_observation(obs2)
-    assert_obj_list_equal([obs1, obs2], proj.observations.sort_by(&:id))
+    proj.add_observation(detailed_unknown_obs)
+    assert_obj_list_equal([minimal_unknown_obs, detailed_unknown_obs],
+                          proj.observations.sort_by(&:id))
     assert_obj_list_equal(imgs, proj.images.sort_by(&:id))
 
-    proj.add_observation(obs2)
-    assert_obj_list_equal([obs1, obs2], proj.observations.sort_by(&:id))
+    proj.add_observation(detailed_unknown_obs)
+    assert_obj_list_equal([minimal_unknown_obs, detailed_unknown_obs],
+                          proj.observations.sort_by(&:id))
     assert_obj_list_equal(imgs, proj.images.sort_by(&:id))
 
-    obs1.images << imgs.first
-    proj.remove_observation(obs2)
-    assert_obj_list_equal([obs1], proj.observations)
+    minimal_unknown_obs.images << imgs.first
+    proj.remove_observation(detailed_unknown_obs)
+    assert_obj_list_equal([minimal_unknown_obs], proj.observations)
     # should keep first img because it is reused by another observation still attached to project
     assert_obj_list_equal([imgs.first], proj.images)
 
-    proj.remove_observation(obs1)
+    proj.remove_observation(minimal_unknown_obs)
     assert_obj_list_equal([], proj.observations)
     # should lose it now because no observations left which use it
     assert_obj_list_equal([], proj.images)
 
-    proj.remove_observation(obs1)
+    proj.remove_observation(minimal_unknown_obs)
     assert_obj_list_equal([], proj.observations)
     assert_obj_list_equal([], proj.images)
   end
 
   def test_add_and_remove_images
+    in_situ_img = images(:in_situ_image)
+    turned_over_img = images(:turned_over_image)
     proj = projects(:eol_project)
-    img1 = images(:in_situ_image)
-    img2 = images(:turned_over_image)
+
     assert_obj_list_equal([], proj.images)
 
-    proj.add_image(img1)
-    assert_obj_list_equal([img1], proj.images.sort_by(&:id))
+    proj.add_image(in_situ_img)
+    assert_obj_list_equal([in_situ_img], proj.images)
 
-    proj.add_image(img2)
-    assert_obj_list_equal([img1, img2], proj.images.sort_by(&:id))
+    proj.add_image(turned_over_img)
+    assert_obj_list_equal([in_situ_img, turned_over_img].sort_by(&:id),
+                          proj.images.sort_by(&:id))
 
-    proj.add_image(img2)
-    assert_obj_list_equal([img1, img2], proj.images.sort_by(&:id))
+    proj.add_image(turned_over_img)
+    assert_obj_list_equal([in_situ_img, turned_over_img].sort_by(&:id),
+                          proj.images.sort_by(&:id))
 
-    proj.remove_image(img1)
-    assert_obj_list_equal([img2], proj.images)
+    proj.remove_image(in_situ_img)
+    assert_obj_list_equal([turned_over_img], proj.images)
 
-    proj.remove_image(img2)
+    proj.remove_image(turned_over_img)
     assert_obj_list_equal([], proj.images)
 
-    proj.remove_image(img2)
+    proj.remove_image(turned_over_img)
     assert_obj_list_equal([], proj.images)
   end
 
   def test_add_and_remove_species_lists
     proj = projects(:bolete_project)
-    spl1 = species_lists(:first_species_list)
-    spl2 = species_lists(:unknown_species_list)
-    assert_obj_list_equal([spl2], proj.species_lists)
+    first_list = species_lists(:first_species_list)
+    another_list = species_lists(:unknown_species_list)
+    assert_obj_list_equal([another_list], proj.species_lists)
 
-    proj.add_species_list(spl1)
-    assert_obj_list_equal([spl1, spl2], proj.species_lists.sort_by(&:id))
+    proj.add_species_list(first_list)
+    assert_obj_list_equal([first_list, another_list].sort_by(&:id),
+                          proj.species_lists.sort_by(&:id))
 
-    proj.add_species_list(spl2)
-    assert_obj_list_equal([spl1, spl2], proj.species_lists.sort_by(&:id))
+    proj.add_species_list(another_list)
+    assert_obj_list_equal([first_list, another_list].sort_by(&:id),
+                          proj.species_lists.sort_by(&:id))
 
-    proj.remove_species_list(spl2)
-    assert_obj_list_equal([spl1], proj.species_lists)
+    proj.remove_species_list(another_list)
+    assert_obj_list_equal([first_list], proj.species_lists)
 
-    proj.remove_species_list(spl1)
+    proj.remove_species_list(first_list)
     assert_obj_list_equal([], proj.species_lists)
 
-    proj.remove_species_list(spl1)
+    proj.remove_species_list(first_list)
     assert_obj_list_equal([], proj.species_lists)
   end
 end
