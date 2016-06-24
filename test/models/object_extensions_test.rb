f97a4ee93f4f1266b2dd96f30992dbeb12f19c24
Joe Cohen <jdcohenesq@gmail.com>
2016-06-24 13:23:17 -0700

    Cover ObjectExtensions.

---
+++ test/models/object_extensions_test.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require "test_helper"
+
+# test MO extensions to Ruby's Object class
+class ObjectExtensionsTest < UnitTestCase
+  def test_stacktrace_removes_caller
+    # Reflect on this test method's name (in case someone chooses to rename it).
+    caller = __method__.to_s
+    refute(_stacktrace.first.include?(caller),
+           "Trace should start with #{caller}'s caller, not #{caller}")
+  end
+
+  def test_stacktrace_appends_executable
+    assert(_stacktrace.last.end_with?($PROGRAM_NAME),
+           "Trace should end with #{$PROGRAM_NAME}")
+  end
+end
