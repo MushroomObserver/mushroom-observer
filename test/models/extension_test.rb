8c59ed1a5fc9c6865e68a38fda628c80ac233d74
Joe Cohen <jdcohenesq@gmail.com>
2016-06-25 19:19:17 -0700

    Increase coverage of MO extensions to Ruby & Rails date and time classes.
    Put all tests in separate file for date/time test extensions.
    Prove DateTime, Time, and TimeWithZone public interfaces.
    Add coverage of fancy_time.

--- test/models/extension_test.rb
+++ test/models/extension_test.rb
@@ -77,30 +77,4 @@ class ExtensionTest < UnitTestCase
     hash.remove_nils!
     assert_equal({ a: 1, c: 3 }, hash)
   end
-
-  # ----------------------------
-  #  :section: Time Tests
-  # ----------------------------
-
-  def test_fancy_time
-    assert_fancy_time(0.seconds, :time_just_seconds_ago)
-    assert_fancy_time(59.seconds, :time_just_seconds_ago)
-    assert_fancy_time(60.seconds, :time_one_minute_ago)
-    assert_fancy_time(119.seconds, :time_one_minute_ago)
-    assert_fancy_time(120.seconds, :time_minutes_ago, n: 2)
-    assert_fancy_time(59.minutes, :time_minutes_ago, n: 59)
-    assert_fancy_time(60.minutes, :time_one_hour_ago)
-    assert_fancy_time(119.minutes, :time_one_hour_ago)
-    assert_fancy_time(120.minutes, :time_hours_ago, n: 2)
-    assert_fancy_time(23.hours, :time_hours_ago, n: 23)
-    assert_fancy_time(24.hours, :time_one_day_ago)
-  end
-
-  def assert_fancy_time(diff, tag, args = {})
-    ref = Time.now
-    time = ref - diff
-    expect = tag.l(args.merge(date: time.web_date))
-    actual = time.fancy_time(ref)
-    assert_equal(expect, actual)
-  end
 end
