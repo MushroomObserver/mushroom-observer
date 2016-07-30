006001d327178ee78c9aaa8f3f827f430d25e3ba
Jason <pellaea@gmail.com>
2016-07-30 11:59:43 -0700

    Did ObservationOfChildren.

--- app/classes/query/observation_of_children.rb
+++ app/classes/query/observation_of_children.rb
@@ -1,9 +1,18 @@
 class Query::ObservationOfChildren < Query::Observation
+  include Query::OfChildren
+
   def self.parameter_declarations
     super.merge(
+      name: Name,
+      all?: :boolean
     )
   end
 
   def initialize
+    name = find_cached_parameter_instance(Name, :name)
+    title_args[:name] = name.display_name
+    params[:by] ||= "name"
+    add_name_condition(name)
+    add_join(:names)
   end
 end
