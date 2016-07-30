04f659a56bf3295811af23848730265108c5ab97
Jason <pellaea@gmail.com>
2016-07-30 11:45:39 -0700

    Did ObservationInSet.

--- app/classes/query/observation_in_set.rb
+++ app/classes/query/observation_in_set.rb
@@ -1,9 +1,13 @@
 class Query::ObservationInSet < Query::Observation
   def self.parameter_declarations
     super.merge(
+      ids: [Observation]
     )
   end
 
   def initialize
+    set = clean_id_set(params[:ids])
+    self.where << "observations.id IN (#{set})"
+    self.order = "FIND_IN_SET(observations.id,'#{set}') ASC"
   end
 end
