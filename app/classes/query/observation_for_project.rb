89a0e4c6be629fe4e609dfeaa88455f9eed3301d
Jason <pellaea@gmail.com>
2016-07-30 11:43:22 -0700

    Did ObservationForProject

--- app/classes/query/observation_for_project.rb
+++ app/classes/query/observation_for_project.rb
@@ -1,9 +1,14 @@
 class Query::ObservationForProject < Query::Observation
   def self.parameter_declarations
     super.merge(
+      project: Project
     )
   end
 
   def initialize
+    project = find_cached_parameter_instance(Project, :project)
+    title_args[:project] = project.title
+    self.where << "observations_projects.project_id = '#{project.id}'"
+    add_join("observations_projects")
   end
 end
