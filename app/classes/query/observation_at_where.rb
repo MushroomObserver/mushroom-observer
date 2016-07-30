89bb48d7659b665427e52100e04608f8461ba8c8
Jason <pellaea@gmail.com>
2016-07-30 11:33:16 -0700

    Did ObservationAtWhere.

--- app/classes/query/observation_at_where.rb
+++ app/classes/query/observation_at_where.rb
@@ -1,9 +1,16 @@
 class Query::ObservationAtWhere < Query::Observation
   def self.parameter_declarations
     super.merge(
+      location: :string,
+      user_where: :string
     )
   end
 
   def initialize
+    title_args[:where] = params[:where]
+    pattern = clean_pattern(params[:location])
+    add_join(:names)
+    self.where << "locations.where LIKE '%#{pattern}%'"
+    params[:by] ||= "name"
   end
 end
