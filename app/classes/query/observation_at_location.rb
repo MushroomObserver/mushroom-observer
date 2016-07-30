dc33951a53794a7eda8c6c751f1e089c923dba60
Jason <pellaea@gmail.com>
2016-07-30 11:41:28 -0700

    Did ObservationByUser.

--- app/classes/query/observation_at_location.rb
+++ app/classes/query/observation_at_location.rb
@@ -9,7 +9,7 @@ class Query::ObservationAtLocation < Query::Observation
     location = find_cached_parameter_instance(Location, :location)
     title_args[:location] = location.display_name
     add_join(:names)
-    self.where << "locations.location_id = '#{params[:location]}'"
+    self.where << "locations.location_id = '#{location.id}'"
     params[:by] ||= "name"
   end
 end
