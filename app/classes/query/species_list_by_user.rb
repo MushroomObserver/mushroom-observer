af81e8dbed9476e27e14fe90a7ccd9ecb2a63cb4
Joe Cohen <jdcohenesq@gmail.com>
2016-07-31 13:58:02 -0700

    species_list_by_rss_log

---
+++ app/classes/query/species_list_by_user.rb
@@ -0,0 +1,15 @@
+class Query::SpeciesListByUser < Query::SpeciesList
+  def parameter_declarations
+    super.merge(
+      user: User
+    )
+  end
+
+  def initialize
+    user = find_cached_parameter_instance(User, :user)
+    title_args[:user] = user.legal_name
+    self.where << "species_lists.user_id = '#{user.id}'"
+    params[:by] ||= "title"
+    super
+  end
+end
