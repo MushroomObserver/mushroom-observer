3ca75f52cb6b147988623f465e630ced5f156e0a
Joe Cohen <jdcohenesq@gmail.com>
2016-07-30 17:22:30 -0700

    Finish Query:::Specimen
    Add Query::SpecimenPatternSearch
    **Please review `initialize`**

---
+++ app/classes/query/specimen_pattern_search.rb
@@ -0,0 +1,17 @@
+class Query::SpecimenPatternSearch < Query::Specimen
+  include Query::PatternSearch
+
+  def parameter_declarations
+    super.merge(
+      pattern: :string
+    )
+  end
+
+  def initialize
+    search = google_parse_pattern
+    add_search_conditions(search,
+      "specimens.herbarium_label",
+      "COALESCE(specimens.notes,'')"
+    )
+  end
+end
