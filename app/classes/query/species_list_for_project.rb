1c936ade9699be73abae46fad64932a828336044
Joe Cohen <jdcohenesq@gmail.com>
2016-08-01 07:07:36 -0700

    Add species_list_for_project

---
+++ app/classes/query/species_list_for_project.rb
@@ -0,0 +1,15 @@
+class Query::SpeciesListForProject < Query::SpeciesList
+  def parameter_declarations
+    super.merge(
+      project: Project
+    )
+  end
+
+  def initialize
+    project = find_cached_parameter_instance(Project, :project)
+    title_args[:project] = project.title
+    self.where << "species_lists_projects.project_id = '#{params[:project]}'"
+    add_join("species_lists_projects")
+    super
+  end
+end
