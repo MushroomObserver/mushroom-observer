b8b199f90050f4ddf623df34997bc0953915d3a0
Joe Cohen <jdcohenesq@gmail.com>
2016-08-01 09:10:39 -0700

    Complete Project query classes
    Add ProjectPatternSearch

---
+++ app/classes/query/project_pattern_search.rb
@@ -0,0 +1,18 @@
+class Query::ProjectPatternSearch < Query::Project
+  include Query::Initializers::PatternSearch
+
+  def parameter_declarations
+    super.merge(
+      pattern: :string
+    )
+  end
+
+  def initialize_flavor
+    search = google_parse_pattern
+    add_search_conditions(search,
+      "projects.title",
+      "COALESCE(projects.summary,'')",
+    )
+    super
+  end
+end
