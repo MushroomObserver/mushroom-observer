b0e4d024650c5545429f34d5d459588faabb76c3
Joe Cohen <jdcohenesq@gmail.com>
2016-08-01 16:29:30 -0700

    Create LocationDescriptionByUser

---
+++ app/classes/query/location_description_by_user.rb
@@ -0,0 +1,18 @@
+class Query::LocationDescriptionByUser < Query::LocationDescription
+  def parameter_declarations
+    super.merge(
+      user: User
+    )
+  end
+
+  def initialize_flavor
+    user = find_cached_parameter_instance(User, :user)
+    title_args[:user] = user.legal_name
+    self.where << "location_descriptions.user_id = '#{user.id}'"
+    super
+  end
+
+  def default_order
+    "name"
+  end
+end
