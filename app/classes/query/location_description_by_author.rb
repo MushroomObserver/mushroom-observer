cdaee6f39f0c41255b08ea62b20766b256a04467
Joe Cohen <jdcohenesq@gmail.com>
2016-08-01 16:21:10 -0700

    Add LocationDescriptionByAuthor

---
+++ app/classes/query/location_description_by_author.rb
@@ -0,0 +1,21 @@
+class Query::LocationDescriptionByAuthor < Query::LocationDescription
+  def parameter_declarations
+    super.merge(
+      user: User
+    )
+  end
+
+  def initialize_flavor
+    user = find_cached_parameter_instance(User, :user)
+    title_args[:user] = user.legal_name
+
+    glue_table = "location_decriptions_authors".to_sym
+    add_join(glue_table)
+    self.where << "#{glue_table}.user_id = '#{params[:user]}'"
+    super
+  end
+
+  def default_order
+    "name"
+  end
+end
