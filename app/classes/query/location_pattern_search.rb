aa287ffd67a90a9a441674e7a482d4449e13fe24
Jason <pellaea@gmail.com>
2016-08-01 17:45:40 -0700

    Done with location_pattern_search hopefully.

---
+++ app/classes/query/location_pattern_search.rb
@@ -0,0 +1,23 @@
+class Query::LocationPatternSearch < Query::Location
+  include Query::Initializers::PatternSearch
+
+  def parameter_declarations
+    super.merge(
+      pattern: :string
+    )
+  end
+
+  def initialize_flavor
+    search = google_parse_pattern
+    note_fields =
+      [
+        "locations.name",
+      ] +
+      LocationDescription.all_note_fields.map do |x|
+        "COALESCE(name_descriptions.#{x},'')"
+      end
+    add_search_conditions(search, *note_fields)
+    add_join(:"location_descriptions.default!")
+    super
+  end
+end
