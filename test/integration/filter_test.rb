150afb63b29b479c935ba8cf1ec8f2ac391ca742
Joe Cohen <jdcohenesq@gmail.com>
2016-08-18 07:57:01 -0600

    Add placeholder Filters section to user preferences page.
    Also clean up db.

--- test/integration/filter_test.rb
+++ test/integration/filter_test.rb
@@ -6,11 +6,13 @@ class FilterTest < IntegrationTestCase
   def test_user_preferences
     user = users(:mary)
 
-    visit("account/login")
+    visit("/account/login")
     fill_in("User name or Email address:", with: user.login)
     fill_in("Password:", with: "testpassword")
     click_button("Login")
 
     click_on("Preferences", match: :first)
+    assert(page.has_content?("Observation Filters"),
+           "Preference page lacks Observation Filters section")
   end
 end
