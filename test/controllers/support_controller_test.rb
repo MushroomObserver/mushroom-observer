82b221b607959e8dca34a46307ffe65837bb4a15
Nathan Wilson <nathan@collectivesource.com>
2016-12-23 13:29:48 +0000

    Rubocop and coverage related cleanup

--- test/controllers/support_controller_test.rb
+++ test/controllers/support_controller_test.rb
@@ -2,21 +2,24 @@
 require "test_helper"
 
 class SupportControllerTest < FunctionalTestCase
-  def test_donors
-    get_with_dump(:donors)
-    assert_template(:donors)
+  def test_gets
+    [:donors, :confirm, :thanks,
+     :letter, :wrapup_2011, :wrapup_2012].each do |template|
+      assert_template_with_dump(template)
+      get_with_dump(template)
+      assert_template(template)
+    end
+  end
+
+  def assert_template_with_dump(template)
+    get_with_dump(template)
+    assert_template(template)
   end
 
   def test_donate
     login("rolf")
-    get_with_dump(:donate)
-    assert_template(:donate)
+    assert_template_with_dump(:donate)
     assert_select("form input[value=\"#{users(:rolf).name}\"]")
-  end
-
-  def test_confirm
-    get_with_dump(:confirm)
-    assert_template(:confirm)
   end
 
   def test_confirm_post
@@ -26,25 +29,33 @@ class SupportControllerTest < FunctionalTestCase
   def confirm_post(amount, other_amount)
     donations = Donation.count
     anon = false
-    final_amount = (amount == "other") ? other_amount : amount
-    params = {
+    final_amount = amount == "other" ? other_amount : amount
+    params = donation_params(amount, rolf, anon)
+    params[:donation][:other_amount] = other_amount
+    post(:confirm, params)
+    assert_template(:confirm)
+    assert_donations(donations + 1, final_amount, rolf, anon, false)
+  end
+
+  def assert_donations(count, amount, user, anon, reviewed)
+    donation = Donation.all.order("created_at DESC")[0]
+    assert_equal([count, amount,
+                  donation.user && user, user.name, user.email,
+                  anon, reviewed],
+                 [Donation.count, donation.amount,
+                  donation.user, donation.who, donation.email,
+                  donation.anonymous, donation.reviewed])
+  end
+
+  def donation_params(amount, user, anon)
+    {
       donation: {
         amount: amount,
-        other_amount: other_amount,
-        who: rolf.name,
-        email: rolf.email,
+        who: user.name,
+        email: user.email,
         anonymous: anon
       }
     }
-    post(:confirm, params)
-    assert_template(:confirm)
-    assert_equal(donations + 1, Donation.count)
-    donation = Donation.all.order("created_at DESC")[0]
-    assert_equal(final_amount, donation.amount)
-    assert_equal(rolf.name, donation.who)
-    assert_equal(rolf.email, donation.email)
-    assert_equal(anon, donation.anonymous)
-    assert_equal(false, donation.reviewed)
   end
 
   def test_confirm_other_amount_post
@@ -54,37 +65,21 @@ class SupportControllerTest < FunctionalTestCase
   def test_create_donation
     get(:create_donation)
     assert_response(:redirect)
-
     make_admin
-    get_with_dump(:create_donation)
-    assert_template(:create_donation)
+    assert_template_with_dump(:create_donation)
+  end
+
+  def test_create_donation_post
+    create_donation_post(false)
   end
 
   def create_donation_post(anon)
     make_admin
     amount = 100.00
     donations = Donation.count
-    params = {
-      donation: {
-        amount: amount,
-        who: rolf.name,
-        email: rolf.email,
-        anonymous: anon
-      }
-    }
+    params = donation_params(amount, rolf, anon)
     post(:create_donation, params)
-    assert_equal(donations + 1, Donation.count)
-    donation = Donation.all.order("created_at DESC")[0]
-    assert_equal(amount, donation.amount)
-    assert_equal(rolf.name, donation.who)
-    assert_equal(rolf.email, donation.email)
-    assert_equal(anon, donation.anonymous)
-    assert_equal(true, donation.reviewed)
-    assert_equal(rolf, donation.user)
-  end
-
-  def test_create_donation_post
-    create_donation_post(false)
+    assert_donations(donations + 1, amount, rolf, anon, true)
   end
 
   def test_create_donation_anon_post
@@ -94,43 +89,17 @@ class SupportControllerTest < FunctionalTestCase
   def test_review_donations
     get(:review_donations)
     assert_response(:redirect)
-
     make_admin
-    get_with_dump(:review_donations)
-    assert_template(:review_donations)
+    assert_template_with_dump(:review_donations)
   end
 
   def test_review_donations_post
     make_admin
     unreviewed = donations(:unreviewed)
     assert_equal(false, unreviewed.reviewed)
-    params = {
-      reviewed: {
-        unreviewed.id => true
-      }
-    }
+    params = { reviewed: { unreviewed.id => true } }
     post(:review_donations, params)
     reloaded = Donation.find(unreviewed.id)
     assert(reloaded.reviewed)
-  end
-
-  def test_thanks
-    get_with_dump(:thanks)
-    assert_template(:thanks)
-  end
-
-  def test_letter
-    get_with_dump(:letter)
-    assert_template(:letter)
-  end
-
-  def test_wrapup_2011
-    get_with_dump(:wrapup_2011)
-    assert_template(:wrapup_2011)
-  end
-
-  def test_wrapup_2012
-    get_with_dump(:wrapup_2012)
-    assert_template(:wrapup_2012)
   end
 end
