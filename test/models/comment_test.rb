af0ac480b9cf2c6509e8a5dc1ec09432dec266e5
Jason <pellaea@gmail.com>
2016-12-25 11:34:32 -0800

    Complete rewrite to try (in vain!) to make rubocop happy.  I really don't think it's a good idea to try to make unit tests pass rubocop.

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,91 +2,133 @@
 require "test_helper"
 
 class CommentTest < UnitTestCase
-  def num_emails
-    ActionMailer::Base.deliveries.length
-  end
-
-  def test_oil_and_water
-    num = num_emails
-    MO.water_users = [users(:rolf).id, users(:mary).id]
-    MO.oil_users   = [users(:dick).id, users(:katrina).id]
-    Comment.create!(user: rolf, summary: "1")
-    assert_equal(num, num_emails)
-    Comment.create!(user: mary, summary: "2")
-    assert_equal(num, num_emails)
-    Comment.create!(user: roy, summary: "3")
-    assert_equal(num, num_emails)
-    Comment.create!(user: katrina, summary: "4")
-    assert_equal(num + 1, num_emails)
-    Comment.create!(user: roy, summary: "5")
-    assert_equal(num + 2, num_emails)
-  end
-
   def test_user_highlighting_parsing
-    x = Comment.first
-    assert_user_list_equal([],     x.send(:highlighted_users, ""))
-    assert_user_list_equal([mary], x.send(:highlighted_users, "_user #{mary.id}_"))
-    assert_user_list_equal([mary], x.send(:highlighted_users, "@Mary Newbie@"))
-    assert_user_list_equal([mary], x.send(:highlighted_users, "@mary blah blah"))
-    assert_user_list_equal([mary, rolf, dick],
-                           x.send(:highlighted_users, "@mary,@rolf,@dick"))
-    assert_user_list_equal([mary, rolf, dick],
-                           x.send(:highlighted_users, "@mary@,@rolf@,@dick@"))
+    do_highlight_test([], "")
+    do_highlight_test([mary], "_user #{mary.id}_")
+    do_highlight_test([mary], "@Mary Newbie@")
+    do_highlight_test([mary], "@mary foo bar")
+    do_highlight_test([mary, rolf, dick], "@mary,@rolf,@dick")
+    do_highlight_test([mary, rolf, dick], "@mary@,@rolf@,@dick@")
+  end
+
+  def do_highlight_test(expected, string)
+    comment = Comment.first
+    assert_user_list_equal(expected, comment.send(:highlighted_users, string))
   end
 
   def test_user_highlighting_emails
-    obs = observations(:coprinus_comatus_obs)
-    num = num_emails
-    mary.update_attributes!(email_comments_response: false)
-    dick.update_attributes!(email_comments_response: false)
-    katrina.update_attributes!(email_comments_response: false)
+    # owned by rolf, namings by rolf and mary, no comments
+    obs = observations(:coprinus_comatus_obs).reload
+    do_highlight_comment_where_everyone_opts_out(obs)
+    do_highlight_comment_where_mary_opts_in(obs)
+    do_highlight_comment_where_everyone_opts_in(obs)
+  end
 
-    Comment.create!(target: obs, user: rolf, summary: "@mary", comment: "what about this?")
-    assert_equal(num, num_emails)
+  def do_highlight_comment_where_everyone_opts_out(obs)
+    opt_out_of_comment_responses(mary, dick, katrina)
+    do_comment_test(0, obs, rolf, "@mary", "what about this?")
+  end
 
-    mary.update_attributes!(email_comments_response: true)
-    Comment.create!(target: obs, user: rolf, summary: "@mary", comment: "what about this?")
-    assert_equal(num + 1, num_emails)
+  def do_highlight_comment_where_mary_opts_in(obs)
+    opt_in_to_comment_responses(mary)
+    opt_out_of_comment_responses(dick, katrina)
+    do_comment_test(1, obs, rolf, "@mary", "what about this?")
+    do_comment_test(1, obs, rolf, "checked", "My name is @rolf.")
+    do_comment_test(1, obs, rolf, "checked",
+                    "@dick - yes\n\n@mary - no\n\n@katrina - maybe")
+  end
 
-    Comment.create!(target: obs, user: rolf, summary: "checked", comment: "My name is @rolf.")
-    assert_equal(num + 1, num_emails)
-
-    Comment.create!(target: obs, user: rolf, summary: "checked", comment: "@dick - yes\n\n@mary - no\n\n@katrina - maybe")
-    assert_equal(num + 2, num_emails)
-
-    dick.update_attributes!(email_comments_response: true)
-    katrina.update_attributes!(email_comments_response: true)
-    Comment.create!(target: obs, user: rolf, summary: "checked", comment: "@dick - yes\n\n@mary - no\n\n@katrina - maybe")
-    assert_equal(num + 5, num_emails)
+  def do_highlight_comment_where_everyone_opts_in(obs)
+    opt_in_to_comment_responses(mary, dick, katrina)
+    do_comment_test(3, obs, rolf, "checked",
+                    "@dick - yes\n\n@mary - no\n\n@katrina - maybe")
   end
 
   def test_comment_notification
     # owned by rolf, namings by rolf and mary, no comments
-    obs = observations(:coprinus_comatus_obs)
-    num = num_emails
-    rolf.update_attributes!(email_comments_owner:    false)
-    rolf.update_attributes!(email_comments_response: false)
-    mary.update_attributes!(email_comments_response: false)
-    dick.update_attributes!(email_comments_response: false)
+    obs = observations(:coprinus_comatus_obs).reload
+    rolf.update_attributes!(email_comments_owner: false)
+    do_comment_response_where_everyone_opts_out(obs)
+    do_comment_response_where_mary_opts_in(obs)
+    do_comment_response_where_mary_and_dick_opt_in(obs)
+    do_comment_response_where_everyone_opts_in(obs)
+  end
 
-    Comment.create!(target: obs, user: rolf, summary: "1")
-    assert_equal(num, num_emails)
+  def do_comment_response_where_everyone_opts_out(obs)
+    opt_out_of_comment_responses(rolf, mary, dick)
+    do_comment_test(0, obs, rolf, "1")
+  end
 
-    mary.update_attributes!(email_comments_response: true)
-    assert(obs.id, namings(:coprinus_comatus_other_naming).observation_id)
-    assert(mary.id, namings(:coprinus_comatus_other_naming).user_id)
-    Comment.create!(target: obs.reload, user: dick, summary: "2")
-    assert_equal(num + 1, num_emails) # mary because of naming
+  def do_comment_response_where_mary_opts_in(obs)
+    opt_in_to_comment_responses(mary)
+    opt_out_of_comment_responses(rolf, dick)
+    do_comment_test(1, obs, dick, "2") # mary because of naming
+    do_comment_test(0, obs, mary, "3")
+  end
 
-    Comment.create!(target: obs, user: mary, summary: "3")
-    assert_equal(num + 1, num_emails)
+  def do_comment_response_where_mary_and_dick_opt_in(obs)
+    opt_in_to_comment_responses(mary, dick)
+    opt_out_of_comment_responses(rolf)
+    do_comment_test(1, obs, mary, "4") # dick because of comment
+  end
 
-    dick.update_attributes!(email_comments_response: true)
-    Comment.create!(target: obs.reload, user: mary, summary: "4")
-    assert_equal(num + 2, num_emails) # dick because of comment
-    
-    rolf.update_attributes!(email_comments_response: true)
-    Comment.create!(target: obs.reload, user: katrina, summary: "5")
-    assert_equal(num + 5, num_emails) # rolf, mary, dick all have comments
+  def do_comment_response_where_everyone_opts_in(obs)
+    opt_in_to_comment_responses(rolf, mary, dick)
+    do_comment_test(3, obs, katrina, "5") # rolf, mary, dick all have comments
+  end
+
+  def test_oil_and_water
+    old_water_users = MO.water_users
+    old_oil_users   = MO.oil_users
+    MO.water_users  = [users(:rolf).id, users(:mary).id]
+    MO.oil_users    = [users(:dick).id, users(:katrina).id]
+    do_oil_and_water_test
+  ensure
+    MO.water_users = old_water_users
+    MO.oil_users   = old_oil_users
+  end
+
+  def do_oil_and_water_test
+    do_comment_test(0, nil, rolf, "1")
+    do_comment_test(0, nil, mary, "2")
+    do_comment_test(0, nil, roy, "3")
+    do_comment_test(1, nil, katrina, "4")
+    do_comment_test(1, nil, roy, "5")
+  end
+
+  def opt_out_of_comment_responses(*users)
+    users.each do |user|
+      user.update_attributes!(email_comments_response: false)
+    end
+  end
+
+  def opt_in_to_comment_responses(*users)
+    users.each do |user|
+      user.update_attributes!(email_comments_response: true)
+    end
+  end
+
+  def do_comment_test(chg, obs, user, summary, comment = "")
+    old = num_emails
+    obs.reload if obs # (to ensure it sees chgs in user prefs)
+    Comment.create!(
+      target:  obs,
+      user:    user,
+      summary: summary,
+      comment: comment
+    )
+    assert_equal(chg, num_emails-old, sent_emails(old))
+  end
+
+  def num_emails
+    ActionMailer::Base.deliveries.length
+  end
+
+  def sent_emails(start)
+    return "No emails were sent" if num_emails == start
+    strs = ActionMailer::Base.deliveries[start..-1].map do |mail|
+      "to: #{mail["to"]}, subject: #{mail["subject"]}"
+    end
+    "These emails were sent:\n" + strs.join("\n")
   end
 end
