c30e1535de1f08ff317af9bffde413c499551ef3
Jason <pellaea@gmail.com>
2016-12-28 21:20:01 -0800

    One lingering little bug caught by integration test.

--- app/classes/query/rss_log_all.rb
+++ app/classes/query/rss_log_all.rb
@@ -7,29 +7,33 @@ class Query::RssLogAll < Query::RssLogBase
   end
 
   def coerce_into_location_query
-    Query.lookup(:Location, :by_rss_log, params_minus_type)
+    do_coerce(:Location)
   end
 
   def coerce_into_name_query
-    Query.lookup(:Name, :by_rss_log, params_minus_type)
+    do_coerce(:Name)
   end
 
   def coerce_into_observation_query
-    Query.lookup(:Observation, :by_rss_log, params_minus_type)
+    do_coerce(:Observation)
   end
 
   def coerce_into_project_query
-    Query.lookup(:Project, :by_rss_log, params_minus_type)
+    do_coerce(:Project)
   end
 
   def coerce_into_species_list_query
-    Query.lookup(:SpeciesList, :by_rss_log, params_minus_type)
+    do_coerce(:SpeciesList)
+  end
+
+  def do_coerce(new_model)
+    Query.lookup(new_model, :by_rss_log, params_minus_type)
   end
 
   def params_minus_type
     return params unless params.has_key?(:type)
     params2 = params.dup
-    params.delete(:type)
+    params2.delete(:type)
     params2
   end
 end
