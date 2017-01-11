62ebd35bb20fcd0be8b41d9e4209e38aa2520c5a
Joe Cohen <jdcohenesq@gmail.com>
2017-01-11 13:28:14 -0800

    Test previously uncovered ObservationController#map_observations

--- test/integration/observer_controller_supplemental_test.rb
+++ test/integration/observer_controller_supplemental_test.rb
@@ -8,4 +8,14 @@ class ObserverControllerSupplementalTest < IntegrationTestCase
     click_button("Test")
     page.assert_text("Jabberwocky", count: 2)
   end
+
+  def test_map_observations
+    name = names(:boletus_edulis)
+    visit("/name/map/#{name.id}")
+    click_link("Show Observations")
+    click_link("Show Map")
+    title = page.find_by_id("title")
+
+    title.assert_text("Observations of #{name.text_name}")
+  end
 end
