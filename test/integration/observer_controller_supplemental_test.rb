8546c56a17b55869f889c7f9a3b282144d83dd2c
Joe Cohen <jdcohenesq@gmail.com>
2017-01-11 08:14:14 -0800

    Shorten argument to Capybara session method.
    * Protocol and domain are unnecessary.

--- test/integration/observer_controller_supplemental_test.rb
+++ test/integration/observer_controller_supplemental_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 # Tests which supplement controller/observer_controller_test.rb
 class ObserverControllerSupplementalTest < IntegrationTestCase
   def test_post_textile
-    visit ("http://mushroomobserver.org/observer/textile_sandbox")
+    visit("/observer/textile_sandbox")
     fill_in("code", with: "Jabberwocky")
     click_button("Test")
     page.assert_text("Jabberwocky", count: 2)
