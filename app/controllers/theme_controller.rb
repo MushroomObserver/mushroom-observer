5270b5df3752d6599909e5e908da14a0a27d5229
Joe Cohen <jdcohenesq@gmail.com>
2017-01-24 17:09:03 -0800

    Document public actions of ThemeController
    Includes an explanation why the actions for the individual themes (e.g., `theme/Agaricus`) are not defined in the controller.

--- app/controllers/theme_controller.rb
+++ app/controllers/theme_controller.rb
@@ -1,4 +1,10 @@
 # Theme controller
+# Shows information about available user color themes.
+#
+#  ==== Public Actions =====
+#  color_themes::       Show general information about color themes
+#  Agaricus, etc.::     Show sample page, explaining color derivation.
+#
 class ThemeController < ApplicationController
   # callbacks
   before_action :login_required, except: MO.themes + [
@@ -6,7 +12,12 @@ class ThemeController < ApplicationController
   ]
   before_action :disable_link_prefetching
 
-  # Displays info on color themes.
+  # Show general information on color themes
   def color_themes # :nologin:
   end
+
+  # Individual theme actions. Each shows a sample page in that theme,
+  # explaining the colors for that theme.
+  # These actions are not defined here, but rather are automagically created
+  # if there's a view template corresponding to the theme.
 end
