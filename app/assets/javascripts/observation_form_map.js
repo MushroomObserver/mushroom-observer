4a3ae8b6e8ad4b7da8c51a9d79066687f9f3b57e
scott meyers <scottmey@gmail.com>
2017-03-25 15:05:33 -0400

    removes clear map button, hooks in prepopulated form data, drops pin on click event, adds elevation service

--- app/assets/javascripts/observation_form_map.js
+++ app/assets/javascripts/observation_form_map.js
@@ -1,96 +1,113 @@
 // Observation Form Map - Lat/Long/Alt Helper
+/* globals $, google */
 
 // ./observer/create_observation
 if ( $('#observationFormMap').length ) {
 
     (function() {
         var searchInput = $('#observation_place_name'),
-            latInput = $('#observation_lat'),
-            lngInput = $('#observation_long');
+            latInput    = $('#observation_lat'),
+            lngInput    = $('#observation_long'),
+            elvInput    = $('#observation_alt'),
+            marker;
 
-        // init map object
+        // init map
         var map = new google.maps.Map(document.getElementById('observationFormMap'), {
-            zoom: 1,
-            center: { lat: 8.38321792370903, lng: -66.56186083652352 }
+            center: { lat: -7, lng: -47 },
+            zoom: 1
         });
 
-        // init marker
-        var marker = new google.maps.Marker({
-            title: 'Location',
-            map: map,
-            draggable: true,
-            visible: false
+        // init elevation service
+        var elevation = new google.maps.ElevationService();
+
+        google.maps.event.addListener(map, 'click', function(e) {
+            placeMarker(e.latLng);
+            updateFields();
         });
 
-        // disable user from submitting on this element
-        $(searchInput).keypress(function(e) {
-            if (e.keyCode == 13) { e.preventDefault(); }
-        });
-
-        // dragged marker event
-        google.maps.event.addListener(marker, 'drag', function(e) {
-            latLngInputs.update(e.latLng.lat(), e.latLng.lng());
-        });
-
-        // clear the text inputs
-        $('.map-clear').click(function(e) {
-            e.preventDefault();
-
-            latLngInputs.empty();
-        });
-
-        // updates map on text input change
+        // adjust marker on field input
         $([latInput, lngInput]).each(function(index, value) {
-            $(this).keyup(function() {
-                var latLng = new google.maps.LatLng($(latInput).val(), $(lngInput).val());
+            var location;
 
-                marker.setPosition(latLng);
+            $(this).keyup(function() {
+                location = { lat: parseFloat($(latInput).val()), lng: parseFloat($(lngInput).val()) };
+                placeMarker(location);
             });
         });
 
-        // find on map
-        $('.map-locate').click(function(e) {
-            var address =  $(searchInput).val(),
-                geocoder = new google.maps.Geocoder();
+        // check if `Lat` & `Lng` fields are populated on load
+        // if so, drop a pin on that location and center
+        // otherwise, check if a `Where` field has been prepopulated and use that to focus map
+        if ($(latInput) !== '' && $(lngInput).val() !== '') {
+            var location = { lat: parseFloat($(latInput).val()), lng: parseFloat($(lngInput).val()) };
 
-            // we need a value, even a single letter will suffice
+            placeMarker(location);
+            map.setCenter(location);
+            map.setZoom(8);
+        } else if ($(searchInput).val() !== '') {
+            focusMap();
+        }
+
+        // set bounds on map
+        $('.map-locate').click(function(e) {
+            focusMap();
+        });
+
+        // use the geocoder to focus on a specific region on the map
+        function focusMap() {
+            var geocoder = new google.maps.Geocoder();
+
+            // even a single letter will return a result
             if ( $(searchInput).val().length <= 0 )
                 return false;
 
-            latLngInputs.empty();
-
-            geocoder.geocode({'address': address }, function(results, status) {
+            geocoder.geocode({'address': $(searchInput).val() }, function(results, status) {
                 if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
-                    var location = results[0].geometry.location;
-
-                    marker.setPosition(location);
-                    marker.setVisible(true);
-
                     if (results[0].geometry.viewport) {
                         map.fitBounds(results[0].geometry.viewport);
-                    } else {
-                        map.setZoom(1);
                     }
-
-                    latLngInputs.update(location.lat(), location.lng());
-                } else {
-                    console.log('Invalid address');
                 }
             });
-        });
+        }
 
-        var latLngInputs = {
-            update: function(lat,lng) {
-                $(latInput).val(lat);
-                $(lngInput).val(lng);
-            },
-            empty: function() {
-                $(latInput).val('');
-                $(lngInput).val('');
+        // updates or creates a marker at a specific location
+        function placeMarker(location) {
+            if (marker) {
+                marker.setPosition(location);
+            } else {
+                marker = new google.maps.Marker({
+                    draggable: true,
+                    map: map,
+                    position: location,
+                    visible: true
+                });
 
-                // hide the marker
-                marker.setVisible(false);
+                // when dragged
+                google.maps.event.addListener(marker, 'drag', function(e) {
+                    placeMarker(e.latLng);
+                    updateFields();
+                });
             }
-        };
+        }
+
+        // updates lat & lng + elevaton fields
+        function updateFields() {
+            var requestElevation = {
+              'locations': [marker.getPosition()]
+            };
+
+            $(latInput).val(marker.position.lat());
+            $(lngInput).val(marker.position.lng());
+
+            elevation.getElevationForLocations(requestElevation, function(results, status) {
+                if (status == google.maps.ElevationStatus.OK) {
+                    if (results[0]) {
+                        $(elvInput).val(parseFloat(results[0].elevation));
+                    } else {
+                        $(elvInput).val('');
+                    }
+                }
+            });
+        }
     })();
 }
