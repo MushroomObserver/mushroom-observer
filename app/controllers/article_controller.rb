b5bafbf668f7d22959baa16aa41acbc26ad21f04
Joe Cohen <jdcohenesq@gmail.com>
2017-04-22 15:39:07 -0700

    Add get create_article
    - Add test
    - Add ArticleController#create with just `get` action
    - Add template
    - Add some “translations” for this and other templates

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,116 @@
+# Actions for news articles
+#
+#  create_article:: Create new news article.
+#
+class ArticleController < ApplicationController
+  # Callbacks
+  before_action :login_required, except: [
+    :index,
+    :show_article
+  ]
+
+  # Create a new article
+  # :norobots:
+  def create_article
+    raise(:create_article_not_allowed.t) unless in_admin_mode?
+    return unless request.method == "POST"
+
+    store_location
+    pass_query_params
+    create_new_article
+    redirect_to(action: "show_article", id: article.id)
+  end
+
+
+  def create_glossary_term
+    if request.method == "POST"
+      glossary_term = GlossaryTerm.
+                      new(user: @user, name: params[:glossary_term][:name],
+                          description: params[:glossary_term][:description])
+      image_args = {
+        copyright_holder: params[:copyright_holder],
+        when: Time.local(params[:date][:copyright_year]),
+        license: License.safe_find(params[:upload][:license_id]),
+        user: @user,
+        image: params[:glossary_term][:upload_image]
+      }
+      glossary_term.add_image(process_image(image_args))
+      glossary_term.save
+      redirect_to(action: "show_glossary_term", id: glossary_term.id)
+    else
+      @copyright_holder = @user.name
+      @copyright_year = Time.now.year
+      @upload_license_id = @user.license_id
+      @licenses = License.current_names_and_ids(@user.license)
+    end
+  end
+
+  def show_glossary_term # :nologin:
+    store_location
+    @glossary_term = GlossaryTerm.find(params[:id].to_s)
+    @canonical_url = "#{MO.http_domain}/glossary/show_glossary_term/#{@glossary_term.id}"
+    @layout = calc_layout_params
+    @objects = @glossary_term.images
+  end
+
+  def index # :nologin:
+    store_location
+    @glossary_terms = GlossaryTerm.all.order(:name)
+  end
+
+  def process_image(args)
+    image = nil
+    name = nil
+    upload = args[:image]
+    if upload.blank?
+      name = upload.original_filename.force_encoding("utf-8") if
+        upload.respond_to?(:original_filename)
+
+      image = Image.new(args)
+      if !image.save
+        flash_object_errors(image)
+      elsif !image.process_image
+        logger.error("Unable to upload image")
+        name = image.original_name
+        name = "???" if name.empty?
+        flash_error(:runtime_image_invalid_image.t(name: name))
+        flash_object_errors(image)
+      else
+        name = image.original_name
+        name = "##{image.id}" if name.empty?
+        flash_notice(:runtime_image_uploaded_image.t(name: name))
+      end
+    end
+    image
+  end
+
+  def edit_glossary_term # :norobots:
+    # Expand to any MO user,
+    # but make them owned and editable only by that user or an admin
+    if request.method == "POST"
+      glossary_term = GlossaryTerm.find(params[:id].to_s)
+      glossary_term.attributes = params[:glossary_term].
+        permit(:name, :description)
+      glossary_term.user = @user
+      glossary_term.save
+      redirect_to(action: "show_glossary_term", id: glossary_term.id)
+    else
+      @glossary_term = GlossaryTerm.find(params[:id].to_s)
+    end
+  end
+
+  # Show past version of GlossaryTerm.
+  # Accessible only from show_glossary_term page.
+  def show_past_glossary_term # :nologin: :prefetch: :norobots:
+    pass_query_params
+    store_location
+    if @glossary_term = find_or_goto_index(GlossaryTerm, params[:id].to_s)
+      if params[:version]
+        @glossary_term.revert_to(params[:version].to_i)
+      else
+        flash_error(:show_past_location_no_version.t)
+        redirect_to(action: show_glossary_term, id: @glossary_term.id)
+      end
+    end
+  end
+end
---
+++ test/controllers/article_controller_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+# Controller tests for news articles
+class ArticleControllerTest < FunctionalTestCase
+  # ----------------------------
+  #  Create article
+  # ----------------------------
+  def test_create_article_get
+    # Prove article cannot be created unless in admin mode
+    user = users(:rolf)
+    login(user.login)
+    error = assert_raises(RuntimeError) { get(:create_article) }
+    assert_equal(:create_article_not_allowed.t, error.message)
+
+    make_admin
+    get(:create_article)
+    assert_form_action(action: "create_article")
+  end
+
+  def test_create_article_post
+    name = "Test article"
+    text = "The text of a new text article."
+    params = {
+      article: {
+        name: name,
+        text: text
+      }
+    }
+    post_requires_login(:create_article, params)
+    assert_redirected_to(action: :show_article, id: Article.last.id)
+  end
+end
