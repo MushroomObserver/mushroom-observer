d4f1b7126cf998dd5fae21f629450694e1e63252
Joe Cohen <jdcohenesq@gmail.com>
2017-04-22 18:13:09 -0700

    rails generate model Article
    - create migration
    - create Article skeleton
    - create ArticleTest skeleton
    - create Article Fixture skeleton

---
+++ db/migrate/20170423010922_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :name
+      t.string :author
+      t.text :body
+      t.integer :user_id
+      t.integer :rss_log_id
+
+      t.timestamps null: false
+    end
+  end
+end
