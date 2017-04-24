fcd5f55c993048126c78b374681a9ed2770d86f2
Joe Cohen <jdcohenesq@gmail.com>
2017-04-24 10:12:11 -0700

    Revise custom View Helper documentation

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,7 +1,16 @@
-# helpers for show Article views
+# Custom View Helpers for Article views
+#
+#   xx_tabset::     List of links to display in xx tabset; Includes links which
+#                     write Articles only if user has write permission
+#   xx_title::      Title of x page; includes any markup
+#
 module ArticleHelper
+  def index_tabs
+   return [] unless permitted?
+   [link_to(:create_article.t, action: :create_article)]
+  end
+
   # Headline: (News Article 5)
-  # displays Headline in bold
   def show_article_title(article)
     capture do
       concat(article.display_name.t)
