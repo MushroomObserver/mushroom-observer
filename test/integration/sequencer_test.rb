e45a7722166f1d7dbf2f7a7f6bf23f4b15d3b7a7
Joe Cohen <jdcohenesq@gmail.com>
2017-06-04 20:50:05 -0700

    Update (temporary) integration test to click Add Sequence
    (Fails because there's no add_sequence action.)

--- test/integration/sequencer_test.rb
+++ test/integration/sequencer_test.rb
@@ -5,6 +5,15 @@ require "test_helper"
 class SequencerTest < IntegrationTestCase
   def test_sequence
     obs = observations(:coprinus_comatus_obs)
+    owner = obs.user
+
+    # login
+    reset_session!
+    visit(root_path)
+    first(:link, "Login").click
+    fill_in("User name or Email address:", with: owner.login)
+    fill_in("Password:", with: "testpassword")
+    click_button("Login")
 
     visit("/#{obs.id}")
     click_link(:show_observation_add_sequence.l)
