08c5f66009626735cd7246b42ca2c4eba701816c
Joe Cohen <jdcohenesq@gmail.com>
2017-06-05 15:16:16 -0700

    Revise SequenceControllerTest#test_add_sequence_get
    - Use Observation not owned by Rolf (needed so we can test requires_login)
    - Fix logging in of non-owner
    
    (Fails @ l.20 because of lack of add_sequence template.)

--- test/controllers/sequence_controller_test.rb
+++ test/controllers/sequence_controller_test.rb
@@ -3,15 +3,15 @@ require "test_helper"
 # Controller tests for nucleotide sequences
 class SequenceControllerTest < FunctionalTestCase
   def test_add_sequence_get
-    # happy paths
-    obs   = observations(:coprinus_comatus_obs)
+    # obs not owned by Rolf (because `requires_login` will login Rolf)
+    obs   = observations(:minimal_unknown_obs)
     owner = obs.user
 
     # Prove method requires login
     requires_login(:add_sequence, id: obs.id)
 
     # Prove user cannot add Sequence to Observation he doesn't own
-    login(users(:zero_user.login))
+    login(users(:zero_user).login)
     get(:add_sequence, id: observations(:minimal_unknown_obs).id)
     assert_redirected_to(controller: :observer, action: :show_observation)
 
