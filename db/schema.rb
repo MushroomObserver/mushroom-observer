a67bc13a2f9b7c150271422c4dd3132235d3af4c
Joe Cohen <jdcohenesq@gmail.com>
2017-06-07 21:17:10 -0700

    Migrate after rebasing on master commit 7ff5a27

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426123736) do
+ActiveRecord::Schema.define(version: 20170513222059) do
 
   create_table "api_keys", force: :cascade do |t|
     t.datetime "created_at"
@@ -549,6 +549,17 @@ ActiveRecord::Schema.define(version: 20170426123736) do
     t.integer  "project_id",       limit: 4
     t.integer  "glossary_term_id", limit: 4
     t.integer  "article_id",       limit: 4
+  end
+
+  create_table "sequences", force: :cascade do |t|
+    t.integer  "observation_id", limit: 4
+    t.text     "locus",          limit: 65535
+    t.text     "bases",          limit: 65535
+    t.string   "archive",        limit: 255
+    t.string   "accession",      limit: 255
+    t.text     "notes",          limit: 65535
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "species_lists", force: :cascade do |t|
