87911374bb8a2760fe9a9c726d07f14d55e373de
Joe Cohen <jdcohenesq@gmail.com>
2017-06-09 07:00:09 -0700

    Tableize list of sequence in show_observation
    (Show this list as an html table.)
    - Change from hand-made list to <table>.
    - New partial for table row
    - In sequence helper, add data structure and methods for links to external sequence repositories

--- app/helpers/sequence_helper.rb
+++ app/helpers/sequence_helper.rb
@@ -11,4 +11,41 @@ module SequenceHelper
       concat(obs.name.format_name.t)
     end
   end
+
+  # on-line primary-source repositories (Archives) for nucleotide sequences
+  # in menu order
+  #   name::    short name, used in drop-down menu
+  #   home::    home page
+  #   prefix::  prefix for Accession (When Accession is appended to this,
+  #             it will land on the page for that Accession in the Archive.)
+  def archives
+    [
+      { name:   "GenBank",
+        home:   "https://www.ncbi.nlm.nih.gov/genbank/",
+        prefix: "https://www.ncbi.nlm.nih.gov/nuccore/"},
+      { name:   "ENA",
+        home:   "http://www.ebi.ac.uk/ena",
+        prefix: "http://www.ebi.ac.uk/ena/data/view/" },
+      { name:   "UNITE",
+        home:   "https://unite.ut.ee/",
+        prefix: "https://unite.ut.ee/search.php?qresult=yes&accno="}
+    ]
+  end
+
+  # returns the archive hash for the named archive
+  def archive(name)
+    archives.find {|r| r[:name] == name}
+  end
+
+  # url of a search for accession in the named external archive
+  def search_for_accession_url(name, accession)
+    archive(name)[:prefix] + accession
+  end
+
+  # dropdown list for add_sequence
+  def archive_dropdown
+    archives.each_with_object([]) do |archive, array|
+      array << [archive[:name], archive[:name]]
+    end
+  end
 end
