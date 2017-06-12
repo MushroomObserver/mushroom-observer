f964b0bb2d39102a5f0655c175f437f2b7f12f92
Jason <pellaea@gmail.com>
2017-06-12 14:20:40 -0700

    Added some new tests.  Fixed some rubocop violations.

--- test/models/observation_report_test.rb
+++ test/models/observation_report_test.rb
@@ -8,12 +8,12 @@ class ObservationReportTest < UnitTestCase
     table = CSV.parse(report)
     assert_equal(query.num_results + 1, table.count)
     idx = query.results.sort_by(&block).index(obs)
-    assert_equal(expect, table[idx+1], "(coprinus_comatus_obs)")
+    assert_equal(expect, table[idx + 1], "(coprinus_comatus_obs)")
   end
 
   def test_adolf
     obs = observations(:agaricus_campestris_obs)
-    do_report_test(ObservationReport::Adolf, obs, [
+    expect = [
       nil,
       nil,
       nil,
@@ -54,12 +54,13 @@ class ObservationReportTest < UnitTestCase
       nil,
       obs.id.to_s,
       nil
-    ], &:text_name)
+    ]
+    do_report_test(ObservationReport::Adolf, obs, expect, &:text_name)
   end
 
   def test_darwin
     obs = observations(:detailed_unknown_obs)
-    do_report_test(ObservationReport::Darwin, obs, [
+    expect = [
       "2006-05-12 17:21:00 UTC",
       "MushroomObserver",
       nil,
@@ -84,13 +85,14 @@ class ObservationReportTest < UnitTestCase
       "148",
       "294",
       "Found in a strange place... & with śtrangè characters™"
-    ], &:id)
+    ]
+    do_report_test(ObservationReport::Darwin, obs, expect, &:id)
   end
 
   def test_mycoflora
     obs = observations(:detailed_unknown_obs)
     img1, img2 = obs.images.sort_by(&:id)
-    do_report_test(ObservationReport::Mycoflora, obs, [
+    expect = [
       obs.id.to_s,
       "Fungi",
       nil,
@@ -107,12 +109,13 @@ class ObservationReportTest < UnitTestCase
       "http://mushroomobserver.org/#{obs.id}",
       "http://mushroomobserver.org//remote_images/orig/#{img1.id}.jpg " \
         "http://mushroomobserver.org//remote_images/orig/#{img2.id}.jpg"
-    ], &:id)
+    ]
+    do_report_test(ObservationReport::Mycoflora, obs, expect, &:id)
   end
 
   def test_raw
     obs = observations(:detailed_unknown_obs)
-    do_report_test(ObservationReport::Raw, obs, [
+    expect = [
       obs.id.to_s,
       obs.user.id.to_s,
       "mary",
@@ -142,12 +145,13 @@ class ObservationReportTest < UnitTestCase
       "X",
       obs.thumb_image.id.to_s,
       "Found in a strange place... & with śtrangè characters™"
-    ], &:id)
+    ]
+    do_report_test(ObservationReport::Raw, obs, expect, &:id)
   end
 
   def test_symbiota
     obs = observations(:unknown_with_lat_long)
-    do_report_test(ObservationReport::Symbiota, obs, [
+    expect = [
       "Fungi",
       nil,
       "Kingdom",
@@ -170,10 +174,52 @@ class ObservationReportTest < UnitTestCase
       "294",
       "2010-07-22 09:21:00 UTC",
       "unknown_with_lat_long"
-    ], &:id)
+    ]
+    do_report_test(ObservationReport::Symbiota, obs, expect, &:id)
   end
 
-  def test_row_formatting_directly
-    # TODO
+  def test_rounding_of_latitudes_etc
+    row = ObservationReport::Row.new(vals = [])
+    vals[2] = 1.20456
+    assert_equal(1.2, row.obs_lat(2))
+    assert_equal(1.205, row.obs_lat(3))
+    assert_equal(1.2046, row.obs_lat(4))
+    vals[2] = -123.00045
+    assert_equal(-123, row.obs_lat(3))
+    assert_equal(-123.0005, row.obs_lat(4))
+    assert_equal(-123.00045, row.obs_lat(5))
+    vals[4] = 123.4999
+    assert_equal(123, row.obs_alt)
+    vals[4] = -123.5000
+    assert_equal(-124, row.obs_alt)
   end
+
+  def test_cleaning_of_notes
+    row = ObservationReport::Row.new(vals = [])
+    vals[9] = " abc  def "
+    assert_equal("abc  def", row.obs_notes)
+  end
+
+  def test_split_date
+    row = ObservationReport::Row.new(vals = [])
+    vals[1] = "2017-01-03"
+    assert_equal("2017", row.year)
+    assert_equal("1", row.month)
+    assert_equal("3", row.day)
+  end
+
+  # This is not working yet...
+  # def test_split_location
+  #   row = ObservationReport::Row.new(vals = [])
+  #   vals[19] = "Park, City, Some, Alameda Co., California, USA"
+  #   assert_equal("USA", row.country)
+  #   assert_equal("California", row.state)
+  #   assert_equal("Alameda", row.county)
+  #   assert_equal("Some, City, Park", row.locality)
+  #   vals[19] = "Central Park, Los Angeles, California, USA"
+  #   assert_equal("USA", row.country)
+  #   assert_equal("California", row.state)
+  #   assert_equal(nil, row.county)
+  #   assert_equal("Los Angeles, Central Park", row.locality)
+  # end
 end
