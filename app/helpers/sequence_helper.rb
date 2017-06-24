038fbd7c62cea9d39ac73f66f291806633c621be
Joe Cohen <jdcohenesq@gmail.com>
2017-06-23 18:55:38 -0700

    bug- and misc fixes to SequenceHelper
    - correct typo
    - fix alignment
    - change name of block parameter for clarity

--- app/helpers/sequence_helper.rb
+++ app/helpers/sequence_helper.rb
@@ -17,14 +17,14 @@ module SequenceHelper
     link_with_query(:cancel_and_show.t(type: :observation),
                     @sequence.observation.show_link_args),
   ]
-  retun tabs unless check_permission(@sequence)
+  return tabs unless check_permission(@sequence)
   tabs.push(link_with_query(:EDIT.t, @sequence.edit_link_args)).
        push(link_with_query(:DESTROY.t, @sequence.destroy_link_args))
   end
 
   def sequence_accession_link(sequence)
     link_to(truncate(sequence.accession,
-                               length: sequence.locus_width/2).t,
+                              length: sequence.locus_width/2).t,
                       search_for_accession_url(sequence.archive,
                                                sequence.accession),
                       target: "_blank")
@@ -38,27 +38,27 @@ module SequenceHelper
 
   # on-line primary-source repositories (Archives) for nucleotide sequences
   # in menu order
-  #   name::    short name, used in drop-down menu
-  #   home::    home page
-  #   prefix::  prefix for Accession (When Accession is appended to this,
-  #             it will land on the page for that Accession in the Archive.)
+  #   name    short name, used in drop-down menu
+  #   home    home page
+  #   prefix  prefix for Accession (When Accession is appended to this,
+  #           it will land on the page for that Accession in the Archive.)
   def archives
     [
       { name:   "GenBank",
         home:   "https://www.ncbi.nlm.nih.gov/genbank/",
-        prefix: "https://www.ncbi.nlm.nih.gov/nuccore/"},
+        prefix: "https://www.ncbi.nlm.nih.gov/nuccore/" },
       { name:   "ENA",
         home:   "http://www.ebi.ac.uk/ena",
         prefix: "http://www.ebi.ac.uk/ena/data/view/" },
       { name:   "UNITE",
         home:   "https://unite.ut.ee/",
-        prefix: "https://unite.ut.ee/search.php?qresult=yes&accno="}
+        prefix: "https://unite.ut.ee/search.php?qresult=yes&accno=" }
     ]
   end
 
-  # returns the archive hash for the named archive
+  # return the archive hash for the named archive
   def archive(name)
-    archives.find {|r| r[:name] == name}
+    archives.find {|archive| archive[:name] == name}
   end
 
   # url of a search for accession in the named external archive
