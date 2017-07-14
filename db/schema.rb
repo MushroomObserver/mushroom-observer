462f8338c3fe788f64d2e932497656d9e05ef4d8
Joe Cohen <jdcohenesq@gmail.com>
2017-07-13 20:00:54 -0400

    Migrate Observation notes to new format
    - Write migration, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615205453) do
+ActiveRecord::Schema.define(version: 20170713204001) do
 
   create_table "api_keys", force: :cascade do |t|
     t.datetime "created_at"
