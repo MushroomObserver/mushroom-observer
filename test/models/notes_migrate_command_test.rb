b81344991b85f2d8c3df3bb0d45c1805b6d53032
Joe Cohen <jdcohenesq@gmail.com>
2017-07-17 21:48:09 -0400

    Fix some bugs in migration
    Migration wasn't doing what I thought because it was always serializing when writing.  Ditto for the tests.
    - Write directly to db when necessary
    - Add methods which escape a string for inclusion in a sql statement
    - Test more note strings

--- test/models/notes_migrate_command_test.rb
+++ test/models/notes_migrate_command_test.rb
@@ -7,109 +7,120 @@ class NotesMigrateCommandTest < UnitTestCase
   # temporary test of commands to up-migrate notes format to YAML
   # and down-migrate back to original
   def test_up_and_down
-    # Modify notes of some fixtures
-    # I don't want to put all this junk into the fixtures because it will
-    # have to be removed after up-migration
+    # Modify some random fixtures' notes to pre-migration form
+    observations   = []
+    original_notes = []
 
-    # Give some of them complicated pre-migration notes
-    observations(:vouchered_obs).notes = "_Agaricus_ but what species?  I was going to guess _A. subrutilescens_, but then noticed that cap flesh stains yellow near cuticle when cut.  Both Arora and Trudell & Ammirati say that _A. subrutilescens_,is non-staining.
-In duff (and possibly bark dust).  Nearest tree was a spruce.
-I do not have a mature specimen, so cannot tell real maximum sizes and other characters.
-Cap
-* appears brown from a distance, but actually white, with abundant brown fibrils
-* max diameter 6.5 cm
-* mild almond odor and taste
-* flesh white, 1 cm thick
-* cap flesh stains yellow near cuticle when cut.  See 3rd photo.
-Gills
-* free
-* close
-* young gills off-white
-* 6mm
-Stipe
-* shaggy white starting at partial veil downward for about 1/2 length of stipe, then brown fibrils
-* almost all the stipe was below the duff; only the cap was showing.  See 2nd photo
-* 17.5 cm long
-* 2.5 cm thick
-* slightly bulbous base
-* almond taste
-* hollow
-* membranous partial veil still present in all specimens, so I cannot be certain what annulus will look like.  But by how it's attached to the stipe, I'm guessing it will be skirt-like
-* stipe does not stain on cutting or bruising
-    "
-    observations(:vouchered_obs).save
+    # duplicates a later one as observations.first
+    observations   << observations(:agaricus_campestras_obs)
+    original_notes << %Q{\\}
 
-    observations(:vouchered_imged_obs).notes = %{
-+Collectors+: Sally Visher, Joseph D. Cohen
-+Substrate+: Soil
-+Habitat+:  below undergrowth in relatively open area in coastal "??Picea sitchensis??":http://eol.org/pages/1033696 (Sitka Spruce) forest.
-+Nearest large tree+:  "??Picea sitchensis??":http://eol.org/pages/1033696 (Sitka Spruce).  (No other conifer species anywhere near collection.)
-+Chemical+: Cap cuticle may stain dark red in KOH; it's hard to tell because of the age and water-logged state of this mushroom. Cap flesh negative in KOH.
-+Spores+: Brown
-bq. +Measurements+:
-Piximètre 5.9 R 1530 : le 20/06/2017 à 16:55:06.1172147
-(12.4) 12.5 - 14.5 (15.8) × (4.9) 5.8 - 6.7 (6.8) µm
-Q = 2 - 2.3 (2.7) ; N = 10
-**Me = 13.6 × 6.2 µm ; Qe = 2.2**
-+Other+:  The blue staining seen in some photos appears to originate in the cap flesh around its edges.  The final 3 macro photos (with white background) were taken ~2.5 hours after collecting this mushroom.}
-    observations(:vouchered_imged_obs).save
+    # nil
+    nil_notes_obs   = observations(:sortable_obs_users_second_obs)
+    observations   << nil_notes_obs
+    original_notes << nil
 
-    observations(:updated_2013_obs).notes = %{ Used “North American Boletes” by Bessette and Roody for ID.
-[admin – Sat Aug 14 02:00:21 +0000 2010]: Changed location name from ‘Fairfield, Ct.’ to ‘Fairfield, Connecticut, USA’}
-    observations(:updated_2013_obs).save
+    # blank
+    blank_notes_obs = observations(:sortable_obs_users_first_obs)
+    observations   << blank_notes_obs
+    original_notes << ""
 
-    # make pre-migration notes blank
-    blank_notes_obs = observations(:minimal_unknown_obs)
-    blank_notes_obs.notes = ""
-    blank_notes_obs.save
+    # single quote
+    observations   << observations(:unlisted_rolf_obs)
+    original_notes << %Q{with 'french' fries}
 
-    # make pre-migration notes nil
-    nil_notes_obs = observations(:unlisted_rolf_obs)
-    nil_notes_obs.notes = nil
-    nil_notes_obs.save
+    # double quote
+    observations   << observations(:unequal_positive_namings_obs)
+    original_notes <<  %Q{with "french" fries}
 
-    observations     = Observation.all
-    original_notes   = observations.map(&:notes)
-    original_updated = observations.map(&:updated_at)
+    # newline
+    observations   << observations(:imged_unvouchered_obs)
+    original_notes << %Q{a\nb}
 
-    #### Prove that up method works ###
-    up
+    # carriage return
+    observations   << observations(:all_namings_deprecated_obs)
+    original_notes << %Q{a\rb}
 
-    # Up migrated notes should be translatable to original
-    observations.size.times do |i|
-      up_notes = observations[i].reload.notes
-      if original_notes[i].empty?
-        assert_nil(up_notes,
-                   "Obs #{observations[i].id} up notes should be nil")
-        assert_nil(to_down_notes(up_notes),
-                   "Obs #{observations[i].id} nil up notes not convertible")
+    # backslash
+    observations   << observations(:authored_with_naming_obs)
+    original_notes << %Q{\\}
+
+    # simple notes
+    observations   << observations(:imageless_unvouchered_obs)
+    original_notes << "simple notes"
+
+   # linefeeds, textile markup, single quote
+    observations   << observations(:vouchered_obs)
+    original_notes << %Q{_Agaricus_ but what species?  I was going to guess _A. subrutilescens_, but then noticed that cap flesh stains yellow near cuticle when cut.  Both Arora and Trudell & Ammirati say that _A. subrutilescens_,is non-staining.\nIn duff (and possibly bark dust).  Nearest tree was a spruce.\nI do not have a mature specimen, so cannot tell real maximum sizes and other characters.\nCap\n* appears brown from a distance, but actually white, with abundant brown fibrils\n* max diameter 6.5 cm\n* mild almond odor and taste\n* flesh white, 1 cm thick\n* cap flesh stains yellow near cuticle when cut.  See 3rd photo.\nGills\n* free\n* close\n* young gills off-white\n* 6mm\nStipe\n* shaggy white starting at partial veil downward for about 1/2 length of stipe, then brown fibrils\n* almost all the stipe was below the duff; only the cap was showing.  See 2nd photo\n* 17.5 cm long\n* 2.5 cm thick\n* slightly bulbous base\n* almond taste\n* hollow\n* membranous partial veil still present in all specimens, so I cannot be certain what annulus will look like.  But by how it's attached to the stipe, I'm guessing it will be skirt-like\n* stipe does not stain on cutting or bruising\n    }
+
+    # linefeeds, textile markup, double quotes
+    observations   << observations(:vouchered_imged_obs)
+    original_notes << %Q{\n+Collectors+: Sally Visher, Joseph D. Cohen\n+Substrate+: Soil\n+Habitat+:  below undergrowth in relatively open area in coastal "??Picea sitchensis??":http://eol.org/pages/1033696 (Sitka Spruce) forest.\n+Nearest large tree+:  "??Picea sitchensis??":http://eol.org/pages/1033696 (Sitka Spruce).  (No other conifer species anywhere near collection.)\n+Chemical+: Cap cuticle may stain dark red in KOH; it's hard to tell because of the age and water-logged state of this mushroom. Cap flesh negative in KOH.\n+Spores+: Brown\nbq. +Measurements+:\nPiximètre 5.9 R 1530 : le 20/06/2017 à 16:55:06.1172147\n(12.4) 12.5 - 14.5 (15.8) × (4.9) 5.8 - 6.7 (6.8) µm\nQ = 2 - 2.3 (2.7) ; N = 10\n**Me = 13.6 × 6.2 µm ; Qe = 2.2**\n+Other+:  The blue staining seen in some photos appears to originate in the cap flesh around its edges.  The final 3 macro photos (with white background) were taken ~2.5 hours after collecting this mushroom.}
+
+    # something which looks like a symbol mid-line
+    observations   << observations(:updated_2013_obs)
+    original_notes << %Q{ Used “North American Boletes” by Bessette and Roody for ID.\n[admin – Sat Aug 14 02:00:21 +0000 2010]: Changed location name from ‘Fairfield, Ct.’ to ‘Fairfield, Connecticut, USA’}
+
+    observations.each_index do |i|
+      if original_notes[i].nil?
+        write_notes_without_serializing(
+          obs: observations[i], notes: ""
+        )
       else
-        assert_equal(
-          original_notes[i], to_down_notes(up_notes),
-          "Obs #{observations[i].id} up notes not convertible"
+        write_notes_without_serializing(
+          obs: observations[i], notes: original_notes[i]
         )
       end
     end
 
-    # Prove notes which were originally empty notes were up-migrated to nil
-    assert_nil(blank_notes_obs.reload.notes)
+    original_updated = observations.map(&:updated_at)
 
-    # Prove notes which were originally nil are still nil
-    assert_nil(nil_notes_obs.reload.notes)
+    ############################################################################
+    #
+    # Prove that up method works
+    #
+    ############################################################################
 
-    #### prove that down method works ###
+    up
+
+    # Prove that unserialized notes are up-migrated to
+    # serialized { other: original_notes },
+    # Except nil notes are up-migrated to serialized empty string
+    observations.size.times do |i|
+      up_notes = observations[i].reload.notes
+      if original_notes[i].nil? || original_notes[i] == ""
+        assert_equal(
+          "", up_notes,
+          "Obs #{observations[i].id} up notes should be empty string"
+          )
+      else
+        assert_equal(
+          original_notes[i], up_notes[:other],
+          "Obs #{observations[i].id} notes not upmigrated correctly"
+        )
+      end
+      assert_equal(original_updated[i], observations[i].updated_at)
+    end
+
+    ############################################################################
+    #
+    # Prove that down method works
+    #
+    ############################################################################
+
     # It should restore everything except those which were originally ""
     # because both "" and nil were up-migrated to nil.
     # So after up-mgration there's no way to differentiate them.
-    # Both will be down-migrated to nil.
+    # Both will be down-migrated to empty string.
     down
 
     observations.size.times do |i|
       obs = observations[i]
-      if original_notes[i].empty?
-        assert_nil(obs.reload.notes)
+      raw_down_notes = read_notes_without_serializing(obs)
+      if original_notes[i].nil? || original_notes[i].empty?
+        assert_equal("", raw_down_notes)
       else
-        assert_equal(original_notes[i], obs.reload.notes)
+        assert_equal(original_notes[i], raw_down_notes)
       end
     end
   end
@@ -117,38 +128,69 @@ Q = 2 - 2.3 (2.7) ; N = 10
   ##########################################################################
   # remainder of this file is what gets moved to the migration file
 
-  # empty notes get up-migrated to nil
+
+  # ***** THIS MIGRATION MUST BE RUN WITH Observation::serialize :notes ********
+  #
+  # nil notes get up-migrated to YAML serialized empty string
   # others to a YAML serialized hash { other: old notes }
   def up
-    # individually migrate notes where notes.present?
-    fulls = Observation.where.not(notes: nil).where.not(notes: "")
-    # set column directly to vaoid validation, time-stamping
-    fulls.each { |obs| obs.update_column(:notes, to_up_notes(obs.notes)) }
-
-    # Do the rest in a single SQL statement; drastically cuts migration time
-    sql = "
-      UPDATE observations
-      SET notes = NULL
-      WHERE notes IS null OR notes = '';
-    "
-    ActiveRecord::Base.connection.execute(sql)
+    Observation.all.each do |obs|
+      raw_notes = read_notes_without_serializing(obs)
+      # write them with serializing
+      obs.update_column(:notes, to_up_notes(raw_notes))
+    end
   end
 
-  # put non-empty notes into the "other:" field
-  def to_up_notes(notes)
-    notes.empty? ? nil : { other: notes }.to_yaml
-  end
-
-  # convert Observation notes from a YAML hash, except if they're nil (which
-  # remain nil after down-migration, so we can leave them alone for speed)
+  # convert Observation notes from a YAML hash
   def down
-    non_nils = Observation.where.not(notes: nil)
-    non_nils.each { |obs| obs.update_column(:notes, to_down_notes(obs.notes)) }
+    Observation.all.each do |obs|
+      serialized_notes = obs.reload.notes
+      down_notes = to_down_notes(serialized_notes)
+      write_notes_without_serializing(obs: obs, notes: down_notes)
+    end
   end
 
-  # extract non-empty notes from the "other:" field
+  # Read notes, skipping serialization, callbacks, validation
+  def read_notes_without_serializing(obs)
+    ActiveRecord::Base.connection.exec_query("
+      SELECT notes FROM observations WHERE id = #{obs.id}
+    ").rows.first.first
+  end
+
+  # Write notes, skipping serialization, callbacks, validation
+  def write_notes_without_serializing(obs:, notes:)
+    ActiveRecord::Base.connection.execute("
+      UPDATE observations
+      SET notes = \"#{escape_for_sql(notes)}\"
+      WHERE id = #{obs.id}
+    ")
+  end
+
+  # Return desired up-migrated notes post-serialization
+  # put non-empty notes into the "other:" field
+  def to_up_notes(raw_notes)
+    if raw_notes.present?
+      { other: raw_notes }
+    elsif raw_notes.nil?
+      ""
+    else
+      raw_notes
+    end
+  end
+
+  # Return desired reverted notes
+  # Extract the "other:" field; otherwise return a blank string
   def to_down_notes(notes)
-    return nil if notes.nil?
-    YAML.load(notes).empty? ? "" : YAML.load(notes)[:other]
+    notes.is_a?(Hash) ? (notes)[:other] : ""
+  end
+
+  # returns a string suitable for inclusion in a SQL statement,
+  # escaping the characters for which MySQL requires escaping
+  # input is a double-quoted string
+  # The 2nd gsub is needed because I can't figure out how to get
+  # a double quote to behave properly inside character class
+  # inside the capture group
+  def escape_for_sql(str)
+    str.gsub(/([\0\b\n\r\t\\])/, '\\\\\1').gsub(%q{"}, %q{\\\\"})
   end
 end
