c3c7303132502481ad181f481301b249bb048326
Joe Cohen <jdcohenesq@gmail.com>
2017-07-30 18:18:33 -0400

    Some Rubocopping of User
    Fix offenses in additions made by this branch.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1042,7 +1042,7 @@ class User < AbstractModel
   # :nodoc
   def notes_template_bad_parts
     return [] unless notes_template.present?
-    bad_parts = notes_template.split(",").each_with_object([]) do |part, a|
+    notes_template.split(",").each_with_object([]) do |part, a|
       next unless notes_template_reserved_words.include?(part.squish.downcase)
       a << part.strip
     end
@@ -1061,6 +1061,6 @@ class User < AbstractModel
 
   # :nodoc
   def notes_other_translations
-    %w[andere altro altra autre autres otra otras otro otros outros]
+    %w(andere altro altra autre autres otra otras otro otros outros)
   end
 end
