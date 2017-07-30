2f7c46e3adbe23768af2b877cd3192a2390e0a59
Joe Cohen <jdcohenesq@gmail.com>
2017-07-30 10:38:07 -0400

    Fix a few RuboCop offenses in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -799,8 +799,7 @@ class User < AbstractModel
   # notes_template: ""
   # notes_template_parts # => []
   def notes_template_parts
-    return [] unless notes_template?
-    (notes_template.split(",")).map(&:squish)
+    notes_template? ? notes_template.split(",").map(&:squish) : []
   end
 
   ##############################################################################
@@ -1022,7 +1021,7 @@ class User < AbstractModel
   validate :notes_template_forbid_other
   # :nodoc
   def notes_template_forbid_other
-    notes_template_bad_parts.each do | part |
+    notes_template_bad_parts.each do |part|
       errors.add(:notes_template, :prefs_notes_template_no_other.t(part: part))
     end
   end
