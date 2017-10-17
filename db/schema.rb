11708388840dd8337dc5730fa1339fa8b299b736
Joe Cohen <jdcohenesq@gmail.com>
2017-10-16 17:20:24 -0700

    Non-substantive schema change
    I don't know why Rails keeps doing this, but I do want to get it committed.

--- db/schema.rb
+++ db/schema.rb
@@ -650,7 +650,7 @@ ActiveRecord::Schema.define(version: 20170827000729) do
     t.text     "mailing_address",              limit: 65535
     t.integer  "layout_count"
     t.boolean  "view_owner_id",                              default: false, null: false
-    t.string   "content_filter",               limit: 255
+    t.string   "content_filter"
     t.text     "notes_template",               limit: 65535
   end
 
