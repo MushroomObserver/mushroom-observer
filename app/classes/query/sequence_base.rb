25dc2e963f7b9335d3883bb9069e9e7b6ccd802d
Jason <pellaea@gmail.com>
2017-11-10 20:20:08 -0800

    Added a bunch of observation-based filters for sequence queries.

--- app/classes/query/sequence_base.rb
+++ app/classes/query/sequence_base.rb
@@ -7,18 +7,39 @@ module Query
 
     def parameter_declarations
       super.merge(
-        created_at?:    [:time],
-        updated_at?:    [:time],
-        observations?:  [Observation],
-        users?:         [User],
-        locus_has?:     :string,
-        archive_has?:   :string,
-        accession_has?: :string,
-        notes_has?:     :string
+        # These apply to sequence itself:
+        created_at?:     [:time],
+        updated_at?:     [:time],
+        observations?:   [Observation],
+        users?:          [User],
+        locus_has?:      :string,
+        archive_has?:    :string,
+        accession_has?:  :string,
+        notes_has?:      :string
+        # These apply to parent observation:
+        date?:           [:date],
+        observers?:      [User],
+        names?:          [:string],
+        synonym_names?:  [:string],
+        children_names?: [:string],
+        locations?:      [:string],
+        projects?:       [:string],
+        species_lists?:  [:string],
+        confidence?:     [:float],
+        north?:          :float,
+        south?:          :float,
+        east?:           :float,
+        west?:           :float
       )
     end
 
     def initialize_flavor
+      initialize_sequence_filters
+      initialize_observation_filters
+      super
+    end
+
+    def initialize_sequence_filters
       initialize_model_do_time(:created_at)
       initialize_model_do_time(:updated_at)
       initialize_model_do_objects_by_id(:observations)
@@ -31,7 +52,45 @@ module Query
       initialize_model_do_search(:archive_has, :archive)
       initialize_model_do_search(:accession_has, :accession)
       initialize_model_do_search(:notes_has, :notes)
-      super
+    end
+
+    def initialize_observation_filters
+      initialize_model_do_date(
+        :date, "observations.when", join: :observations
+      )
+      initialize_model_do_objects_by_id(
+        :observers, "observations.user_id", join: :observations
+      )
+      initialize_model_do_objects_by_name(
+        Name, :names, "observations.name_id", join: :observations
+      )
+      initialize_model_do_objects_by_name(
+        Name, :synonym_names, "observations.name_id",
+        filter: :synonyms, join: :observations
+      )
+      initialize_model_do_objects_by_name(
+        Name, :children_names, "observations.name_id",
+        filter: :all_children, join: :observations
+      )
+      initialize_model_do_objects_by_name(
+        Location, :locations, "observations.location_id", join: :observations
+      )
+      initialize_model_do_objects_by_name(
+        Project, :projects, "observations_projects.project_id",
+        join: { observations: :observations_projects }
+      )
+      initialize_model_do_objects_by_name(
+        SpeciesList, :species_lists,
+        "observations_species_lists.species_list_id",
+        join: { observations: :observations_species_lists }
+      )
+      initialize_model_do_range(
+        :confidence, "observations.vote_cache", join: :observations
+      )
+      if params[:north] || params[:south] || params[:east] || params[:west]
+        add_join(observations: :locations)
+      end
+      initialize_model_do_bounding_box(:observation)
     end
 
     def default_order
