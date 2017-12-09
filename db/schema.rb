b45cc95a3ffeed180aea5b6e410d1535b5762a72
Jason <pellaea@gmail.com>
2017-12-08 19:07:49 -0800

    Added quick migration to correct existing names which are deprecated but in bold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208172500) do
+ActiveRecord::Schema.define(version: 20171208190100) do
 
   create_table "api_keys", force: :cascade do |t|
     t.datetime "created_at"
