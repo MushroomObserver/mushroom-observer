2c178e980674dea327ac485b45eafe830acac9de
Jason <pellaea@gmail.com>
2017-12-10 16:51:33 -0800

    Cleaned up ambiguity about what User#locale meant.  Should definitely include the region in the users table.  I think it is misnamed in languages table.  But whatever, for now, so long as they are all consistent within a single column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209004800) do
+ActiveRecord::Schema.define(version: 20171210164400) do
 
   create_table "api_keys", force: :cascade do |t|
     t.datetime "created_at"
