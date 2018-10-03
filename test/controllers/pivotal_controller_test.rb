bcd6794e9541bcc6663918c9da33f50680b8f583
Nathan Wilson <nathan@collectivesource.com>
2018-10-03 13:46:22 -0400

    Add test to cover Pivotal::story_order

--- test/controllers/pivotal_controller_test.rb
+++ test/controllers/pivotal_controller_test.rb
@@ -1,18 +1,45 @@
 require "test_helper"
 
 class PivotalControllerTest < FunctionalTestCase
-  def test_index
-    # Don't create entries on Pivotal unless enabled.
-    # Unit tests for Pivotal actually post (and then delete) test comments
-    # on the live pivotal account.  This is great if we really wanted to test
-    # the pivotal code.  But the vast majority of the time no one was touching
-    # the pivotal code, and this level of testing was not required.
-    # And it generated email notifications via pivotal each time,
-    # and just generally didn't seem respectful to pivotal.
-    # -- per JPH
-    return unless MO.pivotal_enabled
-
+  def test_index_disabled
+    enabled = MO.pivotal_enabled
+    MO.pivotal_enabled = false
     get_with_dump(:index)
     assert_response("index")
+    MO.pivotal_enabled = enabled
+  end
+
+  def test_index_enabled
+    stub_request(:get, "https://www.pivotaltracker.com/services/v5/projects/224629/stories?fields=story_type,estimate,current_state,name,description,updated_at,labels(name),comments(created_at,text)&filter=state:unscheduled,started,unstarted&limit=500").
+      with(
+        headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Ruby',
+          'X-Trackertoken'=>'xxx'
+        }).
+      to_return(status: 200,
+                body: '[{"id":"1",
+                         "story_type":"fairy tale",
+                         "estimate":"Never",
+                         "current_state":"Languishing",
+                         "name":"Steve",
+                         "description":"",
+                         "labels":[{"name":"specimen"}],
+                         "comments":[]},
+                         {"id":"2",
+                         "story_type":"biography",
+                         "estimate":"Someday",
+                         "current_state":"Ready to go",
+                         "name":"Martha",
+                         "description":"",
+                         "labels":[{"name":"critical"}],
+                         "comments":[]}]',
+                headers: {})
+    enabled = MO.pivotal_enabled
+    MO.pivotal_enabled = true
+    get_with_dump(:index)
+    assert_response("index")
+    MO.pivotal_enabled = enabled
   end
 end
