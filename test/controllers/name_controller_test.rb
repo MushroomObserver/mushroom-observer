fb656189898a05848e0088f793b7713227d76616
Nathan Wilson <nathan@collectivesource.com>
2018-12-03 15:08:38 -0500

    Coverage for DescriptionControllerHelpers::make_description_default

--- test/controllers/name_controller_test.rb
+++ test/controllers/name_controller_test.rb
@@ -147,6 +147,14 @@ class NameControllerTest < FunctionalTestCase
     end
   end
 
+  def make_description_default_helper(desc)
+    user = desc.user
+    params = {
+      id: desc.id
+    }
+    requires_login(:make_description_default, params, user.login)
+  end
+
   # Destroy a draft of a project.
   def destroy_draft_helper(draft, user, success = true)
     assert(draft)
@@ -3866,6 +3874,23 @@ class NameControllerTest < FunctionalTestCase
     )
   end
 
+  def test_make_description_default
+    desc = name_descriptions(:peltigera_alt_desc)
+    assert_not_equal(desc, desc.parent.description)
+    make_description_default_helper(desc)
+    desc.parent.reload
+    assert_equal(desc, desc.parent.description)
+  end
+
+  def test_non_public_description_cannot_be_default
+    desc = name_descriptions(:peltigera_user_desc)
+    current_default = desc.parent.description
+    make_description_default_helper(desc)
+    desc.parent.reload
+    assert_not_equal(desc, desc.parent.description)
+    assert_equal(current_default, desc.parent.description)
+  end
+  
   # Owner can publish.
   def test_publish_draft
     publish_draft_helper(name_descriptions(:draft_coprinus_comatus), nil,
