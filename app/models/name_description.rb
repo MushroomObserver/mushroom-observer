6c4d280304c5501dcc8d4657a9c1fc3499d5e720
Joe Cohen <jdcohenesq@gmail.com>
2019-01-13 18:09:26 -0800

    Fix deprecation warnings caused by MO code in name_description.rb
    - change `<attribute>_changed?` to `saved_change_to_<attribute>?`
    - change `<attribute>_changed` to `saved_change_to_<attribute>`
    
    But this file still causes warnings with `altered?` (line 235) which is a call to the cure_acts_as_version_gem.

--- app/models/name_description.rb
+++ app/models/name_description.rb
@@ -230,9 +230,9 @@ class NameDescription < Description
   def notify_users
     # Even though
     # changing review_status doesn't cause a new version to be created, I want
-    # to notify authors of that change.  (review_status_changed? is an implicit
-    # method created by ActiveRecord)
-    if altered? || review_status_changed?
+    # to notify authors of that change.
+    # (saved_change_to_<attribute>? is a Rails automagical method)
+    if altered? || saved_change_to_review_status?
       sender = User.current || User.admin
       recipients = []
 
@@ -273,7 +273,7 @@ class NameDescription < Description
       # Send notification to all except the person who triggered the change.
       for recipient in recipients.uniq - [sender]
         QueuedEmail::NameChange.create_email(sender, recipient, name, self,
-                                             review_status_changed?)
+                                             saved_change_to_review_status?)
       end
     end
 
