a139cdb9aeacb7f0de34bd72e649f1ded025b061
Nathan Wilson <nathan@collectivesource.com>
2019-01-27 17:37:52 -0500

    Fix hash syntax issue

--- app/models/triple.rb
+++ app/models/triple.rb
@@ -3,7 +3,7 @@ class Triple < ApplicationRecord
   def self.delete_predicate_matches(predicate)
     return unless valid_predicate(predicate)
 
-    Triple.connection.delete(:conditions => ["predicate = ?", predicate])
+    Triple.connection.delete(conditions: ["predicate = ?", predicate])
   end
 
   def self.valid_predicate(predicate)
