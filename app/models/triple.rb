f3fb0c11ec3a18bc69d296836331fb1845e2c519
Nathan Wilson <nathan@collectivesource.com>
2019-01-27 17:52:07 -0500

    Better fix for SQL injection

--- app/models/triple.rb
+++ app/models/triple.rb
@@ -3,8 +3,7 @@ class Triple < ApplicationRecord
   def self.delete_predicate_matches(predicate)
     return unless valid_predicate(predicate)
 
-    query = "DELETE FROM triples WHERE predicate = '#{predicate}'"
-    Triple.connection.delete(query)
+    Triple.where(predicate: predicate).destroy_all
   end
 
   def self.valid_predicate(predicate)
