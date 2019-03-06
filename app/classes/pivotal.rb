e5378111f9bbbb69a9036b13abd7d2b507c07f5b
Joe Cohen <jdcohenesq@gmail.com>
2019-03-05 18:18:56 -0600

    Fix pivotal LineLength offenses

--- app/classes/pivotal.rb
+++ app/classes/pivotal.rb
@@ -1,7 +1,8 @@
 class Pivotal
-  require_dependency "pivotal/http"       # Deals with HTTP requests.
-  require_dependency "pivotal/story"      # Encapsulates a single story.
-  require_dependency "pivotal/comment"    # Encapsulates a comment on a story.
-  require_dependency "pivotal/vote"       # Encapsulates a vote for a story.
-  require_dependency "pivotal/user"       # Encapsulates a user for a story or comment.
+  require_dependency "pivotal/http"    # Deals with HTTP requests.
+  require_dependency "pivotal/story"   # Encapsulates a single story.
+  require_dependency "pivotal/comment" # Encapsulates a comment on a story.
+  require_dependency "pivotal/vote"    # Encapsulates a vote for a story.
+  require_dependency "pivotal/user"    # Encapsulates a user for a story
+                                       # or comment.
 end
