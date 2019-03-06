ba7940dad860802050b0a01465dfa9343a666c7c
Joe Cohen <jdcohenesq@gmail.com>
2019-03-05 18:07:27 -0600

    Fix pattern_search/errors LineLength offenses

--- app/classes/pattern_search/errors.rb
+++ app/classes/pattern_search/errors.rb
@@ -33,79 +33,94 @@ module PatternSearch
 
   class BadBooleanError < Error
     def to_s
-      :pattern_search_bad_boolean_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_boolean_error.t(term: args[:var].inspect,
+                                          value: args[:val].inspect)
     end
   end
 
   class BadYesError < Error
     def to_s
-      :pattern_search_bad_yes_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_yes_error.t(term: args[:var].inspect,
+                                      value: args[:val].inspect)
     end
   end
 
   class BadYesNoBothError < Error
     def to_s
-      :pattern_search_bad_yes_no_both_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_yes_no_both_error.t(term: args[:var].inspect,
+                                              value: args[:val].inspect)
     end
   end
 
   class BadFloatError < Error
     def to_s
-      :pattern_search_bad_float_error.t(term: args[:var].inspect, value: args[:val].inspect, min: args[:min].inspect, max: args[:max].inspect)
+      :pattern_search_bad_float_error.t(term: args[:var].inspect,
+                                        value: args[:val].inspect,
+                                        min: args[:min].inspect,
+                                        max: args[:max].inspect)
     end
   end
 
   class BadNameError < Error
     def to_s
-      :pattern_search_bad_name_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_name_error.t(term: args[:var].inspect,
+                                       value: args[:val].inspect)
     end
   end
 
   class BadHerbariumError < Error
     def to_s
-      :pattern_search_bad_herbarium_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_herbarium_error.t(term: args[:var].inspect,
+                                            value: args[:val].inspect)
     end
   end
 
   class BadLocationError < Error
     def to_s
-      :pattern_search_bad_location_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_location_error.t(term: args[:var].inspect,
+                                           value: args[:val].inspect)
     end
   end
 
   class BadProjectError < Error
     def to_s
-      :pattern_search_bad_project_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_project_error.t(term: args[:var].inspect,
+                                          value: args[:val].inspect)
     end
   end
 
   class BadSpeciesListError < Error
     def to_s
-      :pattern_search_bad_species_list_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_species_list_error.t(term: args[:var].inspect,
+                                               value: args[:val].inspect)
     end
   end
 
   class BadUserError < Error
     def to_s
-      :pattern_search_bad_user_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_user_error.t(term: args[:var].inspect,
+                                       value: args[:val].inspect)
     end
   end
 
   class BadConfidenceError < Error
     def to_s
-      :pattern_search_bad_confidence_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_confidence_error.t(term: args[:var].inspect,
+                                             value: args[:val].inspect)
     end
   end
 
   class BadDateRangeError < Error
     def to_s
-      :pattern_search_bad_date_range_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_date_range_error.t(term: args[:var].inspect,
+                                             value: args[:val].inspect)
     end
   end
 
   class BadRankRangeError < Error
     def to_s
-      :pattern_search_bad_rank_range_error.t(term: args[:var].inspect, value: args[:val].inspect)
+      :pattern_search_bad_rank_range_error.t(term: args[:var].inspect,
+                                             value: args[:val].inspect)
     end
   end
 end
