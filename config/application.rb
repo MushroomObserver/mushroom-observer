363a49460097203ab5de2076b5091f767745a3c0
Joe Cohen <jdcohenesq@gmail.com>
2019-03-17 15:10:39 -0700

    Fix config/application LineLength offenses

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module MushroomObserver
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those
+    # specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # Custom directories with classes and modules you want to be autoloadable.
@@ -17,12 +18,16 @@ module MushroomObserver
       #{config.root}/app/extensions
     ]
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Set Time.zone default to the specified zone and
+    # make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names.
+    # Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
+    # The default locale is :en and all translations from
+    # config/locales/*.rb, yml are auto loaded.
+    # config.i18n.load_path +=
+    #  Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = true
 
