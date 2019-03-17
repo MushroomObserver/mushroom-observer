9749b918919c4228d843a033d4446f1c9820b56d
Joe Cohen <jdcohenesq@gmail.com>
2019-03-17 16:44:32 -0700

    Fix initializers/session_store LineLength offenses

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-MushroomObserver::Application.config.session_store :cookie_store,
-                                                   key: "_mushroom-observer_session_3.1"
+MushroomObserver::Application.
+  config.session_store :cookie_store, key: "_mushroom-observer_session_3.1"
