881c2c1b97d22c68c0d125fb656f5a22de0fb891
Jason <pellaea@gmail.com>
2019-07-06 09:59:46 -0400

    Converted pattern search to use new include_subtaxa/synonyms query parameters. Added hack to convert "pattern_search" query to "all" query if include_xxx parameters are present.  This will get ripped out in a future PR when we do away with the pattern_search flavors.

--- app/classes/pattern_search/name.rb
+++ app/classes/pattern_search/name.rb
@@ -1,30 +1,30 @@
 module PatternSearch
   class Name < Base
     PARAMS = {
-      created:            [:created_at,         :parse_date_range],
-      modified:           [:updated_at,         :parse_date_range],
+      created:              [:created_at,         :parse_date_range],
+      modified:             [:updated_at,         :parse_date_range],
 
-      rank:               [:rank,               :parse_rank_range],
-      synonym_of:         [:synonym_names,      :parse_list_of_names],
-      child_of:           [:children_names,     :parse_list_of_names],
-      has_synonyms:       [:has_synonyms,       :parse_boolean],
-      deprecated:         [:is_deprecated,      :parse_boolean],
-      misspelled:         [:misspellings,       :parse_yes_no_both],
-      lichen:             [:lichen,             :parse_boolean],
+      deprecated:           [:is_deprecated,      :parse_boolean],
+      has_synonyms:         [:has_synonyms,       :parse_boolean],
+      lichen:               [:lichen,             :parse_boolean],
+      include_misspellings: [:misspellings,       :parse_yes_no_both],
+      include_subtaxa:      [:include_subtaxa,    :parse_boolean],
+      include_synonyms:     [:include_synonyms,   :parse_boolean],
+      rank:                 [:rank,               :parse_rank_range],
 
-      has_author:         [:has_author,         :parse_boolean],
-      has_citation:       [:has_citation,       :parse_boolean],
-      has_classification: [:has_classification, :parse_boolean],
-      has_notes:          [:has_notes,          :parse_boolean],
-      has_comments:       [:has_comments,       :parse_yes],
-      has_description:    [:has_default_desc,   :parse_boolean],
-      has_observations:   [:has_observations,   :parse_yes],
+      has_author:           [:has_author,         :parse_boolean],
+      has_citation:         [:has_citation,       :parse_boolean],
+      has_classification:   [:has_classification, :parse_boolean],
+      has_notes:            [:has_notes,          :parse_boolean],
+      has_comments:         [:has_comments,       :parse_yes],
+      has_description:      [:has_default_desc,   :parse_boolean],
+      has_observations:     [:has_observations,   :parse_yes],
 
-      author:             [:author_has,         :parse_string],
-      citation:           [:citation_has,       :parse_string],
-      classification:     [:classification_has, :parse_string],
-      notes:              [:notes_has,          :parse_string],
-      comments:           [:comments_has,       :parse_string]
+      author:               [:author_has,         :parse_string],
+      citation:             [:citation_has,       :parse_string],
+      classification:       [:classification_has, :parse_string],
+      notes:                [:notes_has,          :parse_string],
+      comments:             [:comments_has,       :parse_string]
     }.freeze
 
     def self.params
@@ -41,6 +41,19 @@ module PatternSearch
 
     def model
       self.class.model
+    end
+
+    def build_query
+      super
+
+      # Temporary hack to get include_subtaxa/synonyms to work.
+      # Will rip out when we do away with pattern search query flavor.
+      if flavor == :pattern_search &&
+         (!args[:include_subtaxa].nil? || !args[:include_synonyms].nil?)
+        self.flavor = :all
+        args[:names] = args[:pattern]
+        args.delete(:pattern)
+      end
     end
   end
 end
