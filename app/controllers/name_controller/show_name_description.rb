105e18c54f1c25fdd5873aafc5e402438a923a7a
Jason <pellaea@gmail.com>
2019-07-07 12:35:28 -0400

    Rubocopped name_controller ignoring metrics.

--- app/controllers/name_controller/show_name_description.rb
+++ app/controllers/name_controller/show_name_description.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 # see app/controllers/name_controller.rb
 class NameController
-  def show_name_description # :nologin: :prefetch:
+  before_action :login_required, except: [
+    :show_name_description
+  ]
+
+  before_action :disable_link_prefetching, except: [
+    :show_name_description
+  ]
+
+  def show_name_description
     store_location
     pass_query_params
     @description = find_or_goto_index(NameDescription, params[:id].to_s)
@@ -14,6 +24,8 @@ class NameController
     @canonical_url = description_canonical_url
     @projects = users_projects_which_dont_have_desc_of_this_name
   end
+
+  # ----------------------------------------------------------------------------
 
   protected
 
@@ -39,8 +51,8 @@ class NameController
   def redirect_to_name_or_project
     if @description.project
       redirect_to(controller: "project",
-                  action: "show_project",
-                  id: @description.project_id)
+                  action:     "show_project",
+                  id:         @description.project_id)
     else
       redirect_to(action: "show_name", id: @description.name_id)
     end
@@ -54,7 +66,7 @@ class NameController
     return [] unless @user
 
     @user.projects_member.select do |project|
-      !@name.descriptions.any? { |d| d.belongs_to_project?(project) }
+      @name.descriptions.none? { |d| d.belongs_to_project?(project) }
     end
   end
 end
