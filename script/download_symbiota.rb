a223a31f8a985cdec81b9ca1da85123779a41b2b
Nathan Wilson <nathan@collectivesource.com>
2019-07-14 02:26:33 +0000

    Added three scripts from the production server.

--- script/download_symbiota.rb
+++ script/download_symbiota.rb
@@ -11,13 +11,11 @@ def do_report(year, do_labels = false)
   report.sub!(/^[^\n]*\n/, "") unless do_labels
   puts report
   $stderr.puts("  #{query.num_results} observations\n")
+  sleep 60
 end
 
-do_report("2010-01-01", :do_labels)
-do_report("2010-01-02")
-
-# do_report(["1000", "1999"], :do_labels)
-# (2000..2019).each do |year|
-#   do_report(year.to_s)
-# end
+do_report(["1000", "1999"], :do_labels)
+(2000..2019).each do |year|
+  do_report([year.to_s, year.to_s])
+end
 exit 0
