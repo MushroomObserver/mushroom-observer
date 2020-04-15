c93a1d0c94af1a0e7ccde3e665923190cd3c02d7
Joe Cohen <jdcohenesq@gmail.com>
2020-04-15 16:19:56 -0700

    Use modified base class for test
    - Fixes a test Failure.
    The failure occurs because the `browser` bot thinks the `get` request is coming from a bot.
    That problem is fixed in our IntegrationTestCase class.

--- test/controllers/policy_controller_test.rb
+++ test/controllers/policy_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PolicyControllerTest < ActionDispatch::IntegrationTest
+class PolicyControllerTest < IntegrationTestCase
   test "should get privacy" do
     get policy_privacy_url
     assert_response :success
