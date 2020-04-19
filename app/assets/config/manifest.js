dc018ed45101249416bf6fb698114fa5d4d6d750
Joe Cohen <jdcohenesq@gmail.com>
2020-04-19 10:10:11 -0700

    Add manifest for use by sprockets
    When sprockets is upgraded to v4, it needs a manifest.
    
    The file I added combines `config.assets.precompile` plus `application.js` and `images`, as suggested by @pellaea:
    >The new default behavior is very different than the old default behavior.
    The new default compiles *all* js and css files, the old default only compiled application.js and application.css plus anything in config.assets.precompile.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,26 @@
+//= link_tree ../images
+
+//= link advanced_search.js
+//= link api_key.js
+//= link application.js
+//= link date_select.js
+//= link donate.js
+//= link edit_location.js
+//= link image_slider.js
+//= link multi_image_upload.js
+//= link name_lister.js
+//= link pivotal.js
+//= link rss_feed_select_helper.js
+//= link selectize.min.js
+//= link single_image_uploader.js
+//= link suggestions.js
+//= link thumbnail_map.js
+//= link translations.js
+//= link vote_popup.js
+
+//= link Admin.css
+//= link Agaricus.css
+//= link Amanita.css
+//= link BlackOnWhite.css
+//= link Cantharellaceae.css
+//= link Hygrocybe.css
