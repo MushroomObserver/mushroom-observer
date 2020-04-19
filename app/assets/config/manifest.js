2bb250cda2cf251c8614459ac1b1b72d5fa52a2c
Joe Cohen <jdcohenesq@gmail.com>
2020-04-19 12:02:57 -0700

    Remove references to unfound js files
    References in the manifest to nonexistent files cause (multiple) test Errors.
    - Remove them from manifest
    - Also remove the only reference in Views
    
    Sample Error:
    Minitest::UnexpectedError:         ActionView::Template::Error: couldn't find file 'selectize.min.js'
            Checked in these paths:
              /vagrant/mushroom-observer/app/assets/config
              /vagrant/mushroom-observer/app/assets/fonts
              /vagrant/mushroom-observer/app/assets/images
              /vagrant/mushroom-observer/app/assets/javascripts
              /vagrant/mushroom-observer/app/assets/stylesheets
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/jquery-slick-rails-1.9.0/app/assets/fonts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/jquery-slick-rails-1.9.0/app/assets/images
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/jquery-slick-rails-1.9.0/app/assets/javascripts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/jquery-slick-rails-1.9.0/app/assets/stylesheets
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/coffee-rails-5.0.0/lib/assets/javascripts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/jquery-rails-4.3.5/vendor/assets/javascripts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/actioncable-5.2.4.2/lib/assets/compiled
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/activestorage-5.2.4.2/app/assets/javascripts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/actionview-5.2.4.2/lib/assets/compiled
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/bootstrap-sass-3.4.1/assets/stylesheets
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/bootstrap-sass-3.4.1/assets/javascripts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/bootstrap-sass-3.4.1/assets/fonts
              /home/vagrant/.rvm/gems/ruby-2.6.6/gems/bootstrap-sass-3.4.1/assets/images
                app/assets/config/manifest.js:13

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,12 +6,10 @@
 //= link date_select.js
 //= link donate.js
 //= link edit_location.js
-//= link image_slider.js
 //= link multi_image_upload.js
 //= link name_lister.js
 //= link pivotal.js
 //= link rss_feed_select_helper.js
-//= link selectize.min.js
 //= link single_image_uploader.js
 //= link suggestions.js
 //= link thumbnail_map.js
