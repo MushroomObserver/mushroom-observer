811832a860ebda73cd96ebb341b4a5c7349491b6
Joe Cohen <jdcohenesq@gmail.com>
2020-07-07 15:46:03 -0700

    Fix GeneralExtensions#fixturelabel for namespaced fixtures
    -  Cherry picks #66f4cfc5
    - `GeneralExtensions#fixturelabel` worked fine in `master` but threw an error with  bootstrap-4's namespaced fixtures, like Name/Descriptions.

--- test/general_extensions.rb
+++ test/general_extensions.rb
@@ -261,7 +261,7 @@ module GeneralExtensions
   def fixture_label(obj)
     return "" if obj.nil?
 
-    table = obj.class.table_name
+    table = obj.class.name.tableize
     if @loaded_fixtures
       @loaded_fixtures[table].fixtures.each do |name, fixture|
         return "<#{name}>" if fixture["id"] == obj.id
