b088898f99738519ec032b4ffe3a72877a72325e
Joe Cohen <jdcohenesq@gmail.com>
2020-08-02 11:21:37 -0700

    Normalize ArticleHelper
    - Rename file and module
    - Use path helpers instead of action/id
    - Shorten method names (consistent with CRUD names and shortened test method names)
    - Fix calls to method names
    
    Turns test_index green.
    (YAY. I finally have 1/8 tests passing.)

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -6,24 +6,24 @@
 #                  write Articles only if user has write permission
 #   xx_title::     Title of x page; includes any markup
 #
-module ArticleHelper
+module ArticlesHelper
   def index_tabs
     return [] unless permitted?
 
-    [link_to(:create_article_title.t, action: :create_article)]
+    [link_to(:create_article_title.t, new_article_path)]
   end
 
-  def show_article_tabs
-    tabs = [link_to(:index_article.t, action: :index_article)]
+  def show_tabs
+    tabs = [link_to(:index_article.t, articles_path)]
     return tabs unless permitted?
 
-    tabs.push(link_to(:create_article_title.t, action: :create_article),
-              link_to(:EDIT.t, action: :edit_article, id: @article.id),
+    tabs.push(link_to(:create_article_title.t, new_article_path),
+              link_to(:EDIT.t, edit_article_path(@article.id)),
               link_to(:DESTROY.t, action: :destroy_article, id: @article.id))
   end
 
   # "Title (#nnn)" textilized
-  def show_article_title(article)
+  def show_title(article)
     capture do
       concat(article.display_title.t)
       concat(" (##{article.id || "?"})")
@@ -31,10 +31,10 @@ module ArticleHelper
   end
 
   # "Editing: Title (#nnn)"  textilized
-  def edit_article_title(article)
+  def edit_title(article)
     capture do
       concat("#{:EDITING.l}: ")
-      concat(show_article_title(article))
+      concat(show_title(article))
     end
   end
 end
