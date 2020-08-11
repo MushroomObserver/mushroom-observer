b0900498675447635f4b60714050b64f0ef444cf
Joe Cohen <jdcohenesq@gmail.com>
2020-08-11 09:50:18 -0700

    Use path helpers  in RedirectsTest assertions
    More idiomatic than quoted paths.
    Also makes screen a little less noisy

--- test/integration/redirects_test.rb
+++ test/integration/redirects_test.rb
@@ -5,47 +5,47 @@ require("test_helper")
 class RedirectsTest < IntegrationTestCase
   def test_controller
     get("/article")
-    assert_equal("/articles", @response.request.fullpath)
+    assert_equal(articles_path, @response.request.fullpath)
   end
 
   def test_list_article
     get("/article/list_article")
-    assert_equal("/articles", @response.request.fullpath)
+    assert_equal(articles_path, @response.request.fullpath)
   end
 
   def test_index_article
     get("/article/index_article/")
-    assert_equal("/articles", @response.request.fullpath)
+    assert_equal(articles_path, @response.request.fullpath)
   end
 
   def test_show_article
     get("/article/show_article/#{Article.first.id}")
-    assert_equal("/articles/#{Article.first.id}", @response.request.fullpath)
+    assert_equal(article_path(Article.first.id), @response.request.fullpath)
   end
 
   def test_create_article_get
     login("article_writer", "testpassword", true)
     get("/article/create_article")
-    assert_equal("/articles/new", @response.request.fullpath)
+    assert_equal(new_article_path, @response.request.fullpath)
   end
 
   def test_create_article_post
     login("article_writer", "testpassword", true)
     post("/article/create_article")
-    assert_equal("/articles/new", @response.request.fullpath)
+    assert_equal(new_article_path, @response.request.fullpath)
   end
 
   def test_edit_article_get
     login("article_writer", "testpassword", true)
     get("/article/edit_article/#{Article.first.id}")
-    assert_equal("/articles/#{Article.first.id}/edit",
+    assert_equal(edit_article_path(Article.first.id),
                  @response.request.fullpath)
   end
 
   def test_edit_article_post
     login("article_writer", "testpassword", true)
     post("/article/edit_article/#{Article.first.id}")
-    assert_equal("/articles/#{Article.first.id}/edit",
+    assert_equal(edit_article_path(Article.first.id),
                  @response.request.fullpath)
   end
 
