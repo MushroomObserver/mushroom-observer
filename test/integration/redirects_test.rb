3d3c8f016518e279bc0ad7e966b67688684a0dee
Joe Cohen <jdcohenesq@gmail.com>
2020-08-11 10:28:24 -0700

    Add omitted path helper in RedirectsTest
    - In prior commit, I failed to use path helper in `test_destroy_article_post`.
    - Place expects and actuals on separate lines

--- test/integration/redirects_test.rb
+++ test/integration/redirects_test.rb
@@ -5,34 +5,40 @@ require("test_helper")
 class RedirectsTest < IntegrationTestCase
   def test_controller
     get("/article")
-    assert_equal(articles_path, @response.request.fullpath)
+    assert_equal(articles_path,
+                 @response.request.fullpath)
   end
 
   def test_list_article
     get("/article/list_article")
-    assert_equal(articles_path, @response.request.fullpath)
+    assert_equal(articles_path,
+                 @response.request.fullpath)
   end
 
   def test_index_article
     get("/article/index_article/")
-    assert_equal(articles_path, @response.request.fullpath)
+    assert_equal(articles_path,
+                 @response.request.fullpath)
   end
 
   def test_show_article
     get("/article/show_article/#{Article.first.id}")
-    assert_equal(article_path(Article.first.id), @response.request.fullpath)
+    assert_equal(article_path(Article.first.id),
+                 @response.request.fullpath)
   end
 
   def test_create_article_get
     login("article_writer", "testpassword", true)
     get("/article/create_article")
-    assert_equal(new_article_path, @response.request.fullpath)
+    assert_equal(new_article_path,
+                 @response.request.fullpath)
   end
 
   def test_create_article_post
     login("article_writer", "testpassword", true)
     post("/article/create_article")
-    assert_equal(new_article_path, @response.request.fullpath)
+    assert_equal(new_article_path,
+                 @response.request.fullpath)
   end
 
   def test_edit_article_get
@@ -52,7 +58,7 @@ class RedirectsTest < IntegrationTestCase
   def test_destroy_article_post
     login("article_writer", "testpassword", true)
     post("/article/destroy_article/#{Article.first.id}")
-    assert_equal("/articles/#{Article.first.id}",
+    assert_equal(article_path(Article.first.id),
                  @response.request.fullpath)
   end
 
@@ -60,13 +66,15 @@ class RedirectsTest < IntegrationTestCase
     login("article_writer", "testpassword", true)
     patch("/article/destroy_article/#{Article.first.id}")
     # Rails sends patch/put to intermediate page, not the "to:" location
-    assert_equal(article_url(Article.first.id), @response.header["Location"])
+    assert_equal(article_url(Article.first.id),
+                 @response.header["Location"])
   end
 
   def test_destroy_article_put
     login("article_writer", "testpassword", true)
     put("/article/destroy_article/#{Article.first.id}")
     # Rails sends patch/put to intermediate page, not the "to:" location
-    assert_equal(article_url(Article.first.id), @response.header["Location"])
+    assert_equal(article_url(Article.first.id),
+                 @response.header["Location"])
   end
 end
