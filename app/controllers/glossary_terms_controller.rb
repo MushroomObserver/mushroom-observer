13c448f34cdb38f8ecc8d9cfe5d6acf6bdbdd706
Joe Cohen <jdcohenesq@gmail.com>
2020-08-17 20:34:26 -0700

    Initial reorganization of GlossaryTermsController
    - Put actions and other methods in standard order
    - Normalize names of standard actions

--- app/controllers/glossary_terms_controller.rb
+++ app/controllers/glossary_terms_controller.rb
@@ -5,10 +5,17 @@ class GlossaryTermsController < ApplicationController
   before_action :login_required, except: [
     :index,
     :show_past_glossary_term,
-    :show_glossary_term
+    :show
   ]
 
-  def show_glossary_term
+  # ---------- Actions to Display data (index, show, etc.) ---------------------
+
+  def index
+    store_location
+    @glossary_terms = GlossaryTerm.all.order(:name)
+  end
+
+  def show
     store_location
     @glossary_term = GlossaryTerm.find(params[:id].to_s)
     @canonical_url = "#{MO.http_domain}/glossary/show_glossary_term/"\
@@ -17,34 +24,68 @@ class GlossaryTermsController < ApplicationController
     @objects = @glossary_term.images
   end
 
-  def index
-    store_location
-    @glossary_terms = GlossaryTerm.all.order(:name)
-  end
+  # ---------- Actions to Display forms -- (new, edit, etc.) -------------------
 
-  def create_glossary_term # :norobots:
-    if request.method == "POST"
-      glossary_term_post
-    else
-      glossary_term_get
-    end
-  end
-
-  def glossary_term_get
+  def new
     @copyright_holder = @user.name
     @copyright_year = Time.now.utc.year
     @upload_license_id = @user.license_id
     @licenses = License.current_names_and_ids(@user.license)
   end
 
-  def glossary_term_post
+  def edit
+    @glossary_term = GlossaryTerm.find(params[:id].to_s)
+  end
+
+  # ---------- Actions to Modify data: (create, update, destroy, etc.) ---------
+
+  def create
     glossary_term = \
       GlossaryTerm.new(user: @user, name: params[:glossary_term][:name],
                        description: params[:glossary_term][:description])
     glossary_term.add_image(process_image(image_args))
     glossary_term.save
-    redirect_to(action: "show_glossary_term", id: glossary_term.id)
+    redirect_to(glossary_terms_path(glossary_term.id))
   end
+
+  def update
+    glossary_term = GlossaryTerm.find(params[:id].to_s)
+    glossary_term.attributes = params[:glossary_term].
+                               permit(:name, :description)
+    glossary_term.user = @user
+    glossary_term.save
+    redirect_to(glossary_terms_path(glossary_term.id))
+  end
+
+  # TODO: add destroy action
+
+  # ---------- Non-standard CRUD Actions ---------------------------------------
+
+  # Show past version of GlossaryTerm.
+  # Accessible only from show_glossary_term page.
+  def show_past_glossary_term
+    pass_query_params
+    store_location
+    if @glossary_term = find_or_goto_index(GlossaryTerm, params[:id].to_s)
+      if params[:version]
+        @glossary_term.revert_to(params[:version].to_i)
+      else
+        flash_error(:show_past_location_no_version.t)
+        redirect_to(glossary_terms_path(glossary_term.id))
+      end
+    end
+  end
+
+  # ---------- Public methods (unrouted) ---------------------------------------
+
+  ##############################################################################
+
+  private
+
+  # --------- Filters
+
+
+  # --------- Other private methods
 
   def image_args
     {
@@ -86,33 +127,4 @@ class GlossaryTermsController < ApplicationController
     end
   end
 
-  def edit_glossary_term # :norobots:
-    # Expand to any MO user,
-    # but make them owned and editable only by that user or an admin
-    if request.method == "POST"
-      glossary_term = GlossaryTerm.find(params[:id].to_s)
-      glossary_term.attributes = params[:glossary_term].
-                                 permit(:name, :description)
-      glossary_term.user = @user
-      glossary_term.save
-      redirect_to(action: "show_glossary_term", id: glossary_term.id)
-    else
-      @glossary_term = GlossaryTerm.find(params[:id].to_s)
-    end
-  end
-
-  # Show past version of GlossaryTerm.
-  # Accessible only from show_glossary_term page.
-  def show_past_glossary_term # :prefetch: :norobots:
-    pass_query_params
-    store_location
-    if @glossary_term = find_or_goto_index(GlossaryTerm, params[:id].to_s)
-      if params[:version]
-        @glossary_term.revert_to(params[:version].to_i)
-      else
-        flash_error(:show_past_location_no_version.t)
-        redirect_to(action: "show_glossary_term", id: @glossary_term.id)
-      end
-    end
-  end
 end
