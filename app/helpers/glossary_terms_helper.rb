94d73b588c0ed710ece15e33dcbd8fa2ae4f6f93
Joe Cohen <jdcohenesq@gmail.com>
2020-08-29 06:38:40 -0700

    Pluralize GlossaryTerms helper
    Pluralize filename and class

--- app/helpers/glossary_terms_helper.rb
+++ app/helpers/glossary_terms_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# View Helpers for Glossary
-module GlossaryTermHelper
+# View Helpers for GlossaryTerms
+module GlossaryTermsHelper
   def destroy_button(term)
     button_to(
       :destroy_glossary_term.t,
