2714f30c4c8e968882debed077649e80f8da64c3
Joe Cohen <jdcohenesq@gmail.com>
2020-09-02 03:15:27 -0700

    Change Articles destroy link to button
    - Promote and refactor `destroy_button` method
    - Revise GlossaryTermsHelper and view to work with promoted method
    - Use button instead of link to destroy `article`
    
    `destroy` links don't work because they require js,
    so use button instead
    
    Delivers https://www.pivotaltracker.com/story/show/174524961

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -19,9 +19,7 @@ module ArticlesHelper
 
     tabs.push(link_to(:create_article_title.t, new_article_path),
               link_to(:EDIT.t, edit_article_path(article.id)),
-              # Fix link_to DESTROY
-              # https://www.pivotaltracker.com/story/show/174524961
-              link_to(:DESTROY.t, action: :destroy, id: article.id))
+              destroy_button(object: article))
   end
 
   # Can user modify all articles
