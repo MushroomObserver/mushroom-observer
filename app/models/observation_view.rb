ccff634bb733687550da4a7832c35a592d85f139
Jason <pellaea@gmail.com>
2020-10-12 15:55:53 -0400

    More codeclimate / rubocop work.

--- app/models/observation_view.rb
+++ app/models/observation_view.rb
@@ -8,8 +8,8 @@ class ObservationView < AbstractModel
   def self.update_view_stats(observation, user)
     return if observation.blank? || user.blank?
 
-    if view = where(observation: observation, user: user).first
-      view.update_attributes!(last_view: Time.zone.now)
+    if view = find_by(observation: observation, user: user)
+      view.update!(last_view: Time.zone.now)
     else
       create!(observation: observation, user: user, last_view: Time.zone.now)
     end
