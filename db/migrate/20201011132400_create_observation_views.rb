c9d92c62152528a717023b5130721b3fc8380177
Jason <pellaea@gmail.com>
2020-10-12 20:13:45 -0400

    Changed migration to use "change" instead of "up" and "down".  Thanks, Joe!

--- db/migrate/20201011132400_create_observation_views.rb
+++ db/migrate/20201011132400_create_observation_views.rb
@@ -1,13 +1,9 @@
 class CreateObservationViews < ActiveRecord::Migration[5.2]
-  def up
+  def change
     create_table :observation_views, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
       t.integer  :observation_id
       t.integer  :user_id
       t.datetime :last_view
     end
-  end
-
-  def down
-    drop_table :observation_views
   end
 end
