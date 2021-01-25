8df35da33088665106d5edef85871f993ccbdd2e
Joe Cohen <jdcohenesq@gmail.com>
2021-01-25 12:30:17 -0800

    Extract methods from #show_selected_herbaria
    Fixes metric violations

--- app/controllers/herbaria_controller.rb
+++ app/controllers/herbaria_controller.rb
@@ -170,45 +170,50 @@ class HerbariaController < ApplicationController
   include Herbaria::SharedPrivateMethods
 
   def show_selected_herbaria(query, args = {})
-    @links ||= []
-    unless query.flavor == :all
-      @links << [:herbarium_index_list_all_herbaria.l, herbaria_path]
+    args = show_index_args(args)
+
+    # Clean up display by removing user-related stuff from nonpersonal index.
+    if query.flavor == :nonpersonal
+      args[:sorting_links].reject! { |x| x[0] == "user" }
+      @no_user_column = true
     end
-    unless query.flavor == :nonpersonal
-      @links << [:herbarium_index_nonpersonal_herbaria.l,
-                 nonpersonal_herbaria_path]
-    end
-    @links << [:create_herbarium.l, herbaria_path(method: :post)]
+
+    @links = right_tab_links(query, @links)
 
     # If user clicks "merge" on an herbarium, it reloads the page and asks
     # them to click on the destination herbarium to merge it with.
     @merge = Herbarium.safe_find(params[:merge])
 
-    args = {
-      action: :index,
+    show_index_of_objects(query, args)
+  end
+
+  def right_tab_links(query, links)
+    links ||= []
+    unless query.flavor == :all
+      links << [:herbarium_index_list_all_herbaria.l, herbaria_path]
+    end
+    unless query.flavor == :nonpersonal
+      links << [:herbarium_index_nonpersonal_herbaria.l,
+                nonpersonal_herbaria_path]
+    end
+    links << [:create_herbarium.l, herbaria_path(method: :post)]
+  end
+
+  def show_index_args(args)
+    {
       letters: "herbaria.name",
       num_per_page: 100,
       include: [:curators, :herbarium_records, :personal_user]
-    }.merge(args)
-
-    # Add some alternate sorting criteria.
-    args[:sorting_links] = [
-      ["records",     :sort_by_records.t],
-      ["user",        :sort_by_user.t],
-      ["code",        :sort_by_code.t],
-      ["name",        :sort_by_name.t],
-      ["created_at",  :sort_by_created_at.t],
-      ["updated_at",  :sort_by_updated_at.t]
-    ]
-
-    # Clean up display by removing user-related stuff from nonpersonal index.
-    if query.flavor == :nonpersonal
-      @no_user_column = true
-      args[:sorting_links].reject! { |x| x[0] == "user" }
-    end
-
-    args[:action] = :index # render with the index template
-    show_index_of_objects(query, args)
+    }.merge(args,
+            action: :index,  # render with the index template
+            sorting_links: [ # Add some alternate sorting criteria.
+              ["records",     :sort_by_records.t],
+              ["user",        :sort_by_user.t],
+              ["code",        :sort_by_code.t],
+              ["name",        :sort_by_name.t],
+              ["created_at",  :sort_by_created_at.t],
+              ["updated_at",  :sort_by_updated_at.t]
+            ])
   end
 
   def make_sure_can_edit!
