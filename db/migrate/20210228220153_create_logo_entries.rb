27e10343e9d3809f9cec61bfa7963ece3f477ada
Nathan Wilson <nathan@collectivesource.com>
2021-02-28 17:16:38 -0500

    Add an image to a LogoEntry

--- db/migrate/20210228220153_create_logo_entries.rb
+++ db/migrate/20210228220153_create_logo_entries.rb
@@ -2,6 +2,10 @@
 
 class CreateLogoEntries < ActiveRecord::Migration[5.2]
   def change
-    create_table(:logo_entries, &:timestamps)
+    create_table(:logo_entries,
+                 options: "ENGINE=InnoDB DEFAULT CHARSET=utf8") do |t|
+      t.integer(:image_id)
+      t.timestamps
+    end
   end
 end
