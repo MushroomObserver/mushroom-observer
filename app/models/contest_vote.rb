f244dee1a17f33f0424758e83afef00dea598af2
Nathan Wilson <nathan@collectivesource.com>
2021-03-01 07:27:41 -0500

    Rubocop

--- app/models/contest_vote.rb
+++ app/models/contest_vote.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 class ContestVote < ApplicationRecord
 end
--- db/migrate/20210301122443_create_contest_votes.rb
+++ db/migrate/20210301122443_create_contest_votes.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class CreateContestVotes < ActiveRecord::Migration[5.2]
   def change
-    create_table :contest_votes do |t|
-
-      t.timestamps
-    end
+    create_table(:contest_votes, &:timestamps)
   end
 end
