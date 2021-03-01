b99ed1da69912d0ddd330ecb54f64febb84749e7
Nathan Wilson <nathan@collectivesource.com>
2021-02-28 22:18:27 -0500

    Get build_image working

--- app/controllers/logo_entries_controller.rb
+++ app/controllers/logo_entries_controller.rb
@@ -33,4 +33,29 @@ class LogoEntriesController < ApplicationController
       false
     end
   end
+
+  private
+
+  def build_image(param_image, user, date, copyright_holder, license)
+    image = Image.new(image: param_image,
+                      user: user,
+                      when: date,
+                      copyright_holder: copyright_holder,
+                      license: license)
+    if !image.save
+      flash_object_errors(image)
+    elsif !image.process_image
+      logger.error("Unable to upload image")
+      name = image.original_name
+      name = "???" if name.empty?
+      flash_error(:runtime_profile_invalid_image.t(name: name))
+      flash_object_errors(image)
+    else
+      name = image.original_name
+      name = "##{image.id}" if name.empty?
+      flash_notice(:runtime_profile_uploaded_image.t(name: name))
+      return image
+    end
+    nil
+  end
 end
--- app/helpers/logo_entries_helper.rb
+++ app/helpers/logo_entries_helper.rb
@@ -1,24 +1,4 @@
 # frozen_string_literal: true
 
 module LogoEntriesHelper
-  def build_image(param_image, user, date, copyright_holder, license)
-    image = Image.new(image: param_image,
-                      user: user,
-                      when: date,
-                      copyright_holder: copyright_holder,
-                      license: license)
-    if !image.save
-      flash_object_errors(image)
-    elsif !image.process_image
-      logger.error("Unable to upload image")
-      name = image.original_name
-      name = "???" if name.empty?
-      flash_error(:runtime_profile_invalid_image.t(name: name))
-      flash_object_errors(image)
-    else
-      name = image.original_name
-      name = "##{image.id}" if name.empty?
-      flash_notice(:runtime_profile_uploaded_image.t(name: name))
-    end
-  end
 end
