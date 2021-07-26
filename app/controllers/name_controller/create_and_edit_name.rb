655b11dc067e8887e3251cee923b06a59b6eab01
Joseph D Cohen <jdcohenesq@gmail.com>
2021-07-26 07:07:20 -0700

    RuboCop create_and_edit_name.rb

--- app/controllers/name_controller/create_and_edit_name.rb
+++ app/controllers/name_controller/create_and_edit_name.rb
@@ -119,7 +119,8 @@ class NameController
         params: {
           name_id: @name.id,
           # Auricularia Bull. [#17132]
-          new_name_with_icn_id: "#{@parse.search_name} [##{params[:name][:icn_id]}]"
+          new_name_with_icn_id: "#{@parse.search_name} " \
+                                "[##{params[:name][:icn_id]}]"
         }
       )
       return
