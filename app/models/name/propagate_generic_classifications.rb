b5e1f3b38d99a812aa5abc61b08d93cf372bedd3
Jason <pellaea@gmail.com>
2021-08-17 10:45:59 -0400

    Followed Joe's recommendations concerning tightening up various iterators.

--- app/models/name/propagate_generic_classifications.rb
+++ app/models/name/propagate_generic_classifications.rb
@@ -37,31 +37,25 @@ class Name < AbstractModel
     end
 
     def build_accepted_names_lookup_table
-      accepted_names = {}
-      Name.select(:synonym_id, :text_name).
-        where(rank: 0..Name.ranks[:Genus], deprecated: false).
+      Name.where(rank: 0..Name.ranks[:Genus], deprecated: false).
         where.not(synonym_id: nil).
-        each do |name|
-          accepted_names[name.synonym_id] = name.text_name
-        end
-      accepted_names
+        pluck(:synonym_id, :text_name).
+        to_h
     end
 
     def accepted_generic_classification_strings
-      classifications = {}
-      Name.select(:text_name, :classification).
-        where(rank: Name.ranks[:Genus], deprecated: false).
+      Name.where(rank: Name.ranks[:Genus], deprecated: false).
         where("author NOT LIKE 'sensu lato%'").
-        each do |name|
-          next if name.classification.blank?
-
-          if classifications[name.text_name].present?
+        where("LENGTH(classification) > 2").
+        pluck(:text_name, :classification).
+        each_with_object({}) do |vals, classifications|
+          text_name, classification = vals
+          if classifications[text_name].present?
             warn("Multiple accepted non-sensu lato genera for #{text_name}!")
           else
-            classifications[name.text_name] = name.classification
+            classifications[text_name] = classification
           end
         end
-      classifications
     end
 
     def hash_of_names_with_observations
@@ -75,28 +69,26 @@ class Name < AbstractModel
     def execute_propagation_fixes(fixes, dry_run)
       bundles = {}
       used_names = hash_of_names_with_observations
-      msgs = fixes.map do |name, old_class, new_class|
+      fixes.each_with_object([]) do |fix, msgs|
+        name, old_class, new_class = fix
         bundles[new_class] = [] if bundles[new_class].blank?
         bundles[new_class] << name.id
         next unless used_names[name.text_name]
 
-        describe_propagation_fix(name, old_class, new_class)
-      end
-      msgs.reject(&:nil?) + execute_bundled_propagation_fixes(bundles, dry_run)
+        msgs << describe_propagation_fix(name, old_class, new_class)
+      end + execute_bundled_propagation_fixes(bundles, dry_run)
     end
 
     def execute_bundled_propagation_fixes(bundles, dry_run)
-      msgs = []
-      bundles.each do |classification, ids|
+      # Deliberately skip validations
+      # rubocop:disable Rails/SkipsModelValidations
+      bundles.each_with_object([]) do |bundle, msgs|
+        classification, ids = bundle
         msgs << "Setting classifications for #{ids.join(",")}"
-        next if dry_run
-
-        # Deliberately skip validations
-        # rubocop:disable Rails/SkipsModelValidations
-        Name.where(id: ids).update_all(classification: classification)
-        # rubocop:enable Rails/SkipsModelValidations
+        Name.where(id: ids).update_all(classification: classification) \
+          unless dry_run
       end
-      msgs
+      # rubocop:enable Rails/SkipsModelValidations
     end
 
     def describe_propagation_fix(name, old_class, new_class)
@@ -111,13 +103,11 @@ class Name < AbstractModel
     end
 
     def changes_in_classification_string(old_class, new_class)
-      msgs = []
-      Name.all_ranks.each do |rank|
+      Name.all_ranks.each_with_object([]) do |rank, msgs|
         old_name = grab_name_from_classification_string(old_class, rank)
         new_name = grab_name_from_classification_string(new_class, rank)
         msgs << "#{old_name} => #{new_name}" if old_name != new_name
-      end
-      msgs.join(", ")
+      end.join(", ")
     end
 
     def grab_name_from_classification_string(str, rank)
