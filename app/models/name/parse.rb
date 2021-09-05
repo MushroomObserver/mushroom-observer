9a8499ccb18d99cda2afac39a1f50f695af9e6d6
Joe Cohen <jdcohenesq@gmail.com>
2021-09-05 14:42:40 -0700

    RuboCop parse.rb

--- app/models/name/parse.rb
+++ app/models/name/parse.rb
@@ -421,23 +421,29 @@ class Name < AbstractModel
     end
   end
 
+  # matches to ranks that are included in the name proper
+  # subspecies is not included because it's the catchall default
+  RANK_START_MATCHER = /^(f|sect|stirps|subg|subsect|v)/i.freeze
+
+  # convert rank start_match to standard form of rank
+  # subspecies is not included because it's the catchall default
+  STANDARD_SECONDARY_RANKS = {
+    f: "f.",
+    sect: "sect.",
+    stirps: "stirps",
+    subg: "subg.",
+    subsect: "subsect.",
+    v: "var."
+  }.freeze
+
   def self.standardize_name(str)
     words = str.split(" ")
     # every other word, starting next-from-last, is an abbreviation
     i = words.length - 2
     while i.positive?
-      words[i] = if /^f/i.match?(words[i])
-                   "f."
-                 elsif /^v/i.match?(words[i])
-                   "var."
-                 elsif /^sect/i.match?(words[i])
-                   "sect."
-                 elsif /^stirps/i.match?(words[i])
-                   "stirps"
-                 elsif /^subg/i.match?(words[i])
-                   "subg."
-                 elsif /^subsect/i.match?(words[i])
-                   "subsect."
+      words[i] = if (match_start_of_rank = RANK_START_MATCHER.match(words[i]))
+                   start_of_rank = match_start_of_rank[0]
+                   STANDARD_SECONDARY_RANKS[start_of_rank.downcase.to_sym]
                  else
                    "subsp."
                  end
