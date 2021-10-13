9631aeaa7b62ad17f7996ee411ef2ec450a3807e
andrew nimmo <andrnimm@fastmail.fm>
2021-10-13 14:25:01 -0700

    Observations filter resolver work

--- app/graphql/resolvers/observations_search.rb
+++ app/graphql/resolvers/observations_search.rb
@@ -1,60 +1,97 @@
 require("search_object")
 require("search_object/plugin/graphql")
 
-class Resolvers::ObservationsSearch < GraphQL::Schema::Resolver
-  # include SearchObject for GraphQL
-  include SearchObject.module(:graphql)
+module Resolvers
+  class ObservationsSearch < GraphQL::Schema::Resolver
+    # include SearchObject for GraphQL
+    include SearchObject.module(:graphql)
 
-  # scope is starting point for search
-  scope { Observation.all }
+    description "List or filter all observations"
 
-  type types[Types::ObservationType], null: false
+    # scope is starting point for search
+    scope { Observation.all }
 
-  # inline input type definition for the advanced filter
-  class ObservationFilter < ::Types::BaseInputObject
-    # argument :OR, [self], required: false
-    # taxa: search for like string in name.text_name
-    argument :text_name, String, required: false
-    # alternative: use select w/ autocomplete for taxa
-    # argument :name_id, Int, required: false
-    # use select w/ autocomplete and force an ID
-    argument :user_id, Integer, required: false
-    # alternative: search for string in user.name? expensive query
-    # argument :user_like, String, required: false
-    # must search string for location, becase locations are not nested.
-    argument :where, String, required: false # this is location.name
-    argument :before, Boolean, required: false
-    argument :when, GraphQL::Types::ISO8601Date, required: false
-    argument :notes, String, required: false
-    argument :image, Types::EitherWithWithout, required: false # with, without, or either
-    argument :specimen, Types::EitherWithWithout, required: false # with, without or either
-    argument :lichen, Types::EitherWithWithout, required: false # with, without or either
-  end
+    type types[Types::ObservationType], null: false
 
-  # when "filter" is passed "apply_filter" would be called to narrow the scope
-  # note this example was conceived as an OR filter, we could set the filter args as options directly
-  option :filter, type: ObservationFilter, with: :apply_filter
-
-  # apply_filter recursively loops through "OR" branches
-  def apply_filter(scope, value)
-    branches = normalize_filters(value).reduce { |a, b| a.or(b) }
-    scope.merge(branches)
-  end
-
-  def normalize_filters(value, branches = [])
-    scope = Observation.all
-    if value[:text_name]
-      scope = scope.where("text_name LIKE ?", "%#{value[:text_name]}%")
+    # inline input type definition for the advanced filter
+    class ObservationFilter < ::Types::BaseInputObject
+      # argument :OR, [self], required: false
+      # alternative: use select w/ autocomplete for taxa
+      argument :name_id, Int, required: false
+      # taxa: search for like string in name.text_name
+      argument :name_like, String, required: false
+      # use select w/ autocomplete and force an ID
+      argument :user_id, Integer, required: false
+      # alternative: search for string in user.name? expensive query
+      # argument :user_like, String, required: false
+      # must search string for location, becase locations are not nested.
+      argument :where, String, required: false # where == location.name
+      argument :before, Boolean, required: false
+      argument :when, GraphQL::Types::ISO8601Date, required: false
+      argument :notes_like, String, required: false
+      argument :with_image, Boolean, required: false # with, without, or either
+      argument :with_specimen, Boolean, required: false # with, without or either
+      argument :with_lichen, Boolean, required: false # with, without or either
     end
-    scope = scope.where("url LIKE ?", "%#{value[:where]}%") if value[:where]
-    scope = scope.where("notes LIKE ?", "%#{value[:notes]}%") if value[:notes]
 
-    branches << scope
+    # when "filter" is passed "apply_filter" would be called to narrow the scope
+    # note this example was conceived as an OR filter, we could set the filter args as options directly
+    option :filter, type: ObservationFilter, with: :apply_filter
 
-    # if value[:OR].present?
-    #   value[:OR].reduce(branches) { |s, v| normalize_filters(v, s) }
-    # end
+    # apply_filter recursively loops through "OR" branches
+    def apply_filter(scope, value)
+      branches = normalize_filters(value).reduce { |a, b| a.or(b) }
+      scope.merge(branches)
+    end
 
-    branches
+    def normalize_filters(value, branches = [])
+      scope = Observation.all
+      scope = scope.where("name_id = ?", value[:name_id]) if value[:name_id]
+      if value[:name_like]
+        scope = scope.where("text_name LIKE ?", "%#{value[:name_like]}%")
+      end
+      scope = scope.where("user_id = ?", value[:user_id]) if value[:user_id]
+      scope = scope.where("where LIKE ?", "%#{value[:where]}%") if value[:where]
+      if value[:when]
+        scope = if value[:before]
+                  scope.where("created_at <= ?", value[:when])
+                else
+                  scope.where("created_at >= ?", value[:when])
+                end
+      end
+      if value[:notes_like]
+        scope = scope.where("notes LIKE ?", "%#{value[:notes_like]}%")
+      end
+      case value[:with_image]
+      when true
+        puts("___________________________WITH IMAGE")
+        scope.where.not(thumb_image_id: nil)
+      when false
+        puts("___________________________WITHOUT IMAGE")
+        scope.where(thumb_image_id: nil)
+      else
+        puts("___________________________EITHER IMAGE")
+      end
+      case value[:with_specimen]
+      when true
+        scope.where(specimen: true)
+      when false
+        scope.where(specimen: false)
+      end
+      case value[:with_lichen]
+      when true
+        scope.where("lifeform EXISTS")
+      when false
+        scope.where("lifeform NOT EXISTS")
+      end
+
+      branches << scope
+
+      # if value[:OR].present?
+      #   value[:OR].reduce(branches) { |s, v| normalize_filters(v, s) }
+      # end
+
+      branches
+    end
   end
 end
