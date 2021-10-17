883fae4e17098671ec30b9a65c18ff3909d12fce
andrew nimmo <andrnimm@fastmail.fm>
2021-10-16 23:39:43 -0700

    Working login_user.rb

--- app/graphql/mutations/login_user.rb
+++ app/graphql/mutations/login_user.rb
@@ -8,17 +8,25 @@ module Mutations
 
     input_object_class Types::LoginInput
 
-    type Types::UserType
+    # type Types::UserType
+    field :token, String, null: true
+    field :user, Types::UserType, null: true
 
     def resolve(login: String,
                 password: String)
       user = User.authenticate(login, password)
+      user ||= User.authenticate(@login, @password.strip)
+
       return {} unless user
 
+      verified = user.verified
+
       token = Base64.encode64(user.login)
+
       {
         token: token,
-        user: user
+        user: user,
+        verified: verified
       }
     rescue ActiveRecord::RecordNotFound
       raise(GraphQL::ExecutionError.new("user not found"))
