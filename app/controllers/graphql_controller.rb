ab1e6497ef45d7d5db6b2f8d72f18a42d6867312
andrew nimmo <andrnimm@fastmail.fm>
2021-10-28 14:46:21 -0700

    Allow graphql to bypass filters, like API

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,6 +3,8 @@ class GraphqlController < ApplicationController
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
+  # skip_before_action :verify_authenticity_token
+  disable_filters
 
   def execute
     variables = prepare_variables(params[:variables])
