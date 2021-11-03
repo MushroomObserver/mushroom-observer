5f89c7dffe052adb35a38eb6b37a87f9679f1439
andrew nimmo <andrnimm@fastmail.fm>
2021-11-03 00:54:52 -0700

    Update rss_log.rb

--- app/models/rss_log.rb
+++ app/models/rss_log.rb
@@ -143,6 +143,7 @@
 #  unique_format_name:: (same, with id tacked on to make unique)
 #  url::                Return "show_blah/id" URL for associated object.
 #  parse_log::          Parse log, see method for description of return value.
+#  detail::             Figure out the detail message for the most recent update.
 #
 #  == Callbacks
 #
@@ -367,6 +368,49 @@ class RssLog < AbstractModel
       first = false
     end
     results
+  end
+
+  # Figure out the detail message for the most recent update.
+  def detail
+    target_type = target ? target.type_tag : target_type
+    begin
+      tag, args, time = parse_log.first
+    rescue StandardError
+      []
+    end
+    if !target_type
+      result = :rss_destroyed.t(type: :object)
+    elsif !target ||
+          tag.to_s.match(/^log_#{target_type}_(merged|destroyed)/)
+      result = :rss_destroyed.t(type: target_type)
+    elsif !time || time < target.created_at + 1.minute
+      result = :rss_created_at.t(type: target_type)
+      unless [:observation, :species_list].include?(target_type)
+        begin
+          result += " ".html_safe + :rss_by.t(user: target.user.legal_name)
+        rescue StandardError
+          nil
+        end
+      end
+    else
+      if [:observation, :species_list].include?(target_type) &&
+         [target.user.login, target.user.name, target.user.legal_name].
+         include?(args[:user])
+        # This will remove redundant user from observation logs.
+        tag2 = :"#{tag}0"
+        result = tag2.t(args) if tag2.has_translation?
+      end
+      unless result
+        tag2 = tag.to_s.sub(/^log/, "rss").to_sym
+        result = tag2.t(args) if tag2.has_translation?
+      end
+      begin
+        result ||= tag.t(args)
+      rescue StandardError
+        nil
+      end
+    end
+    result
   end
 
   ##############################################################################
