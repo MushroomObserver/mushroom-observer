c0e1c249ceb822e8a8481573774808c30697da67
andrew nimmo <andrnimm@fastmail.fm>
2022-01-22 16:17:41 -0800

    Start to format name query!
    Name type - add RecordLoaders (to route the query to the right column)
    Add names resolver
    Add name input filter
    Split enum orderby:
    Add enum names order by
    Add enum observations order by
    Cleanup - remove queries/observations

---
+++ app/graphql/resolvers/names.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class Names < Resolvers::BaseResolver
+    type [Types::Models::NameType.connection_type], null: false
+    description "List or filter all names"
+
+    argument :filter, type: Inputs::Name::Filters, required: false
+
+    def resolve(filter: nil)
+      names = ::Name.arel_table
+      scope = ::Name.select(names[Arel.star])
+      column_name = "when"
+      desc = true
+
+      if filter
+        scope = scope.where(names[:id].eq(filter[:name_id])) if filter[:name_id]
+
+        if filter[:name_like]
+          scope = scope.where(names[:text_name].matches("%#{filter[:name_like]}%"))
+        end
+
+        if filter[:user_id]
+          scope = scope.where(names[:user_id].eq(filter[:user_id]))
+        end
+
+        column_name = if filter[:order_by]
+                        case filter[:order_by]
+                        when "CREATED_AT"
+                          "created_at"
+                        when "UPDATED_AT"
+                          "updated_at"
+                        when "TEXT_NAME"
+                          "text_name"
+                        else
+                          "when"
+                        end
+                      else
+                        "when"
+                      end
+
+        desc = if filter[:order]
+                 case filter[:order]
+                 when "ASC"
+                   false
+                 when "DESC"
+                   true
+                 end
+               else
+                 true
+               end
+      end
+
+      GraphQL::Connections::Stable.new(scope, keys: %W[#{column_name} id], desc: desc)
+    end
+  end
+end
