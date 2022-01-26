31becd72655b12426b45fb76cd6a9754d59e63ce
andrew nimmo <andrnimm@fastmail.fm>
2022-01-26 14:40:26 -0800

    Remove NON_NULL on created_at
    Because it's being created with a null value

--- db/migrate/20220126211212_add_created_at_to_rss_logs.rb
+++ db/migrate/20220126211212_add_created_at_to_rss_logs.rb
@@ -2,7 +2,6 @@ class AddCreatedAtToRssLogs < ActiveRecord::Migration[5.2]
 
   def up
     add_column :rss_logs, :created_at, :datetime
-    change_column_null :rss_logs, :created_at, false
   end
 
   def down
