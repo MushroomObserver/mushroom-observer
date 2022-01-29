2f0df1fb780ecc71f6de6db0acd42a3de19543a5
andrew nimmo <andrnimm@fastmail.fm>
2022-01-29 11:00:50 -0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,19 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
+# frozen_string_literal: true
+
 # Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests. A different port is considered cross-origin.
 
 # Read more: https://github.com/cyu/rack-cors
 
 MushroomObserver::Application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    # would be your frontend server if production and it's different
-    # origins("*") if Rails.env.development?
-
-    # resource "*",
-    #          headers: any,
-    #          methods: [:get, :post, :put, :patch, :delete, :options, :head]
-
+    # allow /graphql access only from localhost:3001
     origins "http://localhost:3001"
     resource "/graphql", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
