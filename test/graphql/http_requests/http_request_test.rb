a3a7d57d507febc993bbcfdae949aceaa53b0bb4
andrew nimmo <andrnimm@fastmail.fm>
2022-02-01 19:11:44 -0800

    Update http_request_test.rb

--- test/graphql/http_requests/http_request_test.rb
+++ test/graphql/http_requests/http_request_test.rb
@@ -1,4 +1,25 @@
 require("test_helper")
 
 class Mutations::HttpRequestTest < ActionDispatch::IntegrationTest
+  def graphql_path
+    "/graphql"
+  end
+
+  # https://graphql-ruby.org/testing/integration_tests.html
+  def check_current_user
+    query_string = "{ current_user { username } }"
+    post(graphql_path, params: { query: query_string })
+    json_response = JSON.parse(@response.body)
+    assert_nil(json_response["data"]["viewer"], "Unauthenticated requests have no viewer")
+
+    # This time, add some authentication to the HTTP request
+    user = create(:user)
+    post(graphql_path,
+         params: { query: query_string },
+         headers: { "Authorization" => "Bearer #{user.auth_token}" })
+
+    json_response = JSON.parse(@response.body)
+    assert_equal(user.username, json_response["data"]["viewer"], "Authenticated requests load the viewer")
+    assert_equal("x", "y")
+  end
 end
