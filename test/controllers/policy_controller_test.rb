3270aa76ba3d061476f51ce7619baa536dbb1f69
Joe Cohen <jdcohenesq@gmail.com>
2022-02-10 11:06:37 -0800

    Add blank line between PolicyController test setup and assertions

--- test/controllers/policy_controller_test.rb
+++ test/controllers/policy_controller_test.rb
@@ -5,6 +5,7 @@ require("test_helper")
 class PolicyControllerTest < FunctionalTestCase
   def test_privacy
     get(:privacy)
+
     assert_response(:success)
     assert_head_title(:privacy_title.l)
   end
