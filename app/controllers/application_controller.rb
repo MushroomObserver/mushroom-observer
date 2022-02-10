d5278e68b1d2371c4b2a59c00b3c472de3094c59
Joe Cohen <jdcohenesq@gmail.com>
2022-02-10 10:58:15 -0800

    Give anonymous user access to Publications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -211,7 +211,7 @@ class ApplicationController < ActionController::Base
     params[:controller].start_with?("api") ||
     # Filter will not block anonymous user from any action in these controllers
     # (Controller or other filters are responsible for other access controls.)
-      %w[account articles policy].include?(params[:controller])
+      %w[account articles policy publications].include?(params[:controller])
   end
 
   def action_ok_for_anonymous_user?
