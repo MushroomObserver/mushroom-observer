35be00a8b04c11a8a05e3aff5d1cd5dc95256c0c
andrew nimmo <andrnimm@fastmail.fm>
2022-02-14 23:47:28 -0800

    Add user/login.rb

--- app/graphql/types/base_field.rb
+++ app/graphql/types/base_field.rb
@@ -5,7 +5,7 @@ module Types
     argument_class(Types::BaseArgument)
     # attr_accessor :require_admin, :require_owner
 
-    # # Pass `field ..., require_admin: true` to reject non-admin users from field
+    # Pass `field ..., require_admin: true` to reject non-admin users from field
     # def initialize(*args, **kwargs, require_admin: false, &block)
     #   @require_admin = require_admin
     #   # @require_owner = require_owner
@@ -20,9 +20,9 @@ module Types
     #   ctx[:current_user]&.admin? || (ctx[:current_user]&.id === owner_id)
     # end
 
-    # # Field #authorized? methods are called before resolving a field
+    # Field #authorized? methods are called before resolving a field
     # def authorized?(obj, args, ctx)
-    #   # if `require_admin:` was given, then require current user to be an admin
+    #   # if `require_admin:` was given, require current user to be an admin
     #   super && (@require_admin ? ctx[:current_user]&.admin? : true)
     #   super && (@require_owner ? owner_or_admin : true)
     # end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,8 +3,7 @@
 # This file defines all possible graphql mutations
 module Types
   class MutationType < Types::BaseObject
-    # field :user_login, mutation: Mutations::User::Login
-    # field :user_logout, mutation: Mutations::User::Logout
+    field :user_login, mutation: Mutations::User::Login
     # field :user_create, mutation: Mutations::User::Create
     # field :user_update, mutation: Mutations::User::Update
     # field :user_delete, mutation: Mutations::User::Delete
