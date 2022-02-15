8078a78fce2309e167cb195c6497999d16448575
Joseph D Cohen <jdcohenesq@gmail.com>
2022-02-15 15:12:37 -0500

    Fix new RuboCop offense in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -197,7 +197,7 @@ class ApplicationController < ActionController::Base
     return true if verified_user_logged_in?
 
     store_location
-    return redirect_to(account_login_path)
+    redirect_to(account_login_path)
   end
 
   private ##########
