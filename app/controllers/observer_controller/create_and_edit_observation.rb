d1b8c3d93cbedc1d02fccd4c2added5cc7585668
Joseph D Cohen <jdcohenesq@gmail.com>
2022-02-16 19:28:59 -0500

    Delete uncalled, untested methods in CreateAndEditObservation
    The purpose of this commit is to improve test coverage in response to https://coveralls.io/builds/46585561/source?filename=app%2Fcontrollers%2Fobserver_controller%2Fcreate_and_edit_observation.rb#L753.
    
    That run shows newly uncovered lines. Those lines had been reached by anonymous users.
    I cannot test them because anonymous users are no longer allowed there.
    But I don't want to delete them in case we want to one day allow anonymous users.
    
    An alternative way to avoid coverage loss is to improve coverage elsewhere.
    This commit achieves that by removing uncovered lines.

--- app/controllers/observer_controller/create_and_edit_observation.rb
+++ app/controllers/observer_controller/create_and_edit_observation.rb
@@ -751,26 +751,6 @@ class ObserverController
   end
 
   ##############################################################################
-  #
-  #  Methods relating to User#notes_template
-  #
-  ##############################################################################
-
-  def use_notes_template?
-    @user.notes_template? && @observation.notes.blank?
-  end
-
-  # String combining the note parts defined in the User's notes_template
-  # with their filled-in values, ignoring parts with blank values
-  def combined_notes_parts
-    @user.notes_template_parts.each_with_object("") do |part, notes|
-      key   = Observation.notes_part_id(part).to_sym
-      value = params[key]
-      notes << "#{part}: #{value}\n" if value.present?
-    end
-  end
-
-  ##############################################################################
 
   private
 
