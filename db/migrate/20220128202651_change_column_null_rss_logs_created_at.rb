3e4762e8b8c06b6a751953d24b588dd8bf8b405d
Nathan Wilson <nathan@collectivesource.com>
2022-02-21 15:33:03 -0500

    Fix column null migration

--- db/migrate/20220128202651_change_column_null_rss_logs_created_at.rb
+++ db/migrate/20220128202651_change_column_null_rss_logs_created_at.rb
@@ -1,5 +1,6 @@
 class ChangeColumnNullRssLogsCreatedAt < ActiveRecord::Migration[5.2]
   def change
-    change_column_null(:rss_logs, :created_at, false)
+    change_column_null(:rss_logs, :created_at, false, Time.now)
+    RssLog.update_all("created_at = updated_at")
   end
 end
