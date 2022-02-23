772fe7678e8dabd0b45444b02c2c5a1539305a56
andrew nimmo <andrnimm@fastmail.fm>
2022-02-22 19:16:48 -0800

    Delete commented-out lines from refactor in http_request_test

--- test/graphql/http_requests/http_request_test.rb
+++ test/graphql/http_requests/http_request_test.rb
@@ -3,43 +3,11 @@
 require("test_helper")
 require("graphql_request_helper")
 
+# Tests the GraphQL Controller and the most basic queries that reflect
+# controller's `context` hash back to the JSON response, Visitor and Admin
+# https://graphql-ruby.org/testing/integration_tests.html
 class Mutations::HttpRequestTest < ActionDispatch::IntegrationTest
   include GraphQLRequestHelper
-  # Tests the GraphQL Controller and the most basic queries that reflect
-  # controller's `context` hash back to the JSON response, Visitor and Admin
-  # https://graphql-ruby.org/testing/integration_tests.html
-
-  # Setup: We need to set a user agent to get by MO's robot detector here.
-  # Neither setting request.header nor Capybara.page.driver.header works?
-  # https://stackoverflow.com/questions/39096779/set-custom-user-agent-on-rails-testing
-
-  # def setup
-  #   @headers = { "User-Agent" => "iPadApp" }
-  # end
-
-  # def headers_with_auth(token)
-  #   return @headers unless token
-
-  #   headers = @headers
-  #   headers["Authorization"] = "Bearer #{token}"
-  #   headers
-  # end
-
-  # def graphql_path
-  #   "/graphql"
-  # end
-
-  # def visitor_query
-  #   "{ visitor { login }, admin }"
-  # end
-
-  # def invalid_query
-  #   "{ nonsense }"
-  # end
-
-  # def user_query_by_login
-  #   "query findRolf($login: String!){ user( login: $login ){ login, id } }"
-  # end
 
   def invalid_variables
     { "login" => "0" }
@@ -50,38 +18,23 @@ class Mutations::HttpRequestTest < ActionDispatch::IntegrationTest
   end
 
   def test_invalid_query
-    # post(graphql_path,
-    #      params: { query: invalid_query },
-    #      headers: @headers)
-
     do_graphql_request(user: nil, qry: nonsense_query)
 
-    # json_response = JSON.parse(@response.body)
     assert_equal("Field 'nonsense' doesn't exist on type 'Query'",
                  json["errors"][0]["message"], "Field does not exist")
   end
 
   def test_invalid_query_variables
-    # post(graphql_path,
-    #      params: { query: user_query_by_login, variables: invalid_variables },
-    #      headers: @headers)
-
     do_graphql_request(qry: user_query,
                        var: invalid_variables)
 
-    # json_response = JSON.parse(@response.body)
     assert_nil(json["data"], "Wrong variables")
   end
 
   def test_valid_query_variables
-    # post(graphql_path,
-    #      params: { query: user_query_by_login, variables: valid_variables },
-    #      headers: @headers)
-
     do_graphql_request(qry: user_query,
                        var: valid_variables)
 
-    # json_response = JSON.parse(@response.body)
     assert_equal(users(:rolf).id, json["data"]["user"]["id"],
                  "Variable correctly parsed for query")
   end
