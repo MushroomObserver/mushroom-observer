b45abd953b53fe2cf804ccc4f9a3b94dbf22cd0b
andrew nimmo <andrnimm@fastmail.fm>
2022-03-11 19:43:22 -0800

    Fixes for collection_number methods
    Uses activerecord delete and update_all

--- app/models/collection_number.rb
+++ app/models/collection_number.rb
@@ -106,33 +106,13 @@ class CollectionNumber < AbstractModel
     end
   end
 
-  def destroy_without_callbacks
-    delete_manager = arel_delete_collection_numbers
-    self.class.connection.execute(delete_manager.to_sql)
-  end
-
-  private
-
   # DELETE FROM collection_numbers WHERE id = #{id}
-  def arel_delete_collection_numbers
-    cn = Arel::Table.new(:collection_numbers)
-    Arel::DeleteManager.new.from(cn).where(cn[:id].eq(id))
+  def destroy_without_callbacks
+    collection_number.delete
   end
-
-  public
 
   # Mirror changes to collection number in herbarium records.  Do this
   # low-level to avoid redundant rss logs and other callbacks.
-  def change_corresponding_herbarium_records(old_format_name)
-    update_manager = arel_update_corresponding_herbarium_records(
-      old_format_name
-    )
-    # puts(update_manager.to_sql)
-    Observation.connection.execute(update_manager.to_sql)
-  end
-
-  private
-
   # UPDATE collection_numbers_observations cno,
   #   herbarium_records_observations hro,
   #   herbarium_records hr
@@ -141,34 +121,18 @@ class CollectionNumber < AbstractModel
   #   AND cno.observation_id = hro.observation_id
   #   AND hro.herbarium_record_id = hr.id
   #   AND hr.accession_number = #{old_format_name}
-  def arel_update_corresponding_herbarium_records(old_format_name)
-    new_format_name = Observation.connection.quote_string(format_name)
-    old_format_name = Observation.connection.quote_string(old_format_name)
-    join_sources = arel_join_source_corresponding_tables(old_format_name)
-    hr = Arel::Table.new(:herbarium_records)
-
-    # puts(join_sources.to_sql)
-    Arel::UpdateManager.new.
-      table(join_sources).
-      set([[hr[:accession_number], new_format_name]])
-  end
-
-  def arel_join_source_corresponding_tables(old_format_name)
+  def change_corresponding_herbarium_records(old_format_name)
     cno = Arel::Table.new(:collection_numbers_observations)
     hro = Arel::Table.new(:herbarium_records_observations)
     hr = Arel::Table.new(:herbarium_records)
+    new_format_name = Observation.connection.quote_string(format_name)
+    old_format_name = Observation.connection.quote_string(old_format_name)
 
-    on_hro = hr.create_on(hr[:accession_number].eq(old_format_name).
-                          and(hro[:herbarium_record_id].eq(hr[:id])))
-    join_hr_hro = Arel::Nodes::JoinSource.new(
-      hr,
-      [hr.create_join(hro, on_hro)]
-    )
-    on_cno = hr.create_on(cno[:collection_number_id].eq(id).
-                          and(cno[:observation_id].eq(hro[:observation_id])))
-    Arel::Nodes::JoinSource.new(
-      join_hr_hro,
-      [join_hr_hro.create_join(cno, on_cno)]
-    )
+    HerbariumRecord.joins(observations: :collection_numbers).where(
+      hr[:accession_number].eq(old_format_name).
+      and(hro[:herbarium_record_id].eq(hr[:id])).
+      and(cno[:collection_number_id].eq(id)).
+      and(cno[:observation_id].eq(hro[:observation_id]))
+    ).update_all(accession_number: new_format_name)
   end
 end
