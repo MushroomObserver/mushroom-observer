ef71be27d923e18f50c182dc44d724ff0d5d3399
andrew nimmo <andrnimm@fastmail.fm>
2022-05-06 17:52:22 -0700

    Update query_record.rb
    Adding single method rewrite, more likely to stay Arel due to use of `table_name` variable.
    Discussion of conversion in comments.

--- app/models/query_record.rb
+++ app/models/query_record.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+#  = Query Record Model
+#
+#  Utility for MO's Query::Modules::ActiveRecord
+
 # access query records saved in the db
 class QueryRecord < ApplicationRecord
   attr_accessor :query
@@ -15,12 +19,28 @@ class QueryRecord < ApplicationRecord
                   (@@last_cleanup < 5.minutes.ago) ||
                   ::Rails.env.test?
 
-    connection.delete(%(
-      DELETE FROM #{table_name}
-      WHERE
-        access_count = 0 AND updated_at < DATE_SUB(NOW(), INTERVAL 6 HOUR) OR
-        access_count > 0 AND updated_at < DATE_SUB(NOW(), INTERVAL 1 DAY)
-    ))
+    delete_manager = arel_delete_cleanup(table_name)
+    # Nimmo note: Reviewers can examine the generated SQL like so:
+    # puts(delete_manager.to_sql)
+    connection.delete(delete_manager.to_sql)
+
     @@last_cleanup = Time.zone.now
+  end
+
+  # Nimmo Note: This seems like a good place to keep it in Arel.
+  # It enables passing the table name as a variable; can't imagine in AR.
+  # Original SQL:
+  # DELETE FROM #{table_name}
+  # WHERE
+  #   access_count = 0 AND updated_at < DATE_SUB(NOW(), INTERVAL 6 HOUR) OR
+  #   access_count > 0 AND updated_at < DATE_SUB(NOW(), INTERVAL 1 DAY)
+  private_class_method def self.arel_delete_cleanup(table_name)
+    table = Arel::Table.new(table_name)
+    Arel::DeleteManager.new.
+      from(table).
+      where(table[:access_count].eq(0).
+        and(table[:updated_at].lt(Time.zone.now - 6.hours)).
+        or(table[:access_count].gt(0).
+        and(table[:updated_at].lt(Time.zone.now - 1.day))))
   end
 end
