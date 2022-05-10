bd2833b26e39db44244b7da9fd568a56b0fdac91
andrew nimmo <andrnimm@fastmail.fm>
2022-05-10 15:10:32 -0700

    AR/arel rewrites
    Quite a bit of fancy footwork in this rewrite.

--- app/models/language.rb
+++ app/models/language.rb
@@ -27,6 +27,8 @@
 ################################################################################
 
 class Language < AbstractModel
+  require "arel-helpers"
+  include ArelHelpers::ArelTable
   include LanguageExporter
   class << self
     include LanguageTracking
@@ -58,53 +60,107 @@ class Language < AbstractModel
   # Get a list of the top N contributors to a language's translations.
   # This is used by the app layout, so must cause mimimal database load.
   def top_contributors(num = 10)
-    user_ids = self.class.connection.select_rows(%(
-      SELECT user_id
-      FROM translation_strings
-      WHERE language_id = #{id} AND user_id != 0
-      GROUP BY user_id
-      ORDER BY COUNT(id) DESC
-      LIMIT #{num}
-    ))
-    if user_ids.any?
-      user_ids = self.class.connection.select_rows(%(
-        SELECT id, login
-        FROM users
-        WHERE id IN (#{user_ids.join(",")})
-        ORDER BY FIND_IN_SET(id, '#{user_ids.join(",")}')
-      ))
-    end
-    user_ids
+    ts = Arel::Table.new(:translation_strings)
+    u = Arel::Table.new(:users)
+    TranslationString.where(language: self).where.not(user_id: 0).
+      group(:user_id).order(ts[:id].count).limit(num).
+      joins(:user).pluck(u[:id], u[:login])
   end
 
   # Count the number of lines the user has translated.  Include edits, as well.
   # It counts paragraphs, actually, and weights them according to length.
   def self.calculate_users_contribution(user)
     lines = 0
-    for text in Language.connection.select_values(%(
-      SELECT GROUP_CONCAT(CONCAT(text, "\n")) AS x
-      FROM translation_strings_versions
-      WHERE user_id = #{user.id}
-      GROUP BY translation_string_id
-    ))
+    select_manager = arel_select_translation_strings_versions(user)
+    # puts(select_manager.to_sql)
+    values = Language.connection.select_values(select_manager.to_sql)
+    # # puts(values.inspect)
+    for text in values
       lines += score_lines(text)
     end
     lines
   end
 
+  # SELECT GROUP_CONCAT(CONCAT(text, "\n")) AS x
+  # FROM translation_strings_versions
+  # WHERE user_id = #{user.id}
+  # GROUP BY translation_string_id
+  private_class_method def self.arel_select_translation_strings_versions(user)
+    v = Arel::Table.new(:translation_strings_versions)
+    v.where(v[:user_id].eq(user.id)).
+      group(v[:translation_string_id]).
+      project(arel_function_group_concat_strings(v))
+  end
+
   def calculate_users_contribution(user)
     lines = 0
-    for text in Language.connection.select_values(%(
-      SELECT GROUP_CONCAT(CONCAT(v.text, "\n")) AS x
-      FROM translation_strings t, translation_strings_versions v
-      WHERE t.language_id = #{id}
-        AND v.translation_string_id = t.id
-        AND v.user_id = #{user.id}
-      GROUP BY t.id
-    ))
+    select_manager = arel_select_translation_strings(user)
+    # puts(select_manager.to_sql)
+    values = Language.connection.select_values(select_manager.to_sql)
+    # puts(values.inspect)
+    for text in values
       lines += Language.score_lines(text)
     end
     lines
+  end
+
+  # def calculate_users_contribution(user)
+  #   v = Arel::Table.new(:translation_strings_versions)
+  #   hash = {}
+  #   strings = TranslationString.joins(:versions).
+  #             where(language_id: id,
+  #                   translation_strings_versions: { user_id: user.id }).
+  #             group(:id).pluck(:id, v[:text])
+  #   strings.each do |id, text|
+  #     text.split("\n").each do |str|
+  #       (hash[id] ||= {}) && (hash[id][str] = true) if str.present?
+  #     end
+  #     puts(hash)
+  #   end
+  #   score_lines(hash)
+  # end
+
+  # def score_lines(hash)
+  #   score = 0
+  #   hash.each_value do |hash2|
+  #     hash2.each_key do |key|
+  #       score += (key.length.to_f / CHARACTERS_PER_LINE).truncate + 1
+  #     end
+  #   end
+  #   score
+  # end
+
+  private
+
+  # rubocop:disable Metrics/AbcSize
+  # SELECT GROUP_CONCAT(CONCAT(v.text, "\n")) AS x
+  # FROM translation_strings t, translation_strings_versions v
+  # WHERE t.language_id = #{id}
+  #   AND v.translation_string_id = t.id
+  #   AND v.user_id = #{user.id}
+  # GROUP BY t.id
+  def arel_select_translation_strings(user)
+    t = Arel::Table.new(:translation_strings)
+    v = Arel::Table.new(:translation_strings_versions)
+    TranslationString.joins(t.join(v).on(t[:language_id].eq(id).
+                            and(v[:translation_string_id].eq(t[:id])).
+                            and(v[:user_id].eq(user.id))).join_sources).
+      group(t[:id]).select(self.class.arel_function_group_concat_strings(v))
+  end
+
+  # NIMMO QUESTION - Is there a way to rewrite these without the group_concat?
+  # I was thinking maybe just select the :text, and append the newlines in Ruby
+  # via map. But this gives a different score, because of the grouping.
+  # The command in PostgresQL is "STRING_AGG"
+  def self.arel_function_group_concat_strings(table)
+    Arel::Nodes::NamedFunction.new(
+      "GROUP_CONCAT",
+      [Arel::Nodes::NamedFunction.new(
+        "CONCAT",
+        [table[:text],
+         Arel::Nodes.build_quoted("\n")]
+      )]
+    )
   end
 
   def self.score_lines(text)
@@ -127,12 +183,14 @@ class Language < AbstractModel
   def self.update_recent_translations
     cutoff = @@last_update
     @@last_update = Time.zone.now
-    for locale, tag, text in Language.connection.select_rows(%(
-      SELECT locale, tag, text
-      FROM translation_strings t, languages l
-      WHERE t.language_id = l.id
-        AND t.updated_at >= #{Language.connection.quote(cutoff)}
-    ))
+    update_cutoff = Language.connection.quote(cutoff)
+    lang = Language.arel_table
+    ts = TranslationString.arel_table
+
+    strings = TranslationString.joins(:language).
+              where(ts[:updated_at].gteq(update_cutoff)).
+              pluck(lang[:locale], :tag, :text)
+    for locale, tag, text in strings
       TranslationString.translations(locale.to_sym)[tag.to_sym] = text
     end
   end
