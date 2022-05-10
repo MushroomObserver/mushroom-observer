e6acfe0a2a7152d322e6b334f93a2a11159ff61b
andrew nimmo <andrnimm@fastmail.fm>
2022-05-09 22:44:31 -0700

    Fix rewrite of fix_self_referential_misspellings
    Whew. Has to be Arel here, if not a string.

--- app/models/name/spelling.rb
+++ app/models/name/spelling.rb
@@ -179,11 +179,12 @@ class Name < AbstractModel
   # name merges?  Whatever.  This fixes it and will run nightly. -JPH 20210812
   def self.fix_self_referential_misspellings
     msgs = Name.select(:id, :text_name, :author).
-           where(correct_spelling_id: :id).
+           where(Name[:correct_spelling_id].eq(Name[:id])).
            map do |id, text_name, author|
              "Name ##{id} #{text_name} #{author} was a misspelling of itself."
            end
-    Name.where(correct_spelling_id: :id).update_all(correct_spelling_id: nil)
+    Name.where(Name[:correct_spelling_id].eq(Name[:id])).
+      update_all(correct_spelling_id: nil)
     msgs
   end
 end
