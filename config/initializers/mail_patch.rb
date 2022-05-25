517667b22bebd80a12c6d1fd6c8437deda987862
andrew nimmo <andrnimm@fastmail.fm>
2022-05-25 13:12:10 -0700

    Update mail_patch.rb

--- config/initializers/mail_patch.rb
+++ config/initializers/mail_patch.rb
@@ -1,4 +1,5 @@
-# config/initializers/mail_patch.rb
+# frozen_string_literal: true
+
 # Resolves an issue in mail 2.7.1 and above, where newlines are inserted
 # in mail body every x characters. This allows tests to pass
 module Mail
