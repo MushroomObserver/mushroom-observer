acefa41538828d2a1fc47a8152739df48019d6dc
andrew nimmo <andrnimm@fastmail.fm>
2022-05-26 23:16:59 -0700

    Reformat error build/add methods in three places

--- app/models/name.rb
+++ app/models/name.rb
@@ -427,9 +427,8 @@ class Name < AbstractModel
   def icn_id_registrable
     return if icn_id.blank? || registrable?
 
-    errors[:base] << :name_error_unregistrable.t(
-      rank: rank.to_s, name: real_search_name
-    )
+    errors.add(:base, :name_error_unregistrable.t,
+               { rank: rank.to_s, name: real_search_name })
   end
 
   # Require icn_id to be unique
@@ -439,9 +438,8 @@ class Name < AbstractModel
     return if icn_id.nil?
     return if (conflicting_name = other_names_with_same_icn_id.first).blank?
 
-    errors[:base] << :name_error_icn_id_in_use.t(
-      number: icn_id, name: conflicting_name.real_search_name
-    )
+    errors.add(:base, :name_error_icn_id_in_use.t,
+               { number: icn_id, name: conflicting_name.real_search_name })
   end
 
   def other_names_with_same_icn_id
