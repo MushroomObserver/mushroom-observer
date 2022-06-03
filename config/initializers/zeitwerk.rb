6e1d1ab3f80553ae983802a27fecfd3e86997891
andrew nimmo <andrnimm@fastmail.fm>
2022-06-02 17:50:47 -0700

    Update zeitwerk.rb

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -13,15 +13,14 @@ Rails.autoloaders.main.ignore(
   "app/assets",
   "app/classes/api/helpers",
   "app/classes/api2/helpers",
+  "app/classes/report",
   "app/controllers/ajax_controller",
   "app/controllers/name_controller",
   "app/controllers/observer_controller",
   "app/extensions",
   "app/javascripts",
   "app/models/comment",
-  "app/models/image",
   "app/models/name",
-  "app/models/observation",
   "app/models/queued_email",
   "app/views"
 )
