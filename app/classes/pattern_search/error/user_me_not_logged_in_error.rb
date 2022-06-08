5107dcd838fd16d63305e600e1a22a97520d4aa7
andrew nimmo <andrnimm@fastmail.fm>
2022-06-08 00:20:24 -0700

    Last pattern search format fix

--- app/classes/pattern_search/error/user_me_not_logged_in_error.rb
+++ app/classes/pattern_search/error/user_me_not_logged_in_error.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class PatternSearch::UserMeNotLoggedInError < PatternSearch::Error
-  def to_s
-    :pattern_search_user_me_not_logged_in_error.t
+module PatternSearch
+  class UserMeNotLoggedInError < Error
+    def to_s
+      :pattern_search_user_me_not_logged_in_error.t
+    end
   end
 end
