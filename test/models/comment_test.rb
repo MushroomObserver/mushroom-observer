d3750e4f1584b05e012dabc8bb8844be85472c7f
Joe Cohen <jdcohenesq@gmail.com>
2022-06-12 09:50:28 -0700

    Fix offenses introduced in prior commit

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -131,7 +131,6 @@ class CommentTest < UnitTestCase
     strs = ActionMailer::Base.deliveries[start..-1].map do |mail|
       "to: #{mail["to"]}, subject: #{mail["subject"]}"
     end
-    "These emails were sent:\n" \
-    "#{strs.join("\n")}"
+    "These emails were sent:\n#{strs.join("\n")}"
   end
 end
