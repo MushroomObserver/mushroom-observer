242b5b969c6b1c88819937e2d16242f234493cc2
andrew nimmo <andrnimm@fastmail.fm>
2022-06-14 12:02:36 -0700

    Went ahead and wrote the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_070009) do
+ActiveRecord::Schema.define(version: 2022_06_14_184435) do
 
   create_table "api_keys", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at"
