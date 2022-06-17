77435db47b5eac59c219415a2f6895a4d268427e
andrew nimmo <andrnimm@fastmail.fm>
2022-06-16 17:20:50 -0700

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -105,7 +105,7 @@ module ApplicationHelper
 
   # link to previous object in query results
   def link_prev(object)
-    path = if CONVERTED_CONTROLLERS.include? object.type_tag
+    path = if REFACTORED_CONTROLLERS.include? object.type_tag
              send("#{object.type_tag.to_s}_path", object.id, flow: "prev")
            else
              { controller: object.show_controller,
