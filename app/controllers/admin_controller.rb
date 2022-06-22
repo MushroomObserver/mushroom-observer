03f0a5739a79d4b282bea0a9cede1179af7c128e
andrew nimmo <andrnimm@fastmail.fm>
2022-06-21 19:43:26 -0700

    Fix admin controller & nav_left path

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AdminController < ApplicationController
+  before_action :login_required
+
   def test_flash_redirection
     tags = params[:tags].to_s.split(",")
     if tags.any?
