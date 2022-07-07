183e0f639291e9f0a76b45d6925dd7ab48078acd
andrew nimmo <andrnimm@fastmail.fm>
2022-07-06 17:23:11 -0700

    Fix views/search directory name, test_pattern_search action

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -65,38 +65,38 @@ class SearchControllerTest < FunctionalTestCase
   def test_pattern_search
     login
     params = { search: { pattern: "12", type: :observation } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :observer, action: :observation_search,
                          pattern: "12")
 
     params = { search: { pattern: "34", type: :image } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :image, action: :image_search,
                          pattern: "34")
 
     params = { search: { pattern: "56", type: :name } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :name, action: :name_search,
                          pattern: "56")
 
     params = { search: { pattern: "78", type: :location } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :location, action: :location_search,
                          pattern: "78")
 
     params = { search: { pattern: "90", type: :comment } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :comment, action: :comment_search,
                          pattern: "90")
 
     params = { search: { pattern: "12", type: :species_list } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :species_list,
                          action: :species_list_search,
                          pattern: "12")
 
     params = { search: { pattern: "34", type: :user } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(users_path(pattern: "34"))
 
     stub_request(:any, /google.com/)
@@ -104,25 +104,25 @@ class SearchControllerTest < FunctionalTestCase
     params = { search: { pattern: pattern, type: :google } }
     target =
       "https://google.com/search?q=site%3Amushroomobserver.org+#{pattern}"
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(target)
 
     params = { search: { pattern: "", type: :google } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to("/")
 
     params = { search: { pattern: "x", type: :nonexistent_type } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to("/")
 
     params = { search: { pattern: "", type: :observation } }
-    get_with_dump(:pattern_search, params)
+    get_with_dump(:pattern, params)
     assert_redirected_to(controller: :observer, action: :list_observations)
 
     # Make sure this redirects to the index that lists all herbaria,
     # rather than the index that lists query results.
     params = { search: { pattern: "", type: :herbarium } }
-    get(:pattern_search, params: params)
+    get(:pattern, params: params)
     assert_redirected_to(herbaria_path(flavor: :all))
   end
 end
