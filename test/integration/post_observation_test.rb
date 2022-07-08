8a7adc78a495389de8e039d8b9206fd6f0656a2b
andrew nimmo <andrnimm@fastmail.fm>
2022-07-08 01:26:39 -0700

    Give destroy button an ID

--- test/integration/post_observation_test.rb
+++ test/integration/post_observation_test.rb
@@ -116,7 +116,7 @@ class PostObservationTest < IntegrationTestCase
   def destroy_observation
     # puts(response.body)
     assert_template(SHOW_OBSERVATION_TEMPLATE)
-    click_on("Destroy")
+    click_on("Destroy", id: "destroy_observation")
     assert_flash_for_destroy_observation
     assert_template(OBSERVATION_INDEX_TEMPLATE)
   end
