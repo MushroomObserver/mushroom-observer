bcef232881cad6aeb4306070ab08c31fcddacd82
Joe Cohen <jdcohenesq@gmail.com>
2022-07-13 17:19:05 -0700

    Fix Lint/SymbolConversion offenses
    Autocorrect fixable offense
    Disable cop inline for false positive

--- test/controllers/pivotal_controller_test.rb
+++ test/controllers/pivotal_controller_test.rb
@@ -23,7 +23,7 @@ class PivotalControllerTest < FunctionalTestCase
     ).
       with(
         headers: {
-          'Accept': "*/*",
+          Accept: "*/*",
           'Accept-Encoding': "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
           'User-Agent': "Ruby",
           'X-Trackertoken': "xxx"
