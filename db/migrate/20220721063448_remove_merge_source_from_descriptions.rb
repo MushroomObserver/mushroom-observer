6e1c89e7b439fc294439f0beae286851aed02506
Joe Cohen <jdcohenesq@gmail.com>
2022-07-21 05:26:15 -0700

    Fix migration file and class names

--- db/migrate/20220721063448_remove_merge_source_from_descriptions.rb
+++ db/migrate/20220721063448_remove_merge_source_from_descriptions.rb
@@ -1,4 +1,4 @@
-class RemoveMergeSourceId < ActiveRecord::Migration[6.1]
+class RemoveMergeSourceFromDescriptions < ActiveRecord::Migration[6.1]
   def change
     remove_column :location_descriptions, :merge_source_id, :integer
     remove_column :name_descriptions, :merge_source_id, :integer
