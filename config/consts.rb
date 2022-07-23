2215d52327d092dbc5e88630139ffc821f94fb83
Nathan Wilson <nathan@collectivesource.com>
2022-07-23 12:28:52 -0400

    CodeClimate fix

--- config/consts.rb
+++ config/consts.rb
@@ -185,5 +185,5 @@ MushroomObserver::Application.configure do
 
   # List of User ids of users that can see the image recognition
   # "Suggest Names" button on the observation page.
-  config.image_model_beta_testers = [103233]
+  config.image_model_beta_testers = [103_233]
 end
