46f55dd9d91ed50c43e39af1ca3599e03af9cde9
Nathan Wilson <nathan@collectivesource.com>
2022-07-24 10:00:58 -0400

    Make default for ok_for_ml true and avoid nulls

--- db/migrate/20220723194140_add_ok_for_ml_to_images.rb
+++ db/migrate/20220723194140_add_ok_for_ml_to_images.rb
@@ -2,6 +2,6 @@
 
 class AddOkForMlToImages < ActiveRecord::Migration[6.1]
   def change
-    add_column(:images, :ok_for_ml, :boolean)
+    add_column(:images, :ok_for_ml, :boolean, default: true, null: false)
   end
 end
