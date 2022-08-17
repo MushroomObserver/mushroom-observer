MushroomObserver/mushroom-observer

commit c2d35941fd09ca0b0b3d4797c9f27c530d0eb24c

bunch of changes

app/controllers/image_controller.rb
@@ -121,7 +121,7 @@ class ImageController < ApplicationController
     show_selected_images(query)
   rescue StandardError => e
     flash_error(e.to_s) if e.present?
-    redirect_to(controller: :observer, action: :advanced_search)
+    redirect_to(controller: :observations, action: :advanced_search)
   end
 
   # Show selected search results as a matrix with "list_images" template.
@@ -300,7 +300,7 @@ class ImageController < ApplicationController
     return unless @observation
 
     if !check_permission!(@observation)
-      redirect_with_query(controller: :observer,
+      redirect_with_query(controller: :observations,
                           action: :show_observation, id: @observation.id)
     elsif request.method != "POST"
       @image = Image.new
@@ -314,7 +314,7 @@ class ImageController < ApplicationController
       init_project_vars_for_add_or_edit(@observation)
     elsif params[:upload].blank?
       flash_warning(:runtime_no_changes.t)
-      redirect_with_query(controller: :observer,
+      redirect_with_query(controller: :observations,
                           action: :show_observation, id: @observation.id)
     else
       args = params[:image]
@@ -323,7 +323,7 @@ class ImageController < ApplicationController
         process_image(args, params[:upload]["image#{i}"])
         i += 1
       end
-      redirect_with_query(controller: :observer,
+      redirect_with_query(controller: :observations,
                           action: :show_observation, id: @observation.id)
     end
   end
@@ -534,7 +534,7 @@ class ImageController < ApplicationController
       @image.log_remove_from(@observation)
       flash_notice(:runtime_image_remove_success.t(id: @image.id))
     end
-      redirect_with_query(controller: :observer,
+      redirect_with_query(controller: :observations,
                           action: :show_observation, id: @observation.id)
   end
 
@@ -602,7 +602,7 @@ class ImageController < ApplicationController
     # Make sure user owns the observation.
     if (@mode == :observation) &&
        !check_permission!(@observation)
-      redirect_with_query(controller: :observer,
+      redirect_with_query(controller: :observations,
                           action: :show_observation, id: @observation.id)
       done = true
 
@@ -621,7 +621,7 @@ class ImageController < ApplicationController
           error = image.strip_gps!
           flash_error(:runtime_failed_to_strip_gps.t(msg: error)) if error
         end
-        redirect_with_query(controller: :observer,
+        redirect_with_query(controller: :observations,
                             action: :show_observation, id: @observation.id)
         done = true
 

test/controllers/image_controller_test.rb
@@ -297,7 +297,7 @@ class ImageControllerTest < FunctionalTestCase
                        id: observations(:coprinus_comatus_obs).id)
     # Check that image cannot be added to an observation the user doesn't own.
     get_with_dump(:add_image, id: observations(:minimal_unknown_obs).id)
-    assert_redirected_to(controller: :observer, action: :show_observation)
+    assert_redirected_to(controller: :observations, action: :show_observation)
   end
 
   # Test reusing an image by id number.
@@ -308,7 +308,7 @@ class ImageControllerTest < FunctionalTestCase
     assert_not(obs.images.member?(image))
     requires_login(:reuse_image, mode: "observation", obs_id: obs.id,
                                  img_id: image.id)
-    assert_redirected_to(controller: :observer, action: :show_observation,
+    assert_redirected_to(controller: :observations, action: :show_observation,
                          id: obs.id)
     assert(obs.reload.images.member?(image))
     assert(updated_at != obs.updated_at)
@@ -413,7 +413,7 @@ class ImageControllerTest < FunctionalTestCase
       selected: selected
     }
     post_requires_login(:remove_images, params, "mary")
-    assert_redirected_to(controller: :observer, action: :show_observation)
+    assert_redirected_to(controller: :observations, action: :show_observation)
     assert_equal(10, mary.reload.contribution)
     assert(obs.reload.images.member?(keep))
     assert_not(obs.images.member?(remove))
@@ -426,7 +426,7 @@ class ImageControllerTest < FunctionalTestCase
       selected: selected
     }
     post(:remove_images, params: params)
-    assert_redirected_to(controller: :observer, action: :show_observation)
+    assert_redirected_to(controller: :observations, action: :show_observation)
     # Observation gets downgraded to 1 point because it no longer has images.
     # assert_equal(1, mary.reload.contribution)
     assert_equal(10, mary.reload.contribution)
@@ -491,7 +491,7 @@ class ImageControllerTest < FunctionalTestCase
     assert_equal("rolf", obs.user.login)
     requires_user(
       :remove_images,
-      { controller: :observer, action: :show_observation, id: obs.id },
+      { controller: :observations, action: :show_observation, id: obs.id },
       params
     )
     assert_form_action(action: "remove_images", id: obs.id)
@@ -517,7 +517,7 @@ class ImageControllerTest < FunctionalTestCase
     login("mary", "testpassword")
     send(:get, :reuse_image, params: params)
     # assert_redirected_to(%r{/#{obs.id}$})
-    assert_redirected_to(controller: :observer, action: :show_observation,
+    assert_redirected_to(controller: :observations, action: :show_observation,
                          id: obs.id)
 
     login("rolf", "testpassword")
@@ -548,15 +548,15 @@ class ImageControllerTest < FunctionalTestCase
     owner = obs.user.login
     assert_not_equal("mary", owner)
     requires_login(:reuse_image, params, "mary")
-    # assert_template(controller: :observer, action: :show_observation)
-    assert_redirected_to(controller: :observer, action: :show_observation,
+    # assert_template(controller: :observations, action: :show_observation)
+    assert_redirected_to(controller: :observations, action: :show_observation,
                          id: obs.id)
     assert_not(obs.reload.images.member?(image))
 
     login(owner)
     get_with_dump(:reuse_image, params)
-    # assert_template(controller: :observer, action: :show_observation)
-    assert_redirected_to(controller: :observer, action: :show_observation,
+    # assert_template(controller: :observations, action: :show_observation)
+    assert_redirected_to(controller: :observations, action: :show_observation,
                          id: obs.id)
     assert(obs.reload.images.member?(image))
     assert(updated_at != obs.updated_at)

commit 62b7ded6e0261d2c57c4de2b0ec228a6aafb7176

Fix bug exposed by test_advanced_search_error
Error was introduced in Commit c2d35941
See https://github.com/MushroomObserver/mushroom-observer/commit/c2d35941fd09ca0b0b3d4797c9f27c530d0eb24c?diff=unified#diff-9d57b3d06f515652f212fbef92a0f75b6ee3db58284ed83f255c06afea37e6ceL124-L125

app/controllers/image_controller.rb
@@ -126,7 +126,7 @@ class ImageController < ApplicationController
     show_selected_images(query)
   rescue StandardError => e
     flash_error(e.to_s) if e.present?
-    redirect_to(controller: :observations, action: :advanced_search)
+    redirect_to(search_advanced_path)
   end
 
   # Show selected search results as a matrix with "list_images" template.

test/controllers/image_controller_test.rb
@@ -313,8 +313,6 @@ class ImageControllerTest < FunctionalTestCase
     assert_redirected_to(search_advanced_path)
   end
 
-
-
   def test_add_image
     requires_login(:add_image, id: observations(:coprinus_comatus_obs).id)
     assert_form_action(action: "add_image",