b4ccaf91a5d413e5c158cbdb7ba5a7a27b66cedb
JoeCohen <jdcohenesq@gmail.com>
2022-08-18 14:52:51 +0000

    Cover cast_vote with test exposing bug
    ```ruby
    ERROR ImageControllerTest#test_cast_vote (0.90s)
    Minitest::UnexpectedError:         NameError: undefined local variable or method `id' for #<ImageController:0x00000000009e48>
                app/controllers/image_controller.rb:280:in `cast_vote'
                app/controllers/application_controller.rb:236:in `catch_errors_and_log_request_stats'
                test/functional_test_case.rb:22:in `get'
                test/controllers/image_controller_test.rb:303:in `test_cast_vote'
    ```

--- test/controllers/image_controller_test.rb
+++ test/controllers/image_controller_test.rb
@@ -294,6 +294,21 @@ class ImageControllerTest < FunctionalTestCase
                  "Failed to change user's vote for image")
   end
 
+  def test_cast_vote
+    user = users(:mary)
+    image = images(:in_situ_image)
+    value = Image.maximum_vote
+    login(user.login)
+
+    assert_difference("ImageVote.count", 1, "Failed to cast vote") do
+      get(:cast_vote, params: { id: image.id, value: value })
+    end
+    assert_redirected_to("#{image_show_image_path}/#{image.id}")
+    vote = ImageVote.last
+    assert(vote.image == image && vote.user == user && vote.value == value,
+           "Vote not cast correctly")
+  end
+
   def test_image_search
     login
     get_with_dump(:image_search, pattern: "Notes")
