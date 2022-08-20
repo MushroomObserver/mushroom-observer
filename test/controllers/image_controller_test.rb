14b00b9d9536dffccd8f0a4e71907da6c09ba4ea
JoeCohen <jdcohenesq@gmail.com>
2022-08-20 01:44:27 +0000

    Test transform_image
    Exposes bug, e.g.:
    ```ruby
     FAIL ImageControllerTest#test_transform_rotate_left (1.32s)
            Expected response to be a redirect to <http://test.host/image/show_image/688136226> but was a redirect to <http://test.host/image/show_image/688136226?size=medium>.
            Expected "http://test.host/image/show_image/688136226" to be === "http://test.host/image/show_image/688136226?size=medium".
            test/controllers/image_controller_test.rb:1210:in `run_transform'
            test/controllers/image_controller_test.rb:1184:in `test_transform_rotate_left'
    ```

--- test/controllers/image_controller_test.rb
+++ test/controllers/image_controller_test.rb
@@ -1179,4 +1179,34 @@ class ImageControllerTest < FunctionalTestCase
     get(:show_image, params: { id: image.id })
     assert_true(@response.body.include?("type=image&amp;value=1"))
   end
+
+  def test_transform_rotate_left
+    run_transform(opr: "rotate_left")
+  end
+
+  def test_transform_rotate_right
+    run_transform(opr: "rotate_right")
+  end
+
+  def test_transform_mirror
+    run_transform(opr: "mirror")
+  end
+
+  def test_transform_bad_op
+    run_transform(opr: "bad_op", flash: %(Invalid operation "bad_op"))
+  end
+
+  def run_transform(opr:, flash: :image_show_transform_note.l)
+    image = images(:in_situ_image)
+    user = image.user
+    params = { id: image.id, op: opr, size: user.image_size }
+
+    login(user.login)
+    get(:transform_image, params: params)
+
+    # Asserting the flash text is the best I can do because Image.transform
+    # does not transform images in the text environment. 2022-08-19 JDC
+    assert_flash_text(flash)
+    assert_redirected_to("#{image_show_image_path}/#{image.id}")
+  end
 end
