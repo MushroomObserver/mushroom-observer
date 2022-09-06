fc4fe452ab254bdb7eaa3a3bee8c1c43786765e7
Jason Hollinger <pellaea@gmail.com>
2022-09-06 03:13:55 -0400

    Updated labels after feedback from Joanne.

--- app/classes/labels.rb
+++ app/classes/labels.rb
@@ -4,7 +4,6 @@ require "rtf"
 
 # Format observations as fancy labels in RTF format.
 class Labels
-  FUNDIS_HERBARIUM = "Fungal Diversity Survey"
   PARAGRAPH_PREFIX = "\\pard\\plain \\s0\\dbch\\af8\\langfe1081\\dbch\\af8" \
                      "\\afs24\\alang1081\\ql\\keep\\nowidctlpar\\sb0\\sa720" \
                      "\\ltrpar\\hyphpar0\\aspalpha\\cf0\\loch\\f6\\fs24" \
@@ -45,7 +44,8 @@ class Labels
 
   def observations
     query.results(include: [:user, :name, :location, :sequences, :projects,
-                            :herbarium_records, :collection_numbers])
+                            :species_lists, :collection_numbers,
+                            { herbarium_records: :herbarium }])
   end
 
   def format_observations
@@ -55,61 +55,61 @@ class Labels
   def format_observation(obs)
     @para = RTF::CommandNode.new(document, PARAGRAPH_PREFIX, PARAGRAPH_SUFFIX)
     @obs = obs
-    format_number
-    format_name
-    format_location
-    format_date
-    format_sequences
-    format_project
-    format_notes
+    add_number
+    add_name
+    add_location
+    add_date
+    add_observer_if_necessary
+    add_sequences
+    add_projects
+    add_species_lists
+    add_notes
     @para.to_rtf
   end
 
   # RTF class doesn't support smallcaps, but it's really easy.
   def label(str)
     node = RTF::CommandNode.new(@para, "\\rtlch \\ltrch\\scaps\\loch")
-    node << "#{str}: "
+    node.underline { |node2| node2 << str }
+    node << ": "
     @para.store(node)
   end
 
   # --------------------
 
   # Collector's number, e.g.: "John Doe 1234 / MO 56789"
-  def format_number
+  def add_number
     label("number")
-    nums = collection_numbers + [mo_number] + [fundis_number]
-    @para << nums.compact.join(" / ")
+    nums = collection_numbers + herbarium_records + [mo_number]
+    @para << nums.join(" / ")
+    @para << " #{specimen_available}" if nums.length == 1
     @para.line_break
+  end
+
+  def mo_number
+    "MO #{@obs.id}"
+  end
+
+  def specimen_available
+    @obs.specimen ? "(specimen available)" : "(no specimen)"
   end
 
   def collection_numbers
     @obs.collection_numbers.map { |num| "#{num.name} #{num.number}" }
   end
 
-  def mo_number
-    "MO #{@obs.id}" + (@obs.specimen ? "" : " (no specimen)")
-  end
-
-  def fundis_number
-    return nil unless (fundis_herbarium_id = fundis_herbarium&.id)
-
-    @obs.herbarium_records.each do |rec|
-      return "FunDiS #{rec.accession_number}" \
-        if rec.herbarium_id == fundis_herbarium_id
-    end
-    nil
-  end
-
-  def fundis_herbarium
-    # Stick it in a trivial array to ensure that the result is non-nil even
-    # if there is no matching herbarium.  It's a kludge, but it works.
-    (@@fundis_herbarium ||= [Herbarium.find_by(name: FUNDIS_HERBARIUM)]).first
+  def herbarium_records
+    @obs.herbarium_records.
+      select { |rec| rec.herbarium && rec.herbarium.personal_user_id.nil? }.
+      map do |rec|
+        "#{rec.herbarium.code || rec.herbarium.name} #{rec.accession_number}"
+      end
   end
 
   # --------------------
 
   # Mushroom name, in bold and italic.
-  def format_name
+  def add_name
     label("name")
     italic = false
     @para.bold do |bold|
@@ -129,10 +129,10 @@ class Labels
 
   # --------------------
 
-  # Location (scientific order) + GPS.
-  def format_location
+  # Location + GPS.
+  def add_location
     label("location")
-    @para << Location.reverse_name(@obs.location&.name || @obs.where)
+    @para << @obs.place_name
     @para << format_lat_long
     @para << format_alt
     @para.line_break
@@ -141,13 +141,13 @@ class Labels
   def format_lat_long
     loc = @obs.location
     if @obs.lat.present?
-      " – #{format_lat(@obs.lat)} #{format_long(@obs.long)}"
+      " / #{format_lat(@obs.lat)} #{format_long(@obs.long)}"
     elsif loc.present?
       n = format_lat(loc.north)
       s = format_lat(loc.south)
       e = format_lat(loc.east)
       w = format_lat(loc.west)
-      " – #{s}–#{n} #{w}–#{e}"
+      " / #{s}–#{n} #{w}–#{e}"
     end
   end
 
@@ -175,7 +175,7 @@ class Labels
   # --------------------
 
   # Date in format: "5 Dec 2021".
-  def format_date
+  def add_date
     label("date")
     @para << @obs.when.strftime("%e %b %Y").strip
     @para.line_break
@@ -183,36 +183,64 @@ class Labels
 
   # --------------------
 
+  # Observer's name if different from collector's name.
+  def add_observer_if_necessary
+    observer = @obs.user.name || @obs.user.login
+    collector = @obs.collection_numbers.first&.name
+    return if collector == observer
+
+    label("observer")
+    @para << observer
+    @para.line_break
+  end
+
+  # --------------------
+
   # Sequence accession numbers if present.
-  def format_sequences
+  def add_sequences
     return unless @obs.sequences.any?
 
     label("sequences")
     @para << @obs.sequences.map do |seq|
       "#{seq.locus} #{seq.archive} #{seq.accession}".strip_squeeze
-    end.join(", ")
+    end.join(" / ")
     @para.line_break
   end
 
   # --------------------
 
   # Projects if attached to any.
-  def format_project
+  def add_projects
     return unless @obs.projects.any?
 
-    label("project")
-    @para << @obs.projects.map(&:title).join(", ")
+    label("projects")
+    @para << @obs.projects.map(&:title).join(" / ")
+    @para.line_break
+  end
+
+  # --------------------
+
+  # Species lists if attached to any.
+  def add_species_lists
+    return unless @obs.species_lists.any?
+
+    label("species lists")
+    @para << @obs.species_lists.map(&:title).join(" / ")
     @para.line_break
   end
 
   # --------------------
 
   # Any notes fields that are present.
-  def format_notes
+  def add_notes
     @obs.notes.each do |key, val|
-      next unless val.present? && key != Observation.other_notes_key
+      next if val.blank?
 
-      label(key.to_s.downcase.tr("_", " "))
+      if key == Observation.other_notes_key
+        label("notes")
+      else
+        label(key.to_s.downcase.tr("_", " "))
+      end
       @para << val
       @para.line_break
     end
