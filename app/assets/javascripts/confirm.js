8d81dc9cdc01ab897bbd73e8583678d42af4d3dc
Nathan Wilson <nathan@collectivesource.com>
2022-10-02 17:57:11 -0400

    Get rid of extra confirm model

--- app/assets/javascripts/confirm.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function () {
-  jQuery('body').on('click', '[data-confirm]', function (event) {
-    var msg = $(this).attr('data-confirm');
-    return confirm(msg);
-  })
-});
