a8aeeacb4468749cc3dfe0c67e806dd39748697c
andrew nimmo <andrnimm@fastmail.fm>
2022-10-07 15:40:11 -0700

    Fix misspelling

--- app/classes/report/row_extensions.rb
+++ app/classes/report/row_extensions.rb
@@ -204,7 +204,7 @@ module Report
 
     def notes_to_hash
       # prefer safe_load to load for safety & to make RuboCop happy
-      # 2nd argumnet whitelists Symbols, needed because notes have symbol keys
+      # 2nd argument whitelists Symbols, needed because notes have symbol keys
       YAML.safe_load(@vals[9], permitted_classes: [Symbol])
     end
   end
