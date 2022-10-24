42a2a306aaa63c9277e5ee453fa13bdd58a33e81
andrew nimmo <andrnimm@fastmail.fm>
2022-10-24 13:24:36 -0700

    Create donations_controller.rb

---
+++ app/controllers/admin/donations_controller.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+module Admin
+  class DonationsController < ApplicationController
+    before_action :login_required
+
+    def authorize?(_user)
+      in_admin_mode?
+    end
+
+    def access_denied
+      flash_error(:permission_denied.t)
+      if session[:user_id]
+        redirect_to(support_donate_path)
+      else
+        redirect_to(new_account_login_path)
+      end
+    end
+
+    def new
+      # return unless check_donate_admin(:create_donation_not_allowed.t)
+
+      @donation = Donation.new
+    end
+
+    def create
+      # return unless check_donate_admin(:create_donation_not_allowed.t)
+
+      @donation = create_donation(params)
+    end
+
+    # Review donations
+    def edit
+      # return unless check_donate_admin(:review_donations_not_allowed.t)
+
+      @donations = Donation.all.order(created_at: :desc)
+      @reviewed = {}
+      @donations.each do |d|
+        @reviewed[d.id] = d.reviewed
+      end
+    end
+
+    def update
+      # return unless check_donate_admin(:review_donations_not_allowed.t)
+
+      update_donations(params[:reviewed])
+      @donations = Donation.all.order(created_at: :desc)
+      @reviewed = {}
+      @donations.each do |d|
+        @reviewed[d.id] = d.reviewed
+      end
+    end
+
+    private
+
+    def check_donate_admin(error)
+      return true if in_admin_mode?
+
+      flash_error(error)
+      redirect_to(support_donate_path)
+    end
+
+    def create_donation(params)
+      email = params["donation"]["email"]
+      Donation.create(amount: params["donation"]["amount"],
+                      who: params["donation"]["who"],
+                      recurring: params["donation"]["recurring"],
+                      anonymous: params["donation"]["anonymous"],
+                      email: email,
+                      user: find_user(email),
+                      reviewed: true)
+    end
+
+    def find_user(email)
+      users = User.where(email: email)
+      return unless users.length == 1
+
+      users[0]
+    end
+
+    def update_donations(params)
+      params.each do |x, y|
+        d = Donation.find(x)
+        d.reviewed = y
+        d.save
+      end
+    end
+  end
+end
