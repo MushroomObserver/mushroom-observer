42fb31dec9a2d7191364e9efdb4f89887ec017dd
Nathan Wilson <nathan@collectivesource.com>
2022-10-30 11:01:21 -0400

    Remove JSON stuff from VisualModelsController

--- app/controllers/visual_models_controller.rb
+++ app/controllers/visual_models_controller.rb
@@ -3,12 +3,12 @@
 class VisualModelsController < ApplicationController
   before_action :login_required
 
-  # GET /visual_models or /visual_models.json
+  # GET /visual_models
   def index
     @visual_models = VisualModel.all.order(:name)
   end
 
-  # GET /visual_models/1 or /visual_models/1.json
+  # GET /visual_models/1
   def show
     @visual_model = VisualModel.find(params[:id])
   end
@@ -23,63 +23,32 @@ class VisualModelsController < ApplicationController
     @visual_model = VisualModel.find(params[:id])
   end
 
-  # POST /visual_models or /visual_models.json
+  # POST /visual_models
   def create
     @visual_model = VisualModel.new(visual_model_params)
-
-    respond_to do |format|
-      if @visual_model.save
-        format.html do
-          redirect_to(visual_model_url(@visual_model),
-                      notice: :runtime_visual_model_created_at.t)
-        end
-        format.json do
-          render(:show, status: :created,
-                        location: @visual_model)
-        end
-      else
-        format.html { render(:new, status: :unprocessable_entity) }
-        format.json do
-          render(json: @visual_model.errors,
-                 status: :unprocessable_entity)
-        end
-      end
+    if @visual_model.save
+      redirect_to(visual_model_url(@visual_model),
+                  notice: :runtime_visual_model_created_at.t)
     end
   end
 
-  # PATCH/PUT /visual_models/1 or /visual_models/1.json
+  # PATCH/PUT /visual_models/1
   def update
     @visual_model = VisualModel.find(params[:id])
 
-    respond_to do |format|
-      if @visual_model.update(visual_model_params)
-        format.html do
-          redirect_to(visual_model_url(@visual_model),
-                      notice: :runtime_visual_model_updated_at.t)
-        end
-        format.json { render(:show, status: :ok, location: @visual_model) }
-      else
-        format.html { render(:edit, status: :unprocessable_entity) }
-        format.json do
-          render(json: @visual_model.errors,
-                 status: :unprocessable_entity)
-        end
-      end
+    if @visual_model.update(visual_model_params)
+      redirect_to(visual_model_url(@visual_model),
+                  notice: :runtime_visual_model_updated_at.t)
     end
   end
 
-  # DELETE /visual_models/1 or /visual_models/1.json
+  # DELETE /visual_models/1
   def destroy
     @visual_model = VisualModel.find(params[:id])
     @visual_model.destroy
 
-    respond_to do |format|
-      format.html do
-        redirect_to(visual_models_url,
-                    notice: :destroy_visual_model_success.t)
-      end
-      format.json { head(:no_content) }
-    end
+    redirect_to(visual_models_url,
+                notice: :destroy_visual_model_success.t)
   end
 
   private
