74991f3a2b094def48b9110b973097be340b1d1a
andrew nimmo <andrnimm@fastmail.fm>
2022-11-01 18:55:52 -0700

    Update account_test.rb

--- test/integration/capybara/account_test.rb
+++ test/integration/capybara/account_test.rb
@@ -5,7 +5,21 @@ require("test_helper")
 class AccountTest < CapybaraIntegrationTestCase
   def test_preferences; end
 
-  def test_profile; end
+  def test_profile
+    login!("mary")
+
+    # cheating, i'm not going to use selenium just to click a dropdown
+    visit("/users/#{mary[:id]}")
+    click_on(text: "Edit Profile")
+
+    assert_selector("body.profile__edit")
+    within("#account_profile_form") do
+      fill_in("user_name", with: "Merula Marsh")
+      fill_in("user_place_name", with: locations(:mitrula_marsh).name)
+      click_commit
+    end
+    assert_equal("Merula Marsh", mary.name)
+  end
 
   def test_api_keys; end
 
