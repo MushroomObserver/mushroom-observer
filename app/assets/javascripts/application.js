12feb2e584d9b0d846a1bee4c79a817055fed248
Nathan Wilson <nathan@collectivesource.com>
2022-11-14 07:28:48 -0500

    Move references to visual_group_status.js to better places

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //= require external_link
 //= require image_vote
 //= require image_export
-//= require visual_group_status
 //= require theater
 //= require validate_file_input_fields
 //= require main
