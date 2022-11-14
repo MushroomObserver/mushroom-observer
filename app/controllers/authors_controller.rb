e4448977aa9ca8f6c90b4ece3c68d14cdfd44bd3
andrew nimmo <andrnimm@fastmail.fm>
2022-11-14 15:30:34 -0800

    Refactor `:review` for ABC, remove `:email_request` (it moved)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,25 +2,10 @@
 
 class AuthorsController < ApplicationController
   before_action :login_required
+  before_action :pass_query_params
 
-  # Form to compose email for the authors/reviewers.  Linked from show_<object>.
-  # TODO: Use queued_email mechanism.
-  def email_request
-    pass_query_params
-    @object = AbstractModel.find_object(params[:type], params[:id].to_s)
-    return unless request.method == "POST"
-
-    subject = param_lookup([:email, :subject], "")
-    content = param_lookup([:email, :content], "")
-    (@object.authors + UserGroup.reviewers.users).uniq.each do |receiver|
-      AuthorMailer.build(@user, receiver, @object, subject, content).deliver_now
-    end
-    flash_notice(:request_success.t)
-    redirect_with_query(controller: @object.show_controller,
-                        action: @object.show_action, id: @object.id)
-  end
-
-  # Form to adjust permissions for a user with respect to a project.
+  # "Form" (or rather, a page with links back to itself that add params)
+  # to adjust permissions for a user with respect to a project.
   # Linked from: show_(object) and author_request email
   # Inputs:
   #   params[:id]
@@ -33,28 +18,41 @@ class AuthorsController < ApplicationController
   #   Renders show_name.
   #   Outputs: @name, @authors, @users
   def review
-    pass_query_params
     @object = AbstractModel.find_object(params[:type], params[:id].to_s)
     @authors = @object.authors
-    parent = @object.parent
+    deal_with_additions_or_removals
+  end
+
+  private
+
+  def deal_with_additions_or_removals
     if @authors.member?(@user) || @user.in_group?("reviewers")
       @users = User.all.order("login, name").to_a
-      new_author = params[:add] ? User.find(params[:add]) : nil
-      if new_author && !@authors.member?(new_author)
-        @object.add_author(new_author)
-        flash_notice("Added #{new_author.legal_name}")
-        # Should send email as well
-      end
-      old_author = params[:remove] ? User.find(params[:remove]) : nil
-      if old_author && @authors.member?(old_author)
-        @object.remove_author(old_author)
-        flash_notice("Removed #{old_author.legal_name}")
-        # Should send email as well
-      end
+      maybe_add_user_as_author
+      maybe_remove_user_as_author
     else
+      parent = @object.parent
       flash_error(:review_authors_denied.t)
       redirect_with_query(controller: parent.show_controller,
                           action: parent.show_action, id: parent.id)
     end
+  end
+
+  def maybe_add_user_as_author
+    new_author = params[:add] ? User.find(params[:add]) : nil
+    return unless new_author && !@authors.member?(new_author)
+
+    @object.add_author(new_author)
+    flash_notice("Added #{new_author.legal_name}")
+    # Should send email as well
+  end
+
+  def maybe_remove_user_as_author
+    old_author = params[:remove] ? User.find(params[:remove]) : nil
+    return unless old_author && @authors.member?(old_author)
+
+    @object.remove_author(old_author)
+    flash_notice("Removed #{old_author.legal_name}")
+    # Should send email as well
   end
 end
