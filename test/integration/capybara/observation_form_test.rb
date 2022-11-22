2ca96785ff869d1fe803eb89eaee35b2240e2a9d
andrew nimmo <andrnimm@fastmail.fm>
2022-11-22 15:06:29 -0800

    Update observation_form_test.rb
    Add first tests

--- test/integration/capybara/observation_form_test.rb
+++ test/integration/capybara/observation_form_test.rb
@@ -4,10 +4,19 @@ require("test_helper")
 
 class ObservationFormTest < CapybaraIntegrationTestCase
   def test_create_minimal_observation
-    mary = users("mary")
-    login!(mary)
+    rolf = users("rolf")
+    login!(rolf)
 
     click_on("Create Observation")
     assert_selector("body.observations__new")
+    # binding.break
+
+    within("#observation_form") do
+      assert_field("collection_number_name", with: users(:rolf).legal_name)
+      assert_field("herbarium_record_herbarium_name",
+                   with: users(:rolf).preferred_herbarium_name)
+      assert_selector("#where_help",
+                      text: "Albion, Mendocino Co., California")
+    end
   end
 end
