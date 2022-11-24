23df0a8f64c0fc165ca7e46d418ce2d515248dab
andrew nimmo <andrnimm@fastmail.fm>
2022-11-23 18:54:36 -0800

    Fix first obs form test, comments, and change id of naming fields

--- test/integration/capybara/observation_form_test.rb
+++ test/integration/capybara/observation_form_test.rb
@@ -9,7 +9,6 @@ class ObservationFormTest < CapybaraIntegrationTestCase
 
     click_on("Create Observation")
     assert_selector("body.observations__new")
-    # binding.break
 
     within("#observation_form") do
       assert_field("collection_number_name", with: users(:rolf).legal_name)
@@ -17,6 +16,21 @@ class ObservationFormTest < CapybaraIntegrationTestCase
                    with: users(:rolf).preferred_herbarium_name)
       assert_selector("#where_help",
                       text: "Albion, Mendocino Co., California")
+      fill_in("name_name", with: "Elfin saddle")
+      fill_in("observation_place_name", with: locations.first.name)
+      click_commit
+    end
+
+    assert_flash_warning
+    assert_flash_text(
+      :form_observations_there_is_a_problem_with_name.t.html_to_ascii
+    )
+    assert_selector("#name_messages", text: "MO does not recognize the name")
+
+    within("#observation_form") do
+      fill_in("name_name", with: "Coprinus comatus")
+      fill_in("observation_place_name", with: locations.first.name)
+      click_commit
     end
   end
 end
