53d89cdc2aaf9b289bf44c455dcaac40518e0465
Jason Hollinger <pellaea@gmail.com>
2022-12-10 00:16:52 -0500

    Explicitly force backup to overwrite old files.

--- script/backup_blocked_ips.sh
+++ script/backup_blocked_ips.sh
@@ -9,6 +9,6 @@ if [ ! -d config/blocked_ips ]; then
   mkdir config/blocked_ips
 fi
 
-cp config/blocked_ips.txt config/blocked_ips/backup-`date +%d`-daily
-cp config/blocked_ips.txt config/blocked_ips/backup-`date +%m`-monthly
-cp config/blocked_ips.txt config/blocked_ips/backup-`date +%Y`-yearly
+cp -f config/blocked_ips.txt config/blocked_ips/backup-`date +%d`-daily
+cp -f config/blocked_ips.txt config/blocked_ips/backup-`date +%m`-monthly
+cp -f config/blocked_ips.txt config/blocked_ips/backup-`date +%Y`-yearly
