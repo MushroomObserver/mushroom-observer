af05d464b82e67d9c4642b106973ce1477509a26
andrew nimmo <andrnimm@fastmail.fm>
2022-12-16 17:15:48 -0800

    Update remove_controller.rb

--- app/controllers/images/remove_controller.rb
+++ app/controllers/images/remove_controller.rb
@@ -4,6 +4,13 @@ module Images
   class RemoveController < ApplicationController
     before_action :login_required
 
+    # NOTE: Move to new namespaced controllers
+    #
+    # Observations::ImagesController#edit #update
+    # GlossaryTerms::ImagesController#edit #update
+    # Move tests from images_controller_test
+    # No need to remove_images from Account profile: reuse_image removes image
+
     # Form used to remove one or more images from an observation (not destroy!)
     # Linked from: observations/show
     # Inputs:
@@ -15,47 +22,41 @@ module Images
       remove_images_from_object(Observation, params)
     end
 
-    ##############################################################################
+    def remove_images_for_glossary_term
+      remove_images_from_object(GlossaryTerm, params)
+    end
+
+    ############################################################################
 
     private
 
     def remove_images_from_object(target_class, params)
-      pass_query_params
       @object = find_or_goto_index(target_class, params[:id].to_s)
       return unless @object
 
-      redirect_url = if target_class.controller_normalized?
-                       send("#{target_class.to_s.underscore}_path",
-                            @object.id)
-                     else
-                       { controller: target_class.show_controller,
-                         action: target_class.show_action, id: @object.id }
-                     end
-
-      if check_permission!(@object)
-        if request.method == "POST" && (images = params[:selected])
-          images.each do |image_id, do_it|
-            next unless do_it == "yes"
-
-            next unless (image = Image.safe_find(image_id))
-
-            @object.remove_image(image)
-            image.log_remove_from(@object)
-            flash_notice(:runtime_image_remove_success.t(id: image_id))
-          end
-          redirect_with_query(redirect_url)
-        end
-      else
-        redirect_with_query(redirect_url)
+      unless check_permission!(@object)
+        return redirect_with_query(controller: target_class.show_controller,
+                                   action: target_class.show_action,
+                                   id: @object.id)
       end
+
+      return unless request.method == "POST" && (images = params[:selected])
+
+      create_removal(images, target_class)
     end
 
-    public
+    def create_removal(images, target_class)
+      images.each do |image_id, do_it|
+        next unless do_it == "yes"
 
-    ##############################################################################
+        next unless (image = Image.safe_find(image_id))
 
-    def remove_images_for_glossary_term
-      remove_images_from_object(GlossaryTerm, params)
+        @object.remove_image(image)
+        image.log_remove_from(@object)
+        flash_notice(:runtime_image_remove_success.t(id: image_id))
+      end
+      redirect_with_query(controller: target_class.show_controller,
+                          action: target_class.show_action, id: @object.id)
     end
   end
 end
