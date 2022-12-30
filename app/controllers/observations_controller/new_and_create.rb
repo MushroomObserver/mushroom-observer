20860af3ddbd0b5d2f6672ff5fbe1f732c370ee1
andrew nimmo <andrnimm@fastmail.fm>
2022-12-30 11:06:00 -0800

    Update new_and_create.rb

--- app/controllers/observations_controller/new_and_create.rb
+++ app/controllers/observations_controller/new_and_create.rb
@@ -148,6 +148,7 @@ module ObservationsController::NewAndCreate
     observation.updated_at = now
     observation.user       = @user
     observation.name       = Name.unknown
+    observation.source     = "mo_website"
     if Location.is_unknown?(observation.place_name) ||
        (observation.lat && observation.long && observation.place_name.blank?)
       observation.location = Location.unknown
