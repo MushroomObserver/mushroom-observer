c406aa62968066172702e23e7bc756578a93f2ed
andrew nimmo <andrnimm@fastmail.fm>
2023-01-07 10:37:51 -0800

    Break up methods in name descriptions controller

--- app/controllers/names/descriptions_controller.rb
+++ app/controllers/names/descriptions_controller.rb
@@ -144,8 +144,8 @@ module Names
     def new
       store_location
       pass_query_params
-      @name = Name.find(params[:id].to_s)
-      @licenses = License.current_names_and_ids
+      find_name
+      find_licenses
       @description = NameDescription.new
       @description.name = @name
 
@@ -156,39 +156,82 @@ module Names
     def create
       store_location
       pass_query_params
-      @name = Name.find(params[:id].to_s)
-      @licenses = License.current_names_and_ids
+      find_name
+      find_licenses
       @description = NameDescription.new
       @description.name = @name
 
       # Create new description.
-      @description.attributes = whitelisted_name_description_params
+      @description.attributes = allowed_name_description_params
       @description.source_type = @description.source_type.to_sym
 
+      check_create_validity_and_save_or_flash_and_redirect
+    end
+
+    def edit
+      store_location
+      pass_query_params
+      return unless find_description!
+
+      find_licenses
+
+      # check_description_edit_permission is partly broken.
+      # It, LocationController, and NameController need repairs.
+      # See https://www.pivotaltracker.com/story/show/174737948
+      check_description_edit_permission(@description,
+                                        params[:description])
+    end
+
+    def update
+      store_location
+      pass_query_params
+      return unless find_description!
+
+      find_licenses
+
+      check_description_edit_permission(@description,
+                                        params[:description])
+      @description.attributes = allowed_name_description_params
+      @description.source_type = @description.source_type.to_sym
+
+      # Modify permissions based on changes to the two "public" checkboxes.
+      modify_description_permissions(@description)
+
+      # If substantive changes are made by a reviewer, call this act a
+      # "review", even though they haven't actually changed the review
+      # status.  If it's a non-reviewer, this will revert it to "unreviewed".
+      if @description.save_version?
+        @description.update_review_status(@description.review_status)
+      end
+
+      save_if_changes_made_or_flash
+    end
+
+    def destroy
+      pass_query_params
+      return unless find_description!
+
+      check_delete_permission_flash_and_redirect
+    end
+
+    private
+
+    def find_name
+      @name = Name.find(params[:id].to_s)
+    end
+
+    def find_licenses
+      @licenses = License.current_names_and_ids
+    end
+
+    def check_create_validity_and_save_or_flash_and_redirect
       if @description.valid?
         initialize_description_permissions(@description)
         @description.save
 
-        # Make this the "default" description if there isn't one and this is
-        # publicly readable and writable.
-        if !@name.description && @description.fully_public
-          @name.description = @description
-        end
-
-        # Keep the parent's classification cache up to date.
-        if (@name.description == @description) &&
-           (@name.classification != @description.classification)
-          @name.classification = @description.classification
-        end
-
-        # Log action in parent name.
-        @description.name.log(:log_description_created,
-                              user: @user.login,
-                              touch: true,
-                              name: @description.unique_partial_format_name)
-
-        # Save any changes to parent name.
-        @name.save if @name.changed?
+        set_default_description_if_public
+        update_classification_cache(@name)
+        log_description_created
 
         flash_notice(:runtime_name_description_success.t(id: @description.id))
         redirect_to(name_description_path(@description.id))
@@ -197,112 +240,123 @@ module Names
       end
     end
 
-    def edit_name_description
-      store_location
-      pass_query_params
-      @description = NameDescription.find(params[:id].to_s)
-      @licenses = License.current_names_and_ids
+    # Make this the "default" description if there isn't one and this is
+    # publicly readable and writable.
+    def set_default_description_if_public
+      return unless !@name.description && @description.fully_public
 
-      # check_description_edit_permission is partly broken.
-      # It, LocationController, and NameController need repairs.
-      # See https://www.pivotaltracker.com/story/show/174737948
-      if !check_description_edit_permission(@description,
-                                            params[:description])
-        # already redirected
+      @name.description = @description
+    end
 
-      elsif request.method == "POST"
-        @description.attributes = whitelisted_name_description_params
-        @description.source_type = @description.source_type.to_sym
+    # Log action in parent name.
+    def log_description_created
+      @description.name.log(:log_description_created,
+                            user: @user.login,
+                            touch: true,
+                            name: @description.unique_partial_format_name)
+    end
 
-        # Modify permissions based on changes to the two "public" checkboxes.
-        modify_description_permissions(@description)
+    def save_if_changes_made_or_flash
+      # No changes made.
+      if !@description.changed?
+        flash_warning(:runtime_edit_name_description_no_change.t)
+        redirect_to(name_description_path(@description.id))
 
-        # If substantive changes are made by a reviewer, call this act a
-        # "review", even though they haven't actually changed the review
-        # status.  If it's a non-reviewer, this will revert it to "unreviewed".
-        if @description.save_version?
-          @description.update_review_status(@description.review_status)
-        end
+      # There were error(s).
+      elsif !@description.save
+        flash_object_errors(@description)
 
-        # No changes made.
-        if !@description.changed?
-          flash_warning(:runtime_edit_name_description_no_change.t)
-          redirect_to(name_description_path(@description.id))
+      # Updated successfully.
+      else
+        save_flash_success_and_redirect
+      end
+    end
 
-        # There were error(s).
-        elsif !@description.save
-          flash_object_errors(@description)
+    def save_flash_success_and_redirect
+      flash_notice(
+        :runtime_edit_name_description_success.t(id: @description.id)
+      )
+      name = @description.name
+      update_classification_cache(name)
+      log_description_updated(name)
+      resolve_merge_conflicts_and_delete_old_description(name)
+      redirect_to(name_description_path(@description.id))
+    end
 
-        # Updated successfully.
+    # Update name's classification cache.
+    def update_classification_cache(name)
+      if (name.description == @description) &&
+         (name.classification != @description.classification)
+        name.classification = @description.classification
+        name.save if name.changed?
+      end
+    end
+
+    # Log action to parent name.
+    def log_description_updated(name)
+      name.log(:log_description_updated,
+               touch: true,
+               user: @user.login,
+               name: @description.unique_partial_format_name)
+    end
+
+    # Delete old description after resolving conflicts of merge.
+    def resolve_merge_conflicts_and_delete_old_description(name)
+      if (params[:delete_after] == "true") &&
+         (old_desc = NameDescription.safe_find(params[:old_desc_id]))
+        if !in_admin_mode? && !old_desc.is_admin?(@user)
+          flash_warning(:runtime_description_merge_delete_denied.t)
         else
-          flash_notice(
-            :runtime_edit_name_description_success.t(id: @description.id)
-          )
-
-          # Update name's classification cache.
-          name = @description.name
-          if (name.description == @description) &&
-             (name.classification != @description.classification)
-            name.classification = @description.classification
-            name.save
-          end
-
-          # Log action to parent name.
-          name.log(:log_description_updated,
-                   touch: true,
-                   user: @user.login,
-                   name: @description.unique_partial_format_name)
-
-          # Delete old description after resolving conflicts of merge.
-          if (params[:delete_after] == "true") &&
-             (old_desc = NameDescription.safe_find(params[:old_desc_id]))
-            if !in_admin_mode? && !old_desc.is_admin?(@user)
-              flash_warning(:runtime_description_merge_delete_denied.t)
-            else
-              flash_notice(:runtime_description_merge_deleted.
-                             t(old: old_desc.partial_format_name))
-              name.log(:log_object_merged_by_user,
-                       user: @user.login, touch: true,
-                       from: old_desc.unique_partial_format_name,
-                       to: @description.unique_partial_format_name)
-              old_desc.destroy
-            end
-          end
-
-          redirect_to(name_description_path(@description.id))
+          flash_notice(:runtime_description_merge_deleted.
+                          t(old: old_desc.partial_format_name))
+          log_description_merged(name)
+          old_desc.destroy
         end
       end
     end
 
-    def destroy
-      pass_query_params
-      @description = NameDescription.find(params[:id].to_s)
+    # Log merge to parent name.
+    def log_description_merged(name)
+      name.log(:log_object_merged_by_user,
+               touch: true,
+               user: @user.login,
+               from: old_desc.unique_partial_format_name,
+               to: @description.unique_partial_format_name)
+    end
+
+    def check_delete_permission_flash_and_redirect
       if in_admin_mode? || @description.is_admin?(@user)
         flash_notice(:runtime_destroy_description_success.t)
-        @description.name.log(:log_description_destroyed,
-                              user: @user.login,
-                              touch: true,
-                              name: @description.unique_partial_format_name)
+        log_description_destroyed
         @description.destroy
         redirect_to(name_path(@description.name_id, q: get_query_param))
       else
         flash_error(:runtime_destroy_description_not_admin.t)
-        if in_admin_mode? || @description.is_reader?(@user)
-          redirect_to(
-            name_description_path(@description.id, q: get_query_param)
-          )
-        else
-          redirect_to(name_path(@description.name_id, q: get_query_param))
-        end
+        redirect_if_description_not_destroyed
       end
     end
 
-    private
+    def log_description_destroyed
+      @description.name.log(:log_description_destroyed,
+                            user: @user.login,
+                            touch: true,
+                            name: @description.unique_partial_format_name)
+    end
+
+    def redirect_if_description_not_destroyed
+      if in_admin_mode? || @description.is_reader?(@user)
+        redirect_to(
+          name_description_path(@description.id, q: get_query_param)
+        )
+      else
+        redirect_to(name_path(@description.name_id, q: get_query_param))
+      end
+    end
 
     # TODO: should public, public_write and source_type be removed from list?
     # They should be individually checked and set, since we
     # don't want them to have arbitrary values
-    def whitelisted_name_description_params
+    def allowed_name_description_params
       params.required(:description).
         permit(:classification, :gen_desc, :diag_desc, :distribution, :habitat,
                :look_alikes, :uses, :refs, :notes, :source_name, :project_id,
