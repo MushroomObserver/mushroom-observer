c7be424d1643958d8255277080eb1cf6e4546e19
andrew nimmo <andrnimm@fastmail.fm>
2023-01-10 18:46:56 -0800

    Update countries_controller.rb

--- app/controllers/locations/countries_controller.rb
+++ app/controllers/locations/countries_controller.rb
@@ -5,10 +5,10 @@ module Locations
   class CountriesController < ApplicationController
     before_action :login_required
     before_action :disable_link_prefetching
-  end
 
-  # Displays a list of all countries with counts.
-  def index
-    @cc = ::CountryCounter.new
+    # Displays a list of all countries with counts.
+    def index
+      @cc = ::CountryCounter.new
+    end
   end
 end
