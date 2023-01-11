feceaf0b3d5f2db5220a14a4930ed36d1615c771
andrew nimmo <andrnimm@fastmail.fm>
2023-01-11 14:56:54 -0800

    New name tabs helper

---
+++ app/helpers/name_tabs_helper.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# html used in tabsets
+module NameTabsHelper
+  # assemble HTML for "tabset" for show_name
+  def show_name_tabset(name:, user:)
+    tabs = [
+      basic_name_form_links(name),
+      edit_synonym_form_link(name),
+      approve_synonym_form_link(name),
+      deprecate_synonym_form_link(name),
+      name_tracker_form_link(name, user),
+      draw_interest_icons(name)
+    ].flatten.reject(&:empty?)
+    { pager_for: name, right: draw_tab_set(tabs) }
+  end
+
+  def basic_name_form_links(name)
+    [
+      link_with_query(:show_name_edit_name.t, edit_name_path(name.id)),
+      link_with_query(:show_name_add_name.t, new_name_path)
+    ]
+  end
+
+  def edit_synonym_form_link(name)
+    return unless in_admin_mode? || !name.locked
+
+    link_with_query(:show_name_change_synonyms.t,
+                    edit_name_synonyms_path(name.id))
+  end
+
+  def approve_synonym_form_link(name)
+    return unless name.deprecated
+
+    link_with_query(:APPROVE.t, approve_name_synonym_form_path(name.id))
+  end
+
+  def deprecate_synonym_form_link(name)
+    return if name.deprecated
+
+    link_with_query(:DEPRECATE.t, deprecate_name_synonym_form_path(name.id))
+  end
+
+  def name_tracker_form_link(name, user)
+    existing_name_tracker = NameTracker.find_by(name_id: name.id,
+                                                user_id: user.id)
+    if existing_name_tracker
+      link_with_query(:show_name_email_tracking.t,
+                      edit_name_tracker_path(name.id))
+    else
+      link_with_query(:show_name_email_tracking.t,
+                      new_name_tracker_path(name.id))
+    end
+  end
+end
