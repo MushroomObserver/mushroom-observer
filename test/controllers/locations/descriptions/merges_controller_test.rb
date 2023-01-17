89d30456c32700e30c5e7ff07406c0f0bc0f7715
andrew nimmo <andrnimm@fastmail.fm>
2023-01-17 02:01:26 -0800

    Update merges_controller_test.rb

--- test/controllers/locations/descriptions/merges_controller_test.rb
+++ test/controllers/locations/descriptions/merges_controller_test.rb
@@ -6,5 +6,13 @@ require("set")
 module Locations::Descriptions
   class MergesControllerTest < FunctionalTestCase
     include ObjectLinkHelper
+
+    def test_try_merge_descriptions_no_permission; end
+
+    def test_merge_descriptions; end
+
+    def test_merge_descriptions_notes_conflict; end
+
+    def test_merge_incompatible_descriptions; end
   end
 end
