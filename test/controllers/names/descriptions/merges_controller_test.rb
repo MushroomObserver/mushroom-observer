01f4a1d4aa62b5c137fdd1731577b81df6653a0f
andrew nimmo <andrnimm@fastmail.fm>
2023-01-17 16:11:36 -0800

    Update merges_controller_test.rb

--- test/controllers/names/descriptions/merges_controller_test.rb
+++ test/controllers/names/descriptions/merges_controller_test.rb
@@ -15,11 +15,8 @@ module Names::Descriptions
       # The writer and reader groups are mary_only and dick_only
       # Rolf shouldn't be able to merge with it.
       mary_desc = name_descriptions(:peltigera_user_desc)
-      # First test if dick can access the merge form
-      login("dick")
-      get(:new, params: { id: rolf_desc.id })
-      assert_response(:success)
 
+      # login rolf, and try to merge.
       login("rolf")
       get(:new, params: { id: rolf_desc.id })
       assert_response(:success)
@@ -34,6 +31,35 @@ module Names::Descriptions
       # Now try the other way around. Should get redirected to name
       get(:new, params: { id: mary_desc.id })
       assert_redirected_to(name_path(mary_desc.parent_id))
+
+      # Now switch to dick.
+      login("dick")
+      # Mary's desc is restricted. But dick should be able to edit
+      get(:new, params: { id: mary_desc.id })
+      assert_response(:success)
+
+      # Rolf's is public, he should be able to access also
+      get(:new, params: { id: rolf_desc.id })
+      assert_response(:success)
+      params = {
+        id: rolf_desc.id,
+        target: mary_desc.id,
+        delete: 0
+      }
+      post(:create, params: params)
+      assert_response(:success)
+      # dick didn't delete, so the original desc should still be there.
+      assert(rolf_desc.reload)
+
+      # now try with delete. Shouldn't work, there is a conflict
+      params = {
+        id: rolf_desc.id,
+        target: mary_desc.id,
+        delete: 1
+      }
+      post(:create, params: params)
+      assert_flash(/Please merge the two descriptions/)
+      assert(rolf_desc.reload)
     end
 
     def test_merge_descriptions; end
