06f61d8bdc6bd76c71b7041caf1915288e681966
andrew nimmo <andrnimm@fastmail.fm>
2023-01-17 16:53:00 -0800

    Update merges_controller_test.rb

--- test/controllers/names/descriptions/merges_controller_test.rb
+++ test/controllers/names/descriptions/merges_controller_test.rb
@@ -7,28 +7,26 @@ module Names::Descriptions
   class MergesControllerTest < FunctionalTestCase
     include ObjectLinkHelper
 
-    def test_try_merge_descriptions_no_permission
-      # Rolf is the author of an alternative desc.
-      # It's not restricted.
-      rolf_desc = name_descriptions(:peltigera_alt_desc)
-      # Mary wrote this one.
-      # The writer and reader groups are mary_only and dick_only
-      # Rolf shouldn't be able to merge with it.
-      mary_desc = name_descriptions(:peltigera_user_desc)
+    # Rolf is the author of an alternative desc.
+    # It's not restricted.
+    def rolf_desc
+      name_descriptions(:peltigera_alt_desc)
+    end
 
+    # Mary wrote this one.
+    # The writer and reader groups are mary_only and dick_only
+    # Rolf shouldn't be able to merge with it.
+    def mary_desc
+      name_descriptions(:peltigera_user_desc)
+    end
+
+    def test_form_permissions
       # login rolf, and try to merge.
       login("rolf")
       get(:new, params: { id: rolf_desc.id })
       assert_response(:success)
-      params = {
-        id: rolf_desc.id,
-        target: mary_desc.id,
-        delete: 0
-      }
-      post(:create, params: params)
-      assert_flash_error(:runtime_edit_description_denied.t)
 
-      # Now try the other way around. Should get redirected to name
+      # Now try to merge from mary's. Should get bounced, redirected to name
       get(:new, params: { id: mary_desc.id })
       assert_redirected_to(name_path(mary_desc.parent_id))
 
@@ -38,20 +36,36 @@ module Names::Descriptions
       get(:new, params: { id: mary_desc.id })
       assert_response(:success)
 
-      # Rolf's is public, he should be able to access also
+      # Rolf's is public, dick should be able to access also
       get(:new, params: { id: rolf_desc.id })
       assert_response(:success)
+    end
+
+    def test_merge_descriptions_no_permission
+      login("rolf")
       params = {
         id: rolf_desc.id,
         target: mary_desc.id,
         delete: 0
       }
       post(:create, params: params)
-      assert_response(:success)
+      assert_flash_error(:runtime_edit_description_denied.t)
+    end
+
+    def test_merge_descriptions_notes_conflict
+      login("dick")
+      params = {
+        id: rolf_desc.id,
+        target: mary_desc.id,
+        delete: 0
+      }
+      post(:create, params: params)
+      # shouldn't work, there is a conflict. requires manual resolution
+      assert_flash(/Please merge the two descriptions/)
       # dick didn't delete, so the original desc should still be there.
       assert(rolf_desc.reload)
 
-      # now try with delete. Shouldn't work, there is a conflict
+      # now try with delete. Also shouldn't work, there is a conflict
       params = {
         id: rolf_desc.id,
         target: mary_desc.id,
@@ -60,11 +74,22 @@ module Names::Descriptions
       post(:create, params: params)
       assert_flash(/Please merge the two descriptions/)
       assert(rolf_desc.reload)
+
+      # Blank the first gen_desc to avoid conflict. Merge should work
+      rolf_desc.update(gen_desc: nil)
+      rolf_desc.reload
+      assert_nil(rolf_desc.gen_desc)
+
+      params = {
+        id: rolf_desc.id,
+        target: mary_desc.id,
+        delete: 0
+      }
+      post(:create, params: params)
+      assert_flash(/Successfully merged the descriptions/)
     end
 
     def test_merge_descriptions; end
-
-    def test_merge_descriptions_notes_conflict; end
 
     def test_merge_incompatible_descriptions; end
   end
