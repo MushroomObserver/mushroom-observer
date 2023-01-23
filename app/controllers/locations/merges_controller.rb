5dad1a42d66e6d46ab2a8bc694e67d485d016d3c
andrew nimmo <andrnimm@fastmail.fm>
2023-01-22 20:42:57 -0800

    Initial commit. No message for "no results" yet

--- app/controllers/locations/merges_controller.rb
+++ app/controllers/locations/merges_controller.rb
@@ -17,34 +17,9 @@ module Locations
     def new
       store_location
       @where = Location.user_name(@user, params[:where].to_s)
-
-      # Split list of all locations into "matches" and "non-matches".  Try
-      # matches in the following order:
-      #   1) all that start with full "where" string
-      #   2) all that start with everything in "where" up to the comma
-      #   3) all that start with the first word in "where"
-      #   4) there just aren't any matches, give up
-      all = Location.all.order("name")
-      @matches, @others = (
-        split_out_matches(all, @where) ||
-        split_out_matches(all, @where.split(",").first) ||
-        split_out_matches(all, @where.split.first) ||
-        [nil, all]
-      )
-    end
-
-    private
-
-    # Split up +list+ into those that start with +substring+ and those that
-    # don't.  If none match, then return nil.
-    def split_out_matches(list, substring)
-      matches = list.select do |loc|
-        (loc.name.to_s[0, substring.length] == substring)
-      end
-      if matches.empty?
-        nil
-      else
-        [matches, list - matches]
+      @matches = Location.name_includes(@where)
+      @where.split(",").each do |part|
+        @matches = @matches.or(Location.name_includes(part))
       end
     end
   end
