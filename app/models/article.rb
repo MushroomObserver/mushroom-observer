bb0b3c20718c6948d4678ca1e92863e3c31c6174
Joe Cohen <jdcohenesq@gmail.com>
2023-02-07 17:39:32 +0000

    Define Article model default order

--- app/models/article.rb
+++ app/models/article.rb
@@ -78,4 +78,8 @@ class Article < AbstractModel
   def self.news_articles_project
     Project.find_by(title: "News Articles")
   end
+
+  def self.default_order
+    Query::ArticleBase.default_order
+  end
 end
