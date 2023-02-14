7c7047c0f89931e21c4a0f2ce111c6f63eba1baa
Joe Cohen <jdcohenesq@gmail.com>
2023-02-14 23:26:48 +0000

    Refactor user_stats_links to fix Abc offense

--- app/helpers/user_stats_helper.rb
+++ app/helpers/user_stats_helper.rb
@@ -1,39 +1,43 @@
 # frozen_string_literal: true
 
-# View Helpers for GlossaryTerms
+# Helpers for user view
 module UserStatsHelper
-  def user_stats_links(show_user)
-    # NOTE: Second arg is controller name - plural for normalized.
-    # Normalized controller links last arg must be hash of index params.
+  def user_stats_links(user)
     links = {}
-    [
-      [:comments, "/comments", :index, { by_user: show_user.id }],
-      [:comments_for, "/comments", :index, { for_user: show_user.id }],
-      [:images, "/images", :index, { by_user: show_user.id }],
-      [:location_description_authors, "/locations/descriptions",
-       :index, { by_author: show_user.id }],
-      [:location_description_editors, "/locations/descriptions",
-       :index, { by_editor: show_user.id }],
-      [:locations, "/locations", :index, { by_user: show_user.id }],
-      [:locations_versions, "/locations", :index, { by_editor: show_user.id }],
-      [:name_description_authors, "/names/descriptions",
-       :index, { by_author: show_user.id }],
-      [:name_description_editors, "/names/descriptions",
-       :index, { by_editor: show_user.id }],
-      [:names, "/names", :index, { by_user: show_user.id }],
-      [:names_versions, "/names", :index, { by_editor: show_user.id }],
-      [:observations, "/observations", :index, { user: show_user.id }],
-      [:species_lists, "/species_lists", :index, { by_user: show_user.id }],
-      [:life_list, "/checklists", :show, {}]
-    ].each do |key, controller, action, params|
+    user_stats_links_table(user).each do |key, controller, action, params|
       unless [key].intersect?([:location_description_authors,
                                :name_description_authors])
-        params[:id] = show_user.id
+        params[:id] = user.id
       end
 
       links[key] = url_for(controller: controller, action: action,
                            params: params)
     end
     links
+  end
+
+  # NOTE: Second arg is controller name
+  # Last arg is a hash of index params.
+  def user_stats_links_table(user)
+    [
+      [:comments, "/comments", :index, { by_user: user.id }],
+      [:comments_for, "/comments", :index, { for_user: user.id }],
+      [:images, "/images", :index, { by_user: user.id }],
+      [:location_description_authors, "/locations/descriptions",
+       :index, { by_author: user.id }],
+      [:location_description_editors, "/locations/descriptions",
+       :index, { by_editor: user.id }],
+      [:locations, "/locations", :index, { by_user: user.id }],
+      [:locations_versions, "/locations", :index, { by_editor: user.id }],
+      [:name_description_authors, "/names/descriptions",
+       :index, { by_author: user.id }],
+      [:name_description_editors, "/names/descriptions",
+       :index, { by_editor: user.id }],
+      [:names, "/names", :index, { by_user: user.id }],
+      [:names_versions, "/names", :index, { by_editor: user.id }],
+      [:observations, "/observations", :index, { user: user.id }],
+      [:species_lists, "/species_lists", :index, { by_user: user.id }],
+      [:life_list, "/checklists", :show, {}]
+    ]
   end
 end
