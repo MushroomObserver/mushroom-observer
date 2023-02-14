834f6ee9a38381d4f8318aa9a92345a961d76bed
andrew nimmo <andrnimm@fastmail.fm>
2023-02-13 16:52:11 -0800

    Update naming_vote_ajax.js
    Radically simplify. We don't have to handle changing the other selects on submit, because we're getting a new naming table.

--- app/assets/javascripts/naming_vote_ajax.js
+++ app/assets/javascripts/naming_vote_ajax.js
@@ -1,41 +1,22 @@
+// Voting can be slow, so this script disables other form elements
+// while waiting for an AJAX response.
+
 function VoteByAjaxModule(translations) {
   $(document).ready(function () {
-
-    var _haveVotesChanged = false;
-    var _haveVotesBeenSaved = false;
 
     var save_vote_buttons = function () {
       return $("[data-role='save_vote']");
     };
 
+    // Don't need these if AJAX available.
+    save_vote_buttons().hide();
+
     var change_vote_selects = function () {
       return $("[data-role='change_vote']");
     };
 
-    var naming_vote_forms = function () {
-      return $(".naming-vote-form");
-    };
-
     var attach_bindings = function () {
       change_vote_selects().on("change", function (event) {
-        var _this = $(this);
-        var value = _this.val();
-        var naming_id = _this.data("id");
-        _haveVotesChanged = true;
-
-        // If setting vote to 3.0, go through all the rest and downgrade any
-        // old 3.0's to 2.0.  Only one 3.0 vote is allowed. Also disable all
-        // the menus while the AJAX request is pending.
-        if (value == "3.0") {
-          change_vote_selects().each(function () {
-            var _this2 = $(this);
-            if (_this2.data("id") != naming_id && _this2.val() == "3.0") {
-              _this2.val("2.0");
-            }
-            _this2.attr("disabled", "disabled");
-          });
-        }
-
         // bootstrap modal printed in layout already, just activate it
         $('#naming_ajax_progress_caption').empty().append(
           $("<span>").text(translations.show_namings_saving + "... "),
@@ -48,24 +29,8 @@ function VoteByAjaxModule(translations) {
         var nativeFormEl = $(this).parent()[0];
         Rails.fire(nativeFormEl, 'submit');
       });
-
-      // Save initial value in case of error, when we'll need to revert.
-      change_vote_selects().each(function (event) {
-        var _this = $(this);
-        _this.data("old_value", _this.val());
-        _this.attr("disabled", null);
-      });
     };
 
-    // Alert the user if they haven't saved data.
-    window.onbeforeunload = function () {
-      if (_haveVotesChanged && !_haveVotesBeenSaved)
-        return translations.show_namings_lose_changes;
-    }
-
     attach_bindings();
-
-    // Don't need this if AJAX available.
-    save_vote_buttons().hide();
   });
 }
