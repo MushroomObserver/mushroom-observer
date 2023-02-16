203d052eeb614b46cafe88ae58d2077a6e240d0c
andrew nimmo <andrnimm@fastmail.fm>
2023-02-16 15:22:08 -0800

    Move include of modal_ajax_progress to the application layout

--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -190,8 +190,7 @@ module ObservationsHelper
     link_with_query(h(percent),
                     naming_vote_path(naming_id: naming.id),
                     { class: "vote-percent btn btn-link px-0",
-                      remote: true,
-                      onclick: "alert(this);" })
+                      remote: true })
   end
 
   def num_votes_html(naming)
