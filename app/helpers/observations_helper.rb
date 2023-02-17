fa4cb4f29ed2668e7622e0b349c2cc4bc85becbb
andrew nimmo <andrnimm@fastmail.fm>
2023-02-16 17:49:09 -0800

    Add whirly to edit buton, Hide modal on re-form-binding

--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -141,7 +141,7 @@ module ObservationsHelper
     if check_permission(naming)
       edit_link = link_with_query(:EDIT.t, edit_naming_path(id: naming.id),
                                   class: "edit_naming_link_#{naming.id}",
-                                  remote: true)
+                                  remote: true, onclick: "MOEvents.whirly();")
       delete_link = destroy_button(target: naming)
       proposer_links = [tag.br,
                         "[", edit_link, " | ", delete_link, "]"].safe_join
