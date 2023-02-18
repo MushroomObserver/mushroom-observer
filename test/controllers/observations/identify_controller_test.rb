c4155d639ce891d9e77da861ddb743bf89260e32
andrew nimmo <andrnimm@fastmail.fm>
2023-02-18 00:50:55 -0800

    Update identify_controller_test.rb

--- test/controllers/observations/identify_controller_test.rb
+++ test/controllers/observations/identify_controller_test.rb
@@ -6,12 +6,15 @@ module Observations
   class IdentifyControllerTest < FunctionalTestCase
     def test_identify_observations_index
       login("mary")
+      mary = users(:mary)
       obs = Observation.needs_identification(users(:mary))
+      obs_count = obs.count
+      mary.update(layout_count: obs_count + 1)
 
       get(:index)
-      # binding.break
+      # it's not using her layout count. maybe i hardcoded it.
       assert_no_flash
-      assert_equal(obs.length, obs.length)
+      # assert_select(".matrix-box", obs_count)
       assert_response(:success)
     end
   end
