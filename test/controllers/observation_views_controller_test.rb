e582c3df6b0f6b84ada1e945eff90c4e8a7c5459
andrew nimmo <andrnimm@fastmail.fm>
2023-02-18 13:12:50 -0800

    Create observation_views_controller_test.rb

---
+++ test/controllers/observation_views_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require("test_helper")
+
+# ------------------------------------------------------------
+#  Observation views - test updating the `reviewed` column
+# ------------------------------------------------------------
+class ObservationViewsControllerTest < FunctionalTestCase
+  def test_update; end
+end
