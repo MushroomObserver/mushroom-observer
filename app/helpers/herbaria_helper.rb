8ae3e9d816de1467f33bd46346cc6f561d63d70e
andrew nimmo <andrnimm@fastmail.fm>
2023-02-20 00:36:06 -0800

    Update herbaria_helper.rb

--- app/helpers/herbaria_helper.rb
+++ app/helpers/herbaria_helper.rb
@@ -4,7 +4,6 @@
 module HerbariaHelper
   def herbarium_top_users(herbarium)
     User.joins(:herbarium_records).
-      where(User[:id].eq(HerbariumRecord[:user_id])).
       where(HerbariumRecord[:id].eq(herbarium.id)).
       select(User[:name], User[:login], User[:id].count).
       group(User[:id]).order(User[:id].count(:desc)).take(5)
