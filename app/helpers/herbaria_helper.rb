603c6001fe88280ad4081d41922bf90ac1ac0cf2
andrew nimmo <andrnimm@fastmail.fm>
2023-02-20 10:56:08 -0800

    Change method to take an id, not a record (even though we have the record)

--- app/helpers/herbaria_helper.rb
+++ app/helpers/herbaria_helper.rb
@@ -2,9 +2,9 @@
 
 # helper methods for Herbaria views
 module HerbariaHelper
-  def herbarium_top_users(herbarium)
+  def herbarium_top_users(herbarium_id)
     User.joins(:herbarium_records).
-      where(HerbariumRecord[:herbarium_id].eq(herbarium.id)).
+      where(HerbariumRecord[:herbarium_id].eq(herbarium_id)).
       select(User[:name], User[:login], User[:id].count).
       group(User[:id]).order(User[:id].count.desc).take(5)
   end
