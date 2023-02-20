c8d3a391ca32c09928e7304864f618d314d2dafa
andrew nimmo <andrnimm@fastmail.fm>
2023-02-20 10:27:19 -0800

    Get the right column to start with!

--- app/helpers/herbaria_helper.rb
+++ app/helpers/herbaria_helper.rb
@@ -4,7 +4,7 @@
 module HerbariaHelper
   def herbarium_top_users(herbarium)
     User.joins(:herbarium_records).
-      where(HerbariumRecord[:id].eq(herbarium.id)).
+      where(HerbariumRecord[:herbarium_id].eq(herbarium.id)).
       select(User[:name], User[:login], User[:id].count).
       group(User[:id]).order(User[:id].count.desc).take(5)
   end
