18adc0210fea3a555915cda6e2e85cfda7aa4240
andrew nimmo <andrnimm@fastmail.fm>
2023-02-20 00:26:26 -0800

    Update herbaria_helper.rb
    Convert SQL to AR

--- app/helpers/herbaria_helper.rb
+++ app/helpers/herbaria_helper.rb
@@ -3,11 +3,10 @@
 # helper methods for Herbaria views
 module HerbariaHelper
   def herbarium_top_users(herbarium)
-    Herbarium.connection.select_rows(%(
-      SELECT u.name, u.login, COUNT(u.id)
-      FROM herbarium_records hr JOIN users u ON u.id = hr.user_id
-      WHERE hr.herbarium_id = #{herbarium.id}
-      GROUP BY u.id ORDER BY COUNT(u.id) DESC LIMIT 5
-    ))
+    User.joins(:herbarium_records).
+      where(User[:id].eq(HerbariumRecord[:user_id])).
+      where(HerbariumRecord[:id].eq(herbarium.id)).
+      select(User[:name], User[:login], User[:id].count).
+      group(User[:id]).order(User[:id].count(:desc)).take(5)
   end
 end
