ac4de39b640df7acc23964cd0c5647134dc2bb2f
andrew nimmo <andrnimm@fastmail.fm>
2023-02-20 18:52:43 -0800

    Add Bullet's includes
    Whoa!

--- app/controllers/observations/identify_controller.rb
+++ app/controllers/observations/identify_controller.rb
@@ -5,13 +5,19 @@ module Observations
     before_action :login_required
     before_action :disable_link_prefetching
     before_action :pass_query_params
+    # around_action :skip_bullet, if: -> { defined?(Bullet) }, only: [:index]
 
     def index
       @layout = calc_layout_params
       old_query = find_or_create_query(:Observation)
       new_query = Query.lookup_and_save(old_query.model, old_query.flavor,
                                         old_query.params.merge(needs_id: true))
-      show_index_of_objects(new_query, { matrix: true })
+      show_index_of_objects(new_query,
+                            { matrix: true,
+                              include: [:name, :location, :user, :namings,
+                                        :rss_log, { name: :synonym },
+                                        { namings: :name },
+                                        { thumb_image: :image_votes }] })
     end
   end
 end
