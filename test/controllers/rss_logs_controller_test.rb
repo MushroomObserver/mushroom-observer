bc755692f47c28f7ba93c61a0fa9fa6de380c65a
Joe Cohen <jdcohenesq@gmail.com>
2023-02-22 18:23:15 +0000

    fix Codeclimate issue

--- test/controllers/rss_logs_controller_test.rb
+++ test/controllers/rss_logs_controller_test.rb
@@ -63,7 +63,8 @@ class RssLogsControllerTest < FunctionalTestCase
     comments_for_path = comments_path(for_user: User.current_id)
     assert_select(
       "a#nav_mobile_your_comments_link[href='#{comments_for_path}']",
-      true, "LH NavBar 'Commments for` link broken")
+      true, "LH NavBar 'Commments for` link broken"
+    )
   end
 
   def test_user_default_rss_log
