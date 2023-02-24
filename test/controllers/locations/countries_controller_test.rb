2c520ab3573e40b8dc553cd4503a0c61c7eb1f88
Joe Cohen <jdcohenesq@gmail.com>
2023-02-24 18:48:08 +0000

    Revise CountriesControllerTest#test_list_countries
    - Uses `assert_select` instead of `assert_template`.
    See https://github.com/rails/rails/issues/18950#issuecomment-77924771
    - Adds assertion exposing bug on Countries and Other Localities page
    - Removes unneeded`include`

--- test/controllers/locations/countries_controller_test.rb
+++ test/controllers/locations/countries_controller_test.rb
@@ -5,12 +5,23 @@ require("set")
 
 module Locations
   class CountriesControllerTest < FunctionalTestCase
-    include ObjectLinkHelper
+    # include ObjectLinkHelper
 
     def test_list_countries
+      cc = CountryCounter.new
+      links_to_countries_with_obss = cc.known_by_count.length
+      links_to_other_localities_with_obss = cc.unknown_by_count.length
+
       login
       get(:index)
-      assert_template("index")
+
+      assert_select("#title", text: :list_countries_title.l)
+      assert_select(
+        "a:match('href', ?)", %r{^/locations\?country=\S+},
+        { count: links_to_countries_with_obss +
+                 links_to_other_localities_with_obss },
+        "Wrong number of links to countries and other localities"
+      )
     end
   end
 end
