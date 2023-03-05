7be3a0189ebf19c3571320a67b1a8d37fa9a9dfa
andrew nimmo <andrnimm@fastmail.fm>
2023-03-04 20:56:51 -0800

    start

---
+++ app/controllers/images/exif_controller.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# Images::ExifController
+module Images
+  class ExifController < ApplicationController
+    require "English"
+    require("open3")
+
+    before_action :login_required
+
+    GPS_TAGS = /latitude|longitude|gps/i
+
+    # Linked from lightbox and show_image
+    # Get EXIF header of image, return as HTML table.
+    def show
+      image = Image.find(@id)
+      hide_gps = image.observations.any?(&:gps_hidden)
+      if image.transferred
+        cmd = Shellwords.escape("script/exiftool_remote")
+        url = Shellwords.escape(image.original_url)
+        result, @status = Open3.capture2e(cmd, url)
+      else
+        cmd  = Shellwords.escape("exiftool")
+        file = Shellwords.escape(image.local_file_name("orig"))
+        result, @status = Open3.capture2e(cmd, file)
+      end
+
+      @data = @status.success? ? parse_exif_data(result, hide_gps) : nil
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
+
+    def test_parse_exif_data(result, hide_gps)
+      parse_exif_data(result, hide_gps)
+    end
+
+    private
+
+    def render_exif_data(result, hide_gps)
+      render(partial: "images/exif_data", locals: { data: @data })
+    end
+
+    def parse_exif_data(result, hide_gps)
+      result.fix_utf8.split("\n").
+        map { |line| line.split(/\s*:\s+/, 2) }.
+        select { |_key, val| val != "" && val != "n/a" }.
+        reject { |key, _val| hide_gps && key.match(GPS_TAGS) }
+    end
+
+  end
+end
