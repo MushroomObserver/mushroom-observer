be356bc2138795eebb2d51e471b055b8fa866d37
andrew nimmo <andrnimm@fastmail.fm>
2023-03-09 01:29:38 -0800

    Refactor synonyms_controller#change_synonyms

--- app/controllers/names/synonyms_controller.rb
+++ app/controllers/names/synonyms_controller.rb
@@ -30,7 +30,7 @@ module Names
       return if abort_if_name_locked!(@name)
 
       init_ivars_for_edit
-      change_synonyms
+      change_synonyms(prepare_name_sorter)
     end
 
     private
@@ -48,7 +48,7 @@ module Names
       @deprecate_all     = true
     end
 
-    def change_synonyms
+    def prepare_name_sorter
       list = params[:synonym_members].strip_squeeze
       @deprecate_all = (params[:deprecate_all] == "1")
 
@@ -59,9 +59,12 @@ module Names
       sorter = NameSorter.new
       sorter.sort_names(list)
       sorter.append_approved_synonyms(params[:approved_synonyms])
+      sorter
+    end
 
+    def change_synonyms(sorter)
       # Are any names unrecognized (only unapproved names will still be
-      # unrecognized at this point) or ambiguous?
+      # unrecognized at this point) or ambiguous? If so, dump to logger
       if !sorter.only_single_names
         dump_sorter(sorter)
       # Has the user NOT had a chance to choose from among the synonyms of any
@@ -69,45 +72,51 @@ module Names
       elsif !sorter.only_approved_synonyms
         flash_notice(:name_change_synonyms_confirm.t)
       else
-        # Go through list of all synonyms for this name and written-in names.
-        # Exclude any names that have un-checked check-boxes: newly written-in
-        # names will not have a check-box yet, names written-in in previous
-        # attempt to submit this form will have checkboxes and therefore must
-        # be checked to proceed -- the default initial state.
-        proposed_synonyms = params[:proposed_synonyms] || {}
-        sorter.all_synonyms.each do |n|
-          # It is possible these names may be changed by transfer_synonym,
-          # but these *instances* will not reflect those changes, so reload.
-          if proposed_synonyms[n.id.to_s] != "0"
-            @name.transfer_synonym(n.reload)
-          end
-        end
-
-        # De-synonymize any old synonyms in the "existing synonyms" list that
-        # have been unchecked.  This creates a new synonym to connect them if
-        # there are multiple unchecked names -- that is, it splits this
-        # synonym into two synonyms, with checked names staying in this one,
-        # and unchecked names moving to the new one.
-        split_off_desynonymized_names(@name, params[:existing_synonyms] || {})
-
-        # Deprecate everything if that check-box has been marked.
-        success = true
-        if @deprecate_all
-          sorter.all_names.each do |n|
-            unless deprecate_synonym(n)
-              # Already flashed error message.
-              success = false
-            end
-          end
-        end
-
+        success = deprecate_other_names(sorter)
         return redirect_to(name_path(@name.id, q: get_query_param)) if success
 
         flash_object_errors(@name)
         flash_object_errors(@name.synonym)
-
       end
 
+      re_render_edit_form(sorter)
+    end
+
+    def deprecate_other_names(sorter)
+      # Go through list of all synonyms for this name and written-in names.
+      # Exclude any names that have un-checked check-boxes: newly written-in
+      # names will not have a check-box yet, names written-in in previous
+      # attempt to submit this form will have checkboxes and therefore must
+      # be checked to proceed -- the default initial state.
+      proposed_synonym_ids = params[:proposed_synonyms] || {}
+      sorter.all_synonyms.each do |n|
+        # It is possible these names may be changed by transfer_synonym,
+        # but these *instances* will not reflect those changes, so reload.
+        if proposed_synonym_ids[n.id.to_s] != "0"
+          @name.transfer_synonym(n.reload)
+        end
+      end
+
+      # De-synonymize any old synonyms in the "existing synonyms" list that
+      # have been unchecked.  This creates a new synonym to connect them if
+      # there are multiple unchecked names -- that is, it splits this
+      # synonym into two synonyms, with checked names staying in this one,
+      # and unchecked names moving to the new one.
+      split_off_desynonymized_names(@name, params[:existing_synonyms] || {})
+
+      # Deprecate everything if that check-box has been marked.
+      success = true
+      if @deprecate_all
+        sorter.all_names.each do |n|
+          # Already flashed error message.
+          success = false unless deprecate_synonym(n)
+        end
+      end
+
+      success
+    end
+
+    def re_render_edit_form(sorter)
       @list_members      = sorter.all_line_strs.join("\r\n")
       @new_names         = sorter.new_name_strs.uniq
       @synonym_name_ids  = sorter.all_synonyms.map(&:id)
