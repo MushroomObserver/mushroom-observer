285fc315f6ec7c191c0fa4898e497827a49d2fb1
andrew nimmo <andrnimm@fastmail.fm>
2023-03-28 22:21:44 -0700

    Update identify_controller.rb

--- app/controllers/observations/identify_controller.rb
+++ app/controllers/observations/identify_controller.rb
@@ -5,7 +5,6 @@ module Observations
     before_action :login_required
     before_action :disable_link_prefetching
     before_action :pass_query_params
-    # around_action :skip_bullet, if: -> { defined?(Bullet) }, only: [:index]
 
     def index
       @layout = calc_layout_params
@@ -54,10 +53,6 @@ module Observations
     def clade_filter(term)
       # return unless (clade = Name.find_by(text_name: term))
 
-      # Nimmo note: 2023-03-14 to be researched: This obviously sends, and
-      # `Query::ObservationAll` expects, a full `Name` instance, but somehow
-      # the flavor parsing method `needs_id` only receives the name ID.
-      # Changed: just passing the text_name
       query = create_query(:Observation, :needs_id, { in_clade: term })
 
       show_selected_results(query)
@@ -68,13 +63,6 @@ module Observations
 
       show_selected_results(query)
     end
-
-    # def full_index_or_inherited_query
-    #   old_query = find_or_create_query(:Observation)
-    #   new_query = Query.lookup_and_save(old_query.model, old_query.flavor,
-    #                                     old_query.params.merge(needs_id: true))
-    #   show_selected_results(new_query)
-    # end
 
     # TODO: Allow show_index_of_objects to `render` rather than `redirect`,
     # or better yet `respond_to do |format|` and write index.js.erb templates
