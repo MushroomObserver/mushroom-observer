88bb12be5cc71f71c19f40bfdea4ba62ca9d7694
andrew nimmo <andrnimm@fastmail.fm>
2023-04-09 10:37:11 -0700

    Write matrix_box helper

--- app/helpers/matrix_box_helper.rb
+++ app/helpers/matrix_box_helper.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 module MatrixBoxHelper
+  def matrix_box(**args, &block)
+    columns = args[:columns] || "col-xs-12 col-sm-6 col-md-4 col-lg-3"
+    extra_classes = args[:class] || ""
+    box_id = args[:id] ? "box_#{args[:id]}" : ""
+    div_class = "matrix-box #{columns} #{extra_classes}"
+    div_args = args.except(:columns, :class, :id)
+
+    content_tag(:li, class: div_class, id: box_id, **div_args) do
+      capture(&block)
+    end
+  end
+
   # Obs with uncertain name: vote on naming, or propose (if it's "Fungi")
   # used if matrix_box local_assigns identify == true
   def matrix_box_vote_or_propose_ui(identify, object)
