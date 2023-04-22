a37a4534d214e0c72ddb220f21788b3ce5233c66
Joe Cohen <jdcohenesq@gmail.com>
2023-04-22 18:05:26 +0000

    Refactor initialize_order_specs to reduce ABC size
    A "shameless green" refactor that simply:
    - Applies ChatGPT's instructions (see below), and
    - Fixes resulting test Errors/Failures
    
    To reduce the ABC (Assignment, Branching, and Complexity) count of this method, you can start by refactoring the large `case` statement into smaller methods or classes, each responsible for handling a specific sorting criteria. This will make the code easier to read and understand, and also make it easier to test and maintain.
    
    Here are some steps you can take to refactor this method:
    
    1. Extract each sorting criteria into a separate method or class, with a descriptive name that reflects the sorting criteria. For example, you could create a `sort_by_updated_at` method, a `sort_by_date` method, a `sort_by_name` method, and so on.
    
    2. Each method or class should take the `model` and `table` as parameters, and return the appropriate order specification as a string.
    
    3. Replace the `case` statement with a hash table that maps the sorting criteria to the appropriate method or class. For example:
    
    ```ruby
       SORT_METHODS = {
         "updated_at" => :sort_by_updated_at,
         "date" => :sort_by_date,
         "name" => :sort_by_name,
         # Add more sorting criteria here
       }
    
       def initialize_order_specs(by)
         table = model.table_name
         method_name = SORT_METHODS[by]
         if method_name
           send(method_name, model, table)
         else
           raise("Can't figure out how to sort #{model} by :#{by}.")
         end
       end
    ```
    
    4. Define each sorting method as a separate method or class, and implement the logic for each sorting criteria in that method or class. For example:
    
    ```ruby
       def sort_by_updated_at(model, table)
         "#{table}.updated_at DESC" if model.column_names.include?("updated_at")
       end
    
       def sort_by_date(model, table)
         if model.column_names.include?("date")
           "#{table}.date DESC"
         elsif model.column_names.include?("when")
           "#{table}.when DESC"
         elsif model.column_names.include?("created_at")
           "#{table}.created_at DESC"
         end
       end
    
       def sort_by_name(model, table)
         case model
         when Image
           add_join(:observation_images, :observations)
           add_join(:observations, :names)
           self.group = "images.id"
           "MIN(names.sort_name) ASC, images.when DESC"
         when Location
           if User.current_location_format == "scientific"
             "locations.scientific_name ASC"
           else
             "locations.name ASC"
           end
         # Add more cases for other models here
         else
           if model.column_names.include?("sort_name")
             "#{table}.sort_name ASC"
           elsif model.column_names.include?("name")
             "#{table}.name ASC"
           elsif model.column_names.include?("title")
             "#{table}.title ASC"
           end
         end
       end
    ```
    
    5. Continue refactoring the sorting criteria until the method has a lower ABC count and is easier to read and understand.

--- app/classes/query/modules/ordering.rb
+++ app/classes/query/modules/ordering.rb
@@ -15,157 +15,253 @@ module Query::Modules::Ordering
     self.order = reverse ? reverse_order(result) : result
   end
 
+  SORT_METHODS = {
+    "updated_at" => :sort_by_updated_at,
+    "created_at" => :sort_by_created_at,
+    "last_login" => :sort_by_last_login,
+    "num_views" => :sort_by_num_views,
+    "date" => :sort_by_date,
+    "name" => :sort_by_name,
+    "title" => :sort_by_title,
+    "login" => :sort_by_title,
+    "summary" => :sort_by_summary,
+    "copyright_holder" => :sort_by_copyright_holder,
+    "where" => :sort_by_where,
+    "initial_det" => :sort_by_initial_det,
+    "accession_number" => :sort_by_accession_number,
+    "user" => :sort_by_user,
+    "location" => :sort_by_location,
+    "rss_log" => :sort_by_rss_log,
+    "confidence" => :sort_by_confidence,
+    "image_quality" => :sort_by_image_quality,
+    "thumbnail_quality" => :sort_by_thumbnail_quality,
+    "owners_quality" => :sort_by_owners_quality,
+    "owners_thumbnail_quality" => :sort_by_owners_thumbnail_quality,
+    "observation" => :sort_by_observation,
+    "contribution" => :sort_by_contribution,
+    "original_name" => :sort_by_original_name,
+    "url" => :sort_by_url,
+    "herbarium_name" => :sort_by_herbarium_name,
+    "herbarium_label" => :sort_by_herbarium_label,
+    "name_and_number" => :sort_by_name_and_number,
+    "code" => :sort_by_code,
+    "code_then_name" => :sort_by_code_then_name,
+    "records" => :sort_by_records,
+    "id" => :sort_by_id,
+    # Add more sorting criteria here
+  }
+
   def initialize_order_specs(by)
-    table = model.table_name
-    columns = model.column_names
-    case by
-
-    when "updated_at", "created_at", "last_login", "num_views"
-      "#{table}.#{by} DESC" if columns.include?(by)
-
-    when "date"
-      if columns.include?("date")
-        "#{table}.date DESC"
-      elsif columns.include?("when")
-        "#{table}.when DESC"
-      elsif columns.include?("created_at")
-        "#{table}.created_at DESC"
-      end
-
-    when "name"
-      if model == Image
-        add_join(:observation_images, :observations)
-        add_join(:observations, :names)
-        self.group = "images.id"
-        "MIN(names.sort_name) ASC, images.when DESC"
-      elsif model == Location
-        if User.current_location_format == "scientific"
-          "locations.scientific_name ASC"
-        else
-          "locations.name ASC"
-        end
-      elsif model == LocationDescription
-        add_join(:locations)
-        "locations.name ASC, location_descriptions.created_at ASC"
-      elsif model == Name
-        "names.sort_name ASC"
-      elsif model == NameDescription
-        add_join(:names)
-        "names.sort_name ASC, name_descriptions.created_at ASC"
-      elsif model == Observation
-        add_join(:names)
-        "names.sort_name ASC, observations.when DESC"
-      elsif columns.include?("sort_name")
-        "#{table}.sort_name ASC"
-      elsif columns.include?("name")
-        "#{table}.name ASC"
-      elsif columns.include?("title")
-        "#{table}.title ASC"
-      end
-
-    when "title", "login", "summary", "copyright_holder", "where",
-         "initial_det", "accession_number"
-      "#{table}.#{by} ASC" if columns.include?(by)
-
-    when "user"
-      if columns.include?("user_id") || model == Herbarium
-        add_join(:users)
-        'IF(users.name = "" OR users.name IS NULL, users.login, users.name) ASC'
-      end
-
-    when "location"
-      if columns.include?("location_id")
-        # Join Users with null locations, else join records with locations
-        model == User ? add_join(:locations!) : add_join(:locations)
-        if User.current_location_format == "scientific"
-          "locations.scientific_name ASC"
-        else
-          "locations.name ASC"
-        end
-      end
-
-    when "rss_log"
-      if columns.include?("rss_log_id")
-        add_join(:rss_logs)
-        "rss_logs.updated_at DESC"
-      end
-
-    when "confidence"
-      if model == Image
-        add_join(:observation_images, :observations)
-        "observations.vote_cache DESC"
-      elsif model == Observation
-        "observations.vote_cache DESC"
-      end
-
-    when "image_quality"
-      "images.vote_cache DESC" if model == Image
-
-    when "thumbnail_quality"
-      if model == Observation
-        add_join(:"images.thumb_image")
-        "images.vote_cache DESC, observations.vote_cache DESC"
-      end
-
-    when "owners_quality"
-      if model == Image
-        add_join(:image_votes)
-        where << "image_votes.user_id = images.user_id"
-        "image_votes.value DESC"
-      end
-
-    when "owners_thumbnail_quality"
-      if model == Observation
-        add_join(:"images.thumb_image", :image_votes)
-        where << "images.user_id = observations.user_id"
-        where << "image_votes.user_id = observations.user_id"
-        "image_votes.value DESC, " \
-        "images.vote_cache DESC, " \
-        "observations.vote_cache DESC"
-      end
-
-    when "observation"
-      "observation_id DESC" if columns.include?("observation_id")
-
-    when "contribution"
-      "users.contribution DESC" if model == User
-
-    when "original_name"
-      "images.original_name ASC" if model == Image
-
-    when "url"
-      "external_links.url ASC" if model == ExternalLink
-
-    when "herbarium_name"
-      add_join(:herbaria)
-      "herbaria.name ASC"
-
-    when "herbarium_label"
-      "herbarium_records.initial_det ASC, " \
-      "herbarium_records.accession_number ASC"
-
-    when "name_and_number"
-      "collection_numbers.name ASC, collection_numbers.number ASC"
-
-    when "code"
-      where << "herbaria.code != ''"
-      "herbaria.code ASC"
-
-    when "code_then_name"
-      "IF(herbaria.code = '', '~', herbaria.code) ASC, herbaria.name ASC"
-
-    when "records"
-      # outer_join needed to show herbaria with no records
-      add_join(:herbarium_records!)
-      self.group = "herbaria.id"
-      "count(herbarium_records.id) DESC"
-
-    when "id" # (for testing)
-      "#{table}.id ASC"
-
+    method_name = SORT_METHODS[by]
+    if method_name
+      send(method_name, model)
     else
       raise("Can't figure out how to sort #{model} by :#{by}.")
     end
+  end
+
+  def sort_by_updated_at(model)
+    "#{model.table_name}.updated_at DESC" if model.column_names.include?("updated_at")
+  end
+
+  def sort_by_created_at(model)
+    "#{model.table_name}.created_at DESC" if model.column_names.include?("created_at")
+  end
+
+  def sort_by_last_login(model)
+    "#{model.table_name}.last_login DESC" if model.column_names.include?("last_login")
+  end
+
+  def sort_by_num_views(model)
+    "#{model.table_name}.num_views DESC" if model.column_names.include?("num_views")
+  end
+
+  def sort_by_date(model)
+    if model.column_names.include?("date")
+      "#{model.table_name}.date DESC"
+    elsif model.column_names.include?("when")
+      "#{model.table_name}.when DESC"
+    elsif model.column_names.include?("created_at")
+      "#{model.table_name}.created_at DESC"
+    end
+  end
+
+  def sort_by_name(model)
+    case model.name
+    when Image.name
+      add_join(:observation_images, :observations)
+      add_join(:observations, :names)
+      self.group = "images.id"
+      "MIN(names.sort_name) ASC, images.when DESC"
+    when Location.name
+      if User.current_location_format == "scientific"
+        "locations.scientific_name ASC"
+      else
+        "locations.name ASC"
+      end
+    when LocationDescription.name
+      add_join(:locations)
+      "locations.name ASC, location_descriptions.created_at ASC"
+    when Name.name
+      "names.sort_name ASC"
+    when NameDescription.name
+      add_join(:names)
+      "names.sort_name ASC, name_descriptions.created_at ASC"
+    when Observation.name
+      add_join(:names)
+      "names.sort_name ASC, observations.when DESC"
+    else
+      if model.column_names.include?("sort_name")
+        "#{model.table_name}.sort_name ASC"
+      elsif model.column_names.include?("name")
+        "#{model.table_name}.name ASC"
+      elsif model.column_names.include?("title")
+        "#{model.table_name}.title ASC"
+      end
+    end
+  end
+
+  def sort_by_title(model)
+    "#{model.table_name}.title ASC" if model.column_names.include?("title")
+  end
+
+  def sort_by_login(model)
+    "#{model.table_name}.login ASC" if model.column_names.include?("login")
+  end
+
+  def sort_by_summary(model)
+    "#{model.table_name}.summary ASC" if model.column_names.include?("summary")
+  end
+
+  def sort_by_copyright_holder(model)
+    "#{model.table_name}.copyright_holder ASC" if model.column_names.include?("copyright_holder")
+  end
+
+  def sort_by_where(model)
+    "#{model.table_name}.where ASC" if model.column_names.include?("where")
+  end
+
+  def sort_by_initial_det(model)
+    "#{model.table_name}.initial_det ASC" if model.column_names.include?("initial_det")
+  end
+
+  def sort_by_accession_number(model)
+    "#(model.table_name}.accession_number ASC" if model.column_names.include?("accession_number")
+  end
+
+  def sort_by_user(model)
+    if model.column_names.include?("user_id") || model == Herbarium
+      add_join(:users)
+      'IF(users.name = "" OR users.name IS NULL, users.login, users.name) ASC'
+    end
+  end
+
+  def sort_by_location(model)
+    if model.column_names.include?("location_id")
+      # Join Users with null locations, else join records with locations
+      model == User ? add_join(:locations!) : add_join(:locations)
+      if User.current_location_format == "scientific"
+        "locations.scientific_name ASC"
+      else
+        "locations.name ASC"
+      end
+    end
+  end
+
+  def sort_by_rss_log(model)
+    if model.column_names.include?("rss_log_id")
+      add_join(:rss_logs)
+      "rss_logs.updated_at DESC"
+    end
+  end
+
+  def sort_by_confidence(model)
+    if model == Image
+      add_join(:observation_images, :observations)
+      "observations.vote_cache DESC"
+    elsif model == Observation
+      "observations.vote_cache DESC"
+    end
+  end
+
+  def sort_by_image_quality(model)
+    "images.vote_cache DESC" if model == Image
+  end
+
+  def sort_by_thumbnail_quality(model)
+    if model == Observation
+      add_join(:"images.thumb_image")
+      "images.vote_cache DESC, observations.vote_cache DESC"
+    end
+  end
+
+  def sort_by_owners_quality(model)
+    if model == Image
+      add_join(:image_votes)
+      where << "image_votes.user_id = images.user_id"
+      "image_votes.value DESC"
+    end
+  end
+
+  def sort_by_owners_thumbnail_quality(model)
+    if model == Observation
+      add_join(:"images.thumb_image", :image_votes)
+      where << "images.user_id = observations.user_id"
+      where << "image_votes.user_id = observations.user_id"
+      "image_votes.value DESC, " \
+      "images.vote_cache DESC, " \
+      "observations.vote_cache DESC"
+    end
+  end
+
+  def sort_by_observation(model)
+    "observation_id DESC" if model.column_names.include?("observation_id")
+  end
+
+  def sort_by_contribution(model)
+    "users.contribution DESC" if model == User
+  end
+
+  def sort_by_original_name(model)
+    "images.original_name ASC" if model == Image
+  end
+
+  def sort_by_url(model)
+    "external_links.url ASC" if model == ExternalLink
+  end
+
+  def sort_by_herbarium_name(_model)
+    add_join(:herbaria)
+    "herbaria.name ASC"
+  end
+
+  def sort_by_herbarium_label(_model)
+    "herbarium_records.initial_det ASC, " \
+    "herbarium_records.accession_number ASC"
+  end
+
+  def sort_by_name_and_number(_model)
+    "collection_numbers.name ASC, collection_numbers.number ASC"
+  end
+
+  def sort_by_code(_model)
+    where << "herbaria.code != ''"
+    "herbaria.code ASC"
+  end
+
+  def sort_by_code_then_name(_model)
+    "IF(herbaria.code = '', '~', herbaria.code) ASC, herbaria.name ASC"
+  end
+
+  def sort_by_records(_model)
+    # outer_join needed to show herbaria with no records
+    add_join(:herbarium_records!)
+    self.group = "herbaria.id"
+    "count(herbarium_records.id) DESC"
+  end
+
+  def sort_by_id(model) # (for testing)
+    "#{model.table_name}.id ASC"
   end
 
   def reverse_order(order)
