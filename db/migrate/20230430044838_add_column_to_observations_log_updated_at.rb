0e27023be2add090d32e060ccabd0f3218ee3a8c
andrew nimmo <andrnimm@fastmail.fm>
2023-04-30 14:56:21 -0700

    roll back migration while we figure out how to populate null

--- db/migrate/20230430044838_add_column_to_observations_log_updated_at.rb
+++ db/migrate/20230430044838_add_column_to_observations_log_updated_at.rb
@@ -3,9 +3,9 @@ class AddColumnToObservationsLogUpdatedAt < ActiveRecord::Migration[6.1]
     add_column(:observations, :log_updated_at, :datetime)
     Observation.joins(:rss_log).update_all(log_updated_at: RssLog[:updated_at])
     # cleanup the 24113 observations with no rss_log
-    Observation.where(log_updated_at: nil).
-                update_all(log_updated_at: Observation[:updated_at])
-    change_column(:observations, :log_updated_at, :datetime, null: false)
+    # Observation.where(log_updated_at: nil).
+    #             update_all(log_updated_at: Observation[:updated_at])
+    # change_column(:observations, :log_updated_at, :datetime, null: false)
   end
   def down
     remove_column(:observations, :log_updated_at)
