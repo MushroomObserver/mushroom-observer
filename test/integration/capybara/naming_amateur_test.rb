d9ddf96f9d3216bd0eaf9a311d0ca77165e1160b
andrew nimmo <andrnimm@fastmail.fm>
2023-05-01 18:02:19 -0700

    Convert naming test to capybara

--- test/integration/capybara/naming_amateur_test.rb
+++ test/integration/capybara/naming_amateur_test.rb
@@ -9,7 +9,9 @@ class NamingAmateurTest < CapybaraIntegrationTestCase
 
   def test_proposing_names
     namer_session = Capybara::Session.new(:rack_test, Rails.application)
+    voter_session = Capybara::Session.new(:rack_test, Rails.application)
     namer = katrina
+    voter = rolf
 
     obs = observations(:detailed_unknown_obs)
     # (Make sure Katrina doesn't own any comments on this observation yet.)
@@ -17,59 +19,59 @@ class NamingAmateurTest < CapybaraIntegrationTestCase
     # (Make sure the name we are going to suggest doesn't exist yet.)
     text_name = "Xylaria polymorpha"
     assert_nil(Name.find_by(text_name: text_name))
-    orignal_name = obs.name
+    original_name = obs.name
 
+    namer_session.visit("/#{obs.id}")
+    namer_session.assert_selector("body.observations__show")
     login(namer, session: namer_session)
-    namer_session.assert_no_link(class: /edit_naming_link_/)
-    namer_session.assert_no_selector(class: /destroy_naming_link_/)
+    assert_false(namer_session.has_link?(class: /edit_naming/))
+    assert_false(namer_session.has_selector?(class: /destroy_naming_link_/))
+    namer_session.click_link(class: "propose-naming-button")
 
-    click_link(text: /propose.*name/)
     # naming = namer_session.create_name(obs, text_name)
-    assert_selector("namings__new")
+    namer_session.assert_selector("body.namings__new")
     # (Make sure the form is for the correct object!)
-    binding.break
-    assert_selector("form[action*='/comments?target=#{obs.id}']")
-    assert_objs_equal(obs, assigns(:params).observation)
+    namer_session.assert_selector(
+      "form[action*='/observations/#{obs.id}/namings']"
+    )
     # (Make sure there is a tab to go back to observations/show.)
-    assert_select("#right_tabs a[href='/#{obs.id}']")
+    assert_true(namer_session.has_link?(href: "/#{obs.id}"))
 
-    open_form do |form|
-      form.assert_value("naming_name", "")
-      form.assert_value("naming_vote_value", "")
-      form.assert_unchecked("naming_reasons_1_check")
-      form.assert_unchecked("naming_reasons_2_check")
-      form.assert_unchecked("naming_reasons_3_check")
-      form.assert_unchecked("naming_reasons_4_check")
-      form.submit
+    namer_session.within("#naming_form") do |form|
+      assert_true(form.has_field?("naming_name", text: ""))
+      assert_true(form.has_field?("naming_vote_value", text: ""))
+      assert_true(form.has_unchecked_field?("naming_reasons_1_check"))
+      assert_true(form.has_unchecked_field?("naming_reasons_2_check"))
+      assert_true(form.has_unchecked_field?("naming_reasons_3_check"))
+      assert_true(form.has_unchecked_field?("naming_reasons_4_check"))
+      form.first("input[type='submit']").click
     end
-    assert_template("observations/namings/new")
+    namer_session.assert_selector("body.namings__create")
     # (I don't care so long as it says something.)
-    assert_flash_text(/\S/)
+    assert_flash_text(/\S/, session: namer_session)
 
-    open_form do |form|
-      form.change("naming_name", text_name)
-      form.submit
+    namer_session.within("#naming_form") do |form|
+      form.fill_in("naming_name", with: text_name)
+      form.first("input[type='submit']").click
     end
-    assert_template("observations/namings/new")
-    assert_select("div.alert-warning") do |elems|
-      assert(elems.any? do |e|
-               /MO does not recognize the name.*#{text_name}/ =~ e.to_s
-             end,
-             "Expected error about name not existing yet.")
-    end
+    namer_session.assert_selector("body.namings__create")
+    assert_true(namer_session.has_selector?(
+                  ".alert-warning",
+                  text: /MO does not recognize the name.*#{text_name}/
+                ))
 
-    open_form do |form|
-      form.assert_value("naming_name", text_name)
-      form.assert_unchecked("naming_reasons_1_check")
-      form.assert_unchecked("naming_reasons_2_check")
-      form.assert_unchecked("naming_reasons_3_check")
-      form.assert_unchecked("naming_reasons_4_check")
-      form.select(/vote/, /call it that/i)
-      form.submit
+    namer_session.within("#naming_form") do |form|
+      assert_true(form.has_field?("naming_name", with: text_name))
+      assert_true(form.has_unchecked_field?("naming_reasons_1_check"))
+      assert_true(form.has_unchecked_field?("naming_reasons_2_check"))
+      assert_true(form.has_unchecked_field?("naming_reasons_3_check"))
+      assert_true(form.has_unchecked_field?("naming_reasons_4_check"))
+      form.select("I'd Call It That", from: "naming_vote_value")
+      form.first("input[type='submit']").click
     end
-    assert_template("observations/show")
-    assert_flash_text(/success/i)
-    assert_objs_equal(obs, assigns(:observation))
+    namer_session.assert_selector("body.observations__show")
+    assert_flash_success(session: namer_session)
+    assert_true(namer_session.has_text?("Observation #{obs.id}"))
 
     obs.reload
     name = Name.find_by(text_name: text_name)
@@ -79,29 +81,31 @@ class NamingAmateurTest < CapybaraIntegrationTestCase
     assert_equal("", name.author.to_s)
 
     # (Make sure naming shows up somewhere.)
-    assert_match(text_name, response.body)
+    assert_true(namer_session.has_text?(text_name))
     # (Make sure there is an edit and destroy control for the new naming.)
     # (Now one: same for wide-screen as for mobile.)
-    assert_select("a[href*='#{edit_naming_path(naming.id)}']", 1)
-    assert_select("input.destroy_naming_link_#{naming.id}", 1)
+    assert_true(namer_session.has_link?(
+                  class: /edit_naming_link_#{naming.id}/
+                ))
+    namer_session.assert_selector(".destroy_naming_link_#{naming.id}")
 
     # Try changing it.
     author = "(Pers.) Grev."
     reason = "Test reason."
-    click_mo_link(label: /edit/i, href: /#{edit_naming_path(naming.id)}/)
-    assert_template("observations/namings/edit")
-    open_form do |form|
-      form.assert_value("naming_name", text_name)
-      form.assert_checked("naming_reasons_1_check")
+    namer_session.click_link(class: /edit_naming_link_#{naming.id}/)
+    namer_session.assert_selector("body.namings__edit")
+    namer_session.within("#naming_form") do |form|
+      assert_true(form.has_field?("naming_name", with: text_name))
+      assert_true(form.has_checked_field?("naming_reasons_1_check"))
       form.uncheck("naming_reasons_1_check")
-      form.change("naming_name", "#{text_name} #{author}")
+      form.fill_in("naming_name", with: "#{text_name} #{author}")
       form.check("naming_reasons_2_check")
-      form.change("naming_reasons_2_notes", reason)
-      form.select("naming_vote_value", /call it that/i)
-      form.submit
+      form.fill_in("naming_reasons_2_notes", with: reason)
+      form.select("I'd Call It That", from: "naming_vote_value")
+      form.first("input[type='submit']").click
     end
-    assert_template("observations/show")
-    assert_objs_equal(obs, assigns(:observation))
+    namer_session.assert_selector("body.observations__show")
+    assert_true(namer_session.has_text?("Observation #{obs.id}"))
 
     obs.reload
     name.reload
@@ -111,82 +115,64 @@ class NamingAmateurTest < CapybaraIntegrationTestCase
     assert_names_equal(name, obs.name)
 
     # (Make sure author shows up somewhere.)
-    assert_match(author, response.body)
+    assert_true(namer_session.has_text?(author))
     # (Make sure reason shows up, too.)
-    assert_match(reason, response.body)
+    assert_true(namer_session.has_text?(reason))
 
-    click_mo_link(label: /edit/i, href: /#{edit_naming_path(naming.id)}/)
-    assert_template("observations/namings/edit")
-    open_form do |form|
-      form.assert_value("naming_name", "#{text_name} #{author}")
-      form.assert_unchecked("naming_reasons_1_check")
-      form.assert_value("naming_reasons_1_notes", "")
-      form.assert_checked("naming_reasons_2_check")
-      form.assert_value("naming_reasons_2_notes", reason)
-      form.assert_unchecked("naming_reasons_3_check")
-      form.assert_value("naming_reasons_3_notes", "")
+    namer_session.click_link(class: /edit_naming_link_#{naming.id}/)
+    namer_session.assert_selector("body.namings__edit")
+    namer_session.within("#naming_form") do |form|
+      assert_true(
+        form.has_field?("naming_name", with: "#{text_name} #{author}")
+      )
+      assert_true(form.has_unchecked_field?("naming_reasons_1_check"))
+      assert_true(form.has_field?("naming_reasons_1_notes", text: ""))
+      assert_true(form.has_checked_field?("naming_reasons_2_check"))
+      assert_true(form.has_field?("naming_reasons_2_notes", with: reason))
+      assert_true(form.has_unchecked_field?("naming_reasons_3_check"))
+      assert_true(form.has_field?("naming_reasons_3_notes", text: ""))
     end
-    click_mo_link(label: /cancel.*show/i)
-    naming
+    namer_session.click_link(text: "Cancel (Show Observation)")
 
-    voter_session = open_session.extend(VoterDsl)
-    voter_session.login!(rolf)
-    assert_not_equal(namer_session.session[:session_id],
-                     voter_session.session[:session_id])
-    voter_session.vote_on_name(obs, naming)
-    namer_session.failed_delete(obs)
-    voter_session.change_mind(obs, naming)
-    namer_session.successful_delete(obs, naming, text_name, orignal_name)
-  end
-
-  def test_sessions
-    rolf_session = open_session.extend(NamerDsl)
-    rolf_session.login!(rolf)
-    mary_session = open_session.extend(VoterDsl)
-    mary_session.login!(mary)
-    assert_not_equal(mary_session.session[:session_id],
-                     rolf_session.session[:session_id])
-  end
-
-  # Note that this only tests non-JS vote submission.
-  # Most users will have their vote sent via AJAX from naming_vote_ajax.js
-  def vote_on_name(obs, naming)
-    get("/#{obs.id}")
-    open_form("form#naming_vote_#{naming.id}") do |form|
-      form.assert_value("vote_value", /no opinion/i)
-      form.select("vote_value", /call it that/i)
-      form.assert_value("vote_value", "3.0")
-      form.submit("Cast Vote")
+    login(voter, session: voter_session)
+    assert_not_equal(namer_session.driver.request.cookies["mo_user"],
+                     voter_session.driver.request.cookies["mo_user"])
+    # Note that this only tests non-JS vote submission.
+    # Most users will have their vote sent via AJAX from naming_vote_ajax.js
+    # voter_session.vote_on_name(obs, naming)
+    voter_session.visit("/#{obs.id}")
+    voter_session.within("#naming_vote_#{naming.id}") do |form|
+      assert_true(form.has_select?("vote_value", selected: nil))
+      form.select("I'd Call It That", from: "vote_value")
+      assert_true(form.has_select?("vote_value",
+                                   selected: "I'd Call It That"))
+      form.first("input[type='submit']").click
     end
     # assert_template("observations/show")
-    assert_match(/call it that/i, response.body)
-  end
+    assert_true(voter_session.has_text?("I'd Call It That"))
 
-  def change_mind(obs, naming)
-    # "change_mind response.body".print_thing(response.body)
-    get("/#{obs.id}")
-    open_form("form#naming_vote_#{naming.id}") do |form|
-      form.select("vote_value", /as if!/i)
-      form.submit(:show_namings_cast.l)
+    # namer tries to delete
+    # namer_session.failed_delete(obs)
+    namer_session.click_button(class: "destroy_naming_link_#{naming.id}")
+    assert_flash_text("Sorry", session: namer_session)
+
+    # voter_session.change_mind(obs, naming)
+    voter_session.visit("/#{obs.id}")
+    voter_session.within("#naming_vote_#{naming.id}") do |form|
+      form.select("As If!", from: "vote_value")
+      assert_true(form.has_select?("vote_value", selected: "As If!"))
+      form.first("input[type='submit']").click
     end
-  end
 
-  def create_name(obs, text_name); end
-
-  def failed_delete(_obs)
-    click_mo_link(label: /destroy/i, href: /namings/)
-    assert_flash_text(/sorry/i)
-  end
-
-  def successful_delete(obs, naming, text_name, original_name)
-    click_mo_link(label: /destroy/i, href: /#{naming_path(naming.id)}/)
-    assert_template("observations/show")
-    assert_objs_equal(obs, assigns(:observation))
-    assert_flash_text(/success/i)
+    # namer_session.successful_delete(obs, naming, text_name, orignal_name)
+    namer_session.click_button(class: "destroy_naming_link_#{naming.id}")
+    namer_session.assert_selector("body.observations__show")
+    assert_true(namer_session.has_text?("Observation #{obs.id}"))
+    assert_flash_success(session: namer_session)
 
     obs.reload
     assert_names_equal(original_name, obs.name)
     assert_nil(Naming.safe_find(naming.id))
-    assert_no_match(text_name, response.body)
+    assert_false(namer_session.has_text?(text_name))
   end
 end
