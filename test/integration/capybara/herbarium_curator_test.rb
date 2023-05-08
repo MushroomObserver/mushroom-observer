0aaef3bf3622b0b294ecb01a86039d7909046515
andrew nimmo <andrnimm@fastmail.fm>
2023-05-07 20:42:10 -0700

    Update herbarium_curator_test.rb

--- test/integration/capybara/herbarium_curator_test.rb
+++ test/integration/capybara/herbarium_curator_test.rb
@@ -53,7 +53,7 @@ class HerbariumCuratorTest < CapybaraIntegrationTestCase
     assert_selector("a[href*='#{edit_observation_path(id: obs.id)}']")
 
     # The remove button is a rails form patch submit input, not a link
-    click_on(".remove_herbarium_record_link_#{rec.id}")
+    click_on(class: "remove_herbarium_record_link_#{rec.id}")
 
     assert_selector("body.observations__show")
     assert_selector("a[href*='#{edit_observation_path(id: obs.id)}']")
