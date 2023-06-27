0425756c0ac1757618604a4c642309e45e9651bb
Joe Cohen <jdcohenesq@gmail.com>
2023-06-27 17:39:41 +0000

    Reorganize TextileTest
    - Separates tests that call private methods from those that don't
    - Put submethods after all methods
    
    Makes it easier for me to find things.

--- test/models/textile_test.rb
+++ test/models/textile_test.rb
@@ -10,43 +10,30 @@ class Textile
 end
 
 class TextileTest < UnitTestCase
-  def assert_name_link_matches(str, label = nil, name = nil)
-    obj = Textile.new(str)
-    obj.send_private(:convert_name_links_to_tagged_objects!)
-    assert_equal("x{NAME __#{label}__ }{ #{name} }x", obj.to_s)
-  end
+  IMPLICIT_TERMS = [
+    "_amanita_", # lower-case name
+    "_blah blah blah_", # multiple words
+    "_Sonoran Flora_", # title case
+    "_This should be close_",
+    "_A. H. Smith_", # abbreviations ok
+    "_Adnate-Decurrent_", # hyphens ok
+    "_amanita_ followed by non-italized stuff",
+    "_RPB2_", # digits preceded by numbers ok
+    "_NH4OH_"
+  ].freeze
 
-  def assert_name_link_fails(str)
-    obj = Textile.new(str)
-    obj.send_private(:convert_name_links_to_tagged_objects!)
-    assert_equal(str, obj.to_s)
-  end
+  PLAIN_ITALICS = [
+    "_arriba!_", # no exclamations
+    "_A 5-6 inch_", # no numbers
+    "_{bad punctation chars}_" # only allowed Name punctuation is comma, hyphen
+  ].freeze
 
-  def assert_href_equal(url, str)
-    result = str.tl
-    assert_match(/href=.([^"']*)/, result,
-                 "Expected an <a href='...'> tag for #{str.inspect}.\n" \
-                 "Got: #{result.inspect}\n")
-    result =~ /href=.([^"']*)/
-    actual = Regexp.last_match(1)
-    assert_equal(url, actual,
-                 "URL for #{str.inspect} is wrong:\n" \
-                 "url: #{url.inspect}\n" \
-                 "actual: #{actual}\n")
-  end
-
-  def test_glossary_term_lookup
-    term = glossary_terms(:conic_glossary_term).name
-
-    textile = "_glossary_term #{term}_".tpl
-
-    assert_match(
-      "#{MO.http_domain}/lookups/lookup_glossary_term/#{CGI.escape(term)}",
-      textile,
-      "Wrong URL"
-    )
-    assert_match("<i>#{term}</i>", textile, "Wrong anchor text")
-  end
+  ############################################################
+  #
+  # NOTE: The test in this section use Textile private methods
+  # (directly or indirectly). JDC 2023-06-07
+  #
+  ############################################################
 
   def test_name_lookup
     Textile.clear_textile_cache
@@ -112,14 +99,6 @@ class TextileTest < UnitTestCase
     assert_equal({ "A" => "Amanita", "F" => "Fungi" }, Textile.name_lookup)
   end
 
-  def test_textile_name_size
-    Textile.clear_textile_cache
-    assert_equal(0, Textile.textile_name_size)
-
-    assert_name_link_matches("_Agaricus_", "Agaricus", "Agaricus")
-    assert_equal(1, Textile.textile_name_size)
-  end
-
   def test_expand_infra_specific_names
     # Expand subspecies after Textile is told about species
     assert_name_link_matches("_Hydnum album_", "Hydnum album", "Hydnum album")
@@ -163,36 +142,75 @@ class TextileTest < UnitTestCase
     )
   end
 
-  SHOULD_NOT_BE_INTERPRETED_AS_NAMES = [
-    "_amanita_",
-    "_arriba!_",
-    "_blah blah blah_",
-    "_A 5-6 inch_",
-    "_This should be close_",
-    "_Sonoran Flora_",
-    "_A. H. Smith_"
-  ].freeze
+  def test_textile_name_size
+    Textile.clear_textile_cache
+    assert_equal(0, Textile.textile_name_size)
+
+    assert_name_link_matches("_Agaricus_", "Agaricus", "Agaricus")
+    assert_equal(1, Textile.textile_name_size)
+  end
+
+  def test_textile_div_safe
+    str = "Xyz"
+    assert_match(
+      %r{<div class="textile">.*#{str}.*</div>},
+      Textile.textile_div_safe { Textile.textilize(str) },
+      %("#{str}" should be within a <div class="textile>")
+    )
+  end
 
   def test_name_lookup_failures
-    SHOULD_NOT_BE_INTERPRETED_AS_NAMES.each do |phrase|
+    IMPLICIT_TERMS.each do |phrase|
       assert_name_link_fails(phrase)
     end
   end
 
-  IMPLICIT_TERMS = [
-    "_amanita_", # lower-case name
-    "_blah blah blah_", # multiple words
-    "_Sonoran Flora_", # title case
-    "_A. H. Smith_", # abbreviations ok
-    "_Adnate-Decurrent_", # hyphens ok
-    "_amanita_ followed by non-italized stuff"
-  ].freeze
+  def test_other_link_object_tags
+    assert_equal("", do_other_link_object_tag(""))
+    assert_equal("x{GLOSSARY_TERM __term 123__ }{ 123 }x",
+                 do_other_link_object_tag("_term 123_"))
+    assert_equal("x{OBSERVATION __obs 123__ }{ 123 }x",
+                 do_other_link_object_tag("_obs 123_"))
+    assert_equal("x{IMAGE __iMg 765__ }{ 765 }x",
+                 do_other_link_object_tag("_iMg 765_"))
+    assert_equal("x{USER __phooey__ }{ phooey }x x{NAME __gar__ }{ gar }x",
+                 do_other_link_object_tag("_user phooey_ _name gar_"))
+    assert_equal("x{SPECIES_LIST __spl 321__ }{ 321 }x",
+                 do_other_link_object_tag("_spl 321_"))
+  end
 
-  NON_NAME_NON_TERMS = [
-    "_arriba!_", # no exclamations
-    "_A 5-6 inch_", # no numbers
-    "_{bad punctation chars}_" # only allowed punctuation are comma, hyphen
-  ].freeze
+  def assert_name_link_matches(str, label = nil, name = nil)
+    obj = Textile.new(str)
+    obj.send_private(:convert_name_links_to_tagged_objects!)
+    assert_equal("x{NAME __#{label}__ }{ #{name} }x", obj.to_s)
+  end
+
+  def assert_name_link_fails(str)
+    obj = Textile.new(str)
+    obj.send_private(:convert_name_links_to_tagged_objects!)
+    assert_equal(str, obj.to_s)
+  end
+
+  def do_other_link_object_tag(str)
+    obj = Textile.new(str)
+    obj.send_private(:convert_other_links_to_tagged_objects!)
+    obj.to_s
+  end
+
+  ###########################################################
+
+  def test_glossary_term_lookup
+    term = glossary_terms(:conic_glossary_term).name
+
+    textile = "_glossary_term #{term}_".tpl
+
+    assert_match(
+      "#{MO.http_domain}/lookups/lookup_glossary_term/#{CGI.escape(term)}",
+      textile,
+      "Wrong URL"
+    )
+    assert_match("<i>#{term}</i>", textile, "Wrong anchor text")
+  end
 
   def test_implicit_glossary_terms
     IMPLICIT_TERMS.each do |str|
@@ -203,13 +221,12 @@ class TextileTest < UnitTestCase
       assert_match(
         "#{MO.http_domain}/lookups/lookup_glossary_term/#{CGI.escape(inside)}",
         textile,
-        "Wrong URL: " \
-        "'_#{inside}_' not interpreted as a Name; look it up as a Term"
+        "Missing or wrong URL: " \
+        "'_#{inside}_' should create a link that looks up a GlossaryTerm"
       )
       assert_match(
         "<i>#{inside}</i>", textile,
-        "Wrong anchor text: " \
-        "'_#{inside}_' not interpreted as a Name; look it up as a Term"
+        "Wrong anchor text"
       )
     end
 
@@ -234,7 +251,7 @@ class TextileTest < UnitTestCase
   end
 
   def test_plain_italics
-    NON_NAME_NON_TERMS.each do |str|
+    PLAIN_ITALICS.each do |str|
       inside = within_underscores(str)
 
       textile = str.tpl
@@ -259,26 +276,6 @@ class TextileTest < UnitTestCase
 
     html = "<code>_foo_</code>"
     assert_match(/#{html}/, html.tl)
-  end
-
-  def test_other_link_object_tags
-    assert_equal("", do_other_link_object_tag(""))
-    assert_equal("x{GLOSSARY_TERM __term 123__ }{ 123 }x",
-                 do_other_link_object_tag("_term 123_"))
-    assert_equal("x{OBSERVATION __obs 123__ }{ 123 }x",
-                 do_other_link_object_tag("_obs 123_"))
-    assert_equal("x{IMAGE __iMg 765__ }{ 765 }x",
-                 do_other_link_object_tag("_iMg 765_"))
-    assert_equal("x{USER __phooey__ }{ phooey }x x{NAME __gar__ }{ gar }x",
-                 do_other_link_object_tag("_user phooey_ _name gar_"))
-    assert_equal("x{SPECIES_LIST __spl 321__ }{ 321 }x",
-                 do_other_link_object_tag("_spl 321_"))
-  end
-
-  def do_other_link_object_tag(str)
-    obj = Textile.new(str)
-    obj.send_private(:convert_other_links_to_tagged_objects!)
-    obj.to_s
   end
 
   def test_location_lookup
@@ -310,15 +307,6 @@ class TextileTest < UnitTestCase
            "Link text should be truncated with 'x/...'")
   end
 
-  def test_textile_div_safe
-    str = "Xyz"
-    assert_match(
-      %r{<div class="textile">.*#{str}.*</div>},
-      Textile.textile_div_safe { Textile.textilize(str) },
-      %("#{str}" should be within a <div class="textile>")
-    )
-  end
-
   def test_bracketed_integers
     citation = "Hyménomycètes (Alençon): 103 (1874) [1878]"
     assert_equal(
@@ -335,10 +323,21 @@ class TextileTest < UnitTestCase
     )
   end
 
-  private
-
   def within_underscores(str)
     str =~ (/^_+(?<inside>.*)_+/)
     $LAST_MATCH_INFO[:inside]
+  end
+
+  def assert_href_equal(url, str)
+    result = str.tl
+    assert_match(/href=.([^"']*)/, result,
+                 "Expected an <a href='...'> tag for #{str.inspect}.\n" \
+                 "Got: #{result.inspect}\n")
+    result =~ /href=.([^"']*)/
+    actual = Regexp.last_match(1)
+    assert_equal(url, actual,
+                 "URL for #{str.inspect} is wrong:\n" \
+                 "url: #{url.inspect}\n" \
+                 "actual: #{actual}\n")
   end
 end
