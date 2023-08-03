0550c616a9a4c5be8176ea491a58590b04cda493
andrew nimmo <andrnimm@fastmail.fm>
2023-08-02 22:49:56 -0700

    Nest tab helper module defs

--- app/helpers/tabs/articles_helper.rb
+++ app/helpers/tabs/articles_helper.rb
@@ -6,40 +6,42 @@
 #                  write Articles only if user has write permission
 #   xx_title::     Title of x page; includes any markup
 #
-module Tabs::ArticlesHelper
-  def index_tabs_for_user(user)
-    return [] unless permitted?(user)
+module Tabs
+  module ArticlesHelper
+    def index_tabs_for_user(user)
+      return [] unless permitted?(user)
 
-    [link_to(:create_article_title.t, new_article_path)]
-  end
-
-  def show_tabs_for_user(article, user)
-    tabs = [link_to(:index_article.t, articles_path)]
-    return tabs unless permitted?(user)
-
-    tabs.push(link_to(:create_article_title.t, new_article_path),
-              link_to(:EDIT.t, edit_article_path(article.id)),
-              destroy_button(target: article))
-  end
-
-  # Can user modify all articles
-  def permitted?(user)
-    Article.can_edit?(user)
-  end
-
-  # "Title (#nnn)" textilized
-  def show_title(article)
-    capture do
-      concat(article.display_title.t)
-      concat(" (##{article.id || "?"})")
+      [link_to(:create_article_title.t, new_article_path)]
     end
-  end
 
-  # "Editing: Title (#nnn)"  textilized
-  def edit_title(article)
-    capture do
-      concat("#{:EDITING.l}: ")
-      concat(show_title(article))
+    def show_tabs_for_user(article, user)
+      tabs = [link_to(:index_article.t, articles_path)]
+      return tabs unless permitted?(user)
+
+      tabs.push(link_to(:create_article_title.t, new_article_path),
+                link_to(:EDIT.t, edit_article_path(article.id)),
+                destroy_button(target: article))
+    end
+
+    # Can user modify all articles
+    def permitted?(user)
+      Article.can_edit?(user)
+    end
+
+    # "Title (#nnn)" textilized
+    def show_title(article)
+      capture do
+        concat(article.display_title.t)
+        concat(" (##{article.id || "?"})")
+      end
+    end
+
+    # "Editing: Title (#nnn)"  textilized
+    def edit_title(article)
+      capture do
+        concat("#{:EDITING.l}: ")
+        concat(show_title(article))
+      end
     end
   end
 end
--- app/helpers/tabs/images_helper.rb
+++ app/helpers/tabs/images_helper.rb
@@ -1,56 +1,58 @@
 # frozen_string_literal: true
 
 # html used in tabsets
-module Tabs::ImagesHelper
-  # assemble HTML for "tabset" for show_image
-  # actually a list of links and the interest icons
-  def show_image_tabset(image:)
-    tabs = [
-      show_image_obs_links(image),
-      eol_link(image),
-      edit_and_destroy_links(image),
-      email_commercial_inquiry_link(image)
-    ].flatten.reject(&:empty?)
-    { pager_for: image, right: draw_tab_set(tabs) }
-  end
+module Tabs
+  module ImagesHelper
+    # assemble HTML for "tabset" for show_image
+    # actually a list of links and the interest icons
+    def show_image_tabset(image:)
+      tabs = [
+        show_image_obs_links(image),
+        eol_link(image),
+        edit_and_destroy_links(image),
+        email_commercial_inquiry_link(image)
+      ].flatten.reject(&:empty?)
+      { pager_for: image, right: draw_tab_set(tabs) }
+    end
 
-  private
+    private
 
-  def show_image_obs_links(image)
-    return unless image.observations.length == 1
+    def show_image_obs_links(image)
+      return unless image.observations.length == 1
 
-    obs = image.observations.first
-    [
-      link_with_query(:show_object.t(type: :observation),
-                      permanent_observation_path(obs.id)),
-      link_with_query(:show_object.t(type: :name),
-                      name_path(obs.name.id)),
-      link_to(:google_images.t,
-              "http://images.google.com/images?q=#{obs.name.search_name}")
-    ]
-  end
+      obs = image.observations.first
+      [
+        link_with_query(:show_object.t(type: :observation),
+                        permanent_observation_path(obs.id)),
+        link_with_query(:show_object.t(type: :name),
+                        name_path(obs.name.id)),
+        link_to(:google_images.t,
+                "http://images.google.com/images?q=#{obs.name.search_name}")
+      ]
+    end
 
-  def eol_link(image)
-    return unless (eol_url = image.eol_url)
+    def eol_link(image)
+      return unless (eol_url = image.eol_url)
 
-    link_to("EOL", eol_url)
-  end
+      link_to("EOL", eol_url)
+    end
 
-  def edit_and_destroy_links(image)
-    return unless check_permission(image)
+    def edit_and_destroy_links(image)
+      return unless check_permission(image)
 
-    [
-      link_with_query(:edit_object.t(type: :image),
-                      edit_image_path(image.id)),
-      destroy_button(name: :destroy_object.t(type: :image),
-                     target: image)
-    ]
-  end
+      [
+        link_with_query(:edit_object.t(type: :image),
+                        edit_image_path(image.id)),
+        destroy_button(name: :destroy_object.t(type: :image),
+                       target: image)
+      ]
+    end
 
-  def email_commercial_inquiry_link(image)
-    return unless image.user.email_general_commercial && !image.user.no_emails
+    def email_commercial_inquiry_link(image)
+      return unless image.user.email_general_commercial && !image.user.no_emails
 
-    link_with_query(:image_show_inquiry.t,
-                    emails_commercial_inquiry_path(image.id))
+      link_with_query(:image_show_inquiry.t,
+                      emails_commercial_inquiry_path(image.id))
+    end
   end
 end
