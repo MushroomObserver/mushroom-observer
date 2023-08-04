a3deb5f37725523be90ecf2a3bee0a5f36f3a244
andrew nimmo <andrnimm@fastmail.fm>
2023-08-03 22:03:00 -0700

    Herbarium and herbarium_records

--- app/helpers/tabs/herbaria_helper.rb
+++ app/helpers/tabs/herbaria_helper.rb
@@ -4,6 +4,7 @@
 #
 module Tabs
   module HerbariaHelper
+    # link attribute arrays
     def herbaria_index_tabs(query)
       links ||= []
       unless query&.flavor == :all
@@ -17,6 +18,50 @@ module Tabs
       end
       links << [:create_herbarium.l, new_herbarium_path,
                 { id: "new_herbarium_link" }]
+    end
+
+    # Composed links because there's a destroy_button
+    def herbarium_show_tabs(herbarium, user)
+      tabs = []
+      if herbarium.curators.empty? ||
+         herbarium.curator?(user) || in_admin_mode?
+        tabs += [
+          link_with_query(:edit_herbarium.t,
+                          edit_herbarium_path(herbarium.id)),
+          destroy_button(
+            name: :destroy_object.t(type: :herbarium),
+            target: herbarium_path(herbarium,
+                                   back: url_after_delete(herbarium)),
+            id: "delete_herbarium_link"
+          )
+        ]
+      end
+      tabs += [
+        link_with_query(:create_herbarium.t, new_herbarium_path,
+                        id: "new_herbarium_link"),
+        link_with_query(:herbarium_index.t, herbaria_path(flavor: :nonpersonal),
+                        id: "herbarium_index_link")
+      ]
+      tabs
+    end
+
+    # link attribute arrays
+    def herbarium_form_new_tabs
+      [[:herbarium_index.t,
+        add_query_param(herbaria_path(flavor: :nonpersonal)),
+        { id: "herbarium_index_link" }]]
+    end
+
+    # link attribute arrays
+    def herbarium_form_edit_tabs(herbarium)
+      [
+        [:cancel_and_show.t(type: :herbarium),
+         add_query_param(herbarium_path(herbarium)),
+         { id: "herbarium_link" }],
+        [:herbarium_index.t,
+         add_query_param(herbaria_path(flavor: :nonpersonal)),
+         { id: "herbarium_index_link" }]
+      ]
     end
   end
 end
