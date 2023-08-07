0a102920d3b13e178441a131fd4c4b53da43e6d6
andrew nimmo <andrnimm@fastmail.fm>
2023-08-06 23:56:08 -0700

    Location descriptions

--- app/helpers/tabs/locations/descriptions_helper.rb
+++ app/helpers/tabs/locations/descriptions_helper.rb
@@ -4,6 +4,35 @@
 module Tabs
   module Locations
     module DescriptionsHelper
+      def location_description_index_links
+        links = [
+          [:list_place_names_map.t, add_query_param(map_locations_path)],
+          [:all_objects.t(type: :location), locations_path]
+        ]
+        links << coerced_query_link(query, Location)
+        links
+      end
+
+      def location_description_form_new_links(description)
+        [
+          [:cancel_and_show.t(type: :location),
+           add_query_param(description.location.show_link_args)]
+        ]
+      end
+
+      def location_description_form_edit_links(description)
+        [
+          [:show_object.t(type: :location),
+           add_query_param(description.location.show_link_args)],
+          [:cancel_and_show.t(type: :location_description),
+           add_query_param(description.show_link_args)]
+        ]
+      end
+
+      def location_description_version_links(description, desc_title)
+        [:show_location_description.t(description: desc_title),
+         add_query_param(location_description_path(description.id))]
+      end
     end
   end
 end
