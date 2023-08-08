453069221a2df88068dbc9f375dd7961d6fae3dc
andrew nimmo <andrnimm@fastmail.fm>
2023-08-07 23:28:40 -0700

    Update descriptions_helper.rb
    line breaks

--- app/helpers/tabs/descriptions_helper.rb
+++ app/helpers/tabs/descriptions_helper.rb
@@ -41,7 +41,9 @@ module Tabs
     def edit_description_link(description)
       return unless writer?(description)
 
-      link_with_query(:show_description_edit.t, description.edit_link_args)
+      link_with_query(
+        :show_description_edit.t, description.edit_link_args
+      )
     end
 
     def destroy_description_link(description, admin)
@@ -52,68 +54,82 @@ module Tabs
     end
 
     def clone_description_link(description)
-      link_with_query(:show_description_clone.t,
-                      { controller: description.show_controller,
-                        action: :new, id: description.parent_id,
-                        clone: description.id },
-                      help: :show_description_clone_help.l)
+      link_with_query(
+        :show_description_clone.t,
+        { controller: description.show_controller,
+          action: :new, id: description.parent_id,
+          clone: description.id },
+        help: :show_description_clone_help.l
+      )
     end
 
     def merge_description_link(description, admin)
       return unless admin
 
-      link_with_query(:show_description_merge.t,
-                      { controller: "#{description.show_controller}/merges",
-                        action: :new, id: description.id },
-                      help: :show_description_merge_help.l)
+      link_with_query(
+        :show_description_merge.t,
+        { controller: "#{description.show_controller}/merges",
+          action: :new, id: description.id },
+        help: :show_description_merge_help.l
+      )
     end
 
     def move_description_link(description, admin)
       return unless admin
 
       parent_type = description.parent.type_tag.to_s
-      link_with_query(:show_description_move.t,
-                      { controller: "#{description.show_controller}/moves",
-                        action: :new, id: description.id },
-                      help: :show_description_move_help.l(parent: parent_type))
+      link_with_query(
+        :show_description_move.t,
+        { controller: "#{description.show_controller}/moves",
+          action: :new, id: description.id },
+        help: :show_description_move_help.l(parent: parent_type)
+      )
     end
 
     def adjust_permissions_link(description, type, admin)
       return unless admin && type == :name
 
-      link_with_query(:show_description_adjust_permissions.t,
-                      { controller: "#{description.show_controller}/permissions",
-                        action: :edit, id: description.id },
-                      help: :show_description_adjust_permissions_help.l)
+      link_with_query(
+        :show_description_adjust_permissions.t,
+        { controller: "#{description.show_controller}/permissions",
+          action: :edit, id: description.id },
+        help: :show_description_adjust_permissions_help.l
+      )
     end
 
     def make_default_link(description)
       return unless description.public && User.current &&
                     (description.parent.description_id != description.id)
 
-      put_button(name: :show_description_make_default.t,
-                 path: { controller: "#{description.show_controller}/defaults",
-                         action: :update, id: description.id,
-                         q: get_query_param },
-                 help: :show_description_make_default_help.l)
+      put_button(
+        name: :show_description_make_default.t,
+        path: { controller: "#{description.show_controller}/defaults",
+                action: :update, id: description.id,
+                q: get_query_param },
+        help: :show_description_make_default_help.l
+      )
     end
 
     def project_link(description)
       return unless (description.source_type == :project) &&
                     (project = description.source_object)
 
-      link_with_query(:show_object.t(type: :project), project.show_link_args)
+      link_with_query(
+        :show_object.t(type: :project), project.show_link_args
+      )
     end
 
     def publish_draft_link(description, type, admin)
       return unless admin && (type == :name) &&
                     (description.source_type != :public)
 
-      put_button(name: :show_description_publish.t,
-                 path: { controller: "#{description.show_controller}/publish",
-                         action: :update, id: description.id,
-                         q: get_query_param },
-                 help: :show_description_publish_help.l)
+      put_button(
+        name: :show_description_publish.t,
+        path: { controller: "#{description.show_controller}/publish",
+                action: :update, id: description.id,
+                q: get_query_param },
+        help: :show_description_publish_help.l
+      )
     end
   end
 end
