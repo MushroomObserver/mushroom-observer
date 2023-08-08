449a60e61f81fc352d2647ad33f8489c29d9f741
andrew nimmo <andrnimm@fastmail.fm>
2023-08-08 16:36:00 -0700

    descriptions, clean up kwargs

--- app/helpers/tabs/locations/descriptions_helper.rb
+++ app/helpers/tabs/locations/descriptions_helper.rb
@@ -4,43 +4,53 @@
 module Tabs
   module Locations
     module DescriptionsHelper
-      def location_description_index_links
-        links = [
-          [:list_place_names_map.t, add_query_param(map_locations_path)],
-          [:all_objects.t(type: :location), locations_path]
+      def location_description_index_links(query:)
+        [
+          [:list_place_names_map.t, add_query_param(map_locations_path),
+           { class: "map_locations_link" }],
+          [:all_objects.t(type: :location), locations_path,
+           { class: "locations_index_link" }],
+          [*coerced_query_link(query, Location),
+           { class: "location_query_link" }]
         ]
-        links << coerced_query_link(query, Location)
-        links
       end
 
       def location_description_form_new_links(description:)
         [
           [:cancel_and_show.t(type: :location),
-           add_query_param(description.location.show_link_args)]
+           add_query_param(description.location.show_link_args),
+           { class: "location_return_link" }]
         ]
       end
 
       def location_description_form_edit_links(description:)
         [
           [:show_object.t(type: :location),
-           add_query_param(description.location.show_link_args)],
+           add_query_param(description.location.show_link_args),
+           { class: "location_return_link" }],
           [:cancel_and_show.t(type: :location_description),
-           add_query_param(description.show_link_args)]
+           add_query_param(description.show_link_args),
+           { class: "location_description_return_link" }]
         ]
       end
 
       def location_description_form_permissions_links(description:)
         [
           [:show_object.t(type: :location),
-           add_query_param(location_path(description.location_id))],
+           add_query_param(location_path(description.location_id)),
+           { class: "location_return_link" }],
           [:show_object.t(type: :location_description),
-           add_query_param(location_description_path(description.id))]
+           add_query_param(location_description_path(description.id)),
+           { class: "location_description_return_link" }]
         ]
       end
 
       def location_description_version_links(description:, desc_title:)
-        [:show_location_description.t(description: desc_title),
-         add_query_param(location_description_path(description.id))]
+        [
+          [:show_location_description.t(description: desc_title),
+           add_query_param(location_description_path(description.id)),
+           { class: "location_description_return_link" }]
+        ]
       end
     end
   end
--- app/helpers/tabs/names/descriptions_helper.rb
+++ app/helpers/tabs/names/descriptions_helper.rb
@@ -4,46 +4,53 @@
 module Tabs
   module Names
     module DescriptionsHelper
-      def name_description_index_links
-        links = []
-        links << coerced_query_link(query, Name)
-        links
-      end
-
-      def name_description_form_new_links(description)
+      def name_description_index_links(query:)
         [
-          [:cancel_and_show.t(type: :name),
-           add_query_param(name_path(description.name_id))]
+          [*coerced_query_link(query, Name), { class: "name_query_link" }]
         ]
       end
 
-      def name_description_form_edit_links(description, user)
+      def name_description_form_new_links(description:)
+        [
+          [:cancel_and_show.t(type: :name),
+           add_query_param(name_path(description.name_id)),
+           { class: "name_return_link" }]
+        ]
+      end
+
+      def name_description_form_edit_links(description:, user:)
         tabs = [
           [:show_object.t(type: :name),
-           add_query_param(name_path(description.name_id))],
+           add_query_param(name_path(description.name_id)),
+           { class: "name_return_link" }],
           [:cancel_and_show.t(type: :name_description),
-           add_query_param(name_description_path(description.id))]
+           add_query_param(name_description_path(description.id)),
+           { class: "name_description_return_link" }]
         ]
         if description.is_admin?(user) || in_admin_mode?
           tabs << [:show_description_adjust_permissions.t,
-                   edit_name_description_permissions_path(description.id)]
+                   edit_name_description_permissions_path(description.id),
+                   { class: "adjust_permissions_link" }]
         end
         tabs
       end
 
-      def name_description_form_permissions_links(description)
+      def name_description_form_permissions_links(description:)
         [
           [:show_object.t(type: :name),
-           add_query_param(name_path(description.name_id))],
+           add_query_param(name_path(description.name_id)),
+           { class: "name_return_link" }],
           [:show_object.t(type: :name_description),
-           add_query_param(name_description_path(description.id))]
+           add_query_param(name_description_path(description.id)),
+           { class: "name_description_return_link" }]
         ]
       end
 
-      def name_description_version_links(description, desc_title)
+      def name_description_version_links(description:, desc_title:)
         [
           [:show_name_description.t(description: desc_title),
-           add_query_param(name_description_path(description.id))]
+           add_query_param(name_description_path(description.id)),
+           { class: "name_description_return_link" }]
         ]
       end
     end
