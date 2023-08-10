d6366ff16520945a2bbeee64b455dfaf0f200a55
andrew nimmo <andrnimm@fastmail.fm>
2023-08-10 15:16:40 -0700

    themes link helpers

--- app/helpers/tabs/themes_helper.rb
+++ app/helpers/tabs/themes_helper.rb
@@ -2,5 +2,12 @@
 
 module Tabs
   module ThemesHelper
+    def theme_show_links
+      [
+        [:theme_list.t, theme_color_themes_path, { class: "theme_list_link" }],
+        [:app_preferences.t, edit_account_preferences_path,
+         { class: "edit_account_preferences_link" }]
+      ]
+    end
   end
 end
