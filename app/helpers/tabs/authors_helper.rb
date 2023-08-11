17e2329ae3ea322ff9a2ed7c108917a9b6785afc
andrew nimmo <andrnimm@fastmail.fm>
2023-08-10 20:53:27 -0700

    author tab helpers

--- app/helpers/tabs/authors_helper.rb
+++ app/helpers/tabs/authors_helper.rb
@@ -2,5 +2,15 @@
 
 module Tabs
   module AuthorsHelper
+    def author_review_links(obj:)
+      [
+        [:show_object.t(type: obj.parent.type_tag),
+         obj.parent.show_link_args,
+         { class: "show_parent_link" }],
+        [:show_object.t(type: type),
+         obj.show_link_args,
+         { class: "show_object_link" }]
+      ]
+    end
   end
 end
