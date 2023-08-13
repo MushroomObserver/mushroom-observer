7cf3f8b353e350312b9798b500114696a24a5774
andrew nimmo <andrnimm@fastmail.fm>
2023-08-13 00:20:36 -0700

    Try to fix ahead of merge conflict

--- app/helpers/tabs/observations_helper.rb
+++ app/helpers/tabs/observations_helper.rb
@@ -8,29 +8,29 @@ module Tabs
     def show_observation_links(obs:, user:)
       [
         # *show_obs_google_links_for(obs.name),
-        general_questions_link(obs, user),
-        manage_lists_link(obs, user)
-        # map_link(mappable),
+        send_observer_question_link(obs, user),
+        observation_manage_lists_link(obs, user)
+        # observation_map_locations_link(mappable),
         # *obs_change_links(obs)
       ].reject(&:empty?)
     end
 
-    def general_questions_link(obs, user)
+    def send_observer_question_link(obs, user)
       return if obs.user.no_emails
       return unless obs.user.email_general_question && obs.user != user
 
       [:show_observation_send_question.t,
        add_query_param(new_question_for_observation_path(obs.id)),
        { remote: true, onclick: "MOEvents.whirly();",
-         class: "send_observer_question_link" }]
+         class: __method__.to_s }]
     end
 
-    def manage_lists_link(obs, user)
+    def observation_manage_lists_link(obs, user)
       return unless user
 
       [:show_observation_manage_species_lists.t,
        add_query_param(edit_observation_species_lists_path(obs.id)),
-       { class: "manage_lists_link" }]
+       { class: __method__.to_s }]
     end
 
     # generates HTML using create_tabs with xtrargs { class: "d-block" }
@@ -38,7 +38,8 @@ module Tabs
     def name_links_on_mo(name:, mappable:)
       tabs = create_tabs(obs_related_name_links(name), { class: "d-block" })
       tabs += obs_name_description_links(name)
-      tabs += create_tabs([map_link(mappable)], { class: "d-block" })
+      tabs += create_tabs([observation_map_locations_link(mappable)],
+                          { class: "d-block" })
       tabs.reject(&:empty?)
     end
 
@@ -65,32 +66,70 @@ module Tabs
       end
     end
 
-    def map_link(mappable)
+    def observation_map_locations_link(mappable)
       return unless mappable
 
       [:MAP.t, add_query_param(map_locations_path),
-       { class: "observation_map_locations_link" }]
+       { class: __method__.to_s }]
     end
 
-    # generates HTML
+    def obs_change_links(obs)
+      return unless check_permission(obs)
+
+      [
+        edit_observation_link(obs),
+        destroy_observation_link(obs)
+      ]
+    end
+
+    def edit_observation_link(obs)
+      [:edit_object.t(type: Observation),
+       add_query_param(edit_observation_path(obs.id)),
+       { class: "#{__method__}_#{obs.id}" }]
+    end
+
+    def destroy_observation_link(obs)
+      [nil, obs, { button: :destroy }]
+    end
+
+    ########################################################################
+    # Name section -- generates HTML
+
     def name_links_web(name:)
-      tabs = create_tabs(obs_web_name_links(name), { class: "d-block" })
+      tabs = create_tabs(observation_web_name_links(name), { class: "d-block" })
       tabs.reject(&:empty?)
     end
 
-    def obs_web_name_links(name)
+    def observation_web_name_links(name)
       [
-        ["MyCoPortal", mycoportal_url(name),
-         { target: :_blank, rel: :noopener, class: "mycoportal_name_link" }],
-        ["Mycobank", mycobank_name_search_url(name),
-         { target: :_blank, rel: :noopener, class: "mycobank_name_search_link" }],
-        [:google_images.t,
-         format("https://images.google.com/images?q=%s", name.real_text_name),
-         { class: "google_images_link" }],
-        [:show_name_distribution_map.t,
-         add_query_param(map_name_path(id: name.id)),
-         { class: "google_name_distribution_map_link" }]
+        mycoportal_name_link(name),
+        mycobank_name_search_link(name),
+        google_images_for_name_link(name),
+        google_distribution_map_for_name_link(name)
       ]
+    end
+
+    def mycoportal_name_link(name)
+      ["MyCoPortal", mycoportal_url(name),
+       { target: :_blank, rel: :noopener, class: "mycoportal_name_link" }]
+    end
+
+    def mycobank_name_search_link(name)
+      ["Mycobank", mycobank_name_search_url(name),
+       { target: :_blank, rel: :noopener, class: "mycobank_name_search_link" }]
+    end
+
+    def google_images_for_name_link(obs_name)
+      [:google_images.t,
+       format("https://images.google.com/images?q=%s",
+              obs_name.real_text_name),
+       { class: __method__.to_s }]
+    end
+
+    def google_distribution_map_for_name_link(obs_name)
+      [:show_name_distribution_map.t,
+       add_query_param(map_name_path(id: obs_name.id)),
+       { class: __method__.to_s }]
     end
 
     def obs_icon_size
@@ -101,7 +140,7 @@ module Tabs
       "btn-link"
     end
 
-    def obs_change_links(obs:)
+    def obs_change_buttons(obs:)
       return [] unless check_permission(obs)
 
       # icon_size = "fa-lg" # "fa-sm"
@@ -171,113 +210,95 @@ module Tabs
 
     def index_observation_links(query:)
       links = [
-        *at_where_links(query), # maybe multiple links
-        index_map_link(query),
-        *coerced_query_links(query), # multiple links
-        add_to_list_link(query),
-        download_as_csv_link(query)
+        *observations_at_where_links(query), # maybe multiple links
+        map_observations_link(query),
+        *observations_coerced_query_links(query), # multiple links
+        observations_add_to_list_link(query),
+        observations_download_as_csv_link(query)
       ]
       links.reject(&:empty?)
     end
 
-    def at_where_links(query)
+    def observations_at_where_links(query)
       # Add some extra links to the index user is sent to if they click on an
       # undefined location.
       return [] unless query.flavor == :at_where
 
       [
-        [:list_observations_location_define.l,
-         add_query_param(new_location_path(
-                           where: query.params[:user_where]
-                         )),
-         { class: "new_location_link" }],
-        [:list_observations_location_merge.l,
-         add_query_param(location_merges_form_path(
-                           where: query.params[:user_where]
-                         )),
-         { class: "merge_locations_link" }],
-        [:list_observations_location_all.l,
-         add_query_param(locations_path),
-         { class: "locations_index_link" }]
+        define_location_link(query),
+        merge_locations_link(query),
+        locations_index_link
       ]
     end
 
-    def index_map_link(query)
+    def map_observations_link(query)
       [:show_object.t(type: :map),
        map_observations_path(q: get_query_param(query)),
-       { class: "map_observations_link" }]
+       { class: __method__.to_s }]
     end
 
     # NOTE: coerced_query_link returns an array
-    def coerced_query_links(query)
+    def observations_coerced_query_links(query)
       [
-        [*coerced_query_link(query, Location),
-         { class: "location_query_link" }],
-        [*coerced_query_link(query, Name),
-         { class: "name_query_link" }],
-        [*coerced_query_link(query, Image),
-         { class: "image_query_link" }]
+        coerced_location_query_link(query),
+        coerced_name_query_link(query),
+        coerced_image_query_link(query)
       ]
     end
 
-    def add_to_list_link(query)
+    def observations_add_to_list_link(query)
       [:list_observations_add_to_list.t,
        add_query_param(edit_species_list_observations_path, query),
-       { class: "add_to_list_link" }]
+       { class: __method__.to_s }]
     end
 
-    def download_as_csv_link(query)
+    def observations_download_as_csv_link(query)
       [:list_observations_download_as_csv.t,
        add_query_param(new_observations_download_path, query),
-       { class: "download_as_csv_link" }]
+       { class: __method__.to_s }]
     end
 
     ############################################
     # FORMS
 
-    def new_observation_links
-      [[:create_herbarium.t, add_query_param(new_herbarium_path),
-        { class: "new_herbarium_link" }]]
+    def observation_form_new_links
+      [new_herbarium_link]
     end
 
-    def edit_observation_links(obs:)
-      [observation_return_link(obs)]
+    def observation_form_edit_links(obs:)
+      [object_return_link(obs)]
     end
 
     def observation_maps_links(query:)
       [
-        [*coerced_query_link(query, Observation),
-         { class: "observation_query_link" }],
-        [*coerced_query_link(query, Location),
-         { class: "location_query_link" }]
+        coerced_observation_query_link(query),
+        coerced_location_query_link(query)
       ]
     end
 
     def new_naming_links(obs:)
-      [observation_return_link(obs)]
+      [object_return_link(obs)]
     end
 
     def edit_naming_links(obs:)
-      [observation_return_link(obs)]
+      [object_return_link(obs)]
     end
 
     def naming_suggestion_links(obs:)
-      [observation_return_link(obs)]
+      [object_return_link(obs)]
     end
 
     def observation_list_links(obs:)
-      [observation_return_link(obs)]
+      [object_return_link(obs)]
     end
 
     def observation_images_edit_links(image:)
-      [[:cancel_and_show.t(type: :image),
-        add_query_param(image.show_link_args),
-        { class: "image_return_link" }]]
+      [object_return_link(image)]
     end
 
     def observation_images_new_links(obs:)
       [
-        observation_return_link(obs),
+        object_return_link(obs),
         edit_observation_link(obs)
       ]
     end
@@ -285,28 +306,22 @@ module Tabs
     # Note this takes `obj:` not `obs:`
     def observation_images_remove_links(obj:)
       [
-        observation_return_link(obj),
+        object_return_link(obj),
         edit_observation_link(obj)
       ]
     end
 
     def observation_images_reuse_links(obs:)
       [
-        observation_return_link(obs),
+        object_return_link(obs),
         edit_observation_link(obs)
       ]
     end
 
-    def observation_return_link(obs)
-      [:cancel_and_show.t(type: :observation),
-       add_query_param(obs.show_link_args),
-       { class: "observation_return_link" }]
-    end
-
-    def edit_observation_link(obs)
-      [:edit_object.t(type: Observation),
-       add_query_param(edit_observation_path(obs.id)),
-       { class: "edit_observation_link" }]
+    def observations_index_link
+      [:download_observations_back.t,
+       add_query_param(observations_path),
+       { class: __method__.to_s }]
     end
   end
 end
