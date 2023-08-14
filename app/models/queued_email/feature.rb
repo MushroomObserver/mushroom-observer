3d61723fb728df5c5f4775304bdca5b0dc5ea48b
andrew nimmo <andrnimm@fastmail.fm>
2023-08-14 01:45:30 -0700

    nest classes

--- app/models/queued_email/feature.rb
+++ app/models/queued_email/feature.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
 # Feature Email
-class QueuedEmail::Feature < QueuedEmail
-  def content
-    get_note
-  end
+class QueuedEmail
+  class Feature < QueuedEmail
+    def content
+      get_note
+    end
 
-  def self.create_email(receiver, content)
-    result = create(nil, receiver)
-    raise("Missing content!") unless content
+    def self.create_email(receiver, content)
+      result = create(nil, receiver)
+      raise("Missing content!") unless content
 
-    result.set_note(content)
-    result.finish
-    result
-  end
+      result.set_note(content)
+      result.finish
+      result
+    end
 
-  def deliver_email
-    return if to_user.no_emails
-    return unless to_user.email_general_feature # Make sure it hasn't changed
+    def deliver_email
+      return if to_user.no_emails
+      return unless to_user.email_general_feature # Make sure it hasn't changed
 
-    FeaturesMailer.build(to_user, content).deliver_now
+      FeaturesMailer.build(to_user, content).deliver_now
+    end
   end
 end
--- app/models/queued_email/name_tracking.rb
+++ app/models/queued_email/name_tracking.rb
@@ -1,41 +1,43 @@
 # frozen_string_literal: true
 
 # Name Tracking Email
-class QueuedEmail::NameTracking < QueuedEmail
-  def name_tracker
-    get_object(:name_tracker, NameTracker)
-  end
-
-  def naming
-    get_object(:naming, Naming)
-  end
-
-  def self.create_email(name_tracker, naming)
-    raise("Missing name_tracker!") unless name_tracker
-    raise("Missing naming!")       unless naming
-
-    sender = name_tracker.user
-    observer = naming.observation.user
-    result = nil
-    if sender != observer
-      result = create(sender, observer)
-      result.add_integer(:name_tracker, name_tracker.id)
-      result.add_integer(:naming, naming.id)
-      result.finish
+class QueuedEmail
+  class NameTracking < QueuedEmail
+    def name_tracker
+      get_object(:name_tracker, ::NameTracker)
     end
-    result
-  end
 
-  def deliver_email
-    # Make sure naming wasn't deleted since email was queued.
-    if naming
-      result = NamingTrackerMailer.build(user, naming).deliver_now
-      if name_tracker.note_template.present? && name_tracker.approved
-        result = NamingObserverMailer.build(
-          to_user, naming, name_tracker
-        ).deliver_now
+    def naming
+      get_object(:naming, ::Naming)
+    end
+
+    def self.create_email(name_tracker, naming)
+      raise("Missing name_tracker!") unless name_tracker
+      raise("Missing naming!")       unless naming
+
+      sender = name_tracker.user
+      observer = naming.observation.user
+      result = nil
+      if sender != observer
+        result = create(sender, observer)
+        result.add_integer(:name_tracker, name_tracker.id)
+        result.add_integer(:naming, naming.id)
+        result.finish
       end
+      result
     end
-    result
+
+    def deliver_email
+      # Make sure naming wasn't deleted since email was queued.
+      if naming
+        result = NamingTrackerMailer.build(user, naming).deliver_now
+        if name_tracker.note_template.present? && name_tracker.approved
+          result = NamingObserverMailer.build(
+            to_user, naming, name_tracker
+          ).deliver_now
+        end
+      end
+      result
+    end
   end
 end
