5f9393bb62e72784f6c1e7fdfac908830a5c5647
andrew nimmo <andrnimm@fastmail.fm>
2023-08-15 01:55:36 -0700

    Update webmaster.rb

--- app/models/queued_email/webmaster.rb
+++ app/models/queued_email/webmaster.rb
@@ -15,19 +15,25 @@ class QueuedEmail
       get_note
     end
 
-    def self.create_email(sender_email, content, subject)
+    def self.create_email(sender_email:, content:, subject: nil)
       raise("Missing email address!") unless sender_email
       raise("Missing content!") unless content
 
-      result = create(sender_email, nil)
-      result.add_string(:subject, subject)
+      # FIXME: Shouldn't both the mailer and this class take a user arg, rather
+      # than their email address?
+      # Seems backwards when the caller prolly has access to the user
+      user = ::User.find_by(email: sender_email)
+
+      result = create(user, nil)
+      result.add_string(:subject, subject) if subject
       result.set_note(content)
       result.finish
       result
     end
 
     def deliver_email
-      WebmasterMailer.build(sender_email, content, subject).deliver_now
+      WebmasterMailer.build(sender_email: sender_email, content: content,
+                            subject: subject).deliver_now
     end
   end
 end
