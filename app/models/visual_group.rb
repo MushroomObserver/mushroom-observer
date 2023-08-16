b10470d4d5e89a2e61350032c247b98f589a6af1
Nathan Wilson <nathan@collectivesource.com>
2023-08-15 22:02:23 -0400

    Some tests

--- app/models/visual_group.rb
+++ app/models/visual_group.rb
@@ -44,19 +44,13 @@ class VisualGroup < AbstractModel
     Rails.logger.info { "Merging #{visual_group.name} into #{name}" }
     visual_group.visual_group_images.each do |vgi|
       if vgi.image.visual_groups.pluck(:id).include?(id)
-        Rails.logger.info do
-          "Image #{vgi.image.id} already in #{name}"
-        end
+        Rails.logger.info { "Image #{vgi.image.id} already in #{name}" }
       else
         vgi.visual_group = self
         if vgi.save
-          Rails.logger.info do
-            "Moved #{vgi.image.id}"
-          end
+          Rails.logger.info { "Moved #{vgi.image.id}" }
         else
-          Rails.logger.info do
-            "FAILED to move #{vgi.image.id}"
-          end
+          Rails.logger.info { "FAILED to move #{vgi.image.id}" }
           break
         end
       end
--- test/models/visual_group_test.rb
+++ test/models/visual_group_test.rb
@@ -3,4 +3,21 @@
 require "test_helper"
 
 class VisualGroupTest < ActiveSupport::TestCase
+  def test_merge
+    vg1 = visual_groups(:visual_group_one)
+    vg2 = visual_groups(:visual_group_two)
+    total = vg1.images.count + vg2.images.count
+    assert_not_equal(vg1.images.count, total)
+    vg1.merge(vg2)
+    vg1.reload
+    assert_equal(vg1.images.count, total)
+  end
+
+  def test_merge_self
+    vg1 = visual_groups(:visual_group_one)
+    total = vg1.images.count
+    vg1.merge(vg1)
+    vg1.reload
+    assert_equal(vg1.images.count, total)
+  end
 end
