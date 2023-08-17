816994355cfbdb73019672410c4db99240993e37
andrew nimmo <andrnimm@fastmail.fm>
2023-08-17 13:51:31 -0700

    Move all the type filter stuff into title_and_tabset_helper

--- app/helpers/tabs/rss_logs_helper.rb
+++ app/helpers/tabs/rss_logs_helper.rb
@@ -4,7 +4,6 @@
 #
 module Tabs
   module RssLogsHelper
-    # TABSET
     def rss_logs_index_links(user:, types:)
       [
         activity_log_default_types_for_user_link(user, types)
@@ -19,23 +18,6 @@ module Tabs
       [:rss_make_default.t,
        add_query_param(action: :index, make_default: 1),
        { class: __method__.to_s }]
-    end
-
-    # type_filters in #index
-    # The "Everything" tab
-    def log_tab_for_everything(types)
-      label = :rss_all.t
-      link = activity_logs_path(params: { type: :all })
-      help = { title: :rss_all_help.t, class: "filter-only" }
-      types == ["all"] ? label : link_with_query(label, link, **help)
-    end
-
-    # A single tab
-    def log_tab_for_type(types, type)
-      label = :"rss_one_#{type}".t
-      link = activity_logs_path(params: { type: type })
-      help = { title: :rss_one_help.t(type: type.to_sym), class: "filter-only" }
-      types == [type] ? label : link_with_query(label, link, **help)
     end
   end
 end
--- app/helpers/title_and_tabset_helper.rb
+++ app/helpers/title_and_tabset_helper.rb
@@ -97,10 +97,27 @@ module TitleAndTabsetHelper
     end
   end
 
+  # type_filters, currently only used in RssLogsController#index
   def add_type_filters
     content_for(:type_filters) do
       render(partial: "application/content/type_filters")
     end
+  end
+
+  # The "Everything" tab
+  def filter_for_everything(types)
+    label = :rss_all.t
+    link = activity_logs_path(params: { type: :all })
+    help = { title: :rss_all_help.t, class: "filter-only" }
+    types == ["all"] ? label : link_with_query(label, link, **help)
+  end
+
+  # A single tab
+  def filter_for_type(types, type)
+    label = :"rss_one_#{type}".t
+    link = activity_logs_path(params: { type: type })
+    help = { title: :rss_one_help.t(type: type.to_sym), class: "filter-only" }
+    types == [type] ? label : link_with_query(label, link, **help)
   end
 
   # @sorts is set by ApplicationController#show_index_count_results
