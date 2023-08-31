3dd7039fbd3fb3dc3e7fad8f1a3e9cfc9e302cbe
andrew nimmo <andrnimm@fastmail.fm>
2023-08-31 02:42:17 -0700

    changes for 7 pt 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_09_202131) do
+ActiveRecord::Schema[6.1].define(version: 2023_07_09_202131) do
 
   create_table "api_keys", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at"
