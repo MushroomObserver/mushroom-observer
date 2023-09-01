a43e3c3ae14a7459bc607a1c5da3fe881738951a
andrew nimmo <andrnimm@fastmail.fm>
2023-08-31 20:14:10 -0700

    Revert "install importmap-rails, but doesn't work"
    This reverts commit afae789598091927a4b519fb7cb9ea94c347b6e6.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
 //= link_tree ../javascripts .js
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 
 //= link advanced_search.js
 //= link account_api_keys.js
