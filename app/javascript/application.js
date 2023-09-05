d0d366964b7894dc01f6bc114cd54eba621f8bae
andrew nimmo <andrnimm@fastmail.fm>
2023-09-05 13:17:20 -0700

    very small tweaks

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb.
+// Read more: https://github.com/rails/importmap-rails
 // If string literal error, it means the importmap is not functioning
+
+// import "bootstrap"
 import "@hotwired/turbo-rails"
 import Rails from "@rails/ujs"
-Rails.start();import "controllers"
+// import "@hotwired/stimulus"
+// import "@hotwired/stimulus-loading"
+Rails.start(); import "controllers"
