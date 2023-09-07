e2c5f4cc1b43e685612e64b11a62834cd534a6e0
andrew nimmo <andrnimm@fastmail.fm>
2023-09-06 19:09:10 -0700

    Update observations_helper_test.rb

--- test/helpers/observations_helper_test.rb
+++ test/helpers/observations_helper_test.rb
@@ -10,26 +10,30 @@ class ObservationsHelperTest < ActionView::TestCase
 
     # approved name
     current_name = names(:lactarius_alpinus)
-    Observation.new(
+    obs1 = Observation.new(
       name: current_name, user: user, when: Time.current, where: location
     )
     assert_match(
       link_to(current_name.display_name_brief_authors.t,
               name_path(id: current_name.id)),
-      obs_title_consensus_name_link(name: current_name),
+      obs_title_consensus_name_link(obs: obs1),
       "Observation of a current Name should link to that Name"
     )
 
     # deprecated name
     deprecated_name = names(:lactarius_alpigenes)
-    Observation.new(
+    obs2 = Observation.new(
       name: deprecated_name, user: user, when: Time.current, where: location
     )
     assert_match(
-      "#{link_to_display_name_brief_authors(deprecated_name)} (Site ID) " \
-      "(#{link_to_display_name_without_authors(current_name)})",
-      obs_title_consensus_name_link(name: deprecated_name),
-      "Observation of deprecated Name should link to it and approved Name"
+      link_to_display_name_brief_authors(deprecated_name),
+      obs_title_consensus_name_link(obs: obs2).unescape_html,
+      "Observation of deprecated Name should link to it"
+    )
+    assert_match(
+      link_to_display_name_without_authors(current_name),
+      obs_title_consensus_name_link(obs: obs2).unescape_html,
+      "Observation of deprecated Name should link to approved Name"
     )
   end
 end
