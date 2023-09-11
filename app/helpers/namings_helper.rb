c7d51dda9ca118d620eb1a36c4222ba92edca4d5
andrew nimmo <andrnimm@fastmail.fm>
2023-09-11 12:30:44 -0700

    move reasons fields to helper

--- app/helpers/namings_helper.rb
+++ app/helpers/namings_helper.rb
@@ -198,4 +198,40 @@ module NamingsHelper
 
     reasons.map { |reason| content_tag(:div, reason) }.safe_join
   end
+
+  def naming_form_reasons_fields(f_r, reasons)
+    reasons.values.sort_by(&:order).each do |r|
+      collapse = r.used? ? "" : "collapse"
+      concat(
+        [
+          tag.div(class: "checkbox") do
+            f_r.label("#{r.num}_check",
+                      { data: {
+                          toggle: "collapse",
+                          target: "#reasons_#{r.num}_notes"
+                        },
+                        aria: {
+                          expanded: "false",
+                          controls: "reasons_#{r.num}_notes"
+                        } }) do
+              [
+                f_r.check_box(:check,
+                              { index: r.num,
+                                checked: r.used?,
+                                class: "" },
+                              "1"),
+                r.label.t
+              ].safe_join
+            end
+          end,
+          tag.div(id: "reasons_#{r.num}_notes",
+                  class: class_names("form-group mb-3", collapse)) do
+            f_r.text_area(:notes,
+                          index: r.num, rows: 3, value: r.notes,
+                          class: "form-control")
+          end
+        ].safe_join
+      )
+    end
+  end
 end
