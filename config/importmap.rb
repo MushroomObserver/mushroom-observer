2ad93d0ecc2e9839b47cbac43d4d68df5bfb09c9
andrew nimmo <andrnimm@fastmail.fm>
2023-09-12 14:58:37 -0700

    start add stimulus-autocomplete

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,6 +8,7 @@ pin "jquery3", to: "jquery.min.js", preload: true
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.0.7
 pin "@rails/request.js", to: "requestjs.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.1.0/src/autocomplete.js"
