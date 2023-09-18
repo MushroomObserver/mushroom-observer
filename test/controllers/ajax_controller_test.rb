fd59cf6c503a5bc308924f021237922bd94e5064
andrew nimmo <andrnimm@fastmail.fm>
2023-09-18 12:50:28 -0700

    belated Arel

--- test/controllers/ajax_controller_test.rb
+++ test/controllers/ajax_controller_test.rb
@@ -122,12 +122,12 @@ class AjaxControllerTest < FunctionalTestCase
 
   def test_auto_complete_location
     # names of Locations whose names have words starting with "m"
-    m_loc_names = Location.where("name REGEXP ?", "\\bM").
+    m_loc_names = Location.where(Location[:name].matches_regexp("\\bM")).
                   map(&:name)
     # wheres of Observations whose wheres have words starting with "m"
     # need extra "observation" to avoid confusing sql with bare "where".
-    m_obs_wheres = Observation.where("observations.where REGEXP ?", "\\bM").
-                   map(&:where)
+    m_obs_wheres = Observation.where(Observation[:where].
+                   matches_regexp("\\bM")).map(&:where)
     m = m_loc_names + m_obs_wheres
 
     expect = m.sort.uniq
@@ -161,11 +161,13 @@ class AjaxControllerTest < FunctionalTestCase
 
   def test_auto_complete_project
     # titles of Projects whose titles have words starting with "p"
-    b_titles = Project.where("title REGEXP ?", "\\bb").map(&:title).uniq
+    b_titles = Project.where(Project[:title].matches_regexp("\\bb")).
+               map(&:title).uniq
     good_ajax_request(:auto_complete, type: :project, id: "Babushka")
     assert_equal((["B"] + b_titles).sort, @response.body.split("\n").sort)
 
-    p_titles = Project.where("title REGEXP ?", "\\bp").map(&:title).uniq
+    p_titles = Project.where(Project[:title].matches_regexp("\\bb")).
+               map(&:title).uniq
     good_ajax_request(:auto_complete, type: :project, id: "Perfidy")
     assert_equal((["P"] + p_titles).sort, @response.body.split("\n").sort)
 
