7dd8a4060a7838e595d6c1e6d5a5dc16f34d2afd
andrew nimmo <andrnimm@fastmail.fm>
2023-09-24 00:27:27 -0700

    start

--- app/assets/javascripts/translations.js
+++ app/assets/javascripts/translations.js
@@ -1,17 +1,17 @@
 function TranslationsModule(localizedText) {
   jQuery(document).ready(function () {
-    var LOCALE = localizedText.locale,
+    const LOCALE = localizedText.locale,
       CONFIRM_STRING = localizedText.confirm_string,
       LOADING_STRING = localizedText.loading_string,
       SAVING_STRING = localizedText.saving_string,
       LOADED = false,
       CHANGED = false,
-      $whirly = jQuery('#whirly'),
-      $save_button = jQuery('#save_button'),
-      $cancel_button = jQuery('#cancel_button'),
-      $form_div = jQuery('#form_div'),
-      $post_form = jQuery('#post_form'),
-      $tag_links = jQuery('[data-role="show_tag"]')
+      $whirly = document.getElementById('whirly'),
+      $save_button = document.getElementById('save_button'),
+      $cancel_button = document.getElementById('cancel_button'),
+      $form_div = document.getElementById('form_div'),
+      $post_form = document.getElementById('post_form'),
+      $tag_links = document.querySelectorAll('[data-role="show_tag"]')
 
     // EVENT LISTENERS (note the delegates!)
     window.onbeforeunload = function () {
@@ -20,49 +20,53 @@ function TranslationsModule(localizedText) {
     };
 
     // annoying, cannot attach to window.hidden_frame in FF!
-    var iframe = document.getElementById('hidden_frame');
+    const iframe = document.getElementById('hidden_frame');
     iframe.addEventListener('load', iframe_load);
 
-    $tag_links.on('click', function (event) {
-      event.preventDefault();
-      if (CHANGED)
-        confirm
-      var data = $(this).data();
-      show_tag(LOCALE, data.tag);
+    $tag_links.forEach((element) => {
+      element.onclick = function (event) {
+        event.preventDefault();
+        if (CHANGED)
+          confirm
+        show_tag(LOCALE, this.dataset.tag);
+      };
     });
 
     // Override non-javascripty submit target.
-    $post_form.attr({
-      action: 'edit_translations_ajax_post',
-      target: 'hidden_frame'
-    });
+    $post_form.setAttribute('action', 'edit_translations_ajax_post');
+    $post_form.setAttribute('target', 'hidden_frame');
 
     // Attach listeners as delegates since they are injected into the dom.
-    $post_form.on('change, keypress', 'textarea', function() {
-      form_changed();
+    const $textareas = $post_form.querySelectorAll('textarea');
+
+    $textareas.forEach((element) => {
+      element.onchange = () => { form_changed() };
+      element.onkeydown = () => { form_changed() };
     });
 
-    $post_form.on('click', '#cancel_button', function () {
+    $post_form.getElementById('cancel_button').onclick = () => {
       clear_form();
+    };
+
+    $post_form.getElementById('reload_button').onclick = () => {
+      show_tag(LOCALE, this.dataset.tag);
+    };
+
+    $post_form.getElementById('locale').onchange = () => {
+      show_tag(this.value, this.dataset.tag);
+    };
+
+    const $show_old_versions =
+      $post_form.querySelectorAll('[data-role="show_old_version"]');
+
+    $show_old_versions.forEach((element) => {
+      element.onclick = function (event) {
+        event.preventDefault();
+        show_old_version(this.dataset.id);
+      }
     });
 
-    $post_form.on('click', '#reload_button', function () {
-      var data = $(this).data();
-      show_tag(LOCALE, data.tag);
-    });
-
-    $post_form.on('change', '#locale', function () {
-      var data = $(this).data();
-      show_tag($(this).val(), data.tag);
-    });
-
-    $post_form.on('click', '[data-role="show_old_version"]', function(event) {
-      event.preventDefault()
-      var data = $(this).data();
-      show_old_version(data.id);
-    });
-
-    $post_form.submit(function (){
+    $post_form.submit(function () {
       CHANGED = false;
       show_whirly(SAVING_STRING);
       setDisabledOnButtons(true);
@@ -70,12 +74,14 @@ function TranslationsModule(localizedText) {
 
     // Helpers and callbacks
     function iframe_load() {
-      var tag = window.hidden_frame.tag;
-      var str = window.hidden_frame.str;
+      const tag = window.hidden_frame.tag;
+      const str = window.hidden_frame.str;
       if (tag != undefined) {
         // Make tag in left column gray because it's now been translated.
         // Want only untranslated tags to be bold black to stand out better.
-        jQuery('#str_' + tag).html(str).addClass('translated faint');
+        const _str_tag = document.getElementById('str_' + tag);
+        _str_tag.html = str;
+        _str_tag.classList.add('translated faint');
       } else if (LOADED) {
         CHANGED = true;
         setDisabledOnButtons(false);
@@ -93,7 +99,7 @@ function TranslationsModule(localizedText) {
       if (!CHANGED || confirm(CONFIRM_STRING)) {
         show_whirly(LOADING_STRING);
         jQuery.ajax('/translation/edit_translations_ajax_get', {
-          data: {locale: locale, tag: tag, authenticity_token: csrf_token()},
+          data: { locale: locale, tag: tag, authenticity_token: csrf_token() },
           dataType: 'text',
           async: true,
           error: function (response) {
@@ -111,7 +117,7 @@ function TranslationsModule(localizedText) {
     }
 
     function clear_form() {
-      $form_div.html('');
+      $form_div.html = '';
       LOADED = false;
       CHANGED = false;
     }
@@ -119,7 +125,7 @@ function TranslationsModule(localizedText) {
     function show_old_version(id) {
       show_whirly(LOADING_STRING);
       jQuery.ajax('/ajax/old_translation/' + id, {
-        data: {authenticity_token: csrf_token()},
+        data: { authenticity_token: csrf_token() },
         dataType: 'text',
         async: true,
         success: function (text) {
@@ -130,12 +136,12 @@ function TranslationsModule(localizedText) {
     }
 
     function setDisabledOnButtons(disabled) {
-      $save_button.prop('disabled', disabled);
-      $cancel_button.prop('disabled', !disabled);
+      $save_button.setAttribute('disabled', disabled);
+      $cancel_button.setAttribute('disabled', !disabled);
     }
 
     function show_whirly(text) {
-      jQuery('#whirly_text').html(text);
+      document.getElementById('whirly_text').html = text;
       $whirly.center().show();
     }
 
