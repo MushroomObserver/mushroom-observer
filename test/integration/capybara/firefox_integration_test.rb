ced8ecb4dc574553fd9b1add8941bc66ec58cdfa
andrew nimmo <andrnimm@fastmail.fm>
2023-09-24 15:58:09 -0700

    Create firefox_integration_test.rb

---
+++ test/integration/capybara/firefox_integration_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require("test_helper")
+
+class FirefoxIntegrationTest < CapybaraIntegrationTestCase
+  # Uncomment this to try running tests with firefox_headless browser
+  def setup
+    super
+    Capybara.current_driver = :firefox_headless
+  end
+
+  def test_minimal_create_observation
+    rolf = users("rolf")
+    login!(rolf)
+
+    click_on("Create Observation")
+    assert_selector("body.observations__new")
+  end
+end
