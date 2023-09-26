2a15de3443b6d4b59adf1cf4589a82aac6e40039
andrew nimmo <andrnimm@fastmail.fm>
2023-09-25 18:28:16 -0700

    Update main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -133,7 +133,7 @@ const moObserveContent = function () {
       if (element.hasAttribute("id")) {
         const input_id = element.getAttribute("id");
         const type = element.dataset.autocompleter;
-        console.log("Adding autocompleter for " + type)
+        // console.log("Adding autocompleter for " + type)
         // Only initialize the `year` sub-field in Rails date_selects (1i, 2i, 3i)
         if (type != "year" || type == "year" && input_id.indexOf("_1i") > 0) {
           new MOAutocompleter({
