df974e64ac2ba80b37224f263030410b83e6212c
andrew nimmo <andrnimm@fastmail.fm>
2023-09-28 13:37:38 -0700

    Rename test, comments

--- test/system/observation_form_system_test.rb
+++ test/system/observation_form_system_test.rb
@@ -2,7 +2,7 @@
 
 require("application_system_test_case")
 
-class ObservationsSystemTest < ApplicationSystemTestCase
+class ObservationFormSystemTest < ApplicationSystemTestCase
   def test_create_minimal_observation
     rolf = users("rolf")
     login!(rolf)
@@ -20,20 +20,20 @@ class ObservationsSystemTest < ApplicationSystemTestCase
       assert_selector("#where_help",
                       text: "Albion, Mendocino Co., California")
       fill_in("naming_name", with: "Elfin saddle")
-      # don't wait for the autocompleter
+      # don't wait for the autocompleter - we know it's an elfin saddle!
       send_keys(:tab)
       assert_field("naming_name", with: "Elfin saddle")
+      # start typing the location...
       fill_in("observation_place_name", with: locations.first.name[0, 10])
-      # wait for the autocompleter
+      # wait for the autocompleter...
       assert_selector(".auto_complete")
-      send_keys(:down) # to the first match
-      send_keys(:tab)
+      send_keys(:down) # cursor down to the first match
+      send_keys(:tab) # select currently highlighted row
       assert_field("observation_place_name", with: locations.first.name)
       click_commit
     end
 
-    assert_selector("#name_messages",
-                    text: "MO does not recognize the name")
+    assert_selector("#name_messages", text: "MO does not recognize the name")
     assert_flash_warning
     assert_flash_text(
       :form_observations_there_is_a_problem_with_name.t.html_to_ascii
@@ -43,12 +43,13 @@ class ObservationsSystemTest < ApplicationSystemTestCase
       fill_in("naming_name", with: "Coprinus com")
       # wait for the autocompleter!
       assert_selector(".auto_complete")
-      send_keys(:down) # to the first match
-      send_keys(:tab)
-      # unfocus, let field validate. Tab doesn't work here
+      send_keys(:down) # cursor down to the first match
+      send_keys(:tab) # select currently highlighted row
+      # unfocus, let field validate. send_keys(:tab) doesn't work here
       find("#observation_place_name").click
       assert_field("naming_name", with: "Coprinus comatus")
       # Place name should stay filled
+      assert_field("observation_place_name", with: locations.first.name)
       click_commit
     end
 
