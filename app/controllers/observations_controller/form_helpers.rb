9521fa3908aa7df47faf8f5ddef61ba9fa6fb8d6
Joe Cohen <jdcohenesq@example.com>
2023-10-02 01:56:16 +0000

    Improvement to Obs FormHelpers from PR #1619
    - Uses Ruby method `inion` instead of Rails loop
    - Dispenses with one local variable

--- app/controllers/observations_controller/form_helpers.rb
+++ app/controllers/observations_controller/form_helpers.rb
@@ -88,12 +88,13 @@ module ObservationsController::FormHelpers
 
   def init_project_vars_for_reload(obs)
     init_project_vars
-    obs.projects.each do |proj|
-      @projects << proj unless @projects.include?(proj)
-    end
+    @projects = @projects.union(obs.projects)
     @projects.each do |proj|
-      p = params[:project]
-      @project_checks[proj.id] = p.nil? ? false : p["id_#{proj.id}"] == "1"
+      @project_checks[proj.id] = if params[:project].nil?
+                                   false
+                                 else
+                                   params[:project]["id_#{proj.id}"] == "1"
+                                 end
     end
   end
 
@@ -104,9 +105,7 @@ module ObservationsController::FormHelpers
 
   def init_list_vars_for_reload(obs)
     init_list_vars
-    obs.species_lists.each do |list|
-      @lists << list unless @lists.include?(list)
-    end
+    @lists = @lists.union(obs.species_lists)
     @lists.each do |list|
       @list_checks[list.id] = param_lookup([:list, "id_#{list.id}"]) == "1"
     end
