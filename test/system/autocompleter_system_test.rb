3390bccd6a0262bb9cded9d2a32d15fc88b0728b
andrew nimmo <andrnimm@fastmail.fm>
2023-10-02 00:15:05 -0700

    Update autocompleter_system_test.rb

--- test/system/autocompleter_system_test.rb
+++ test/system/autocompleter_system_test.rb
@@ -3,19 +3,20 @@
 require("application_system_test_case")
 
 class AutocompleterSystemTest < ApplicationSystemTestCase
-  roy = users("roy")
-  login!(roy)
+  def test_advanced_search_autocompleters
+    roy = users("roy")
+    login!(roy)
 
-  assert_link("Advanced search")
-  click_on("Advanced search")
+    visit("/search/advanced")
 
-  assert_selector("body.search__advanced")
+    assert_selector("body.search__advanced")
 
-  within("#advanced_search_form") do
-    assert_field("search_name")
-    assert_field("search_user")
-    assert_field("search_location")
-    assert_field("content_filter_region")
-    assert_field("content_filter_clade")
+    within("#advanced_search_form") do
+      assert_field("search_name")
+      assert_field("search_user")
+      assert_field("search_location")
+      assert_field("content_filter_region")
+      assert_field("content_filter_clade")
+    end
   end
 end
