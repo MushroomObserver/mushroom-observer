7f7f474e69c5334ff803154f82c31eb7caff5454
andrew nimmo <andrnimm@fastmail.fm>
2023-10-23 19:06:04 -0700

    Add args to make table_helper more versatile, fix two uses

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -3,7 +3,7 @@
 #  make_table                   # make table from list of arrays
 
 module TableHelper
-  # Create a table out of a list of Arrays.
+  # Create a table out of a list of Arrays and HTML options.
   #
   #   make_table([[1,2],[3,4]])
   #
@@ -20,19 +20,61 @@ module TableHelper
   #     </tr>
   #   </table>
   #
-  def make_table(rows, table_opts = {}, tr_opts = {}, td_opts = {})
-    tag.table(table_opts) do
-      rows.map do |row|
-        make_row(row, tr_opts, td_opts) + make_line(row, td_opts)
-      end.safe_join
+  # args: header, rows, table_opts, header_opts, row_opts, cell_opts
+
+  def make_table(**args)
+    table_opts = default_table_opts(args[:table_opts])
+    args = default_table_args(args)
+
+    tag.table(**table_opts) do
+      if args[:header].present?
+        concat(make_header(args[:header], args[:header_opts], args[:cell_opts]))
+      end
+      if args[:rows].present?
+        args[:rows].map do |row|
+          concat([
+            make_row(row, args[:row_opts], args[:cell_opts]),
+            make_line(args[:cell_opts])
+          ].safe_join)
+        end
+      end
     end
   end
 
-  def make_row(row, tr_opts = {}, td_opts = {})
-    content_tag(:tr, tr_opts) do
+  # Add the default bootstrap `table` CSS class without stomping other classes
+  def default_table_opts(table_opts = {})
+    table_opts[:class] = class_names(table_opts[:class], "table")
+    table_opts
+  end
+
+  # table_opts defaults added above
+  def default_table_args(args)
+    {
+      header: [],
+      rows: [],
+      header_opts: {},
+      row_opts: {},
+      cell_opts: {}
+    }.merge(args)
+  end
+
+  def make_header(header, header_opts, cell_opts)
+    tag.th(**header_opts) do
+      if header.is_a?(Array)
+        header.map do |cell|
+          make_cell(cell, cell_opts)
+        end.safe_join
+      else
+        header
+      end
+    end
+  end
+
+  def make_row(row, row_opts, cell_opts)
+    tag.tr(**row_opts) do
       if row.is_a?(Array)
         row.map do |cell|
-          make_cell(cell, td_opts)
+          make_cell(cell, cell_opts)
         end.safe_join
       else
         row
@@ -40,12 +82,13 @@ module TableHelper
     end
   end
 
-  def make_cell(cell, td_opts = {})
-    tag.td(cell.to_s, td_opts)
+  def make_cell(cell, cell_opts)
+    tag.td(**cell_opts) { cell.to_s }
   end
 
-  def make_line(_row, td_opts)
-    colspan = td_opts[:colspan]
+  # ?
+  def make_line(cell_opts)
+    colspan = cell_opts[:colspan]
     if colspan
       tag.tr(class: "MatrixLine") do
         tag.td(tag.hr, class: "MatrixLine", colspan: colspan)
