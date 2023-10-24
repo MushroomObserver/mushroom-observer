0358fdd5a4c510005758752d286c7522b9327fea
andrew nimmo <andrnimm@fastmail.fm>
2023-10-23 19:54:28 -0700

    Add arg for row headers

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -3,40 +3,46 @@
 #  make_table                   # make table from list of arrays
 
 module TableHelper
-  # Create a table out of a list of Arrays and HTML options.
+  # Create a table out of Arrays and HTML options.
   #
-  #   make_table([[1,2],[3,4]])
+  #   make_table(
+  #     header: ["This", "That"],
+  #     rows: [[1,2],[3,4]]),
+  #     table_opts: { class: "table-striped" },
+  #     cell_opts: { class: "mr-4" }
+  #   )
   #
   # Produces:
   #
-  #   <table>
+  #   <table class="table table-striped">
   #     <tr>
-  #       <td>1</td>
-  #       <td>2</td>
+  #       <th class="mr-4">This</td>
+  #       <th class="mr-4">That</td>
   #     </tr>
   #     <tr>
-  #       <td>3</td>
-  #       <td>4</td>
+  #       <td class="mr-4">1</td>
+  #       <td class="mr-4">2</td>
+  #     </tr>
+  #     <tr>
+  #       <td class="mr-4">3</td>
+  #       <td class="mr-4">4</td>
   #     </tr>
   #   </table>
   #
   # args: header, rows, table_opts, header_opts, row_opts, cell_opts
+  #       row_headers (bool)
 
   def make_table(**args)
     args = default_table_args(args)
     table_opts = default_table_opts(args[:table_opts])
 
     tag.table(**table_opts) do
-      if args[:header].present?
-        concat(make_header(args[:header], args[:header_opts], args[:cell_opts]))
-      end
+      concat(make_col_headers(args)) if args[:header].present?
+
       if args[:rows].present?
-        args[:rows].map do |row|
-          concat([
-            make_row(row, args[:row_opts], args[:cell_opts]),
-            make_line(args[:cell_opts])
-          ].safe_join)
-        end
+        concat(args[:rows].map do |row|
+          make_row(row, args)
+        end.safe_join)
       end
     end
   end
@@ -55,15 +61,19 @@ module TableHelper
       table_opts: {},
       header_opts: {},
       row_opts: {},
-      cell_opts: {}
+      cell_opts: {},
+      row_headers: false
     }.merge(args)
   end
 
-  def make_header(header, header_opts, cell_opts)
-    tag.th(**header_opts) do
-      if header.is_a?(Array)
-        header.map do |cell|
-          make_cell(cell, cell_opts)
+  def make_col_headers(headers, args)
+    tag.tr(**args[:row_opts]) do
+      if headers.is_a?(Array)
+        th_args = args
+        th_args[:cell_opts] = th_args[:cell_opts].merge({ scope: "col" })
+
+        headers.map do |header|
+          make_header(header, th_args)
         end.safe_join
       else
         header
@@ -71,31 +81,44 @@ module TableHelper
     end
   end
 
-  def make_row(row, row_opts, cell_opts)
-    tag.tr(**row_opts) do
+  def make_header(header, args)
+    tag.th(**args[:cell_opts]) { header.to_s }
+  end
+
+  # pass row_headers: true to get a <th scope="row"> instead of <td>
+  # at the head of each row.
+  def make_row(row, args)
+    tag.tr(**args[:row_opts]) do
       if row.is_a?(Array)
-        row.map do |cell|
-          make_cell(cell, cell_opts)
+        th_args = args # don't overwrite args, keep assignment out of loop
+        th_args[:cell_opts] = th_args[:cell_opts].merge({ scope: "row" })
+
+        row.map.with_index do |cell, index|
+          if index.zero? && args[:row_headers] == true
+            make_header(cell, th_args)
+          else
+            make_cell(cell, args)
+          end
         end.safe_join
       else
-        row
+        row # if the row is a string, we're not going to force row headers
       end
     end
   end
 
-  def make_cell(cell, cell_opts)
-    tag.td(**cell_opts) { cell.to_s }
+  def make_cell(cell, args)
+    tag.td(**args[:cell_opts]) { cell.to_s }
   end
 
   # ?
-  def make_line(cell_opts)
-    colspan = cell_opts[:colspan]
-    if colspan
-      tag.tr(class: "MatrixLine") do
-        tag.td(tag.hr, class: "MatrixLine", colspan: colspan)
-      end
-    else
-      safe_empty
-    end
-  end
+  # def make_line(cell_opts)
+  #   colspan = cell_opts[:colspan]
+  #   if colspan
+  #     tag.tr(class: "MatrixLine") do
+  #       tag.td(tag.hr, class: "MatrixLine", colspan: colspan)
+  #     end
+  #   else
+  #     safe_empty
+  #   end
+  # end
 end
