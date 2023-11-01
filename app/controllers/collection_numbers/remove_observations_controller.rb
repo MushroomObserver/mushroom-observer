e0dbe651f4a21e8d5edb82fe41522d29ff7b2281
andrew nimmo <andrnimm@fastmail.fm>
2023-11-01 14:32:48 -0700

    Update remove_observations_controller.rb

--- app/controllers/collection_numbers/remove_observations_controller.rb
+++ app/controllers/collection_numbers/remove_observations_controller.rb
@@ -8,15 +8,34 @@
 module CollectionNumbers
   class RemoveObservationsController < ApplicationController
     before_action :login_required
+    before_action :pass_query_params
+
+    # The edit action exists just to present a dialog box explaining
+    # what the action does, with a remove button (to the :update action)
+    # Should only be hit by turbo_stream
+    def edit
+      return unless init_ivars_for_edit
+      return unless make_sure_can_delete!(@collection_number)
+
+      @title = :show_observation_remove_collection_number.l
+
+      respond_to do |format|
+        format.html
+        format.turbo_stream do
+          render(
+            partial: "shared/modal_form",
+            locals: {
+              title: @title,
+              identifier: "collection_number_observation",
+              form: "collection_numbers/remove_observations/form"
+            }
+          ) and return
+        end
+      end
+    end
 
     def update
-      pass_query_params
-      @collection_number = find_or_goto_index(CollectionNumber,
-                                              params[:collection_number_id])
-      return unless @collection_number
-
-      @observation = find_or_goto_index(Observation, params[:observation_id])
-      return unless @observation
+      return unless init_ivars_for_edit
 
       return unless make_sure_can_delete!(@collection_number)
 
@@ -27,7 +46,7 @@ module CollectionNumbers
         format.html do
           redirect_with_query(observation_path(@observation.id))
         end
-        format.js do
+        format.turbo_stream do
           render(
             partial: "observations/show/section_update",
             locals: { identifier: "collection_numbers" }
@@ -37,6 +56,18 @@ module CollectionNumbers
     end
 
     private
+
+    def init_ivars_for_edit
+      @collection_number = find_or_goto_index(CollectionNumber,
+                                              params[:collection_number_id])
+      return false unless @collection_number
+
+      @observation = find_or_goto_index(Observation,
+                                        params[:observation_id])
+      return false unless @observation
+
+      true
+    end
 
     def make_sure_can_delete!(collection_number)
       return true if collection_number.can_edit? || in_admin_mode?
