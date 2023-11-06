ab96900721375f31b57baaffd10dc2bf6cfd6ef7
andrew nimmo <andrnimm@fastmail.fm>
2023-11-05 23:28:25 -0800

    rename js controlelrs with hyphens, fix year-input

--- app/javascript/controllers/year-input_controller.js
+++ app/javascript/controllers/year-input_controller.js
@@ -3,7 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="year-input"
 export default class extends Controller {
   connect() {
+    this.element.dataset.connected = "true";
     this.id = this.element.getAttribute("id");
+    console.log(this.id)
 
     if (this.id.indexOf("_1i") > 0) {
       this.turnIntoTextField();
@@ -13,15 +15,16 @@ export default class extends Controller {
   turnIntoTextField() {
     // copy the attributes
     this.name = this.element.getAttribute("name");
-    this.classList = this.element.classList;
-    this.style = this.element.getAttribute("style");
+    this.classList = this.element.classList || "";
+    this.style = this.element.getAttribute("style") || "";
     this.value = this.element.value;
+    this.dataset = this.element.dataset;
 
     this.new_elem = document.createElement("input");
     this.new_elem.type = "text";
-    this.new_elem.classList = classList;
-    this.new_elem.style = style;
-    this.new_elem.value = value;
+    this.new_elem.setAttribute("class", this.classList.value);
+    this.new_elem.style = this.style;
+    this.new_elem.value = this.value;
     this.new_elem.setAttribute("size", 4);
 
     if (this.element[0].onchange)
@@ -30,7 +33,5 @@ export default class extends Controller {
     this.element.replaceWith(this.new_elem);
     this.new_elem.setAttribute("id", this.id);
     this.new_elem.setAttribute("name", this.name);
-
-    this.element = this.new_elem;
   }
 }
