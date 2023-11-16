7ad94b672ee49fa4af7ffa2c4a0df1eb47073bd7
andrew nimmo <andrnimm@fastmail.fm>
2023-11-16 01:46:20 -0800

    Clean up map args

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -4,7 +4,7 @@ module MapHelper
   # args could include query_param.
   # returns an array of mapsets, each suitable for a marker or box
   # TODO: remove local_assigns here and figure out what the legit map_args are
-  def make_map(objects, local_assigns = {})
+  def make_map(objects, args = {})
     default_args = {
       map_div: "map_div",
       controller: "map",
@@ -13,13 +13,11 @@ module MapHelper
       controls: [:large_map, :map_type].to_json,
       location_format: User.current_location_format, # has a default
     }
-    map_args = local_assigns.except(:objects, :nothing_to_map,
-                                    :location, :observation, :herbarium)
+    map_args = args.except(:objects, :nothing_to_map)
     map_args = provide_defaults(map_args, **default_args)
     map_args[:collection] = mappable_collection(objects, map_args).to_json
     map_args[:localization] = {
-      nothing_to_map: local_assigns[:nothing_to_map] ||
-                      :runtime_map_nothing_to_map.t,
+      nothing_to_map: args[:nothing_to_map] || :runtime_map_nothing_to_map.t,
       observations: :Observations.t,
       locations: :Locations.t,
       show_all: :show_all.t,
