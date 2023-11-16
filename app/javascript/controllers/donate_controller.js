884246152c7bb622aa2b84e0f279f373ad8727b7
andrew nimmo <andrnimm@fastmail.fm>
2023-11-15 20:27:33 -0800

    Scaffold new controllers

---
+++ app/javascript/controllers/donate_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="donate"
+export default class extends Controller {
+  connect() {
+  }
+}
---
+++ app/javascript/controllers/image_export_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="image-export"
+export default class extends Controller {
+  connect() {
+  }
+}
