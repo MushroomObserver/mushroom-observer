b935ccaa7aecf2c0f1b3fecf183d89683f878a84
andrew nimmo <andrnimm@fastmail.fm>
2023-11-15 21:33:24 -0800

    donate controller

--- app/javascript/controllers/donate_controller.js
+++ app/javascript/controllers/donate_controller.js
@@ -2,6 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="donate"
 export default class extends Controller {
+  static targets = ['otherCheck', 'otherAmount']
+
   connect() {
+  }
+
+  checkOther() {
+    this.otherCheckTarget.checked = true;
+  }
+
+  convert() {
+    this.otherAmountTarget.value =
+      this.otherAmountTarget.value.replace(/[^0-9]/g, '')
   }
 }
