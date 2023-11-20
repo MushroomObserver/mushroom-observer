1578b7c31b110d562d871d3523f64d4095af921f
andrew nimmo <andrnimm@fastmail.fm>
2023-11-19 23:26:50 -0800

    more archiving

---
+++ app/assets/pre-trash/application.js
@@ -0,0 +1,24 @@
+//= require jquery3
+//= require jquery_extensions
+//= require rails-ujs
+//= require bootstrap-sprockets
+//= require jstz
+//= require mo_autocompleter
+//= require box_resizer
+//= require lazyload_verlok
+//= require csrf_token
+//= require external_link
+//= require image_export
+//= require validate_file_input_fields
+//= require main
+//= require mo_events
+//= require mo_images_uploader
+//= require suggestions
+//= require thumbnail_map
+//= require naming_vote_ajax
+//= require lightbox
+
+lightbox.option({
+  'fadeDuration': 0,
+  'resizeDuration': 0
+});
