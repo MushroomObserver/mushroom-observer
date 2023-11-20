3f3ec3abb257a7d86cbf963f63fa3589341e4369
andrew nimmo <andrnimm@fastmail.fm>
2023-11-20 11:45:59 -0800

    Update year-input_controller.js

--- app/javascript/controllers/year-input_controller.js
+++ app/javascript/controllers/year-input_controller.js
@@ -3,22 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="year-input"
 export default class extends Controller {
   connect() {
-    this.element.dataset.connected = "true";
     this.id = this.element.getAttribute("id");
     // console.log(this.id)
 
-    if (this.id.indexOf("_1i") > 0) {
+    if (this.id && this.id.indexOf("_1i") > 0) {
+      this.element.setAttribute("data-stimulus", "connected")
+      // copy the attributes
+      this.name = this.element.getAttribute("name");
+      this.classList = this.element.classList || "";
+      this.style = this.element.getAttribute("style") || "";
+      this.value = this.element.value;
+
       this.turnIntoTextField();
     }
   }
 
-  turnIntoTextField() {
-    // copy the attributes
-    this.name = this.element.getAttribute("name");
-    this.classList = this.element.classList || "";
-    this.style = this.element.getAttribute("style") || "";
-    this.value = this.element.value;
+  disconnect() {
+    this.element.setAttribute("data-stimulus", "disconnected")
+  }
 
+  turnIntoTextField() {
     this.new_elem = document.createElement("input");
     this.new_elem.type = "text";
     this.new_elem.setAttribute("class", this.classList.value);
@@ -28,12 +32,15 @@ export default class extends Controller {
     // dataset must be set one at the time
     if (this.element.dataset != undefined) {
       for (const prop in this.element.dataset) {
-        this.new_elem.setAttribute('data-' + prop, this.element.dataset[prop]);
+        if (!["controller", "stimulus"].includes(prop))
+          this.new_elem.setAttribute('data-' + prop,
+            this.element.dataset[prop]);
       }
     }
 
-    this.element.replaceWith(this.new_elem);
-    this.new_elem.setAttribute("id", this.id);
     this.new_elem.setAttribute("name", this.name);
+    this.new_elem.setAttribute("id", this.id);
+    this.element.replaceWith(this.new_elem);
+    this.element.remove();
   }
 }
