e44e68f2b92e6f42d349485ceb96a76dfcbcc888
andrew nimmo <andrnimm@fastmail.fm>
2023-11-20 19:41:35 -0800

    Create observation_show_system_test.rb

---
+++ test/system/observation_show_system_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require("application_system_test_case")
+
+class ObservationShowSystemTest < ApplicationSystemTestCase
+  def test_add_and_edit_collection_number
+  end
+
+  def test_add_and_edit_fungarium_record
+  end
+
+  def test_add_and_edit_sequence
+  end
+
+  def test_add_and_edit_external_link
+  end
+
+  def test_add_and_edit_naming
+  end
+
+  def test_add_and_edit_naming_vote
+  end
+
+  def test_add_and_edit_comment
+  end
+end
