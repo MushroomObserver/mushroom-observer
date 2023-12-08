426536a6aa7f21e2f044f5abb648c0ecef6136b3
Nathan Wilson <nathan@collectivesource.com>
2023-12-08 17:07:44 -0500

    Fix trust level migration

--- db/migrate/20231208212458_add_trust_level_to_project_members.rb
+++ db/migrate/20231208212458_add_trust_level_to_project_members.rb
@@ -2,7 +2,7 @@ class AddTrustLevelToProjectMembers < ActiveRecord::Migration[6.1]
   def up
     add_column :project_members, :trust_level, :integer, default: 1, null: false
     ProjectMember.all.find_each do |pm|
-      pm.trust_level = ProjectMember.trust_levels[:hidden_gps] if pm.trusted
+      pm.update!(trust_level: "hidden_gps") if pm.trusted
     end
     remove_column :project_members, :trusted, :boolean, default: false, null: false
   end
@@ -10,7 +10,7 @@ class AddTrustLevelToProjectMembers < ActiveRecord::Migration[6.1]
   def down
     add_column :project_members, :trusted, :boolean, default: false, null: false
     ProjectMember.all.find_each do |pm|
-      pm.trusted = (pm.trust_level != ProjectMember.trust_levels[:no_trust])
+      pm.update!(trusted: pm.trust_level != "no_trust")
     end
     remove_column :project_members, :trust_level, :integer, default: 1, null: false
   end
