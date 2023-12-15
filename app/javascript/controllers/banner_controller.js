bc7384168e41325ed2c9ee4ee45f23be15eeb451
andrew nimmo <andrnimm@fastmail.fm>
2023-12-14 19:54:45 -0800

    Fix banner dismiss cookie and timezone cookie

--- app/javascript/controllers/banner_controller.js
+++ app/javascript/controllers/banner_controller.js
@@ -6,15 +6,15 @@ export default class extends Controller {
     this.element.dataset.stimulus = "connected";
   }
 
-  setCookie() {
+  setCookie({ params: { time } }) {
     const date = new Date(),
       expiresInDays = 30,
-      bannerTime = BANNER_TIME,
-      cookie_name = "hideBanner"
+      cookie_name = "hideBanner2=" + time + "; ";
 
     date.setTime(date.getTime() + (expiresInDays * 24 * 60 * 60 * 1000));
-    const expiresText = "expires=" + d.toUTCString();
-    document.cookie = cookie_name + "=" + bannerTime + "; " + expiresText
-      + ";samesite=lax;path=/";
+    const expiresText = "expires=" + date.toUTCString() + "; ";
+
+    document.cookie =
+      cookie_name + expiresText + "samesite=lax;path=/";
   }
 }
