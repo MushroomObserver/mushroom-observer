a5221f042c1982f5e855af628c074cb8ebed928a
andrew nimmo <andrnimm@fastmail.fm>
2023-12-27 00:51:41 -0500

    Set active_support.cache_format_version

--- config/application.rb
+++ config/application.rb
@@ -91,6 +91,9 @@ module MushroomObserver
     # Rails 6.1 can auto-generate HTML comments with the template filename
     # Unfortunately this is also added to email templates!
     # config.action_view.annotate_rendered_view_with_filenames = true
+
+    # Not sure we're even using Rails caching yet, but this gets us current
+    config.active_support.cache_format_version = 7.1
   end
 end
 
