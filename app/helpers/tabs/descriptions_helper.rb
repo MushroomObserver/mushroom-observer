9e23b2db9e6b0581b12e4567342a249426c2bd57
andrew nimmo <andrnimm@fastmail.fm>
2024-01-06 19:17:26 -0800

    Update descriptions_helper.rb
    Clean up

--- app/helpers/tabs/descriptions_helper.rb
+++ app/helpers/tabs/descriptions_helper.rb
@@ -54,9 +54,6 @@ module Tabs
          send(:"new_#{type}_description_path",
               { clone: description.id, id: description.parent_id })
        ),
-      #  { controller: description.show_controller,
-      #    action: :new, id: description.parent_id,
-      #    clone: description.id, q: get_query_param },
        { help: :show_description_clone_help.l,
          class: tab_id(__method__.to_s), icon: :clone }]
     end
@@ -68,8 +65,6 @@ module Tabs
        add_query_param(
          send(:"#{type}_description_merges_form_path", description.id)
        ),
-      #  { controller: "#{description.show_controller}/merges",
-      #    action: :new, id: description.id, q: get_query_param },
        { help: :show_description_merge_help.l,
          class: tab_id(__method__.to_s), icon: :merge }]
     end
@@ -82,8 +77,6 @@ module Tabs
        add_query_param(
          send(:"#{type}_description_moves_form_path", description.id)
        ),
-      #  { controller: "#{description.show_controller}/moves",
-      #    action: :new, id: description.id, q: get_query_param },
        { help: :show_description_move_help.l(parent: parent_type),
          class: tab_id(__method__.to_s), icon: :move }]
     end
@@ -95,8 +88,6 @@ module Tabs
        add_query_param(
          send(:"edit_#{type}_description_permissions_path", description.id)
        ),
-      #  { controller: "#{description.show_controller}/permissions",
-      #    action: :edit, id: description.id, q: get_query_param },
        { help: :show_description_adjust_permissions_help.l,
          class: tab_id(__method__.to_s), icon: :adjust }]
     end
@@ -109,9 +100,6 @@ module Tabs
        add_query_param(
          send(:"make_default_#{type}_description_path", description.id)
        ),
-      #  { controller: "#{description.show_controller}/defaults",
-      #    action: :update, id: description.id,
-      #    q: get_query_param },
        { button: :put, help: :show_description_make_default_help.l,
          class: tab_id(__method__.to_s), icon: :make_default }]
     end
@@ -132,9 +120,6 @@ module Tabs
        add_query_param(
          send(:"#{type}_description_publish_path", description.id)
        ),
-      #  { controller: "#{description.show_controller}/publish",
-      #    action: :update, id: description.id,
-      #    q: get_query_param },
        { button: :put, help: :show_description_publish_help.l,
          class: tab_id(__method__.to_s), icon: :publish }]
     end
@@ -145,9 +130,6 @@ module Tabs
          send(:"new_#{type}_description_path",
               { project: project.id, source: "project", id: object.id })
        ),
-      #  { controller: "#{object.show_controller}/descriptions",
-      #    action: :new, id: object.id,
-      #    project: project.id, source: "project" },
        { class: tab_id(__method__.to_s) }]
     end
 
