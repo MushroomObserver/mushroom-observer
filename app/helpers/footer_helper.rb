d06a8b87d23bee207f96a6f25eba90b336862577
andrew nimmo <andrnimm@fastmail.fm>
2024-01-06 20:43:02 -0800

    Few more erb refinements

--- app/helpers/footer_helper.rb
+++ app/helpers/footer_helper.rb
@@ -21,7 +21,7 @@ module FooterHelper
                          html_undescribed_obj_authors_and_editors(obj)
                        end
 
-    content_tag(:p, authors + safe_br + editors)
+    tag.p(authors + safe_br + editors)
   end
 
   ###############################################################
