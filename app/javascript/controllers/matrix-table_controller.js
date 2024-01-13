d03acfd81d78da63072f4d861e96da6810e3c152
andrew nimmo <andrnimm@fastmail.fm>
2024-01-12 23:28:37 -0800

    Add spacers in js

--- app/javascript/controllers/matrix-table_controller.js
+++ app/javascript/controllers/matrix-table_controller.js
@@ -5,9 +5,47 @@ export default class extends Controller {
   connect() {
     this.element.dataset.stimulus = "connected";
 
+    this.matrixBoxes = document.querySelectorAll('.matrix-box')
     this.boxes = document.querySelectorAll('.matrix-box .panel-sizing')
     this.footers = document.querySelectorAll('.matrix-box .log-footer')
+
+    this.add_spacers()
     this.rearrange()
+  }
+
+  add_spacers() {
+    this.matrixBoxes.forEach((element, i) => {
+      if (i % 2 == 0) {
+        element.insertAdjacentElement(this.smSpacer())
+      }
+      if (i % 3 == 0) {
+        element.insertAdjacentElement(this.mdSpacer())
+      }
+      if (i % 4 == 0) {
+        element.insertAdjacentElement(this.lgSpacer())
+      }
+    })
+  }
+
+  smSpacer() {
+    const spacer = document.createElement('div')
+    spacer.classList.add('hidden').add('visible-sm-block')
+    spacer.style.clear = "left"
+    return spacer
+  }
+
+  mdSpacer() {
+    const spacer = document.createElement('div')
+    spacer.classList.add('hidden').add('visible-md-block')
+    spacer.style.clear = "left"
+    return spacer
+  }
+
+  lgSpacer() {
+    const spacer = document.createElement('div')
+    spacer.classList.add('hidden').add('visible-lg-block')
+    spacer.style.clear = "left"
+    return spacer
   }
 
   rearrange() {
