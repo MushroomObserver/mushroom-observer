17e3dc7b63a7dcc1825668e153687447f1b26e02
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2024-01-12 16:54:02 -1000

    Revise failing assertion
    I'm changing my approach to solving the underlying problem.
    Instead of adding a message below:
    Add a flash error that's complete without additional explanation.
    
    Adding a message below is too convoluted for me to figure out.

--- test/integration/capybara/namings_integration_test.rb
+++ test/integration/capybara/namings_integration_test.rb
@@ -49,10 +49,8 @@ class NamingsIntegrationTest < CapybaraIntegrationTestCase
     namer_session.assert_selector("body.namings__create")
     # (I don't care so long as it says something.)
     assert_flash_text(/\S/, session: namer_session)
-    namer_session.assert_selector("#flash_notices", text: "There is a problem with the name. ")
-    namer_session.assert_selector("#flash_notices", text: "See message below")
-    namer_session.assert_selector("#name_messages", text: :form_naming_what_missing.l)
-
+    namer_session.assert_selector("#flash_notices", text: "There is a problem with the name.")
+s
     namer_session.within("#obs_#{obs.id}_naming_form") do |form|
       form.fill_in("naming_name", with: text_name)
       form.first("input[type='submit']").click
