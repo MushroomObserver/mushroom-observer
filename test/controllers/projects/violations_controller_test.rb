ade6b8c24f66570d069bc01c3a39fae697d128f9
Joe Cohen <jdcohenesq@gmail.com>
2024-01-26 12:59:07 -0800

    Finish 1st draft of Violations constraints_summary

--- test/controllers/projects/violations_controller_test.rb
+++ test/controllers/projects/violations_controller_test.rb
@@ -16,6 +16,25 @@ module Projects
       get(:index, params: { project_id: project.id })
 
       assert_response(:success)
+
+      assert_select("#title", { text: /#{project.title}/ },
+                    "Page title should include project name")
+
+      assert_select("#constraints_summary", { text: /#{:CONSTRAINTS.l}/ })
+      assert_select(
+        "#constraints_summary", { text: /#{project.date_range}/ },
+        "Missing Project date range"
+      )
+      assert_select(
+        "#constraints_summary",
+        { text: /#{project.location.north} \S+ #{project.location.south}/ },
+        "Missing Project latitude range"
+      )
+      assert_select(
+        "#constraints_summary",
+        { text: /#{project.location.west} \S+ #{project.location.east}/ },
+        "Missing Project longitude rants"
+      )
     end
   end
 end
