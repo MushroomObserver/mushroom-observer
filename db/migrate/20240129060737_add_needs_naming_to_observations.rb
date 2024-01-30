80403923b23b1be962f83c96e399728673732c97
andrew nimmo <andrnimm@fastmail.fm>
2024-01-30 00:31:07 -0800

    Update 20240129060737_add_needs_naming_to_observations.rb

--- db/migrate/20240129060737_add_needs_naming_to_observations.rb
+++ db/migrate/20240129060737_add_needs_naming_to_observations.rb
@@ -6,7 +6,7 @@ class AddNeedsNamingToObservations < ActiveRecord::Migration[7.1]
   end
 
   def down
-    remove_column :observations, :needs_naming
     remove_index :observations, :needs_naming, name: :needs_naming_index
+    remove_column :observations, :needs_naming
   end
 end
