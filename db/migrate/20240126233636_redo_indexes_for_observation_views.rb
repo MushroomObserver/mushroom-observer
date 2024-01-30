6b0b7e15767bc561ba0b6bb15bf9914967708729
andrew nimmo <andrnimm@fastmail.fm>
2024-01-30 00:31:14 -0800

    Update 20240126233636_redo_indexes_for_observation_views.rb

--- db/migrate/20240126233636_redo_indexes_for_observation_views.rb
+++ db/migrate/20240126233636_redo_indexes_for_observation_views.rb
@@ -8,7 +8,7 @@ class RedoIndexesForObservationViews < ActiveRecord::Migration[7.1]
 
   def down
     remove_index :observation_views, :observation_id, name: :observation_index
-    remove_index :observation_views, :observation_id, name: :user_index
+    remove_index :observation_views, :user_id, name: :user_index
     add_index :observation_views, [:observation_id, :user_id],
               name: :user_observation_index
   end
