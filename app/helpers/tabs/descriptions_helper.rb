650d8fd940f72beb78efccaf705d2ec8cc99969c
andrew nimmo <andrnimm@fastmail.fm>
2024-02-11 19:51:08 -0800

    Clean up and fix test for edit link

--- app/helpers/tabs/descriptions_helper.rb
+++ app/helpers/tabs/descriptions_helper.rb
@@ -6,18 +6,18 @@ module Tabs
     # Links for the tabset
     def show_description_tabs(description:)
       type = description.parent.type_tag
-      admin = is_admin?(description)
+      # admin = is_admin?(description)
       # assemble HTML for "tabset" for show_{type}_description
       [
-        description_parent_tab(description, type),
-        edit_description_tab(description, type),
-        destroy_description_tab(description, admin),
-        clone_description_tab(description, type),
-        merge_description_tab(description, type, admin),
-        adjust_description_permissions_tab(description, type, admin),
-        make_description_default_tab(description, type),
-        description_project_tab(description),
-        publish_description_draft_tab(description, type, admin)
+        description_parent_tab(description, type)
+        # edit_description_tab(description, type),
+        # destroy_description_tab(description, admin),
+        # clone_description_tab(description, type),
+        # merge_description_tab(description, type, admin),
+        # adjust_description_permissions_tab(description, type, admin),
+        # make_description_default_tab(description, type),
+        # description_project_tab(description),
+        # publish_description_draft_tab(description, type, admin)
       ].reject(&:empty?)
     end
 
@@ -25,8 +25,8 @@ module Tabs
       type = desc.parent.type_tag
       admin = is_admin?(desc)
       [
-        edit_button(target: desc, icon: :edit),
-        destroy_button(target: desc, icon: :delete),
+        writer?(desc) ? edit_button(target: desc, icon: :edit) : nil,
+        admin ? destroy_button(target: desc, icon: :delete): nil,
         icon_link_to(*clone_description_tab(desc, type)),
         icon_link_to(*merge_description_tab(desc, type, admin)),
         icon_link_to(*adjust_description_permissions_tab(desc, type, admin)),
