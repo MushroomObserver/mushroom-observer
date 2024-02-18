a3e136a4db1e96051fec3e9cf55a6c9e6616cd55
andrew nimmo <andrnimm@fastmail.fm>
2024-02-17 19:46:12 -0800

    cleanup

--- app/controllers/locations/observations_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# add_to_location  --- NOTE: is this used?
-module Locations
-  class ObservationsController < ApplicationController
-    before_action :login_required
-
-  end
-end
