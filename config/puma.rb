e428f924bd8c553defcebe1caa6102e7fb04b821
andrew nimmo <andrnimm@fastmail.fm>
2024-02-20 01:24:55 -0800

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+threads_count = Integer(ENV["MAX_THREADS"] || 4)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup          DefaultRackup
+port            ENV["PORT"]     || 3000
+environment     ENV["RACK_ENV"] || "production"
+redirect_stderr "#{APP_PATH}/log/puma.stderr.log"
+redirect_stdout "#{APP_PATH}/log/puma.stdout.log"
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
+
