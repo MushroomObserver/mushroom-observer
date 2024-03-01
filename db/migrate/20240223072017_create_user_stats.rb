2675a37d5bd86fd51fb4f7aa6f8680d9a0016f43
andrew nimmo <andrnimm@fastmail.fm>
2024-03-01 01:11:20 -0800

    Update 20240223072017_create_user_stats.rb
    alpha

--- db/migrate/20240223072017_create_user_stats.rb
+++ db/migrate/20240223072017_create_user_stats.rb
@@ -2,6 +2,7 @@ class CreateUserStats < ActiveRecord::Migration[7.1]
   def up
     create_table :user_stats do |t|
       t.integer(:user_id, default: 0, null: false)
+
       t.integer(:comments, default: 0, null: false)
       t.integer(:images, default: 0, null: false)
       t.integer(:location_description_authors, default: 0, null: false)
@@ -18,24 +19,24 @@ class CreateUserStats < ActiveRecord::Migration[7.1]
       t.integer(:sequenced_observations, default: 0, null: false)
       t.integer(:species_list_entries, default: 0, null: false)
       t.integer(:species_lists, default: 0, null: false)
-      t.integer(:votes, default: 0, null: false)
       t.integer(:translation_strings_versions, default: 0, null: false)
+      t.integer(:votes, default: 0, null: false)
 
-      t.integer(:pl, default: 0, null: false)
-      t.integer(:el, default: 0, null: false)
-      t.integer(:es, default: 0, null: false)
-      t.integer(:ru, default: 0, null: false)
-      t.integer(:pt, default: 0, null: false)
-      t.integer(:de, default: 0, null: false)
-      t.integer(:fr, default: 0, null: false)
-      t.integer(:it, default: 0, null: false)
-      t.integer(:tr, default: 0, null: false)
-      t.integer(:jp, default: 0, null: false)
-      t.integer(:zh, default: 0, null: false)
-      t.integer(:uk, default: 0, null: false)
       t.integer(:ar, default: 0, null: false)
       t.integer(:be, default: 0, null: false)
+      t.integer(:de, default: 0, null: false)
+      t.integer(:el, default: 0, null: false)
+      t.integer(:es, default: 0, null: false)
       t.integer(:fa, default: 0, null: false)
+      t.integer(:fr, default: 0, null: false)
+      t.integer(:it, default: 0, null: false)
+      t.integer(:jp, default: 0, null: false)
+      t.integer(:pl, default: 0, null: false)
+      t.integer(:pt, default: 0, null: false)
+      t.integer(:ru, default: 0, null: false)
+      t.integer(:tr, default: 0, null: false)
+      t.integer(:uk, default: 0, null: false)
+      t.integer(:zh, default: 0, null: false)
 
       t.timestamps
     end
