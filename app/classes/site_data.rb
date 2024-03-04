2c7242a73277cfe4f2cf76f5918cabe29d793f24
andrew nimmo <andrnimm@fastmail.fm>
2024-03-04 14:40:55 -0800

    Fix get_applicable_field
    It needs to look up the key by the value, not the other way around

--- app/classes/site_data.rb
+++ app/classes/site_data.rb
@@ -178,10 +178,9 @@ class SiteData
   end
 
   def self.get_applicable_field(obj)
-    table = obj.class.to_s.tableize
-    field = table.to_sym
+    table = obj.class.to_s.tableize.to_sym
 
-    ALL_FIELDS.key?(field) ? (ALL_FIELDS[field][:table] || field) : field
+    ALL_FIELDS.select { |_f, e| e[:table] == table }.keys.first || table
   end
 
   # Return stats for entire site. Returns simple hash mapping category to
