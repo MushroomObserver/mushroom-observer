1cc330bdb03ff93220370310ad7bd5cdf0de611f
Nathan Wilson <nathan@collectivesource.com>
2024-03-11 20:32:37 -0400

    Rubocop

--- app/controllers/field_slips_controller.rb
+++ app/controllers/field_slips_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class FieldSlipsController < ApplicationController
-  before_action :set_field_slip, only: %i[ show edit update destroy ]
+  before_action :set_field_slip, only: [:show, :edit, :update, :destroy]
 
   # GET /field_slips or /field_slips.json
   def index
@@ -7,8 +9,7 @@ class FieldSlipsController < ApplicationController
   end
 
   # GET /field_slips/1 or /field_slips/1.json
-  def show
-  end
+  def show; end
 
   # GET /field_slips/new
   def new
@@ -16,8 +17,7 @@ class FieldSlipsController < ApplicationController
   end
 
   # GET /field_slips/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /field_slips or /field_slips.json
   def create
@@ -25,11 +25,16 @@ class FieldSlipsController < ApplicationController
 
     respond_to do |format|
       if @field_slip.save
-        format.html { redirect_to field_slip_url(@field_slip), notice: "Field slip was successfully created." }
-        format.json { render :show, status: :created, location: @field_slip }
+        format.html do
+          redirect_to(field_slip_url(@field_slip),
+                      notice: field_slip_created.t)
+        end
+        format.json { render(:show, status: :created, location: @field_slip) }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @field_slip.errors, status: :unprocessable_entity }
+        format.html { render(:new, status: :unprocessable_entity) }
+        format.json do
+          render(json: @field_slip.errors, status: :unprocessable_entity)
+        end
       end
     end
   end
@@ -38,11 +43,16 @@ class FieldSlipsController < ApplicationController
   def update
     respond_to do |format|
       if @field_slip.update(field_slip_params)
-        format.html { redirect_to field_slip_url(@field_slip), notice: "Field slip was successfully updated." }
-        format.json { render :show, status: :ok, location: @field_slip }
+        format.html do
+          redirect_to(field_slip_url(@field_slip),
+                      notice: field_slip_updated.t)
+        end
+        format.json { render(:show, status: :ok, location: @field_slip) }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @field_slip.errors, status: :unprocessable_entity }
+        format.html { render(:edit, status: :unprocessable_entity) }
+        format.json do
+          render(json: @field_slip.errors, status: :unprocessable_entity)
+        end
       end
     end
   end
@@ -52,19 +62,23 @@ class FieldSlipsController < ApplicationController
     @field_slip.destroy!
 
     respond_to do |format|
-      format.html { redirect_to field_slips_url, notice: "Field slip was successfully destroyed." }
-      format.json { head :no_content }
+      format.html do
+        redirect_to(field_slips_url,
+                    notice: field_slip_destroyed.t)
+      end
+      format.json { head(:no_content) }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_field_slip
-      @field_slip = FieldSlip.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def field_slip_params
-      params.require(:field_slip).permit(:observation_id, :project_id, :code)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_field_slip
+    @field_slip = FieldSlip.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def field_slip_params
+    params.require(:field_slip).permit(:observation_id, :project_id, :code)
+  end
 end
--- db/migrate/20240310132813_create_field_slips.rb
+++ db/migrate/20240310132813_create_field_slips.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateFieldSlips < ActiveRecord::Migration[7.1]
   def change
-    create_table :field_slips do |t|
-      t.integer :observation_id
-      t.integer :project_id
-      t.string :code
+    create_table(:field_slips) do |t|
+      t.integer(:observation_id)
+      t.integer(:project_id)
+      t.string(:code)
 
       t.timestamps
     end
