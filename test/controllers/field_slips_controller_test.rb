f5ecc8e2367cae8593b3441d34cb15963b5f9aff
Nathan Wilson <nathan@collectivesource.com>
2024-03-15 20:12:00 -0400

    Test the tests passing

--- test/controllers/field_slips_controller_test.rb
+++ test/controllers/field_slips_controller_test.rb
@@ -2,53 +2,62 @@
 
 require "test_helper"
 
-class FieldSlipsControllerTest < ActionDispatch::IntegrationTest
+class FieldSlipsControllerTest < FunctionalTestCase
   setup do
     @field_slip = field_slips(:field_slip_one)
   end
 
   test "should get index" do
-    get field_slips_url
+    requires_login(:index)
     assert_response :success
   end
 
   test "should get new" do
-    get new_field_slip_url
+    requires_login(:new)
     assert_response :success
   end
 
   test "should create field_slip" do
+    login
     assert_difference("FieldSlip.count") do
-      post field_slips_url,
-           params: { field_slip: { code: @field_slip.code,
-                                   observation_id: @field_slip.observation_id,
-                                   project_id: @field_slip.project_id } }
+      post(:create,
+           params: {
+             field_slip: {
+               code: "X#{@field_slip.code}",
+               observation: observations(:coprinus_comatus_obs),
+               project: projects(:eol_project)
+             }
+           })
     end
 
     assert_redirected_to field_slip_url(FieldSlip.last)
   end
 
   test "should show field_slip" do
-    get field_slip_url(@field_slip)
+    get(:show, params: { id: @field_slip.id })
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_field_slip_url(@field_slip)
+    login
+    get(:edit, params: { id: @field_slip.id })
     assert_response :success
   end
 
   test "should update field_slip" do
-    patch field_slip_url(@field_slip),
-          params: { field_slip: { code: @field_slip.code,
+    login
+    patch(:update,
+          params: { id: @field_slip.id,
+                    field_slip: { code: @field_slip.code,
                                   observation_id: @field_slip.observation_id,
-                                  project_id: @field_slip.project_id } }
+                                  project_id: @field_slip.project_id } })
     assert_redirected_to field_slip_url(@field_slip)
   end
 
   test "should destroy field_slip" do
+    login
     assert_difference("FieldSlip.count", -1) do
-      delete field_slip_url(@field_slip)
+      delete(:destroy, params: { id: @field_slip.id })
     end
 
     assert_redirected_to field_slips_url
