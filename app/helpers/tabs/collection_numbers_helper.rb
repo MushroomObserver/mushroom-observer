ea7dbe325a5be80c4ea0b50ec4ff574229071b0a
Nathan Wilson <nathan@collectivesource.com>
2024-03-17 19:25:06 -0400

    Initial attempt

--- app/helpers/tabs/collection_numbers_helper.rb
+++ app/helpers/tabs/collection_numbers_helper.rb
@@ -55,9 +55,10 @@ module Tabs
 
     def show_collection_number_tab(c_n, obs)
       cn_query = Query.lookup(:CollectionNumber, :all, observations: obs.id)
+      q_id = cn_query.id ? cn_query.id.alphabetize : nil
 
       [tag.i(c_n.format_name.t),
-       collection_number_path(id: c_n.id, q: cn_query),
+       collection_number_path(id: c_n.id, q: q_id),
        { class: "#{tab_id(__method__.to_s)}_#{c_n.id}" }]
     end
 
