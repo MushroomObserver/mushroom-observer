74da15b7b215c08e7e4740dc887b63639a9debfa
Nathan Wilson <nathan@collectivesource.com>
2024-03-17 15:52:43 -0400

    /qr/ route for field_slips#show

--- config/routes.rb
+++ config/routes.rb
@@ -398,6 +398,7 @@ MushroomObserver::Application.routes.draw do # rubocop:todo Metrics/BlockLength
 
   # ----- Field Slip Records: standard actions --------------------------------
   resources :field_slips
+  get("qr/:id", to: "field_slips#show", id: /.*[^\d].*/)
 
   # ----- Herbaria: standard actions -------------------------------------------
   namespace :herbaria do
