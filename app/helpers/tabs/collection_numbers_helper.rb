94215478ee231f5561997f5ffce5cc07d864197f
Nathan Wilson <nathan@collectivesource.com>
2024-03-17 20:03:57 -0400

    Rubocop

--- app/helpers/tabs/collection_numbers_helper.rb
+++ app/helpers/tabs/collection_numbers_helper.rb
@@ -55,12 +55,12 @@ module Tabs
 
     def show_collection_number_tab(c_n, obs)
       cn_query = Query.lookup(:CollectionNumber, :all, observations: obs.id)
-      q_id = cn_query.id ? cn_query.id.alphabetize : nil
+      q_id = cn_query.id&.alphabetize
 
       [tag.i(c_n.format_name.t),
        collection_number_path(id: c_n.id, q: q_id),
        { class: "#{tab_id(__method__.to_s)}_#{c_n.id}",
-         data: { turbo_frame: "_top" }}]
+         data: { turbo_frame: "_top" } }]
     end
 
     def collection_number_mod_tabs(c_n)
