56232dbd86150dfd76774733dd5dadafbaf180ac
Nathan Wilson <nathan@collectivesource.com>
2024-03-19 21:22:19 -0400

    Add Field Slip to Observation#show page

--- app/models/field_slip.rb
+++ app/models/field_slip.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
-class FieldSlip < ApplicationRecord
+class FieldSlip < AbstractModel
   belongs_to :observation
   belongs_to :project
 
   validates :code, uniqueness: true
   validates :code, presence: true
   validate do |field_slip|
-    unless field_slip.code.match(/[^\d.-]/)
-      errors.add :code, :format, message: :field_slip_code_format_error.t
+    unless field_slip.code.match?(/[^\d.-]/)
+      errors.add(:code, :format, message: :field_slip_code_format_error.t)
     end
   end
 
   def code=(val)
     self[:code] = val.upcase
-    unless project
-      prefix_match = code.match(/(^.+)[ -]\d+$/)
-      if prefix_match
-        self.project = Project.find_by(field_slip_prefix: prefix_match[1])
-      end
-    end
+    return if project
+
+    prefix_match = code.match(/(^.+)[ -]\d+$/)
+    return unless prefix_match
+
+    self.project = Project.find_by(field_slip_prefix: prefix_match[1])
+  end
+
+  def title
+    code
   end
 end
