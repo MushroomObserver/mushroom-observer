522672675b58b587cb666ec44aa90543b607e66b
Nathan Wilson <nathan@collectivesource.com>
2024-03-22 19:28:16 -0400

    Add user_id to FieldSlips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_231729) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_22_232703) do
   create_table "api_keys", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", precision: nil
     t.datetime "last_used", precision: nil
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_231729) do
     t.string "code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["code"], name: "index_field_slips_on_code", unique: true
   end
 
