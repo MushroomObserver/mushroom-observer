e0ab307308e0f50ac7b2f57ff140242c63898b3f
Joe Cohen <jdcohenesq@gmail.com>
2024-03-23 20:31:21 -0700

    Test (red) exposing qr-new obs-violation bug
    Exposes a bug when:
    - scanning Field Slip
    - choosing to Create New Observation
    - completing Observation fields with Constraint Violation(s)
    - then clicking Create
    
    See https://github.com/MushroomObserver/mushroom-observer/pull/2024#issuecomment-2016604962 et seq.

---
+++ test/integration/capybara/field_slips_integration_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require("test_helper")
+
+# Test relating to projects
+class FieldSlipsIntegrationTest < CapybaraIntegrationTestCase
+
+  def test_new_observation_violates_project_constraints
+    project = projects(:falmouth_2023_09_project)
+    user = users(:roy)
+    assert(project.member?(user),
+           "Test needs user who is member of #{project.title} Project")
+
+    login(user)
+    visit("/qr/NFAL-0001")
+    click_on(:field_slip_create_obs.l)
+
+    fill_in(:WHERE.l, with: locations(:albion).name)
+    assert_no_difference(
+      "Observation.count",
+      "Observation shouldn't be created before confirming constraint violation"
+    ) do
+      first(:button, "Create").click
+    end
+  end
+end
