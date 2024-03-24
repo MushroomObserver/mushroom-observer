18a713110957fa48fd6670b7b26bdb3ef47f2ad0
Nathan Wilson <nathan@collectivesource.com>
2024-03-24 09:52:46 -0400

    Start fixing test/system/field_slips_test.rb

--- test/system/field_slips_test.rb
+++ test/system/field_slips_test.rb
@@ -8,24 +8,21 @@ class FieldSlipsTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
+    login!(mary)
     visit field_slips_url
-    assert_selector "h1", text: "Field slips"
+    assert_selector "h1", text: :FIELD_SLIPS.t
   end
 
-  test "should create field slip" do
+  test "navigate to should field slip" do
+    login!(mary)
     visit field_slips_url
-    click_on "New field slip"
-
-    fill_in "Code", with: @field_slip.code
-    fill_in "Observation", with: @field_slip.observation_id
-    fill_in "Project", with: @field_slip.project_id
-    click_on "Create Field slip"
-
-    assert_text "Field slip was successfully created"
+    click_on :field_slip_show.t
+    assert_text :field_slip_index.t
     click_on "Back"
   end
 
   test "should update Field slip" do
+    login!(mary)
     visit field_slip_url(@field_slip)
     click_on "Edit this field slip", match: :first
 
@@ -39,8 +36,9 @@ class FieldSlipsTest < ApplicationSystemTestCase
   end
 
   test "should destroy Field slip" do
+    login!(mary)
     visit field_slip_url(@field_slip)
-    click_on "Destroy this field slip", match: :first
+    click_on :field_slip_destroy.t, match: :first
 
     assert_text "Field slip was successfully destroyed"
   end
