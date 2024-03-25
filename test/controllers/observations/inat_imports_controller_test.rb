815baa1b7f05bc185759c56a94ac26aaedf0dc52
Joe Cohen <jdcohenesq@gmail.com>
2024-03-25 06:15:29 -0700

    Add (red) test_new_inat_import

--- test/controllers/observations/inat_imports_controller_test.rb
+++ test/controllers/observations/inat_imports_controller_test.rb
@@ -3,5 +3,15 @@ require "test_helper"
 # test importing iNaturalist Observations to Mushroom Observer
 module Observations
   class InatImportsControllerTest < FunctionalTestCase
+    def test_new_inat_import
+      user = users(:rolf)
+      inat_id = 12345
+      params = { ids: [inat_id] }
+
+      login(user.login)
+      get(:new, params: params)
+
+      assert_response(:success)
+    end
   end
 end
