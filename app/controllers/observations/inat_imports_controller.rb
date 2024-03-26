e0970d6e49e231dfccf755c8f86d9a23514fc676
Joe Cohen <jdcohenesq@gmail.com>
2024-03-26 06:36:25 -0700

    Skeleton (empty) #new & #create actions

--- app/controllers/observations/inat_imports_controller.rb
+++ app/controllers/observations/inat_imports_controller.rb
@@ -3,5 +3,14 @@
 # import iNaturalist Observations as MO Observations
 module Observations
   class InatImportsController < ApplicationController
+    before_action :login_required
+    before_action :store_location
+    before_action :pass_query_params
+
+    def new
+    end
+
+    def create
+    end
   end
 end
