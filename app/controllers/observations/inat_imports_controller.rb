eccd9b2e68ff139e2f808d9390a0e1f0f77502ad
Joe Cohen <jdcohenesq@gmail.com>
2024-03-30 20:24:39 -0700

    Refactor to better handle bad iNat ID entry
    - Revises tests, including addtional tests
    - Refactors controller
    - Adds comment to form

--- app/controllers/observations/inat_imports_controller.rb
+++ app/controllers/observations/inat_imports_controller.rb
@@ -10,22 +10,69 @@ module Observations
     def new; end
 
     def create
-      inat_ids = params[:inat_ids].split
+      # make it an array for bulk-import compatibility, easier manipulation
+      inat_id_array = params[:inat_ids].split
+
+      return redirect_to(new_observation_path) if inat_id_array.none?
+      return reload_form if bad_inat_id_param?(inat_id_array)
+
+      # return unless check_inat_ids(inat_id_array)
+
+=begin from ProjectsController
+      if title.blank?
+        flash_error(:add_project_need_title.t)
+      elsif project
+        flash_error(:add_project_already_exists.t(title: project.title))
+      elsif ProjectConstraints.new(params).ends_before_start?
+        flash_error(:add_project_ends_before_start.t)
+      elsif user_group
+        flash_error(:add_project_group_exists.t(group: title))
+      elsif admin_group
+        flash_error(:add_project_group_exists.t(group: admin_name))
+      else
+        return create_project(title, admin_name, params[:project][:place_name])
+      end
+      @project = Project.new
+      image_ivars
+      render(:new, location: new_project_path(q: get_query_param))
+=end
+
+      # Etiher of these get iNat Obs
+      # curl -X GET --header 'Accept: application/json' 'https://api.inaturalist.org/v1/observations?id=202555552'
+      # https://api.inaturalist.org/v1/observations?id=202555552
+    end
+
+    # ---------------------------------
+
+    private
+
+    def reload_form
+      @inat_ids = params[:inat_ids]
+      render(:new)
+    end
+
+    def bad_inat_id_param?(inat_id_array)
+      inat_id_array.none? ||
+        multiple_ids?(inat_id_array) ||
+        illegal_ids?(inat_id_array)
+    end
+
+    def multiple_ids?(inat_id_array)
+      return false unless inat_id_array.many?
+
+      flash_warning(:inat_not_single_id.l)
+      true
+    end
+
+    def illegal_ids?(inat_id_array)
       illegal_ids = []
-      inat_ids.each do |id|
+      inat_id_array.each do |id|
         next if /\A\d+\z/.match?(id)
 
         illegal_ids << id
         flash_warning(:runtime_illegal_inat_id.l(id: id))
       end
-      if illegal_ids.any?
-        @inat_ids = params[:inat_ids]
-        render(:new)
-      end
-
-      # Etiher of these get iNat Obs
-      # curl -X GET --header 'Accept: application/json' 'https://api.inaturalist.org/v1/observations?id=202555552'
-      # https://api.inaturalist.org/v1/observations?id=202555552
+      illegal_ids.any?
     end
   end
 end
--- test/controllers/observations/inat_imports_controller_test.rb
+++ test/controllers/observations/inat_imports_controller_test.rb
@@ -12,7 +12,7 @@ module Observations
       assert_response(:success)
       assert_form_action(action: :create)
       assert_select("input#inat_ids", true,
-                    "Form need a field for inputting iNat ids")
+                    "Form needs a field for inputting iNat ids")
     end
 
     def test_create_inat_import
@@ -23,16 +23,31 @@ module Observations
       put(:create, params: params)
 
       assert_response(:success)
+      # TODO: Fixme this assert redirected to? Observation create
+      # assert params?
     end
 
-    def test_create_inat_import_bad_inat_id
+    def test_create_inat_import_too_many_ids
       user = users(:rolf)
-      params = { inat_ids: "badID" }
+      params = { inat_ids: "12345 6789" }
 
       login(user.login)
       put(:create, params: params)
 
-      assert_flash_warning("Missing flash warning about illegal iNat id")
+      assert_flash_text(:inat_not_single_id.l)
+      assert_form_action(action: :create)
+    end
+
+    def test_create_inat_import_bad_inat_id
+      user = users(:rolf)
+      id = "badID"
+      params = { inat_ids: id }
+
+      login(user.login)
+      put(:create, params: params)
+
+      assert_flash_text(:runtime_illegal_inat_id.l(id: id))
+      assert_form_action(action: :create)
     end
   end
 end
