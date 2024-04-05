0cce68934ea5720abac9e8e2cd2f073d5fbbe263
Nathan Wilson <nathan@collectivesource.com>
2024-04-05 08:40:47 -0400

    Support for multiple field slip pages

--- app/controllers/projects/field_slips_controller.rb
+++ app/controllers/projects/field_slips_controller.rb
@@ -13,18 +13,24 @@ module Projects
 
     def new
       flash_error(:field_slip_no_project.t) unless find_project!
+      @page_max = page_max
     end
 
     def create
       return unless find_project!
+      return unless ok_page_count
 
       prefix = @project.field_slip_prefix
       FileUtils.mkdir_p(PDF_DIR)
-      filename = "#{PDF_DIR}/#{prefix}-#{Time.now.to_i}.pdf"
       start = @project.next_field_slip
-      @project.next_field_slip = start + 6
+      count = 6 * pages
+      last = start + count
+      @project.next_field_slip = last
+      filename = "#{PDF_DIR}/#{prefix}-" \
+                 "#{code_num(start)}-#{code_num(last)}-" \
+                 "#{Time.now.to_i}.pdf"
       if @project.save
-        FieldSlipJob.perform_later(@project.id, start, 6, filename)
+        FieldSlipJob.perform_later(@project.id, start, count, filename)
         flash_notice(:field_slips_created_job.t(filename:))
       else
         flash_error(:field_slips_project_update_fail.t(title: @project.title))
@@ -34,8 +40,34 @@ module Projects
 
     private
 
+    def code_num(num)
+      num.to_s.rjust(5, "0")
+    end
+
     def find_project!
       @project = find_or_goto_index(Project, params[:project_id].to_s)
+    end
+
+    def pages
+      @pages ||= params[:pages].to_i
+    end
+
+    def page_max
+      if @project.is_admin?(User.current)
+        2000
+      elsif @project.member?(User.current)
+        10
+      else
+        1
+      end
+    end
+
+    def ok_page_count
+      return true if pages <= page_max
+
+      flash_error(:field_slips_too_many_pages.t(max: page_max))
+      redirect_to(new_project_field_slip_path(project_id: @project.id))
+      return false
     end
   end
 end
