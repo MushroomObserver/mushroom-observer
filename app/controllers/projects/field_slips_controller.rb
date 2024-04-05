a7ed650c39ca3daa51a18478da0c619c7b381964
Nathan Wilson <nathan@collectivesource.com>
2024-04-05 19:42:12 -0400

    Stop non-members from creating field slips

--- app/controllers/projects/field_slips_controller.rb
+++ app/controllers/projects/field_slips_controller.rb
@@ -16,6 +16,8 @@ module Projects
 
     def create
       return unless find_project!
+
+      redirect_to(project_url(@project))
       return unless ok_page_count
 
       tracker = FieldSlipJobTracker.create(prefix: @project.field_slip_prefix,
@@ -33,7 +35,6 @@ module Projects
       else
         flash_error(:field_slips_tracker_fail.t(title: @project.title))
       end
-      redirect_to(project_url(@project))
     end
 
     private
@@ -47,16 +48,20 @@ module Projects
     end
 
     def page_max
-      if @project.is_admin?(User.current)
-        2000
-      elsif @project.member?(User.current)
-        10
-      else
-        1
-      end
+      @page_max ||= if @project.is_admin?(User.current)
+                      2000
+                    elsif @project.member?(User.current)
+                      10
+                    else
+                      0
+                    end
     end
 
     def ok_page_count
+      if page_max.zero?
+        flash_error(:field_slips_must_be_member.t)
+        return false
+      end
       return true if pages <= page_max
 
       flash_error(:field_slips_too_many_pages.t(max: page_max))
