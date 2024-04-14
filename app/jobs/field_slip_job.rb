be9859f69eaef8f43b4f3300cc0656fff300c53a
Nathan Wilson <nathan@collectivesource.com>
2024-04-14 16:38:24 -0400

    Cleanup old FieldSlipJobTrackers and associated files after a week

--- app/jobs/field_slip_job.rb
+++ app/jobs/field_slip_job.rb
@@ -4,6 +4,8 @@ class FieldSlipJob < ApplicationJob
   queue_as :default
 
   def perform(project_id, tracker_id)
+    log("Starting FieldSlipJob.perform(#{project_id}, #{tracker_id})")
+    cleanup_old_pdfs(tracker_id)
     project = Project.find(project_id)
     raise(:field_slip_job_no_project.t(id: project_id)) unless project
 
@@ -17,6 +19,34 @@ class FieldSlipJob < ApplicationJob
     view.render
     view.save_as(tracker.filepath)
     tracker.done
+    log("Done with FieldSlipJob.perform(#{project_id}, #{tracker_id})")
     tracker.filepath
+  end
+
+  private
+
+  MAX_JOB_AGE = 1.week
+
+  def cleanup_old_pdfs(tracker_id)
+    FieldSlipJobTracker.where.not(id: tracker_id).find_each do |tracker|
+      next unless tracker.updated_at < Time.zone.now - MAX_JOB_AGE
+
+      if File.exist?(tracker.filepath)
+        File.delete(tracker.filepath)
+        log("Deleted #{tracker.filepath}")
+      else
+        log("#{tracker.filepath} does not exist")
+      end
+      log("Destroying #{FieldSlipJobTracker.first.serializable_hash}")
+      tracker.destroy
+    end
+  end
+
+  def log(str)
+    time = Time.zone.now.to_s
+    log_entry = "#{time}: #{str}\n"
+    open("log/job.log", "a") do |f|
+      f.write(log_entry)
+    end
   end
 end
