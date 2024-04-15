3b50c98fddb190d65a93013d42b8fedaa2a461fe
Nathan Wilson <nathan@collectivesource.com>
2024-04-15 19:56:05 -0400

    Enable translation in validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -83,8 +83,9 @@ class Project < AbstractModel # rubocop:disable Metrics/ClassLength
   before_destroy :orphan_drafts
   validates :field_slip_prefix, uniqueness: true, allow_blank: true
   validates :field_slip_prefix,
+            allow_blank: true,
             format: { with: /\A[A-Z0-9]+\z/,
-                      message: :alphanumerics_only.t }
+                      message: proc { :alphanumerics_only.t } }
 
   scope :show_includes, lambda {
     strict_loading.includes(
