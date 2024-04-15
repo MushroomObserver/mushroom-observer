e379602d3c3e64d451b485a7314b18499ad91da7
andrew nimmo <andrnimm@fastmail.fm>
2024-04-15 13:43:59 -0700

    Add suggestion to flash

--- app/classes/pattern_search/error/bad_term_error.rb
+++ app/classes/pattern_search/error/bad_term_error.rb
@@ -3,8 +3,22 @@
 module PatternSearch
   class BadTermError < Error
     def to_s
-      :pattern_search_bad_term_error.tp(type: args[:type], help: args[:help],
-                                        term: args[:term].var.to_s.inspect)
+      param = args[:term].var.to_s
+      value = args[:term].vals.join(",")
+      # Add a message about switching params if there is a "has_" param.
+      if param.include?("has_")
+        new_term = param.sub("has_", "with_")
+        gen = "#{:pattern_search_bad_term_error_suggestion.tp(
+          term: param, new_term: new_term, vals: value
+        )}\n"
+      else
+        gen = ""
+      end
+      gen += :pattern_search_bad_term_error.tp(
+        type: args[:type], help: args[:help],
+        term: param.inspect
+      )
+      gen
     end
   end
 end
