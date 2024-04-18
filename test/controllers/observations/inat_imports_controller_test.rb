7c0ae2e529c7af992ee76ce2c0ca9f8c24742bb9
Joe Cohen <jdcohenesq@gmail.com>
2024-04-18 06:07:19 -0700

    Refactor test_create_public_import
    - Improves method & variable names
    - Deletes unneeded non-model fixture

--- test/controllers/observations/inat_imports_controller_test.rb
+++ test/controllers/observations/inat_imports_controller_test.rb
@@ -5,11 +5,6 @@ require "test_helper"
 # test importing iNaturalist Observations to Mushroom Observer
 module Observations
   class InatImportsControllerTest < FunctionalTestCase
-    def setup
-      @inats = YAML.load_file("test/fixtures/inat.yaml", aliases: true).
-               deep_symbolize_keys
-    end
-
     def test_new_inat_import
       login(users(:rolf).login)
       get(:new)
@@ -20,31 +15,33 @@ module Observations
                     "Form needs a field for inputting iNat ids")
     end
 
-    def test_create_inat_import
-      inat_obs = @inats[:somion_unicolor]
-      inat_id = inat_obs[:inat_id].to_s
-      user = users(inat_obs[:user])
+    def test_create_public_import
+      inat_id = "202555552"
       params = { inat_ids: inat_id }
 
-      path = "test/fixtures/inat/one_obs_public.txt"
-      body = File.read(path)
+      inat_response_body = File.read("test/fixtures/inat/one_obs_public.txt")
       WebMock.stub_request(
         :get,
         "https://api.inaturalist.org/v1" \
         "/observations?id=#{inat_id}" \
         "&order=desc&order_by=created_at&only_id=false"
-      ).to_return(body: body)
+      ).to_return(body: inat_response_body)
 
-      login(user.login)
+      login
+
       assert_difference("Observation.count", 1, "Failed to create Obs") do
         put(:create, params: params)
       end
 
-      obs = Observation.order(created_at: :desc).first
-
-      assert_not_nil(obs.rss_log)
-
+      imported_obs = Observation.order(created_at: :desc).first
+      assert_not_nil(imported_obs.rss_log)
       assert_redirected_to(observations_path)
+    end
+
+    def inat_id(path)
+      foo = File.read(path)
+      goo = JSON.parse(foo, symbolize_names: true)
+      goo[:results].first[:id]
     end
 
     def test_create_inat_import_too_many_ids
