d2ec7f4d131c35e0e47cf9dc4d62a0811456e775
andrew nimmo <andrnimm@fastmail.fm>
2024-04-18 18:55:41 -0700

    Ok, also works without separate partial.

--- app/controllers/field_slip_job_trackers_controller.rb
+++ app/controllers/field_slip_job_trackers_controller.rb
@@ -10,8 +10,11 @@ class FieldSlipJobTrackersController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream do
-        render(partial: "projects/field_slips/update_row",
-               locals: { tracker: @tracker })
+        render(turbo_stream: turbo_stream.replace(
+          :"field_slip_job_tracker_#{@tracker.id}", # id of div to replace
+          partial: "projects/field_slips/tracker_row",
+          locals: { tracker: @tracker }
+        ))
       end
     end
   end
