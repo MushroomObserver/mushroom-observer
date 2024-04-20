6c5574bc0d304653307ed802d82611885448dabe
andrew nimmo <andrnimm@fastmail.fm>
2024-04-19 18:48:47 -0700

    Not sure how we would test this controller
    other than a system test that would be flakey

---
+++ test/controllers/field_slip_job_trackers_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class FieldSlipJobTrackersControllerTest < ActionDispatch::IntegrationTest
+  # def test_show
+  #   assert true
+  # end
+end
