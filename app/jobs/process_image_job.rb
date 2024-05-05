7e71840ede1a69c32f114365e45c90a3292f4227
andrew nimmo <andrnimm@fastmail.fm>
2024-05-05 00:04:51 -0700

    Update process_image_job.rb

--- app/jobs/process_image_job.rb
+++ app/jobs/process_image_job.rb
@@ -6,6 +6,16 @@
 class ProcessImageJob < ApplicationJob
   queue_as :default
 
+  rescue_from(StandardError) do |exception|
+    # Handle the error here. For example, you might send a notification email,
+    # log the error to a monitoring service, or mark the upload as failed in your database.
+    # You have access to the job's arguments in the 'arguments' instance method.
+    image = Image.find(args[:id])
+    image.update_attribute(:transferred, false)
+    log("Error processing image #{args[:id]}: #{exception.message}")
+    image.update_attribute(:upload_status, exception.message)
+  end
+
   def perform(args)
     desc = args.pluck(:id, :ext, :set_size, :strip_gps).join(", ")
     log("Starting ProcessImageJob.perform(#{desc})")
@@ -14,12 +24,13 @@ class ProcessImageJob < ApplicationJob
     # raise(:process_image_job_no_image.t) unless image
 
     cmd = MO.process_image_command.
-          gsub("<id>", id.to_s).
-          gsub("<ext>", ext).
-          gsub("<set>", set).
-          gsub("<strip>", strip)
+          gsub("<id>", args[:id].to_s).
+          gsub("<ext>", args[:ext]).
+          gsub("<set>", args[:set]).
+          gsub("<strip>", args[:strip])
     if !Rails.env.test? && !system(cmd)
-      errors.add(:image, :runtime_image_process_failed.t(id: id))
+      # job cannot return errors to caller
+      # errors.add(:image, :runtime_image_process_failed.t(id: args[:id]))
     end
     log("Done with ProcessImageJob.perform(#{desc})")
   end
