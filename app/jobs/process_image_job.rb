23855b343de9999d7d4d4e3a0149ef1619a4d950
andrew nimmo <andrnimm@fastmail.fm>
2024-05-06 00:03:41 -0700

    Update process_image_job.rb

--- app/jobs/process_image_job.rb
+++ app/jobs/process_image_job.rb
@@ -6,6 +6,16 @@
 class ProcessImageJob < ApplicationJob
   queue_as :default
 
+  rescue_from(StandardError) do |exception|
+    # Handle the error here. For example, you might send a notification email,
+    # log the error to a monitoring service, or mark the upload as failed in your database.
+    # You have access to the job's arguments in the 'arguments' instance method.
+    image = args[:image]
+    image.update_attribute(:transferred, false)
+    # log("Error processing image #{args[:id]}: #{exception.message}")
+    image.update_attribute(:upload_status, exception.message)
+  end
+
   def perform(args)
     # desc = args.pluck(:image, :ext, :set_size, :strip_gps, :user).join(", ")
     # log("Starting ProcessImageJob.perform(#{desc})")
