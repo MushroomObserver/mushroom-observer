0ab9f2cf3d7db4ad12346de5764e4da280f72fbc
andrew nimmo <andrnimm@fastmail.fm>
2024-05-15 17:30:13 -0700

    Update observation_comment_system_test.rb
    Add tests for API comment crud actions

--- test/system/observation_comment_system_test.rb
+++ test/system/observation_comment_system_test.rb
@@ -149,5 +149,83 @@ class ObservationCommentSystemTest < ApplicationSystemTestCase
         assert_no_selector(".destroy_comment_link_#{com.id}")
       end
     end
+
+    # UFO test: comment incoming via the API!
+    create_params = {
+      method: :post,
+      action: :comment,
+      target: "observation ##{obs.id}",
+      api_key: api_keys(:marys_api_key).key,
+      summary: "UFO",
+      content: "I am a UFO!"
+    }
+    api = API2.execute(create_params)
+    ufo = api.results.first
+
+    # Both users should get the UFO comment without refreshing the page.
+    # It's not editable by them.
+    using_session("rolf_session") do
+      scroll_to(find("#comments_for_object"), align: :center)
+      assert_selector("#comment_#{ufo.id}")
+      within("#comment_#{ufo.id}") do
+        assert_text("I am a UFO!")
+        assert_selector(".show_user_link_#{mary.id}")
+        assert_no_selector(".edit_comment_link_#{ufo.id}")
+        assert_no_selector(".destroy_comment_link_#{ufo.id}")
+      end
+    end
+
+    using_session("katrina_session") do
+      scroll_to(find("#comments_for_object"), align: :center)
+      assert_selector("#comment_#{ufo.id}")
+      within("#comment_#{ufo.id}") do
+        assert_text("I am a UFO!")
+        assert_selector(".show_user_link_#{mary.id}")
+        assert_no_selector(".edit_comment_link_#{ufo.id}")
+        assert_no_selector(".destroy_comment_link_#{ufo.id}")
+      end
+    end
+
+    update_params = create_params.
+                    except(:summary, :content).
+                    merge({ method: :patch,
+                            set_content: "Right on!" })
+    api = API2.execute(update_params)
+    ufo = api.results.first
+    assert_equal(ufo.comment, "Right on!")
+
+    # Both users should get updated UFO comment without refreshing the page.
+    using_session("rolf_session") do
+      assert_selector("#comment_#{ufo.id}")
+      within("#comment_#{ufo.id}") do
+        assert_text("Right on!")
+        assert_no_selector(".edit_comment_link_#{ufo.id}")
+        assert_no_selector(".destroy_comment_link_#{ufo.id}")
+      end
+    end
+
+    using_session("katrina_session") do
+      assert_selector("#comment_#{ufo.id}")
+      within("#comment_#{ufo.id}") do
+        assert_text("Right on!")
+        assert_no_selector(".edit_comment_link_#{ufo.id}")
+        assert_no_selector(".destroy_comment_link_#{ufo.id}")
+      end
+    end
+
+    delete_params = create_params.
+                    except(:target, :summary, :content).
+                    merge({ method: :delete, id: ufo.id })
+    api = API2.execute(delete_params)
+    ufo = api.results.first
+
+    # Both users should find the UFO comment deleted without refreshing.
+    using_session("rolf_session") do
+      assert_no_selector("#comment_#{ufo.id}")
+    end
+
+    using_session("katrina_session") do
+      assert_no_selector("#comment_#{ufo.id}")
+    end
   end
 end
