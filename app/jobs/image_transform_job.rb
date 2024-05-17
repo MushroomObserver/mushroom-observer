cdb8469620175b9f8b5eef285562f49e00bc7a1f
andrew nimmo <andrnimm@fastmail.fm>
2024-05-16 17:48:58 -0700

    Update image_transform_job.rb

--- app/jobs/image_transform_job.rb
+++ app/jobs/image_transform_job.rb
@@ -11,10 +11,10 @@ class ImageTransformJob < ApplicationJob
     # the error to a monitoring service, or mark the upload as failed in the
     # database. We have access to the job's arguments in the 'arguments'
     # instance method.
-    image = Image.find(args[:id])
+    image = Image.find(arguments[:id])
     # I think this is right: a transformed image needs to be retransferred
     image.update_attribute(:transferred, false)
-    log("Error transforming image #{args[:id]}: #{exception.message}")
+    log("Error transforming image #{arguments[:id]}: #{exception.message}")
     image.update_attribute(:upload_status, exception.message)
   end
 
