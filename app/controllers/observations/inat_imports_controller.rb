ce63113de55b13a698fe8eada5458b32552e9cc7
Joe Cohen <jdcohenesq@gmail.com>
2024-05-17 08:37:46 -0700

    Fix API2::Uploads#cleanup Error

--- app/controllers/observations/inat_imports_controller.rb
+++ app/controllers/observations/inat_imports_controller.rb
@@ -105,36 +105,12 @@ module Observations
           method: :post,
           action: :image,
           api_key: api_key.key,
-          upload_url: photo.url,
-          # content_type: photo.content_type,
-          # copyright_holder: photo.copyright_holder,
-          # license_id: photo.license_id,
-          # notes: photo.notes
-          # t.datetime "created_at", precision: nil
-          # t.datetime "updated_at", precision: nil
-          # t.integer "user_id"
-          # t.date "when"
-          # t.integer "num_views", default: 0, null: false
-          # t.datetime "last_view", precision: nil
-          # t.integer "width"
-          # t.integer "height"
-          # t.float "vote_cache"
-          # t.boolean "ok_for_export", default: true, null: false
-          # t.string "original_name", limit: 120, default: ""
-          # t.boolean "transferred", default: false, null: false
-          # t.boolean "gps_stripped", default: false, null: false
-          # t.boolean "diagnostic", default: true, null: false
+          upload_url: photo.url
         }
 
-        begin
-          # FIXME: Why does API2.execute(params) return nil?
-          # Need to get the id of the image which it creates
-          api = API2.execute(params)
-
-        # FIXME:
-        # Temporary hack to ignore failure to delete non-existent temp file
-        rescue(Errno::ENOENT)
-        end
+        # FIXME: Why does API2.execute(params) return nil?
+        # Need to get the id of the image which it creates
+        api = API2.execute(params)
 
         # FIXME: Need to get the ID of the image I just added
         # instead of whatever image someone happened to have added last
