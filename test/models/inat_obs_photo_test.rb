051ea3406fd9011218101ac2fbc5202a05abc5f2
Joe Cohen <jdcohenesq@gmail.com>
2024-05-17 17:12:39 -0700

    Improve local variable name

--- test/models/inat_obs_photo_test.rb
+++ test/models/inat_obs_photo_test.rb
@@ -2,7 +2,7 @@
 
 require("test_helper")
 
-# iNat obs_photo key/values
+# iNat inat_obs_photo key/values
 # {:id=>351481052,
 #  :position=>0,
 #  :uuid=>"6c223538-04d6-404c-8e84-b7d881dbe550",
@@ -10,24 +10,24 @@ require("test_helper")
 #  :photo=>
 #   {:id=>377332865,
 #    :license_code=>"cc-by-nc",
+#    :original_dimensions=>{:width=>2048, :height=>1534},
+#    :url=>"https://inaturalist-open-data.s3.amazonaws.com/photos/377332865/square.jpeg",
+#    :attribution=>"(c) Tim C., some rights reserved (CC BY-NC)",
+#    :flags=>[],
+#    :moderator_actions=>[],
+#    :hidden=>false}}
 
-# MO image attributes
-#  datetime "created_at", precision: nil
-#  datetime "updated_at", precision: nil
+# some MO image attributes
 #  string "content_type", limit: 100
 #  integer "user_id"
 #  date "when"
 #  text "notes"
 #  string "copyright_holder", limit: 100
 #  integer "license_id", default: 1, null: false
-#  integer "num_views", default: 0, null: false
-#  datetime "last_view", precision: nil
 #  integer "width"
 #  integer "height"
-#  float "vote_cache"
 #  boolean "ok_for_export", default: true, null: false
 #  string "original_name", limit: 120, default: ""
-#  boolean "transferred", default: false, null: false
 #  boolean "gps_stripped", default: false, null: false
 #  boolean "diagnostic", default: true, null: false
 
@@ -37,7 +37,7 @@ class InatObsPhotoTest < UnitTestCase
     inat_obs_data = ImportedInatObs.new(
       File.read("test/fixtures/inat/tremella_mesenterica.txt")
     )
-    obs_photo = InatObsPhoto.new(
+    inat_obs_photo = InatObsPhoto.new(
       inat_obs_data.obs[:observation_photos].first
     )
 
@@ -45,17 +45,17 @@ class InatObsPhotoTest < UnitTestCase
       License.where(License[:form_name] =~ "ccbync").where(deprecated: false).
       order(id: :asc).last
 
-    assert_equal("img/jpeg", obs_photo.content_type)
+    assert_equal("img/jpeg", inat_obs_photo.content_type)
     assert_equal("(c) Tim C., some rights reserved (CC BY-NC)",
-                 obs_photo.copyright_holder)
+                 inat_obs_photo.copyright_holder)
     assert_equal("iNat photo uuid 6c223538-04d6-404c-8e84-b7d881dbe550",
-                 obs_photo.original_name)
+                 inat_obs_photo.original_name)
     assert_equal("original dimensions: 2048 x 1534",
-                 obs_photo.notes)
+                 inat_obs_photo.notes)
     assert_equal(expected_license.id,
-                 obs_photo.license_id,
+                 inat_obs_photo.license_id,
                  "Wrong license, expecting #{expected_license.display_name}")
     assert_equal("https://inaturalist-open-data.s3.amazonaws.com/photos/377332865/original.jpeg",
-                 obs_photo.url)
+                 inat_obs_photo.url)
   end
 end
