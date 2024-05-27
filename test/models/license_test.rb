5fe0292e8dc0ba2f815208a9c703173e85d7e6df
Joe Cohen <jdcohenesq@gmail.com>
2024-05-27 07:41:39 -0700

    Add License fixture

--- test/models/license_test.rb
+++ test/models/license_test.rb
@@ -5,7 +5,7 @@ require("test_helper")
 class LicenseTest < UnitTestCase
   def test_current_names_and_ids
     names_and_ids = License.current_names_and_ids
-    assert_equal(3, names_and_ids.length)
+    assert_equal(4, names_and_ids.length)
     names_and_ids.each do |(_name, id)|
       license = License.find(id)
       assert_not(
@@ -17,6 +17,6 @@ class LicenseTest < UnitTestCase
 
   def test_current_names_and_ids_ccnc25
     names_and_ids = License.current_names_and_ids(licenses(:ccnc25))
-    assert_equal(4, names_and_ids.length)
+    assert_equal(5, names_and_ids.length)
   end
 end
