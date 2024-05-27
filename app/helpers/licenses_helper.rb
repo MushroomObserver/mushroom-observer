e3d67ccc9a6feaf1b8b379917e759391b755ab42
Joe Cohen <jdcohenesq@gmail.com>
2024-05-27 08:58:55 -0700

    Move helper methods inside of module.
    How did this ever work?

--- app/helpers/licenses_helper.rb
+++ app/helpers/licenses_helper.rb
@@ -8,26 +8,26 @@ module LicensesHelper
       license_title_id(license)
     ].safe_join(" ")
   end
-end
 
-# "Editing: Creative Commons Non-commercial v3.0 (#nnn)"  textilized
-def license_edit_title(license)
-  capture do
-    concat("#{:EDITING.l}: ")
-    concat(show_license_title(license))
+  # "Editing: Creative Commons Non-commercial v3.0 (#nnn)"  textilized
+  def license_edit_title(license)
+    capture do
+      concat("#{:EDITING.l}: ")
+      concat(show_license_title(license))
+    end
   end
-end
 
-def license_title_id(license)
-  tag.span(class: "smaller") do
-    tag.span("#(#{license.id || "?"}):")
+  def license_title_id(license)
+    tag.span(class: "smaller") do
+      tag.span("#(#{license.id || "?"}):")
+    end
   end
-end
 
-def license_updated_at(license)
-  if license.updated_at
-    license.updated_at.strftime("%Y-%m-%d %H:%M:%S")
-  else
-    "nil"
+  def license_updated_at(license)
+    if license.updated_at
+      license.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    else
+      "nil"
+    end
   end
 end
