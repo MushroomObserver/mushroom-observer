68cc6b7dea3ab1146795aeddbfa0d1ee5144c8f0
andrew nimmo <andrnimm@fastmail.fm>
2024-06-07 14:29:01 -0700

    Update translations.rb

--- public/translations.rb
+++ public/translations.rb
@@ -1,7 +1,5 @@
 # search_term_images
 search_term_has_images = [
-  { "version" => 1, "language_id" => 2, "tag" => "search_term_has_images",
-    "text" => "images", "user_id" => 0 },
   { "version" => 1, "language_id" => 13, "tag" => "search_term_has_images",
     "text" => "зображення", "user_id" => 64_460 },
   { "version" => 1, "language_id" => 16, "tag" => "search_term_has_images",
@@ -10,8 +8,6 @@ search_term_has_images = [
 
 # search_term_sequence
 search_term_has_sequence = [
-  { "version" => 1, "language_id" => 2, "tag" => "search_term_has_sequence",
-    "text" => "sequence", "user_id" => 0 },
   { "version" => 1, "language_id" => 12, "tag" => "search_term_has_sequence",
     "text" => "序列", "user_id" => 3525 },
   { "version" => 1, "language_id" => 13, "tag" => "search_term_has_sequence",
@@ -22,23 +18,19 @@ search_term_has_sequence = [
 
 # search_term_specimen
 search_term_has_specimen = [
-  { "version" => 1, "language_id" => 2, "tag" => "search_term_has_specimen",
-    "text" => "specimen", "user_id" => 0 },
   { "version" => 1, "language_id" => 13, "tag" => "search_term_has_specimen",
     "text" => "зразок", "user_id" => 64_460 }
 ]
 
 # prefs_filters_has_images
 prefs_filters_with_images = [
-  { "version" => 2, "language_id" => 2, "tag" => "prefs_filters_with_images",
-    "text" => "Hide observations with no images", "user_id" => 0 },
   { "version" => 1, "language_id" => 4, "tag" => "prefs_filters_with_images",
     "text" => "Ocultar observaciones sin imágenes", "user_id" => 3525 },
   { "version" => 1, "language_id" => 6, "tag" => "prefs_filters_with_images",
     "text" => "Esconder Observações sem imagens", "user_id" => 8078 },
   { "version" => 1, "language_id" => 8, "tag" => "prefs_filters_with_images",
     "text" => "Masquer les observations sans Images", "user_id" => 3928 },
-  { "version" => 2, "language_id" => 12, "tag" => "prefs_filters_with_images",
+  { "version" => 1, "language_id" => 12, "tag" => "prefs_filters_with_images",
     "text" => "隐藏没有图像的观察记录", "user_id" => 11_038 },
   { "version" => 1, "language_id" => 13, "tag" => "prefs_filters_with_images",
     "text" => "Сховайся на картин без образів", "user_id" => 3525 },
@@ -48,8 +40,6 @@ prefs_filters_with_images = [
 
 # prefs_filters_has_specimen
 prefs_filters_with_specimen = [
-  { "version" => 2, "language_id" => 2, "tag" => "prefs_filters_with_specimen",
-    "text" => "Hide observations with no specimens", "user_id" => 0 },
   { "version" => 1, "language_id" => 6, "tag" => "prefs_filters_with_specimen",
     "text" => "Esconder Observações sem espécimes", "user_id" => 8078 },
   { "version" => 1, "language_id" => 8, "tag" => "prefs_filters_with_specimen",
@@ -62,72 +52,51 @@ prefs_filters_with_specimen = [
 
 # advanced_search_filter_has_images
 advanced_search_filter_with_images = [
-  { "version" => 2,
-    "language_id" => 2,
-    "tag" => "advanced_search_filter_with_images",
-    "text" => "Filter observations based on whether they have images",
-    "user_id" => 0 },
   { "version" => 1,
     "language_id" => 6,
     "tag" => "advanced_search_filter_with_images",
     "text" => "Filtrar Observações \"com\" e/ou \"sem\" imagens",
     "user_id" => 8078 },
-  { "version" => 2, "language_id" => 12,
+  { "version" => 1, "language_id" => 12,
     "tag" => "advanced_search_filter_with_images",
     "text" => "根据是否有图像筛选观察记录", "user_id" => 11_038 }
 ]
 
 # advanced_search_filter_has_images_off
 advanced_search_filter_with_images_off = [
-  { "version" => 2,
-    "language_id" => 2,
-    "tag" => "advanced_search_filter_with_images_off",
-    "text" => "Include both imaged and imageless observations",
-    "user_id" => 0 },
   { "version" => 1, "language_id" => 6,
     "tag" => "advanced_search_filter_with_images_off",
     "text" => "Incluir Observações com e sem imagens", "user_id" => 8078 },
-  { "version" => 2, "language_id" => 12,
+  { "version" => 1, "language_id" => 12,
     "tag" => "advanced_search_filter_with_images_off",
     "text" => "包括有图像和没有图像的观察记录", "user_id" => 11_038 }
 ]
 
 # advanced_search_filter_has_images_yes
 advanced_search_filter_with_images_yes = [
-  { "version" => 2, "language_id" => 2,
-    "tag" => "advanced_search_filter_with_images_yes",
-    "text" => "Exclude imageless observations", "user_id" => 0 },
   { "version" => 1, "language_id" => 6,
     "tag" => "advanced_search_filter_with_images_yes",
     "text" => "Excluir Observações sem imagens", "user_id" => 8078 },
   { "version" => 1, "language_id" => 8,
     "tag" => "advanced_search_filter_with_images_yes",
     "text" => "Exclure les observations sans image", "user_id" => 3525 },
-  { "version" => 2, "language_id" => 12,
+  { "version" => 1, "language_id" => 12,
     "tag" => "advanced_search_filter_with_images_yes",
     "text" => "不包括没有图像的观察记录", "user_id" => 11_038 }
 ]
 
 # advanced_search_filter_has_images_no
 advanced_search_filter_with_images_no = [
-  { "version" => 2, "language_id" => 2,
-    "tag" => "advanced_search_filter_with_images_no",
-    "text" => "Exclude imaged observations", "user_id" => 0 },
   { "version" => 1, "language_id" => 6,
     "tag" => "advanced_search_filter_with_images_no",
     "text" => "Excluir Observações com imagens", "user_id" => 8078 },
-  { "version" => 2, "language_id" => 12,
+  { "version" => 1, "language_id" => 12,
     "tag" => "advanced_search_filter_with_images_no",
     "text" => "不包括有图像的观察记录", "user_id" => 11_038 }
 ]
 
 # advanced_search_filter_has_specimen
 advanced_search_filter_with_specimen = [
-  { "version" => 3,
-    "language_id" => 2,
-    "tag" => "advanced_search_filter_with_specimen",
-    "text" => "Filter observations based on whether they have specimens",
-    "user_id" => 0 },
   { "version" => 1,
     "language_id" => 6,
     "tag" => "advanced_search_filter_with_specimen",
@@ -145,11 +114,6 @@ advanced_search_filter_with_specimen = [
 
 # advanced_search_filter_has_specimen_off
 advanced_search_filter_with_specimen_off = [
-  { "version" => 3,
-    "language_id" => 2,
-    "tag" => "advanced_search_filter_with_specimen_off",
-    "text" => "Include both observations with and without specimens",
-    "user_id" => 0 },
   { "version" => 1,
     "language_id" => 6,
     "tag" => "advanced_search_filter_with_specimen_off",
@@ -170,9 +134,6 @@ advanced_search_filter_with_specimen_yes = [
   { "version" => 1, "language_id" => 6,
     "tag" => "advanced_search_filter_with_specimen_yes",
     "text" => "Excluir Observações sem exsicatas", "user_id" => 8078 },
-  { "version" => 3, "language_id" => 2,
-    "tag" => "advanced_search_filter_with_specimen_yes",
-    "text" => "Exclude observations without specimens", "user_id" => 0 },
   { "version" => 1,
     "language_id" => 8,
     "tag" => "advanced_search_filter_with_specimen_yes",
@@ -185,9 +146,6 @@ advanced_search_filter_with_specimen_yes = [
 
 # advanced_search_filter_has_specimen_no
 advanced_search_filter_with_specimen_no = [
-  { "version" => 3, "language_id" => 2,
-    "tag" => "advanced_search_filter_with_specimen_no",
-    "text" => "Exclude observations with specimens", "user_id" => 0 },
   { "version" => 1, "language_id" => 6,
     "tag" => "advanced_search_filter_with_specimen_no",
     "text" => "Excluir Observações com exsicatas", "user_id" => 8078 },
