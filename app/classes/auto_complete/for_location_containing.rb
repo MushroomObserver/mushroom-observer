5eae09453d82fb08f99d54c81b4da5ae0c460f34
andrew nimmo <andrnimm@fastmail.fm>
2024-06-15 15:35:50 -0700

    query for_location_containing

---
+++ app/classes/auto_complete/for_location_containing.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Autocompleter for location names that encompass a given lat/lng.
+class AutoComplete::ForLocationContaining < AutoComplete::ByWord
+  attr_accessor :reverse
+
+  include Mappable::BoxMethods
+
+  def initialize(string, params)
+    super(string, params)
+    self.reverse = (params[:format] == "scientific")
+    self.lat = params[:lat]
+    self.lng = params[:lng]
+  end
+
+  # We don't care about matching a user input letter, this list should be short
+  # rubocop:disable Style/MultilineBlockChain
+  def rough_matches(_letter)
+    matches =
+      Location.contains(lat: lat, lng: lng).reject do |loc|
+        location_box(loc).vague?
+      end.sort_by! do |loc|
+        location_box(loc).box_area
+      end.pluck(:name)
+
+    matches.map! { |m| Location.reverse_name(m) } if reverse
+    matches.uniq
+  end
+  # rubocop:enable Style/MultilineBlockChain
+
+  def location_box(loc)
+    Mappable::Box.new(north: loc[:north], south: loc[:south],
+                      east: loc[:east], west: loc[:west])
+  end
+end
