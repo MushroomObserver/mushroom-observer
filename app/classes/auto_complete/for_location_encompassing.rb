94de1a116e0f58cf34aefa9669a607bcbf243915
andrew nimmo <andrnimm@fastmail.fm>
2024-06-15 01:36:11 -0700

    Sketch out autocompleter for locations encompassing a lat/lng

---
+++ app/classes/auto_complete/for_location_encompassing.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class AutoComplete::ForLocationEncompassing < AutoComplete::ByWord
+  attr_accessor :reverse
+
+  def initialize(string, params)
+    super(string, params)
+    self.reverse = (params[:format] == "scientific")
+    self.lat = params[:lat]
+    self.lng = params[:lng]
+  end
+
+  def rough_matches(letter)
+    matches =
+      Location.contains(lat: lat, lng: lng).select(:name).distinct.
+      where(Location[:name].matches("#{letter}%").
+        or(Location[:name].matches("% #{letter}%"))).pluck(:name)
+
+    matches.map! { |m| Location.reverse_name(m) } if reverse
+    matches.sort.uniq
+  end
+end
