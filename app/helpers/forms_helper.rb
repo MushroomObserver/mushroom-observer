133e91b0625ca7025ead8d536112c84bd43e4412
andrew nimmo <andrnimm@fastmail.fm>
2024-06-23 15:02:36 -0700

    Move hidden before input

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -164,8 +164,11 @@ module FormsHelper
     ac_args[:wrap_data] = { controller: :autocompleter, type: args[:type],
                             separator: args[:separator],
                             autocompleter_target: "wrap" }
-    ac_args[:append] = capture do
+    ac_args[:between] = capture do
       concat(autocompleter_hidden_field(**args)) if args[:form]
+      concat(args[:between])
+    end
+    ac_args[:append] = capture do
       concat(autocompleter_dropdown)
       concat(args[:append])
     end
@@ -196,8 +199,8 @@ module FormsHelper
 
   def autocompleter_hidden_field(**args)
     type = autocompleter_type_to_model(args[:type])
-    args[:form].hidden_field(:"#{type}_id",
-                             data: { autocompleter_target: "hidden" })
+    args[:form].text_field(:"#{type}_id",
+                           data: { autocompleter_target: "hidden" })
   end
 
   def autocompleter_type_to_model(type)
