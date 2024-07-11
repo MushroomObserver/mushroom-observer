d7fc8dd50a4514c20e7b91452b618893a53a6de6
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2024-07-11 11:49:41 -0700

    Draft iNat callback
    - Completes first of callback side of allowing MO to access iNat users' data
    (still need to provide the url to `#create`)
    - Adds a callback urlifor use by iNat
    - Temporarily sets callback uri to local uri for test purposes
    - Updates Rails credentials with `inat.id` and `inat.secret` (both provided by iNat)
    - Adds a auth template

--- app/controllers/observations/inat_imports_controller.rb
+++ app/controllers/observations/inat_imports_controller.rb
@@ -22,9 +22,60 @@ module Observations
       redirect_to(observations_path)
     end
 
-    # iNat redirects user here if they authorize MO to access their iNat data
+    # https://www.inaturalist.org/pages/api+reference#authorization_code_flow
+    site = "https://www.inaturalist.org"
+    app_id = Rails.application.credentials.inat.id
+    app_secret = Rails.application.credentials.inat.secret
+    # you can set this to some URL you control for testing
+    # redirect_uri = "https://mushroomobserver.org/observations/inat_imports/auth"
+    redirect_uri = "http://localhost:3000/observations/inat_imports/auth"
+
     # https://www.inaturalist.org/pages/api+reference#authorization_code_flow
     def auth
+      # REQUEST AN AUTHORIZATION CODE
+      # Your web app should redirect the user to this url.
+      # They should see a screen offering them the choice to
+      # authorize your app. If they aggree,
+      # they will be redirected to your redirect_uri with a "code" parameter
+      # url = "#{site}/oauth/authorize?client_id=#{app_id}&redirect_uri=#{redirect_uri}&response_type=code"  # rubocop:disable Layout/LineLength
+
+      #       # REQUEST AN AUTH TOKEN
+      #       # Once your app has that code parameter,
+      #       # you can exchange it for an access token:
+      #       puts("Go to #{url}, approve the app, and you should be redirected to your " +
+      #            "redirect_uri. Copy and paste the 'code' param here.")
+      #       print("Code: ")
+      #       auth_code = gets.strip
+      #       puts
+      @params = params
+      #
+      #       payload = {
+      #         client_id: app_id,
+      #         client_secret: app_secret,
+      #         code: auth_code,
+      #         redirect_uri: redirect_uri,
+      #         grant_type: "authorization_code"
+      #       }
+      #       puts("POST #{site}/oauth/token, payload: #{payload.inspect}")
+      #       puts(response = RestClient.post("#{site}/oauth/token", payload))
+      #       puts
+      #       # response will be a chunk of JSON looking like
+      #       # {
+      #       #   "access_token":"xxx",
+      #       #   "token_type":"bearer",
+      #       #   "expires_in":null,
+      #       #   "refresh_token":null,
+      #       #   "scope":"write"
+      #       # }
+      #
+      #       # Store the token (access_token) in your web app.
+      #       # You can now use it to make authorized
+      #       # requests on behalf of the user, like retrieving profile data:
+      #       token = JSON.parse(response)["access_token"]
+      #       headers = { "Authorization" => "Bearer #{token}" }
+      #       puts("GET /users/edit.json, headers: #{headers.inspect}")
+      #       puts(RestClient.get("#{site}/users/edit.json", headers))
+      #       puts
     end
 
     # ---------------------------------
