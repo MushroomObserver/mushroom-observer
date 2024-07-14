3ba63be961a92b22d479c3e897ad12e70e96af7e
Joe Cohen <jdcohenesq@gmail.com>
2024-07-13 17:59:20 -0700

    enum-ize InatImport#state

--- app/models/inat_import.rb
+++ app/models/inat_import.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
+# Encapsulates a single user's iNatImport
+#
+# == Attributes
+#
+#  user::
+#  state::  state of the import
+#  token::  JSON Web Token (JWT) supplied by iNat
+#
+# == methods
+#
 class InatImport < ApplicationRecord
+  enum state:
+  {
+    Unstarted: 0,
+    Authorizing: 1, # waiting for User to authorize MO to access iNat data
+    Authenticating: 2,
+    Importing: 3,
+    Done: 4
+  }
+
+  belongs_to :user
 end
