638b939bad739cc0799d134add6f8595fd941793
Nathan Wilson <nathan@collectivesource.com>
2024-07-16 20:04:05 -0400

    Remove test credentials and fix test

--- test/controllers/observations/inat_imports_controller_test.rb
+++ test/controllers/observations/inat_imports_controller_test.rb
@@ -124,15 +124,7 @@ module Observations
                   "client_secret" => Rails.application.credentials.inat.secret,
                   "code" => "MockCode",
                   "grant_type" => "authorization_code",
-                  "redirect_uri" => REDIRECT_URI },
-          headers: {
-            "Accept" => "*/*",
-            "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-            "Content-Length" => "177",
-            "Content-Type" => "application/x-www-form-urlencoded",
-            "Host" => "www.inaturalist.org",
-            "User-Agent" => "rest-client/2.1.0 (darwin23 x86_64) ruby/3.3.0p0"
-          }
+                  "redirect_uri" => REDIRECT_URI }
         ).
         to_return(status: 200, body: "MockToken", headers: {})
 
