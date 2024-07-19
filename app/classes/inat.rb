d625d0e326f5bd6fb234b9c240aae9390f5ce536
Joe Cohen <jdcohenesq@gmail.com>
2024-07-19 08:53:51 -0700

    TODO re adding `verb` param to Inat class

--- app/classes/inat.rb
+++ app/classes/inat.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-# Class to handle **all** requests to iNat API
+# The value returned by calling the iNat API
+# This class should eventually handle **all** iNat API requests
 # in order to rate limit per iNat policy
-# 60 requests/minute, <10K requests/day
+# (60 requests/minute, <10K requests/day)
 # https://api.inaturalist.org/v1/docs/
 class Inat
   API_BASE = "https://api.inaturalist.org/v1"
 
+  # TODO: Add a verb param so that this can be used for non-GET requests
   def initialize(operation:, token: "")
     sleep(1.second) # 60 requests/minute rate limit per iNat policy
     # https://www.inaturalist.org/pages/api+reference#authorization_code_flow
