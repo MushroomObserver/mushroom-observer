d6aadd6cb6c42f64673fe8cc47439b4b5a487592
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2024-07-24 13:02:32 -0700

    Add inat_username column to InatImports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_234201) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_200050) do
   create_table "api_keys", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", precision: nil
     t.datetime "last_used", precision: nil
@@ -198,6 +198,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_234201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.string "inat_username"
   end
 
   create_table "interests", id: :integer, charset: "utf8mb3", force: :cascade do |t|
