62b710a4c7c538893528b9a50bf158c692df88d5
Nathan Wilson <nathan@collectivesource.com>
2024-08-02 05:38:48 -0400

    Rubocop

--- db/migrate/20240802092653_remove_tmp_fields_from_field_slip_job_trackers.rb
+++ db/migrate/20240802092653_remove_tmp_fields_from_field_slip_job_trackers.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class RemoveTmpFieldsFromFieldSlipJobTrackers < ActiveRecord::Migration[7.1]
   def change
-    remove_column :field_slip_job_trackers, :notes, :text
-    remove_column :field_slip_job_trackers, :version, :integer
+    remove_column(:field_slip_job_trackers, :notes, :text)
+    remove_column(:field_slip_job_trackers, :version, :integer)
   end
 end
