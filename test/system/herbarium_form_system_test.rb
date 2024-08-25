057450edbd7f166364138813def0466e67ee3a62
andrew nimmo <andrnimm@fastmail.fm>
2024-08-25 01:04:09 -0700

    Update herbarium_form_system_test.rb

--- test/system/herbarium_form_system_test.rb
+++ test/system/herbarium_form_system_test.rb
@@ -3,14 +3,45 @@
 require("application_system_test_case")
 
 class HerbariumFormSystemTest < ApplicationSystemTestCase
-  def test_fungarium_new_location
+  def test_create_fungarium_new_location
     # browser = page.driver.browser
     rolf = users("rolf")
     login!(rolf)
 
     visit("/herbaria/new")
     assert_selector("body.herbaria__new")
+    create_herbarium_with_new_location
 
+    # assert_no_selector("#modal_herbarium")
+    assert_selector("body.herbaria__show")
+    assert_selector("h1", text: "Herbarium des Cévennes (CEV)")
+    assert_selector("#herbarium_location",
+                    text: "Génolhac, Gard, Occitanie, France")
+  end
+
+  def test_observation_form_create_fungarium_new_location
+    rolf = users("rolf")
+    login!(rolf)
+
+    visit("/observations/new")
+    assert_selector("body.observations__new")
+
+    assert_selector("#observation_naming_specimen")
+    scroll_to(find("#observation_naming_specimen"), align: :top)
+    check("observation_specimen")
+    assert_selector("#herbarium_record_herbarium_name")
+    assert_selector(".create-link", text: :create_herbarium.l)
+    click_link(:create_herbarium.l)
+
+    assert_selector("#modal_herbarium")
+    create_herbarium_with_new_location
+
+    assert_no_selector("#modal_herbarium")
+    assert_field("herbarium_record_herbarium_name",
+                 with: "Herbarium des Cévennes")
+  end
+
+  def create_herbarium_with_new_location
     assert_selector("#herbarium_place_name")
     fill_in("herbarium_place_name", with: "genohlac gard france")
     assert_link(:form_observations_create_locality.l)
@@ -33,11 +64,5 @@ class HerbariumFormSystemTest < ApplicationSystemTestCase
       fill_in("herbarium_code", with: "CEV")
       click_commit
     end
-
-    # assert_no_selector("#modal_herbarium")
-    assert_selector("body.herbaria__show")
-    assert_selector("h1", text: "Herbarium des Cévennes (CEV)")
-    assert_selector("#herbarium_location",
-                    text: "Génolhac, Gard, Occitanie, France")
   end
 end
