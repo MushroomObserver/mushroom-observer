16a2156204e987e91f290e93e0a42ec82863216c
andrew nimmo <andrnimm@fastmail.fm>
2024-08-31 01:10:36 -0700

    pattern_search form builder

--- app/helpers/pattern_search_helper.rb
+++ app/helpers/pattern_search_helper.rb
@@ -3,6 +3,30 @@
 # helpers for pattern search forms. These call field helpers in forms_helper.
 # args should provide form, field, label at a minimum.
 module PatternSearchHelper
+  def pattern_search_field(**args)
+    field = args[:field]
+    klass = args[:klass]
+    args[:label] ||= :"search_#{field}".l
+    helper = pattern_search_helper_for_field(field, klass)
+    send(helper, **args)
+  end
+
+  # The subclasses say how they're going to parse their fields, so we can use
+  # that to determine which helper to use.
+  def pattern_search_helper_for_field(field, klass)
+    type = klass.params[field][1]
+    PATTERN_SEARCH_FIELD_HELPERS[type]
+  end
+
+  # Convenience for subclasses to access helper methods via PARAMS
+  PATTERN_SEARCH_FIELD_HELPERS = {
+    parse_boolean: :pattern_search_boolean_field,
+    parse_yes_no_both: :pattern_search_yes_no_both_field,
+    parse_date_range: :pattern_search_date_range_field,
+    parse_rank_range: :pattern_search_rank_range_field,
+    parse_string: :text_field_with_label
+  }.freeze
+
   def pattern_search_yes_field(**args)
     check_box_with_label(value: "yes", **args)
   end
