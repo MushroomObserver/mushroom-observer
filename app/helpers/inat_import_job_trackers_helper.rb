2053ae70ee5401476cf8d9a3306371024979d597
Joe Cohen <jdcohenesq@gmail.com>
2024-09-16 20:37:39 -0700

    Revise job tracker show page
    - Prominently display refresh button if job is incomplete
    - Alway display Your Observations button if job is done (including when there are errors).

--- app/helpers/inat_import_job_trackers_helper.rb
+++ app/helpers/inat_import_job_trackers_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module InatImportJobTrackersHelper
-  def import_successful?(inat_import)
-    inat_import.state == "Done" && inat_import.imported_count.positive?
+  def import_done?(inat_import)
+    inat_import.state == "Done"
   end
 
   def import_incomplete?(inat_import)
