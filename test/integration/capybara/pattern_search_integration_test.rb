1a72425eb2dce18edf4c394c55a2361189234270
andrew nimmo <andrnimm@fastmail.fm>
2024-09-21 21:42:27 -0700

    Create pattern_search_integration_test.rb

---
+++ test/integration/capybara/pattern_search_integration_test.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require("test_helper")
+
+# Integration tests of the Observations controller and views.
+class ObservationsIntegrationTest < CapybaraIntegrationTestCase
+end
