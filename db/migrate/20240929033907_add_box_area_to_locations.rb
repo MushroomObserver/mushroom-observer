84fc92c6f56ff5148292dd294006609a3605bd57
Nathan Wilson <nathan@collectivesource.com>
2024-09-29 20:21:40 +0100

    Add location_versions to the box_area migration

--- db/migrate/20240929033907_add_box_area_to_locations.rb
+++ db/migrate/20240929033907_add_box_area_to_locations.rb
@@ -3,9 +3,11 @@ class AddBoxAreaToLocations < ActiveRecord::Migration[7.1]
 
   def up
     add_column :locations, :box_area, :decimal, precision: 21, scale: 10
+    add_column :location_versions, :box_area, :decimal, precision: 21, scale: 10
   end
 
   def down
+    remove_column :location_versions, :box_area
     remove_column :locations, :box_area
   end
 end
