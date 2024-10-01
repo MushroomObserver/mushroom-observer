b92cbdc9d2026f66d92458f4abae832726fb5905
andrew nimmo <andrnimm@fastmail.fm>
2024-10-01 13:58:17 -0700

    Bring up w main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_01_191905) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_01_233907) do
   create_table "api_keys", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", precision: nil
     t.datetime "last_used", precision: nil
