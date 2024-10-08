5145a3da8bd5cbc16afcdb362951625fbd3d10a3
andrew nimmo <andrnimm@fastmail.fm>
2024-10-08 14:29:23 -0700

    Better comments

--- test/models/fixtures_test.rb
+++ test/models/fixtures_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class FixturesTest < ActiveSupport::TestCase
   def test_observations_all_have_rss_logs
     Observation.find_each do |obs|
-      name = ActiveRecord::FixtureSet.reverse_lookup("observations", obs.id)
+      name = ActiveRecord::FixtureSet.reverse_lookup(:observations, obs.id)
       assert_not_nil(
         obs.rss_log_id,
         "Observation #{name} needs a corresponding RssLog fixture."
