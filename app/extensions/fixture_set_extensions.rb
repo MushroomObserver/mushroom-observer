7c4ea381a5b34398e7dbe3d14d91c60a4d52a143
andrew nimmo <andrnimm@fastmail.fm>
2024-10-08 14:29:23 -0700

    Better comments

--- app/extensions/fixture_set_extensions.rb
+++ app/extensions/fixture_set_extensions.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class ActiveRecord::FixtureSet
-  # Reverse lookup of fixture label (how we refer to fixtures) by id.
+  # Reverse lookup of a fixture's label (e.g. `deprecated_name_obs`) by its id.
+  # For better fixture test messages.
+  # https://stackoverflow.com/a/32322296/3357635
   def self.reverse_lookup(table, id)
     ActiveRecord::FixtureSet.all_loaded_fixtures[table.to_s].
       fixtures.each_key do |key|
