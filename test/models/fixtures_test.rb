5271b9d79936cf064e66766ba93d958c3b802a09
andrew nimmo <andrnimm@fastmail.fm>
2024-10-08 14:30:39 -0700

    Fix null values

--- test/models/fixtures_test.rb
+++ test/models/fixtures_test.rb
@@ -8,11 +8,11 @@ class FixturesTest < ActiveSupport::TestCase
       name = ActiveRecord::FixtureSet.reverse_lookup(:observations, obs.id)
       assert_not_nil(
         obs.rss_log_id,
-        "Observation #{name} needs a corresponding RssLog fixture."
+        "Observation #{name} needs a corresponding RssLog fixture"
       )
       assert_not_nil(
         obs.log_updated_at,
-        "Observation #{name} needs a log_updated_at value."
+        "Observation #{name} needs a log_updated_at value"
       )
     end
   end
