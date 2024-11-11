58bd0dd60c1e624cdd615c4a56a8cf8d2c24d14e
Jason Hollinger <pellaea@gmail.com>
2024-11-11 13:04:45 -0500

    Have it download file to temp file, then move into place, to ensure the download is atomic.

--- app/jobs/image_loader_job.rb
+++ app/jobs/image_loader_job.rb
@@ -5,12 +5,14 @@ class ImageLoaderJob < ApplicationJob
 
   def perform(image_id)
     image = Image.find(image_id)
+    file = image.cached_original_file_path
     return if image.id >= MO.next_image_id_to_go_to_cloud  # on image server?
-    return if File.exist?(image.cached_original_file_path) # already cached?
+    return if File.exist?(file)                            # already cached?
 
     bucket = Google::Cloud::Storage.new.bucket(MO.image_bucket_name)
     blob = bucket.file("orig/#{image.id}.jpg")
-    FileUtils.mkdir_p(File.dirname(image.cached_original_file_path))
-    blob.download(image.cached_original_file_path)
+    FileUtils.mkdir_p(File.dirname(file))
+    blob.download("#{file}.#{Process.pid}")
+    FileUtils.move("#{file}.#{Process.pid}", file)
   end
 end
