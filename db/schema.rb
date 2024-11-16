d49fecfbbfabd462a14d4d628dbf3dd9d6a6622e
Joe Cohen <jdcohenesq@gmail.com>
2024-11-16 14:28:47 -0800

    Remove now-unused InatImport log column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_01_233907) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_16_222241) do
   create_table "api_keys", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", precision: nil
     t.datetime "last_used", precision: nil
@@ -209,7 +209,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_01_233907) do
     t.integer "importables"
     t.integer "imported_count"
     t.string "response_errors"
-    t.text "log"
   end
 
   create_table "interests", id: :integer, charset: "utf8mb3", force: :cascade do |t|
