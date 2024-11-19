62092ad2f9f22148372b4f0f1e3d249b9841f8a5
Jason Hollinger <pellaea@gmail.com>
2024-11-18 20:52:24 -0500

    Still trying to get github tester to work with the GCS configuration.

--- config/initializers/google_cloud_storage.rb
+++ config/initializers/google_cloud_storage.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require "google/cloud/storage"
 require "json"
 
-unless Rails.env.test?
+file = File.new(Rails.root.join("config/credentials",
+                                "mo-image-archive-service-account.json"))
+# For some reason the autotester on github will still try to load this file
+# even though it should presumably be in test mode.
+if File.exist? && !Rails.env.test?
   Google::Cloud::Storage.configure do |config|
     config.project_id  = "mo-image-archive"
-    config.credentials = JSON.load(
-      File.new(Rails.root.join("config", "credentials",
-                               "mo-image-archive-service-account.json"))
-    )
+    config.credentials = JSON.parse(file)
   end
 end
