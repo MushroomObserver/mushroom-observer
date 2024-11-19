61169fe53ff9e1a08accf458559ab095c781928a
Jason Hollinger <pellaea@gmail.com>
2024-11-18 20:57:03 -0500

    Arg.

--- config/initializers/google_cloud_storage.rb
+++ config/initializers/google_cloud_storage.rb
@@ -3,13 +3,13 @@
 require "google/cloud/storage"
 require "json"
 
-file = File.new(Rails.root.join("config/credentials",
-                                "mo-image-archive-service-account.json"))
+file = Rails.root.join("config/credentials",
+                       "mo-image-archive-service-account.json")
 # For some reason the autotester on github will still try to load this file
 # even though it should presumably be in test mode.
-if File.exist? && !Rails.env.test?
+if File.exist?(file) && !Rails.env.test?
   Google::Cloud::Storage.configure do |config|
     config.project_id  = "mo-image-archive"
-    config.credentials = JSON.parse(file)
+    config.credentials = JSON.parse(File.new(file))
   end
 end
