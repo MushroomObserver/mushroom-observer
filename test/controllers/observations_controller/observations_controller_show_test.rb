ac29920ad895f779939bd17777c3c460f7c63069
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2024-11-30 15:11:28 -0800

    Add collection (mycoportal) links to show_observation

--- test/controllers/observations_controller/observations_controller_show_test.rb
+++ test/controllers/observations_controller/observations_controller_show_test.rb
@@ -137,6 +137,46 @@ class ObservationsControllerShowTest < FunctionalTestCase
     end
   end
 
+  def test_show_observation_curator_with_mcp_link
+    obs = observations(:agaricus_campestris_obs)
+    herbarium_record = herbarium_records(:agaricus_campestris_spec)
+    assert(
+      herbarium_record&.herbarium&.mcp_searchable?,
+      "Test needs Obs fixture with HerbariumRecord " \
+      "that's searchable via MyCoPortal"
+    )
+    user = users(:dick)
+    assert(user.curated_herbaria.any?,
+           "Test needs User who's a Herbarium curator")
+
+    login(user.login)
+    get(:show, params: { id: obs.id })
+
+    assert_match(:herbarium_record_collection.l, @response.body)
+    assert_select("a[href=?]", herbarium_record.mcp_url, true,
+                  "Missing link to MyCoPortal record")
+  end
+
+  def test_show_observation_non_curator_with_mcp_link
+    obs = observations(:agaricus_campestris_obs)
+    herbarium_record = herbarium_records(:agaricus_campestris_spec)
+    assert(
+      herbarium_record&.herbarium&.mcp_searchable?,
+      "Test needs Obs fixture with HerbariumRecord " \
+      "that's searchable via MyCoPortal"
+    )
+    user = users(:zero_user)
+    assert(user.curated_herbaria.none?,
+           "Test needs User who's not a Herbarium curator")
+
+    login(user.login)
+    get(:show, params: { id: obs.id })
+
+    assert_match(:herbarium_record_collection.l, @response.body)
+    assert_select("a[href=?]", herbarium_record.mcp_url, true,
+                  "Missing link to MyCoPortal record")
+  end
+
   ##############################################################################
 
   # ------ Show ----------------------------------------------- #
