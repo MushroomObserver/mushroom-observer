488b26b64cc63fd2fe5e2b862eff29dd344cb14d
andrew nimmo <andrnimm@fastmail.fm>
2024-12-17 11:57:14 -0800

    Add turbo response to trackers controller

--- app/controllers/inat_import_job_trackers_controller.rb
+++ app/controllers/inat_import_job_trackers_controller.rb
@@ -6,5 +6,16 @@ class InatImportJobTrackersController < ApplicationController
   def show
     @tracker = InatImportJobTracker.find(params[:id])
     @inat_import = InatImport.find(@tracker.inat_import)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream { render_current_status }
+    end
+  end
+
+  private
+
+  def render_current_status
+    turbo_stream.update("status") { status }
   end
 end
