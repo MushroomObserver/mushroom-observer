761dd9c0e8dbea92bd3f30aa64680460569547fd
Nathan Wilson <nathan@collectivesource.com>
2024-12-19 11:25:02 -0500

    test/controllers/checklists_controller_test.rb

--- app/helpers/checklist_helper.rb
+++ app/helpers/checklist_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module ChecklistHelper
-  def checklist_name_link(taxon:, user:, project:, list:, data:)
+  def checklist_name_link(taxon:, user:, project:, location:, list:, data:)
     name, name_id, deprecated, synonym_id = taxon
-    link = checklist_name_link_path(name_id, user, project, list)
+    link = checklist_name_link_path(name_id, user, project, location, list)
     content = tag.i(name)
     content += " (#{data.counts[name]})"
     content += " *" if deprecated
@@ -12,7 +12,7 @@ module ChecklistHelper
     tag.li { link_to(link) { content } }
   end
 
-  def checklist_name_link_path(name_id, user, project, list)
+  def checklist_name_link_path(name_id, user, project, location, list)
     prefix = if user
                "user:#{user.id}"
              elsif project
@@ -20,6 +20,9 @@ module ChecklistHelper
              elsif list
                "list:#{list.id}"
              end
+    if location
+      prefix += " location:#{location.id}"
+    end
     return name_path(name_id) unless prefix
 
     observations_path(pattern: "#{prefix} name:#{name_id} " \
