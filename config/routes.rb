bbd60db2ec61d1b9632be98bff231f60808ba15d
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2024-12-20 16:13:15 -0700

    Reapply "Reorder inat_imports routes"
    This reverts commit 135b5acdb28160fcacf73b674aefaadd103a5593.

--- config/routes.rb
+++ config/routes.rb
@@ -440,12 +440,12 @@ MushroomObserver::Application.routes.draw do
   end
 
   # ----- InatImports ----------------------------
-  resources :inat_imports, only: [:show, :new, :create] do
-    resources :job_trackers, only: [:show]
-  end
   get("inat_imports/authorization_response",
       to: "inat_imports#authorization_response",
       as: "inat_import_authorization_response")
+  resources :inat_imports, only: [:show, :new, :create] do
+    resources :job_trackers, only: [:show]
+  end
 
   # ----- Info: no resources, just forms and pages ----------------------------
   get("/info/how_to_help", to: "info#how_to_help")
