6ca5eb838861cbc8898159e1a727c108610d51d9
andrew nimmo <andrnimm@fastmail.fm>
2024-12-20 14:12:00 -0800

    Add field_slip tests

--- test/models/query_test.rb
+++ test/models/query_test.rb
@@ -1251,6 +1251,21 @@ class QueryTest < UnitTestCase
                  :ExternalLink, :all, url: "iNaturalist")
   end
 
+  def test_field_slip_all
+    expect = FieldSlip.all.sort_by(&:date)
+    assert_query(expect, :FieldSlip, :all)
+  end
+
+  def test_field_slip_by_user
+    expect = FieldSlip.by_user(users(:mary)).sort_by(&:date)
+    assert_query(expect, :FieldSlip, :all, by_user: mary)
+  end
+
+  def test_field_slip_for_project
+    expect = FieldSlip.for_project(projects(:eol_project)).sort_by(&:date)
+    assert_query(expect, :FieldSlip, :all, project: projects(:eol_project))
+  end
+
   def test_glossary_term_all
     expect = GlossaryTerm.all.sort_by(&:name)
     assert_query(expect, :GlossaryTerm, :all)
