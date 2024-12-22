e6cb05e1fcfd72ba215d01bfdf818625c4c5019a
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2024-12-22 09:15:38 -0700

    Update Trackers comment
    - Change text that was copied from FieldSlipJobTrackers

--- app/controllers/inat_imports/job_trackers_controller.rb
+++ app/controllers/inat_imports/job_trackers_controller.rb
@@ -4,14 +4,14 @@ module InatImports
   class JobTrackersController < ApplicationController
     before_action :login_required
 
-    # This is only a Turbo endpoint updating the row of a particular tracker.
+    # This is only a Turbo endpoint updating the the status of a job.
     def show
       return unless (@tracker = InatImportJobTracker.find(params[:id]))
 
       respond_to do |format|
         format.turbo_stream do
           render(turbo_stream: turbo_stream.update(
-            :status, # id of div to replace
+            :status, # id of element to replace
             partial: "inat_imports/job_trackers/status",
             locals: { tracker: @tracker }
           ))
