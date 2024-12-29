fd9e4038d0274e656752c3ecd3b898423939ab05
Nathan Wilson <nathan@collectivesource.com>
2024-12-29 15:08:08 -0500

    Some simple tests

--- test/models/project_alias_test.rb
+++ test/models/project_alias_test.rb
@@ -3,7 +3,14 @@
 require "test_helper"
 
 class ProjectAliasTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user aliases" do
+    pa = Project.find_by(target_type: "User")
+    assert_equal("User", pa.target_type)
+    assert_equal(User, pa.target.class)
+  end
+
+  test "valid location aliases" do
+    pa = Project.find_by(target_type: "Location")
+    assert_equal(Location, pa.target.class)
+  end
 end
