d2905661c5b3112e01d4157ac51525bd6a381b2b
andrew nimmo <andrnimm@fastmail.fm>
2025-01-03 15:37:06 -0800

    Update inat_obs_test.rb
    rubocop

--- test/models/inat_obs_test.rb
+++ test/models/inat_obs_test.rb
@@ -182,8 +182,8 @@ class InatObsTest < UnitTestCase
   def test_names_approved_vs_deprecated
     # Make sure fixtures still OK
     names = Name.unscoped.
-              where(text_name: "Lentinellus ursinus", rank: "Species",
-                    deprecated: false)
+            where(text_name: "Lentinellus ursinus", rank: "Species",
+                  deprecated: false)
     assert(names.many? { |name| !name.author.start_with?("sensu ") },
            "Test needs a name with many non-sensu matching fixtures")
     first_name = names.first
