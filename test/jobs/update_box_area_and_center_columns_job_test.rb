5aaa91f092bdcc81a2bba537ffbf92171656307e
andrew nimmo <andrnimm@fastmail.fm>
2025-01-06 15:18:47 -0800

    Update update_box_area_and_center_columns_job_test.rb

--- test/jobs/update_box_area_and_center_columns_job_test.rb
+++ test/jobs/update_box_area_and_center_columns_job_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UpdateBoxAreaAndCenterColumnsJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_update_dry_run
+    # ?
+    # assert_difference("Observation.count", 1,
+    #                   "Failed to create observation") do
+    #   UpdateBoxAreaAndCenterColumnsJob.perform_now(dry_run: true)
+    # end
+  end
 end
