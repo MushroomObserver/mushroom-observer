aabceb35e3df7d3f5d9bb2bb7fd9e4eb259e706d
andrew nimmo <andrnimm@fastmail.fm>
2025-01-09 22:59:35 -0800

    Add tests

--- app/jobs/update_box_area_and_center_columns_job.rb
+++ app/jobs/update_box_area_and_center_columns_job.rb
@@ -3,7 +3,8 @@
 class UpdateBoxAreaAndCenterColumnsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(**args)
+    args ||= {}
     log("Starting UpdateBoxAreaAndCenterColumnsJob.perform")
     # This should be zero, but count just in case.
     loc_count = Location.where(box_area: nil).count
@@ -13,7 +14,7 @@ class UpdateBoxAreaAndCenterColumnsJob < ApplicationJob
     obs_count = Observation.in_box_of_max_area.where(location_lat: nil).count
     log("Found #{obs_count} observations where the associated location was " \
         "small enough, but the obs didn't have a location_lat/lng.")
-    return if args[:dry_run]
+    return [loc_count, obs_count] if args[:dry_run]
 
     # Do the update. This returns counts of locations/observations updated.
     loc_updated, obs_centered, obs_center_nulled =
@@ -22,5 +23,7 @@ class UpdateBoxAreaAndCenterColumnsJob < ApplicationJob
     log("Updated #{loc_updated} locations' box_area and center_lat/lng.")
     log("Updated #{obs_centered} observations' location_lat/lng.")
     log("Nulled #{obs_center_nulled} observations' location_lat/lng.")
+    # Return the values for debugging
+    [loc_updated, obs_centered, obs_center_nulled]
   end
 end
--- test/jobs/update_box_area_and_center_columns_job_test.rb
+++ test/jobs/update_box_area_and_center_columns_job_test.rb
@@ -3,11 +3,28 @@
 require "test_helper"
 
 class UpdateBoxAreaAndCenterColumnsJobTest < ActiveJob::TestCase
-  def test_update_dry_run
-    # ?
-    # assert_difference("Observation.count", 1,
-    #                   "Failed to create observation") do
-    #   UpdateBoxAreaAndCenterColumnsJob.perform_now(dry_run: true)
-    # end
+  def test_update_box_area_and_center_columns
+    # Check that we have some obs that need updating
+    assert_not_empty(Observation.in_box_of_max_area.where(location_lat: nil))
+
+    job = UpdateBoxAreaAndCenterColumnsJob.new
+    job.perform
+
+    assert_empty(Observation.in_box_of_max_area.where(location_lat: nil))
+    # Note: All locations should already have a box_area, so this was nil before
+    assert_empty(Location.where(box_area: nil))
+  end
+
+  def test_update_box_area_dry_run
+    # Check that we have some obs that need updating
+    assert_not_empty(Observation.in_box_of_max_area.where(location_lat: nil))
+
+    job = UpdateBoxAreaAndCenterColumnsJob.new
+    _loc_count, obs_count = job.perform(dry_run: true)
+
+    # check it again
+    needs_update = Observation.in_box_of_max_area.where(location_lat: nil)
+    assert_not_empty(needs_update)
+    assert_equal(obs_count, needs_update.count)
   end
 end
