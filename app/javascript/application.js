012c6e33389203a760e8f49a63cdeb60cd91b1e7
andrew nimmo <andrnimm@fastmail.fm>
2025-01-14 12:38:03 -0800

    Start to fix js stuff

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,7 +12,7 @@ import "@hotwired/turbo-rails"
 // https://stackoverflow.com/questions/70921317/how-can-i-disable-hotwire-turbo-the-turbolinks-replacement-for-all-forms-in
 // form, or button like delete/patch: set data-turbo="true" to opt in
 // link_to with GET: set data-turbo-stream="true" to opt in
-Turbo.setFormMode("optin")
+Turbo.config.forms.mode = "optin"
 // https://stackoverflow.com/a/77434363/3357635
 // use: <%= turbo_stream.close_modal("modal_#{obs.id}_naming") %>
 Turbo.StreamActions.close_modal = function () {
