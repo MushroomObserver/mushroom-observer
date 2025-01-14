43f57e359485f42e6f69bab8dc6166c7c0cc16e6
andrew nimmo <andrnimm@fastmail.fm>
2025-01-14 11:57:52 -0800

    Start query scopes

---
+++ app/classes/query/scopes/conditions.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+
+# Helper methods for turning Query parameters into AR conditions.
+module Query::Scopes::Conditions
+  # Just because these three are used over and over again.
+  def add_owner_and_time_stamp_conditions(table = model.table_name)
+    add_time_condition("#{table}.created_at", params[:created_at])
+    add_time_condition("#{table}.updated_at", params[:updated_at])
+    add_id_condition("#{table}.user_id", lookup_users_by_name(params[:users]))
+  end
+
+  def add_by_user_condition(table = model.table_name)
+    return if params[:by_user].blank?
+
+    user = find_cached_parameter_instance(User, :by_user)
+    @title_tag = :query_title_by_user
+    @title_args[:user] = user.legal_name
+    where << "#{table}.user_id = '#{user.id}'"
+  end
+
+  def add_by_editor_condition(type = model.type_tag)
+    return unless params[:by_editor]
+
+    user = find_cached_parameter_instance(User, :by_editor)
+    @title_tag = :query_title_by_editor
+    @title_args[:user] = user.legal_name
+    @title_args[:type] = type
+    version_table = :"#{type}_versions"
+    add_join(version_table)
+    where << "#{version_table}.user_id = '#{user.id}'"
+    where << "#{type}s.user_id != '#{user.id}'"
+  end
+
+  def add_pattern_condition
+    return if params[:pattern].blank?
+
+    @title_tag = :query_title_pattern_search
+    add_search_condition(search_fields, params[:pattern])
+  end
+
+  def add_boolean_condition(true_cond, false_cond, val, *)
+    return if val.nil?
+
+    @where << (val ? true_cond : false_cond)
+    add_joins(*)
+  end
+
+  def add_exact_match_condition(col, vals, *)
+    return if vals.blank?
+
+    vals = [vals] unless vals.is_a?(Array)
+    vals = vals.map { |v| escape(v.downcase) }
+    @where << if vals.length == 1
+                "LOWER(#{col}) = #{vals.first}"
+              else
+                "LOWER(#{col}) IN (#{vals.join(", ")})"
+              end
+    add_joins(*)
+  end
+
+  def add_search_condition(col, val, *)
+    return if val.blank?
+
+    search = google_parse(val)
+    @where += google_conditions(search, col)
+    add_joins(*)
+  end
+
+  def add_range_condition(col, val, *)
+    return if val.blank?
+    return if val[0].blank? && val[1].blank?
+
+    min, max = val
+    @where << "#{col} >= #{min}" if min.present?
+    @where << "#{col} <= #{max}" if max.present?
+    add_joins(*)
+  end
+
+  def add_string_enum_condition(col, vals, allowed, *)
+    return if vals.empty?
+
+    vals = vals.map(&:to_s) & allowed.map(&:to_s)
+    return if vals.empty?
+
+    @where << "#{col} IN ('#{vals.join("','")}')"
+    add_joins(*)
+  end
+
+  def add_indexed_enum_condition(col, vals, allowed, *)
+    return if vals.empty?
+
+    vals = vals.filter_map { |v| allowed.index_of(v.to_sym) }
+    return if vals.empty?
+
+    @where << "#{col} IN (#{val.join(",")})"
+    add_joins(*)
+  end
+
+  # Generalized so the param can be :obs_ids or :desc_ids
+  def add_ids_condition(table = model.table_name, ids = :ids)
+    return if params[ids].nil? # [] is valid
+
+    set = clean_id_set(params[ids])
+    @where << "#{table}.id IN (#{set})"
+    self.order = "FIND_IN_SET(#{table}.id,'#{set}') ASC"
+
+    @title_tag = :query_title_in_set.t(type: table.singularize.to_sym)
+    # @title_args[:by] = :query_sorted_by.t(field: :original_name)
+    # @title_args[:descriptions] =
+    #   :query_title_in_set.t(type: table.singularize.to_sym)
+  end
+
+  def add_id_condition(col, ids, *)
+    return if ids.empty?
+
+    set = clean_id_set(ids)
+    @where << "#{col} IN (#{set})"
+    add_joins(*)
+  end
+
+  def add_not_id_condition(col, ids, *)
+    return if ids.empty?
+
+    set = clean_id_set(ids)
+    @where << "#{col} NOT IN (#{set})"
+    add_joins(*)
+  end
+end
---
+++ app/classes/query/scopes/datetime.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+# Helper methods for adding date and time conditions to query.
+module Query::Scopes::Datetime
+  def add_date_condition(col, vals, *joins)
+    return if vals.empty?
+
+    # Special case for search by month/day where range of months wraps
+    # around from December to January.
+    if vals[0].to_s.match(/^\d\d-\d\d$/) &&
+       vals[1].to_s.match(/^\d\d-\d\d$/) &&
+       vals[0].to_s > vals[1].to_s
+      add_wrapped_date_condition(col, vals)
+    else
+      add_half_date_condition(true, col, vals[0])
+      add_half_date_condition(false, col, vals[1])
+    end
+    add_joins(*joins)
+  end
+
+  def add_time_condition(col, vals, *joins)
+    return unless vals
+
+    add_half_time_condition(true, col, vals[0])
+    add_half_time_condition(false, col, vals[1])
+    add_joins(*joins)
+  end
+
+  # ----------------------------------------------------------------------------
+
+  private
+
+  def add_wrapped_date_condition(col, vals)
+    m1, d1 = vals[0].to_s.split("-")
+    m2, d2 = vals[1].to_s.split("-")
+    @where << "MONTH(#{col}) > #{m1} OR " \
+              "MONTH(#{col}) < #{m2} OR " \
+              "(MONTH(#{col}) = #{m1} AND DAY(#{col}) >= #{d1}) OR " \
+              "(MONTH(#{col}) = #{m2} AND DAY(#{col}) <= #{d2})"
+  end
+
+  def add_half_date_condition(min, col, val)
+    dir = min ? ">" : "<"
+    if /^\d\d\d\d/.match?(val.to_s)
+      y, m, d = val.split("-")
+      @where << format("#{col} #{dir}= '%04d-%02d-%02d'",
+                       y.to_i,
+                       (m || (min ? 1 : 12)).to_i,
+                       (d || (min ? 1 : 31)).to_i)
+    elsif /-/.match?(val.to_s)
+      m, d = val.split("-")
+      @where << "MONTH(#{col}) #{dir} #{m} OR " \
+                "(MONTH(#{col}) = #{m} AND " \
+                "DAY(#{col}) #{dir}= #{d})"
+    elsif val.present?
+      @where << "MONTH(#{col}) #{dir}= #{val}"
+    end
+  end
+
+  DATE_FORMAT = "STR_TO_DATE('%04d-%02d-%02d %02d:%02d:%02d', %s)"
+  SQL_DATE_FORMAT = "'%Y-%m-%d %H:%i:%s'"
+
+  def add_half_time_condition(min, col, val)
+    return if val.blank?
+
+    y, m, d, h, n, s = val.split("-")
+    @where << format(
+      "#{col} #{min ? ">" : "<"}= #{DATE_FORMAT}",
+      y.to_i,
+      (m || (min ? 1 : 12)).to_i,
+      (d || (min ? 1 : 31)).to_i,
+      (h || (min ? 0 : 23)).to_i,
+      (n || (min ? 0 : 59)).to_i,
+      (s || (min ? 0 : 59)).to_i,
+      SQL_DATE_FORMAT
+    )
+  end
+end
