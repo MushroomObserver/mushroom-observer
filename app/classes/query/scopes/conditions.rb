ebd86227d0f7b1038324296e32e8f97360d0cba7
andrew nimmo <andrnimm@fastmail.fm>
2025-01-17 13:33:07 -0800

    Write scopes for datetime and id conditions

--- app/classes/query/scopes/conditions.rb
+++ app/classes/query/scopes/conditions.rb
@@ -3,19 +3,20 @@
 # Helper methods for turning Query parameters into AR conditions.
 module Query::Scopes::Conditions
   # Just because these three are used over and over again.
-  def add_owner_and_time_stamp_conditions(table = model.table_name)
-    add_time_condition("#{table}.created_at", params[:created_at])
-    add_time_condition("#{table}.updated_at", params[:updated_at])
-    add_id_condition("#{table}.user_id", lookup_users_by_name(params[:users]))
+  def add_owner_and_time_stamp_conditions(_table = model.table_name)
+    add_time_condition(:created_at, params[:created_at])
+    add_time_condition(:updated_at, params[:updated_at])
+    add_id_condition(:user_id, lookup_users_by_name(params[:users]))
   end
 
-  def add_by_user_condition(table = model.table_name)
+  def add_by_user_condition(_table = model.table_name)
     return if params[:by_user].blank?
 
     user = find_cached_parameter_instance(User, :by_user)
     @title_tag = :query_title_by_user
     @title_args[:user] = user.legal_name
-    where << "#{table}.user_id = '#{user.id}'"
+
+    @scopes = @scopes.by_user(user)
   end
 
   def add_by_editor_condition(type = model.type_tag)
@@ -25,10 +26,7 @@ module Query::Scopes::Conditions
     @title_tag = :query_title_by_editor
     @title_args[:user] = user.legal_name
     @title_args[:type] = type
-    version_table = :"#{type}_versions"
-    add_join(version_table)
-    where << "#{version_table}.user_id = '#{user.id}'"
-    where << "#{type}s.user_id != '#{user.id}'"
+    @scopes = @scopes.by_editor(user)
   end
 
   def add_pattern_condition
@@ -96,33 +94,25 @@ module Query::Scopes::Conditions
     add_joins(*)
   end
 
-  # Generalized so the param can be :obs_ids or :desc_ids
+  # Generalized so the param can be :obs_ids or :desc_ids. No joins.
   def add_ids_condition(table = model.table_name, ids = :ids)
     return if params[ids].nil? # [] is valid
 
-    set = clean_id_set(params[ids])
-    @where << "#{table}.id IN (#{set})"
-    self.order = "FIND_IN_SET(#{table}.id,'#{set}') ASC"
-
+    @scope = @scope.in_ids_set(ids)
     @title_tag = :query_title_in_set.t(type: table.singularize.to_sym)
-    # @title_args[:by] = :query_sorted_by.t(field: :original_name)
-    # @title_args[:descriptions] =
-    #   :query_title_in_set.t(type: table.singularize.to_sym)
   end
 
-  def add_id_condition(col, ids, *)
+  def add_id_condition(model, col, ids, *)
     return if ids.empty?
 
-    set = clean_id_set(ids)
-    @where << "#{col} IN (#{set})"
+    @scope = @scope.in_named_id_set(model, col, ids)
     add_joins(*)
   end
 
-  def add_not_id_condition(col, ids, *)
+  def add_not_id_condition(model, col, ids, *)
     return if ids.empty?
 
-    set = clean_id_set(ids)
-    @where << "#{col} NOT IN (#{set})"
+    @scope = @scope.not_in_named_id_set(model, col, ids)
     add_joins(*)
   end
 end
--- app/classes/query/scopes/datetime.rb
+++ app/classes/query/scopes/datetime.rb
@@ -2,40 +2,29 @@
 
 # Helper methods for adding date and time conditions to query.
 module Query::Scopes::Datetime
-  def add_date_condition(vals, *joins)
+  def add_date_condition(col, vals, *joins)
     return if vals.empty?
 
     earliest, latest = vals
-    @scopes << latest ? when_between(earliest, latest) : when_after(earliest)
+    @scope = if latest
+               @scope.date_between(col, earliest, latest)
+             else
+               @scope.date_after(col, earliest)
+             end
+
     add_joins(*joins)
   end
 
   def add_time_condition(col, vals, *joins)
     return unless vals
 
-    add_half_time_condition(true, col, vals[0])
-    add_half_time_condition(false, col, vals[1])
+    earliest, latest = vals
+    @scope = if latest
+               @scope.datetime_between(col, earliest, latest)
+             else
+               @scope.datetime_after(col, earliest)
+             end
+
     add_joins(*joins)
-  end
-
-  # ----------------------------------------------------------------------------
-
-  DATE_FORMAT = "STR_TO_DATE('%04d-%02d-%02d %02d:%02d:%02d', %s)"
-  SQL_DATE_FORMAT = "'%Y-%m-%d %H:%i:%s'"
-
-  def add_half_time_condition(min, col, val)
-    return if val.blank?
-
-    y, m, d, h, n, s = val.split("-")
-    @where << format(
-      "#{col} #{min ? ">" : "<"}= #{DATE_FORMAT}",
-      y.to_i,
-      (m || (min ? 1 : 12)).to_i,
-      (d || (min ? 1 : 31)).to_i,
-      (h || (min ? 0 : 23)).to_i,
-      (n || (min ? 0 : 59)).to_i,
-      (s || (min ? 0 : 59)).to_i,
-      SQL_DATE_FORMAT
-    )
   end
 end
