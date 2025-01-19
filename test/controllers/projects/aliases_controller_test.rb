e6ed733fb0ea052d09422fc14a9fcf18562678ab
Nathan Wilson <nathan@collectivesource.com>
2025-01-19 07:42:36 -0500

    Switch to underscore test names

--- test/controllers/projects/aliases_controller_test.rb
+++ test/controllers/projects/aliases_controller_test.rb
@@ -12,44 +12,44 @@ module Projects
       login
     end
 
-    test "index returns all project aliases" do
+    def test_index_returns_all_project_aliases
       get(:index, params: { project_id: @project.id })
       assert_response(:success)
       assert_not_nil(assigns(:project_aliases))
     end
 
-    test "index returns json when requested" do
+    def test_index_returns_json_when_requested
       get(:index, params: { project_id: @project.id }, format: :json)
       assert_response(:success)
       assert_equal("application/json; charset=utf-8", response.content_type)
     end
 
-    test "show displays the requested project alias" do
+    def test_show_displays_the_requested_project_alias
       get(:show, params: { project_id: @project.id, id: @project_alias.id })
       assert_response(:success)
       assert_equal(@project_alias, assigns(:project_alias))
     end
 
-    test "show returns json when requested" do
+    def test_show_returns_json_when_requested
       get(:show, params: { project_id: @project.id, id: @project_alias.id },
                  format: :json)
       assert_response(:success)
       assert_equal("application/json; charset=utf-8", response.content_type)
     end
 
-    test "new displays form for new project alias" do
+    def test_new_displays_form_for_new_project_alias
       get(:new, params: { project_id: @project.id })
       assert_response(:success)
       assert_not_nil(assigns(:project_alias))
     end
 
-    test "edit displays form to edit project alias" do
+    def test_edit_displays_form_to_edit_project_alias
       get(:edit, params: { project_id: @project.id, id: @project_alias.id })
       assert_response(:success)
       assert_equal(@project_alias, assigns(:project_alias))
     end
 
-    test "create creates new project alias with valid location" do
+    def test_create_creates_new_project_alias_with_valid_location
       assert_difference("ProjectAlias.count") do
         post(:create, params: {
                project_id: @project.id,
@@ -68,7 +68,7 @@ module Projects
                            ))
     end
 
-    test "create creates new project alias with valid user" do
+    def test_create_creates_new_project_alias_with_valid_user
       assert_difference("ProjectAlias.count") do
         post(:create, params: {
                project_id: @project.id,
@@ -87,7 +87,7 @@ module Projects
                            ))
     end
 
-    test "create renders new template with invalid params" do
+    def test_create_renders_new_template_with_invalid_params
       post(:create, params: {
              project_id: @project.id,
              project_alias: { name: "" }  # Invalid params
@@ -97,7 +97,7 @@ module Projects
       assert_template(:new)
     end
 
-    test "create returns error json with invalid params" do
+    def test_create_returns_error_json_with_invalid_params
       post(:create, params: {
              project_id: @project.id,
              project_alias: { name: "" }  # Invalid params
@@ -107,7 +107,7 @@ module Projects
       assert_equal("application/json; charset=utf-8", response.content_type)
     end
 
-    test "update modifies project alias with valid params" do
+    def test_update_modifies_project_alias_with_valid_params
       patch(:update, params: {
               project_id: @project.id,
               id: @project_alias.id,
@@ -121,7 +121,7 @@ module Projects
       assert_equal("Updated Name", @project_alias.reload.name)
     end
 
-    test "update returns json when requested" do
+    def test_update_returns_json_when_requested
       patch(:update, params: {
               project_id: @project.id,
               id: @project_alias.id,
@@ -132,7 +132,7 @@ module Projects
       assert_equal("application/json; charset=utf-8", response.content_type)
     end
 
-    test "update renders edit template with invalid params" do
+    def test_update_renders_edit_template_with_invalid_params
       patch(:update, params: {
               project_id: @project.id,
               id: @project_alias.id,
@@ -143,7 +143,7 @@ module Projects
       assert_template(:edit)
     end
 
-    test "update returns error json with invalid params" do
+    def test_update_returns_error_json_with_invalid_params
       patch(:update, params: {
               project_id: @project.id,
               id: @project_alias.id,
@@ -154,7 +154,7 @@ module Projects
       assert_equal("application/json; charset=utf-8", response.content_type)
     end
 
-    test "destroy removes the project alias" do
+    def test_destroy_removes_the_project_alias
       assert_difference("ProjectAlias.count", -1) do
         delete(:destroy,
                params: { project_id: @project.id, id: @project_alias.id })
@@ -163,7 +163,7 @@ module Projects
       assert_redirected_to(project_aliases_path(project_id: @project.id))
     end
 
-    test "destroy returns no content when json requested" do
+    def test_destroy_returns_no_content_when_json_requested
       delete(:destroy, params: {
                project_id: @project.id,
                id: @project_alias.id
