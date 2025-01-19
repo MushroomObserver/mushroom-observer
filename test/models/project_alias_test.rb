d8f0ba482b686296dae64b37ab30120baef708b5
Nathan Wilson <nathan@collectivesource.com>
2025-01-19 07:39:55 -0500

    PR feedback

--- test/models/project_alias_test.rb
+++ test/models/project_alias_test.rb
@@ -3,13 +3,12 @@
 require "test_helper"
 
 class ProjectAliasTest < ActiveSupport::TestCase
-  test "valid user aliases" do
+  def test_valid_user_aliases
     pa = ProjectAlias.find_by(target_type: "User")
-    assert_equal("User", pa.target_type)
     assert_equal(User, pa.target.class)
   end
 
-  test "valid location aliases" do
+  def test_valid_location_aliases
     pa = ProjectAlias.find_by(target_type: "Location")
     assert_equal(Location, pa.target.class)
   end
