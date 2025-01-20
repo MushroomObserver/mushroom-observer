07d0aa102d4c00bb378a1ad5efeebc403ca52ff8
andrew nimmo <andrnimm@fastmail.fm>
2025-01-20 00:34:38 -0800

    Update bounding_box.rb

--- app/classes/query/scopes/bounding_box.rb
+++ app/classes/query/scopes/bounding_box.rb
@@ -3,111 +3,78 @@
 # Helper methods for adding location extent conditions to query.
 module Query::Scopes::BoundingBox
   def add_bounding_box_conditions_for_locations
-    return unless params[:north] && params[:south] &&
-                  params[:east] && params[:west]
+    return unless (box = we_have_a_box?)
 
-    _, cond2 = bounding_box_conditions
-    @where += cond2
+    @scopes = @scopes.where(Location.in_box(**box))
   end
 
   def add_bounding_box_conditions_for_observations
-    return unless params[:north] && params[:south] &&
-                  params[:east] && params[:west]
+    return unless (box = we_have_a_box?)
 
-    cond1, cond2 = bounding_box_conditions
-    cond0 = lat_lng_plausible
-    cond1 = cond1.join(" AND ")
-    cond2 = cond2.join(" AND ")
-    @where << "IF(locations.id IS NULL OR #{cond0}, #{cond1}, #{cond2})"
+    @scopes = @scopes.where(
+      use_observation_coordinates?.
+      when(true).then(Observation.in_box(**box)).
+      when(false).then(Location.in_box(**box))
+    )
     add_join_to_locations
   end
 
   # ----------------------------------------------------------------------------
 
-  # Condition which returns true if the observation's lat/long is plausible.
-  # (should be identical to Mappable::BoxMethods.lat_lng_close? but is not)
-  def lat_lng_plausible
-    # %(
-    #   observations.lat >= locations.south*1.2 - locations.north*0.2 AND
-    #   observations.lat <= locations.north*1.2 - locations.south*0.2 AND
-    #   if(locations.west <= locations.east,
-    #     observations.lng >= locations.west*1.2 - locations.east*0.2 AND
-    #     observations.lng <= locations.east*1.2 - locations.west*0.2,
-    #     observations.lng >= locations.west*0.8 + locations.east*0.2 + 72 OR
-    #     observations.lng <= locations.east*0.8 + locations.west*0.2 - 72
-    #   )
-    # )
+  def we_have_a_box?
+    if params.values_at(:south, :north, :west, :east).all?(&:present?)
+      params.select(:north, :south, :east, :west)
+    else
+      false
+    end
+  end
+
+  def use_observation_coordinates?
+    Location[:id].eq(nil).or(obs_lat_lng_plausible)
+  end
+
+  # Condition which returns true if the observation's lat/long is plausible
+  # with respect to the location given.
+  # (originally identical to Mappable::BoxMethods.lat_lng_close? but is not)
+  def obs_lat_lng_plausible
+    obs_lat_plausible.and(
+      location_straddles_dateline.
+        when(true).then(obs_lng_plausible_straddling_dateline).
+        when(false).then(obs_lng_plausible)
+    )
+  end
+
+  def obs_lat_plausible
     Observation[:lat].gteq(
       (Location[:south] * 1.2) - (Location[:north] * 0.2)
     ).and(
-      Observation[:lat].gteq(
+      Observation[:lat].lteq(
         (Location[:south] * 1.2) - (Location[:north] * 0.2)
-      )
-    ).and(
-      Location[:west].lteq(Location[:east]).
-      when(true).then(
-        Observation[:lng].gteq(
-          (Location[:west] * 1.2) - (Location[:east] * 0.2)
-        ).and(
-          Observation[:lng].lteq(
-            (Location[:east] * 1.2) - (Location[:west] * 0.2)
-          )
-        )
-      ).when(false).then(
-        Observation[:lng].gteq(
-          (Location[:west] * 0.8) - (Location[:east] * 0.2 + 72)
-        ).or(
-          Observation[:lng].lteq(
-            (Location[:east] * 0.8) - (Location[:west] * 0.2 - 72)
-          )
-        )
       )
     )
   end
 
-  def bounding_box_conditions
-    n, s, e, w = params.values_at(:north, :south, :east, :west)
-    if w < e
-      bounding_box_normal(n, s, e, w)
-    else
-      bounding_box_straddling_date_line(n, s, e, w)
-    end
+  def location_straddles_dateline
+    Location[:west].gt(Location[:east])
   end
 
-  def bounding_box_normal(north, south, east, west)
-    [
-      [
-        # point location inside target box
-        "observations.lat >= #{south}",
-        "observations.lat <= #{north}",
-        "observations.lng >= #{west}",
-        "observations.lng <= #{east}"
-      ], [
-        # box entirely within target box
-        "locations.south >= #{south}",
-        "locations.north <= #{north}",
-        "locations.west >= #{west}",
-        "locations.east <= #{east}",
-        "locations.west <= locations.east"
-      ]
-    ]
+  def obs_lng_plausible
+    Observation[:lng].gteq(
+      (Location[:west] * 1.2) - (Location[:east] * 0.2)
+    ).and(
+      Observation[:lng].lteq(
+        (Location[:east] * 1.2) - (Location[:west] * 0.2)
+      )
+    )
   end
 
-  def bounding_box_straddling_date_line(north, south, east, west)
-    [
-      [
-        # point location inside target box
-        "observations.lat >= #{south}",
-        "observations.lat <= #{north}",
-        "(observations.lng >= #{west} OR observations.lng <= #{east})"
-      ], [
-        # box entirely within target box
-        "locations.south >= #{south}",
-        "locations.north <= #{north}",
-        "locations.west >= #{west}",
-        "locations.east <= #{east}",
-        "locations.west > locations.east"
-      ]
-    ]
+  def obs_lng_plausible_straddling_dateline
+    Observation[:lng].gteq(
+      (Location[:west] * 0.8) - (Location[:east] * 0.2 + 72)
+    ).or(
+      Observation[:lng].lteq(
+        (Location[:east] * 0.8) - (Location[:west] * 0.2 - 72)
+      )
+    )
   end
 end
