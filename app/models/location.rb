cbeb6726756be9b51ccff21b3c50f2a905dbf61f
andrew nimmo <andrnimm@fastmail.fm>
2025-01-21 17:39:56 -0800

    Observation scopes - redo in_clade, add with_notes_fields, reformat location scopes

--- app/models/location.rb
+++ app/models/location.rb
@@ -158,130 +158,108 @@ class Location < AbstractModel # rubocop:disable Metrics/ClassLength
   scope :index_order, -> { order(name: :asc, id: :desc) }
 
   # NOTE: To improve Coveralls display, do not use one-line stabby lambda scopes
-  scope :name_includes,
-        ->(place_name) { where(Location[:name].matches("%#{place_name}%")) }
-  scope :in_region,
-        ->(place_name) { where(Location[:name].matches("%#{place_name}")) }
+  scope :name_includes, lambda { |place_name|
+    where(Location[:name].matches("%#{place_name}%"))
+  }
+  scope :in_region, lambda { |place_name|
+    where(Location[:name].matches("%#{place_name}"))
+  }
   # This returns locations whose bounding box is entirely within the given box.
   # Pass kwargs (:north, :south, :east, :west), any order
-  scope :in_box,
-        lambda { |**args|
-          box = Mappable::Box.new(**args)
-          return none unless box.valid?
+  scope :in_box, lambda { |**args|
+    box = Mappable::Box.new(**args)
+    return none unless box.valid?
 
-          if box.straddles_180_deg?
-            in_box_straddling_dateline(**args)
-          else
-            in_box_regular(**args)
-          end
-        }
-  scope :in_box_straddling_dateline, # mostly a helper for in_box
-        lambda { |**args|
-          box = Mappable::Box.new(**args)
-          return none unless box.valid?
+    if box.straddles_180_deg?
+      in_box_straddling_dateline(**args)
+    else
+      in_box_regular(**args)
+    end
+  }
+  # mostly a helper for in_box
+  scope :in_box_straddling_dateline, lambda { |**args|
+    box = Mappable::Box.new(**args)
+    return none unless box.valid?
 
-          where(
-            (Location[:south] >= box.south).
-              and(Location[:north] <= box.north).
-            # Location[:west] between w & 180 OR between 180 and e
-            and((Location[:west] >= box.west).
-              or(Location[:west] <= box.east)).
-            and((Location[:east] >= box.west).
-              or(Location[:east] <= box.east))
-          )
-        }
-  scope :in_box_regular, # mostly a helper for in_box
-        lambda { |**args|
-          box = Mappable::Box.new(**args)
-          return none unless box.valid?
+    where((Location[:south] >= box.south).and(Location[:north] <= box.north).
+          # Location[:west] between w & 180 OR between 180 and e
+          and((Location[:west] >= box.west).or(Location[:west] <= box.east)).
+          and((Location[:east] >= box.west).or(Location[:east] <= box.east)))
+  }
+  # mostly a helper for in_box
+  scope :in_box_regular, lambda { |**args|
+    box = Mappable::Box.new(**args)
+    return none unless box.valid?
 
-          where(
-            (Location[:south] >= box.south).
-              and(Location[:north] <= box.north).
-            and(Location[:west] >= box.west).
-              and(Location[:east] <= box.east).
-            and(Location[:west] <= Location[:east])
-          )
-        }
-  scope :not_in_box, # Pass kwargs (:north, :south, :east, :west), any order
-        lambda { |**args|
-          box = Mappable::Box.new(**args)
-          return none unless box.valid?
+    where((Location[:south] >= box.south).and(Location[:north] <= box.north).
+          and(Location[:west] >= box.west).and(Location[:east] <= box.east).
+          and(Location[:west] <= Location[:east]))
+  }
+  # Pass kwargs (:north, :south, :east, :west), any order
+  scope :not_in_box, lambda { |**args|
+    box = Mappable::Box.new(**args)
+    return none unless box.valid?
 
-          in_box(**args).invert_where
-        }
-  scope :contains_point, # Use named parameters (lat:, lng:), any order
-        lambda { |**args|
-          args => {lat:, lng:}
-          where(
-            (Location[:south]).lteq(lat).and((Location[:north]).gteq(lat)).
-            and(
-              Location[:west].lteq(lng).and(Location[:east].gteq(lng)).
-              or(
-                Location[:west].gteq(lng).and(Location[:east].lteq(lng))
-              )
-            )
-          )
-        }
-  scope :with_minimum_bounding_box_containing_point,
-        lambda { |**args|
-          args => {lat:, lng:}
-          containers = contains_point(lat: lat, lng: lng)
-          # prevents returning all containers if contaimers empty
-          return none if containers.empty?
+    in_box(**args).invert_where
+  }
+  # Use named parameters (lat:, lng:), any order
+  scope :contains_point, lambda { |**args|
+    args => { lat:, lng: }
+    where((Location[:south]).lteq(lat).and((Location[:north]).gteq(lat)).
+          and(Location[:west].lteq(lng).and(Location[:east].gteq(lng)).
+              or(Location[:west].gteq(lng).and(Location[:east].lteq(lng)))))
+  }
+  # Use named parameters (lat:, lng:), any order
+  scope :with_minimum_bounding_box_containing_point, lambda { |**args|
+    args => {lat:, lng:}
+    containers = contains_point(lat: lat, lng: lng)
+    # prevents returning all containers if contaimers empty
+    return none if containers.empty?
 
-          containers.min_by(&:box_area)
-        }
-  scope :contains_box, # Use named parameters, north:, south:, east:, west:
-        lambda { |**args|
-          args => { north:, south:, east:, west: }
+    containers.min_by(&:box_area)
+  }
+  # Use named parameters, north:, south:, east:, west:
+  #
+  #   w/e    | Location     | Location contains w/e
+  #   ______ | ____________ | ______________________
+  #   w <= e | west <= east | west <= w && e <= east
+  #   w <= e | west > east  | west <= w || e <= east
+  #   w > e  | west <= east | none
+  #   w > e  | west > east  | west <= w && e <= east
+  #
+  scope :contains_box, lambda { |**args|
+    args => { north:, south:, east:, west: }
 
-          # w/e    | Location     | Location contains w/e
-          # ______ | ____________ | ______________________
-          # w <= e | west <= east | west <= w && e <= east
-          # w <= e | west > east  | west <= w || e <= east
-          # w > e  | west <= east | none
-          # w > e  | west > east  | west <= w && e <= east
-
-          if west <= east # w / e don't straddle 180
-            where(
-              Location[:south].lteq(south).and(Location[:north].gteq(north)).
-              # Location doesn't straddle 180
-              and(Location[:west].lteq(Location[:east]).
-              and(Location[:west] <= west).and(Location[:east] >= east).
-              # Location straddles 180
-              or(
-                Location[:west].gt(Location[:east]).
-                and((Location[:west] <= west).or(Location[:east] >= east))
-              ))
-            )
-          else # Location straddles 180
-            where(
-              Location[:south].lteq(south).and(Location[:north].gteq(north)).
-              # Location 100% wrap; necessarily straddles w/e
-              and(Location[:west].eq(Location[:east] - 360)).
-              # Location < 100% wrap-around
-              or(
-                Location[:west].gt(Location[:east]).
-                and(Location[:west] <= west).and(Location[:east] >= east)
-              )
-            )
-          end
-        }
+    if west <= east # w / e don't straddle 180
+      where(Location[:south].lteq(south).and(Location[:north].gteq(north)).
+            # Location doesn't straddle 180
+            and(Location[:west].lteq(Location[:east]).
+            and(Location[:west] <= west).and(Location[:east] >= east).
+            # Location straddles 180
+            or(Location[:west].gt(Location[:east]).
+               and((Location[:west] <= west).or(Location[:east] >= east)))))
+    else # Location straddles 180
+      where(Location[:south].lteq(south).and(Location[:north].gteq(north)).
+            # Location 100% wrap; necessarily straddles w/e
+            and(Location[:west].eq(Location[:east] - 360)).
+            # Location < 100% wrap-around
+            or(Location[:west].gt(Location[:east]).
+               and(Location[:west] <= west).and(Location[:east] >= east)))
+    end
+  }
   scope :with_observations, -> { joins(:observations).distinct }
 
-  scope :show_includes,
-        lambda {
-          strict_loading.includes(
-            { comments: :user },
-            { description: { comments: :user } },
-            { descriptions: [:authors, :editors] },
-            :interests,
-            :observations,
-            :rss_log,
-            :versions
-          )
-        }
+  scope :show_includes, lambda {
+    strict_loading.includes(
+      { comments: :user },
+      { description: { comments: :user } },
+      { descriptions: [:authors, :editors] },
+      :interests,
+      :observations,
+      :rss_log,
+      :versions
+    )
+  }
 
   # On save, calculate bounding box area for the `box_area` column, plus the
   # `center_lat` and `center_lng` values. If box_area is below a threshold, also
