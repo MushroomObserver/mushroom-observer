bc1b513b13398e5e5959d33387b2ce21041a4632
andrew nimmo <andrnimm@fastmail.fm>
2025-01-22 01:23:00 -0800

    Add image with_sizes, with_content_types

--- app/models/image.rb
+++ app/models/image.rb
@@ -268,6 +268,42 @@ class Image < AbstractModel # rubocop:disable Metrics/ClassLength
 
   scope :index_order, -> { order(created_at: :desc, id: :desc) }
 
+  scope :with_sizes, lambda { |min, max = min|
+    if max == min
+      with_min_size(min)
+    else
+      with_min_size(min).with_max_size(max)
+    end
+  }
+  scope :with_min_size, lambda { |min|
+    size = ALL_SIZES_INDEX[min.to_sym]
+    where(Image[:width].gteq(size).or(Image[:height].gteq(size)))
+  }
+  scope :with_max_size, lambda { |max|
+    size = ALL_SIZES_INDEX[max.to_sym]
+    where(Image[:width].lt(size).or(Image[:height].lt(size)))
+  }
+
+  scope :with_content_types, lambda { |types|
+    exts  = ALL_EXTENSIONS.map(&:to_s)
+    mimes = ALL_CONTENT_TYPES.map(&:to_s).compact_blank
+    types &= exts # intersection
+    return if types.empty?
+
+    other = types.include?("raw")
+    types -= ["raw"]
+    types = types.map { |x| mimes[exts.index(x)] }
+    in_types = Image[:content_type].in(types)
+    not_in_mimes = Image[:content_type].not_in(mimes)
+    if types.empty?
+      where(not_in_mimes)
+    elsif other
+      where(in_types.or(not_in_mimes))
+    else
+      where(in_types)
+    end
+  }
+
   scope :interactive_includes, lambda {
     strict_loading.includes(
       :image_votes, :license, :projects, :user
