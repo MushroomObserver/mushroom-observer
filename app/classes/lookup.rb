ad13a58c4943beea3ae2b38a2f04ecd59cdb22cd
andrew nimmo <andrnimm@fastmail.fm>
2025-01-27 01:32:37 -0800

    Update lookup.rb

--- app/classes/lookup.rb
+++ app/classes/lookup.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
+# Lookup
+#
+# A flexible looker-upper of records. It can handle any identifiers we're likely
+# to throw at it: a string, ID, instance, or a mixed array of any of those. The
+# `lookup_method` has to be configured in the Lookup child class, because the
+# lookup column names are different for each model.
+#
+# Primarily used to get a clean set of ids for ActiveRecord query params.
+# For example, indexes like "Observations for (given) Projects" can be filtered
+# for more than one project at a time: "NEMF 2023" and "NEMF 2024".
+# The observation query needs the project IDs, and Lookup just allows callers
+# to send whatever param type is available. This is handy in the API and
+# in searches.
+#
+# Create an instance of a child class with a string, instance or id, or a mixed
+# array of any of these. Returns an array of ids, instances or strings (names)
+# via instance methods `ids`, `instances` and `titles`.
+#
+# Use:
+#   project_ids = Lookup::Projects.new(["NEMF 2023", "NEMF 2024"]).ids
+#   Observation.where(project: project_ids)
+#
+#   fred_ids = Lookup::Users.new(["Fred", "Freddie", "Freda", "Anni Frid"]).ids
+#   Image.where(user: fred_ids)
+#
+# Instance methods:
+#   (all return arrays)
+#
+# ids:        Array of ids of records matching the values sent to the instance
+# instances:  Array of instances of those records
+# titles:     Array of names of those records, from @name_column set in subclass
+#
 class Lookup
   attr_reader :model, :vals, :params
 
