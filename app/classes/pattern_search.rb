1ef1ccb5afd20e9730a2c3b38d372901b291cfeb
andrew nimmo <andrnimm@fastmail.fm>
2025-01-30 11:44:28 -0800

    Note explaining PatternSearch

--- app/classes/pattern_search.rb
+++ app/classes/pattern_search.rb
@@ -3,6 +3,24 @@
 #
 #  = PatternSearch
 #
+#  This is a user-facing interface for Query. It allows users to type a bunch
+#  of parameters into a single text field - potentially search terms, but also
+#  specific params, as key:value pairs. (Search terms are not even required.)
+#  PatternSearch class parses the whole string and translates those keys:vals
+#  into Query params, which it sends to Query. If there are search terms, it
+#  sends those as the `pattern` param.
+#
+#  For each model that we want to be searchable this way by users, we need to
+#  write a separate subclass of PatternSearch, because each set of params is
+#  particular to that model. User-facing param names can be whatever's easiest
+#  to type and understand, and they are translated, so they do not need to be
+#  the same as Query params, even in English. Each subclass has a hash routing
+#  the English keys of the user-facing parameter names to the Query param names.
+#
+#  PatternSearch param values have some differences from Query that require
+#  preliminary parsing. Dates accept translated phrases like `yesterday` and
+#  `1 month ago`, and some strings, Booleans and enums are more tolerant.
+#
 #  == Usage
 #
 #    search = PatternSearch::Observation.new(string)
@@ -25,7 +43,7 @@
 #
 #  == Subclasses
 #
-#    Base -- Base class: handles everything but build_query.
+#    Base -- Base class: handles everything plus build_query.
 #    Parser -- Helper class that parses search string.
 #    Term -- Helper class that parses a single term value.
 #
