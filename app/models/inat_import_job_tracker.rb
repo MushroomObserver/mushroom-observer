8b9bf54bee8ebb63bdd12eefbaf1d87622aaba07
Joe Cohen <jdcohenesq@gmail.com>
2025-01-31 14:49:01 -0800

    Add tracker methods for # imported, importables

--- app/models/inat_import_job_tracker.rb
+++ app/models/inat_import_job_tracker.rb
@@ -4,10 +4,22 @@
 #
 # == Attributes
 #
+#  created_at::   when the tracker was created
 #  inat_import::  the iNatImport for the job
+#  updated_at::   when the tracker was updated
+#
+# == Methods
 #  status::       the state of the iNat import of this tracker
 #
 class InatImportJobTracker < ApplicationRecord
+  def importables
+    InatImport.find(inat_import).importables
+  end
+
+  def imported_count
+    InatImport.find(inat_import).imported_count
+  end
+
   def status
     InatImport.find(inat_import).state
   end
