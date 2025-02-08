26a50e469aab4c7403e2651c73fcf9de177157b3
Joe Cohen <jdcohenesq@gmail.com>
2025-02-08 10:05:53 -0800

    Make inat_authorization_url a constant

--- app/controllers/inat_imports_controller.rb
+++ app/controllers/inat_imports_controller.rb
@@ -12,7 +12,7 @@
 # 2. create
 #      saves some user data in a InatImport instance
 #        attributes include: user, inat_ids, token, state
-#    passes things off (redirects) to iNat at the inat_authorization_url
+#    passes things off (redirects) to iNat at the INAT_AUTHORIZATION_URL
 # 3. iNat
 #    checks if MO is authorized to access iNat user's confidential data
 #      if not, asks iNat user for authorization
@@ -64,6 +64,10 @@ class InatImportsController < ApplicationController
   REDIRECT_URI = Rails.configuration.redirect_uri
   # iNat's id for the MO application
   APP_ID = Rails.application.credentials.inat.id
+  # URL to obtain authorization from iNat user for access to their private data
+  INAT_AUTHORIZATION_URL =
+    "#{SITE}/oauth/authorize?client_id=#{APP_ID}" \
+    "&redirect_uri=#{REDIRECT_URI}&response_type=code".freeze
   # The iNat API. Not called here, but referenced in tests and ActiveJob
   API_BASE = "https://api.inaturalist.org/v1"
 
@@ -99,14 +103,7 @@ class InatImportsController < ApplicationController
   end
 
   def request_inat_user_authorization
-    redirect_to(inat_authorization_url, allow_other_host: true)
-  end
-
-  def inat_authorization_url
-    "#{SITE}/oauth/authorize" \
-    "?client_id=#{APP_ID}" \
-    "&redirect_uri=#{REDIRECT_URI}" \
-    "&response_type=code"
+    redirect_to(INAT_AUTHORIZATION_URL, allow_other_host: true)
   end
 
   # ---------------------------------
