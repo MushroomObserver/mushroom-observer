e5536f939cc26f3e5ffa66b28f315c6448418adb
Nathan Wilson <nathan@collectivesource.com>
2025-02-17 21:14:24 -0500

    Add comment to ProjectAlias model

--- app/models/project_alias.rb
+++ app/models/project_alias.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ProjectAlias < AbstractModel
+  # Project admins can define a set of "aliases" for users and
+  # locations to facilitate data entry in the field. E.g., "NJW" can
+  # be made to represent the user `nathan`, or "Walk 10" for the
+  # location`USA, Massachusetts, Wellfleet, Marconi Beach`.  The
+  # aliases only work in the context of the project (specifically when
+  # filling in field slip forms).
+
   belongs_to :target, polymorphic: true
   belongs_to :project
 
