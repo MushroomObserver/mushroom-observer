3d9c8a71309f2f02067c48a847f442c3b59b095d
Joe Cohen <jdcohenesq@gmail.com>
2025-02-22 15:48:42 -0800

    More RuboCop version checks

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -10,6 +10,24 @@ class Banner < ApplicationRecord
 
   def test_version
     format("Style/RedundantFormat offense added in RuboCop 1.72")
-    /#{%w[Lint ArrayLiteralInRegexp added in RuboCop 1.72]}/
+    /#{%w[Lint ArrayLiteralInRegexp added in RuboCop 1.71]}/
+
+    x = 1 # rubocop:disable Lint/UselessAssignment
+    if x = "Lint/LiteralAssignmentInCondition RuboCop 1.59"
+      do_something
+    end
+
+    x = /[A-z] LintMixedCaseRange RuboCop 1.53/
+
+    case x
+    in "Lint/DuplicateMatchPattern RuboCop 1.50"
+      do_something
+    in "Lint/DuplicateMatchPattern RuboCop 1.50"
+      do_something_else
+    end
+
+    "Lint/AmbiguousRange RuboCop 1.9" || 1..2
+
+    [1, 2].each { |lint_empty_block_rubocop_one_one| }
   end
 end
