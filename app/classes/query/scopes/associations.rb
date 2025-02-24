58d8af9b18274fd418345e46756cb686a957f275
andrew nimmo <andrnimm@fastmail.fm>
2025-02-24 01:20:03 -0800

    fixes for association scopes

--- app/classes/query/scopes/associations.rb
+++ app/classes/query/scopes/associations.rb
@@ -4,7 +4,7 @@
 module Query::Scopes::Associations
   def initialize_users_parameter
     ids = lookup_users_by_name(params[:by_users])
-    add_id_condition(:user_id, ids, title_method: :set_by_user_title)
+    add_association_condition(:user_id, ids, title_method: :set_by_user_title)
   end
 
   def set_by_user_title
@@ -25,13 +25,11 @@ module Query::Scopes::Associations
     @title_args[:type] = type
   end
 
-  # joins = [:observations, :observation_herbarium_records, :herbarium_records]
   def initialize_herbaria_parameter(
     joins = { observations:
               { observation_herbarium_records: :herbarium_record } }
   )
-    add_id_condition(
-      # "herbarium_records.herbarium_id",
+    add_association_condition(
       HerbariumRecord[:herbarium_id],
       lookup_herbaria_by_name(params[:herbaria]),
       joins
@@ -39,140 +37,94 @@ module Query::Scopes::Associations
   end
 
   def initialize_herbarium_records_parameter
-    add_id_condition(
-      # "observation_herbarium_records.herbarium_record_id",
+    add_association_condition(
       ObservationHerbariumRecord[:herbarium_record_id],
       lookup_herbarium_records_by_name(params[:herbarium_records]),
       { observations: :observation_herbarium_records }
     )
   end
 
-  # pass a model (arel_table)
-  def add_observation_location_condition(table, vals, joins)
+  # pass a model (arel_table) for table
+  def initialize_locations_parameter(table, vals, joins)
     return if vals.empty?
 
-    # loc_col   = "#{table}.location_id"
-    # where_col = "#{table}.where"
     ids = clean_id_set(lookup_locations_by_name(vals))
-    # cond      = "#{loc_col} IN (#{ids})"
     conditions = table[:location_id].in(ids)
+    # check for undefined location strings, often coming from mobile app:
     conditions = chain_location_string_conditions(conditions, table, vals)
-    # @where << cond
     @scopes = @scopes.where(conditions)
-    # add_joins(*)
     @scopes = @scopes.joins(**joins) if joins
+    set_at_location_title if [vals].flatten.size == 1
   end
 
-  def chain_location_string_conditions(conditions, table, vals)
+  def chain_location_string_conditions(conditions, model, vals)
     vals.each do |val|
       next unless /\D/.match?(val)
 
-      # cond += " OR #{where_col} LIKE '%#{pattern}%'"
-      conditions = conditions.or(table[:where].matches(val.clean_pattern))
+      conditions = conditions.or(model[:where].matches(val.clean_pattern))
     end
     conditions
   end
 
-  # pass a model (arel_table)
-  def add_at_location_condition(table)
-    return unless params[:location]
-
+  def set_at_location_title
     location = find_cached_parameter_instance(Location, :location)
-    # @where << "#{table}.location_id = '#{location.id}'"
-    @scopes = @scopes.where(table[:location_id].eq(location.id))
-
     @title_args[:location] = location.title_display_name
-  end
-
-  def add_is_collection_location_condition_for_locations
-    return unless model == Location
-
-    # where << "observations.is_collection_location IS TRUE"
-    @scopes = @scopes.where(Observation[:is_collection_location].eq(true))
-  end
-
-  def add_for_observation_condition(
-    table = "Observation#{model}".constantize,
-    joins = table.table_name
-  )
-    return if params[:observation].blank?
-
-    obs = find_cached_parameter_instance(Observation, :observation)
-    # where << "#{table}.observation_id = '#{obs.id}'"
-    @scopes = @scopes.where(table[:observation_id].eq(obs.id))
-    # add_join(*joins)
-    @scopes = @scopes.joins(**joins) if joins
-
-    @title_tag = :query_title_for_observation
-    @title_args[:observation] = obs.unique_format_name
+    location
   end
 
   def initialize_observations_parameter(
-    # table = :"observation_#{model.table_name}",
     table = "Observation#{model}".constantize,
     joins = table
   )
-    add_id_condition(
-      # "#{table}.observation_id",
-      table[:observation_id],
-      params[:observations],
-      joins
+    ids = params[:observations]
+    add_association_condition(
+      table[:observation_id], ids, joins,
+      title_method: :set_for_observation_title
     )
   end
 
-  # pass a model (arel_table)
-  def add_for_project_condition(table = model, joins = table.table_name)
-    return if params[:project].blank?
-
-    project = find_cached_parameter_instance(Project, :project)
-    # where << "#{table}.project_id = '#{params[:project]}'"
-    @scopes = @scopes.where(table[:project_id].eq(project.id))
-    add_is_collection_location_condition_for_locations
-    # add_join(*joins)
-    @scopes = @scopes.joins(**joins) if joins
-
-    @title_tag = :query_title_for_project
-    @title_args[:project] = project.title
+  # Possible issue: the second arg below is the param name.
+  # We're using it for both single and plural params.
+  # It could work anyway, but the param names may soon be plural.
+  def set_for_observation_title
+    obs = find_cached_parameter_instance(Observation, :observation)
+    @title_tag = :query_title_for_observation
+    @title_args[:observation] = obs.unique_format_name
+    obs
   end
 
   def initialize_projects_parameter(
     table = ProjectObservation,
     joins = { observations: :"#{table.table_name}" }
   )
-    add_id_condition(
-      # "#{table}.project_id",
-      table[:project_id],
-      lookup_projects_by_name(params[:projects]),
-      joins
+    ids = lookup_projects_by_name(params[:projects])
+    add_association_condition(
+      table[:project_id], ids, joins, title_method: :set_for_project_title
     )
   end
 
-  def add_in_species_list_condition(
-    table = SpeciesListObservation,
-    joins = { observations: :"#{table.table_name}" }
-  )
-    return if params[:species_list].blank?
-
-    spl = find_cached_parameter_instance(SpeciesList, :species_list)
-    # where << "#{table}.species_list_id = '#{spl.id}'"
-    @scopes = @scopes.where(table[:species_list_id].eq(spl.id))
-    add_is_collection_location_condition_for_locations
-    # add_join(*joins)
-    @scopes = @scopes.joins(**joins) if joins
-
-    @title_tag = :query_title_in_species_list
-    @title_args[:species_list] = spl.format_name
+  def set_for_project_title
+    project = find_cached_parameter_instance(Project, :project)
+    @title_tag = :query_title_for_project
+    @title_args[:project] = project.title
+    project
   end
 
   def initialize_species_lists_parameter(
     table = SpeciesListObservation,
     joins = { observations: :"#{table.table_name}" }
   )
-    add_id_condition(
-      # "#{table}.species_list_id",
-      table[:species_list_id],
-      lookup_species_lists_by_name(params[:species_lists]),
-      joins
+    ids = lookup_species_lists_by_name(params[:species_lists])
+    add_association_condition(
+      table[:species_list_id], ids, joins,
+      title_method: :set_in_species_list_title
     )
+  end
+
+  def set_in_species_list_title
+    spl = find_cached_parameter_instance(SpeciesList, :species_list)
+    @title_tag = :query_title_in_species_list
+    @title_args[:species_list] = spl.format_name
+    spl
   end
 end
