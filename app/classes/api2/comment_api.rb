ad3b86494830ddf208ba3190dacc605b6e74d3f3
andrew nimmo <andrnimm@fastmail.fm>
2025-03-01 16:30:06 -0800

    Update comment_api.rb

--- app/classes/api2/comment_api.rb
+++ app/classes/api2/comment_api.rb
@@ -37,7 +37,7 @@ class API2
         types: parse_array(:enum, :type, limit: Comment::ALL_TYPE_TAGS),
         summary_has: parse(:string, :summary_has, help: 1),
         content_has: parse(:string, :content_has, help: 1),
-        target: @target ? { id: @target.id, type: @target.class.name } : nil,
+        target: @target ? { id: @target.id, type: @target.class.name } : nil
       }
     end
 
