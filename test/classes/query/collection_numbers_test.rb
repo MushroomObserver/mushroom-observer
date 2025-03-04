8f31d4982bc987abd146ee92fe19953917926c04
andrew nimmo <andrnimm@fastmail.fm>
2025-03-04 01:58:03 -0800

    Update collection_numbers_test.rb

--- test/classes/query/collection_numbers_test.rb
+++ test/classes/query/collection_numbers_test.rb
@@ -12,6 +12,37 @@ class Query::CollectionNumbersTest < UnitTestCase
     assert_query(expects, :CollectionNumber)
   end
 
+  def newbie_collections
+    [collection_numbers(:minimal_unknown_coll_num),
+     collection_numbers(:detailed_unknown_coll_num_one)]
+  end
+
+  def test_collection_number_names
+    expects = CollectionNumber.names("Mary Newbie").index_order
+    assert_query(expects, :CollectionNumber, names: "Mary Newbie")
+    expects = newbie_collections
+    assert_query(expects, :CollectionNumber, names: "Mary Newbie")
+  end
+
+  def test_collection_number_name_has
+    expects = CollectionNumber.name_has("Newbie").index_order
+    assert_query(expects, :CollectionNumber, name_has: "Newbie")
+    expects = newbie_collections
+    assert_query(expects, :CollectionNumber, name_has: "Newbie")
+  end
+
+  def test_collection_number_numbers
+    expects = CollectionNumber.numbers("07-123a").index_order
+    assert_query(expects, :CollectionNumber, numbers: "07-123a")
+  end
+
+  def test_collection_number_number_has
+    expects = CollectionNumber.number_has("n").index_order
+    assert_query(expects, :CollectionNumber, number_has: "n")
+    expects = [collection_numbers(:detailed_unknown_coll_num_two)]
+    assert_query(expects, :CollectionNumber, number_has: "n")
+  end
+
   def test_collection_number_for_observation
     obs = observations(:detailed_unknown_obs)
     expects = CollectionNumber.index_order.observations(obs)
