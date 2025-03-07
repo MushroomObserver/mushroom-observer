9acc92cdabc39ba4628b9081686c727660e77fea
andrew nimmo <andrnimm@fastmail.fm>
2025-03-07 15:15:16 -0800

    initialize_names_and_related_names_parameters

--- app/classes/query/observations.rb
+++ app/classes/query/observations.rb
@@ -189,6 +189,44 @@ class Query::Observations < Query::Base # rubocop:disable Metrics/ClassLength
     )
   end
 
+  def initialize_names_and_related_names_parameters(*joins)
+    return force_empty_results if irreconcilable_naming_parameters?
+
+    table = if params.dig(:names, :include_all_name_proposals)
+              "namings"
+            else
+              "observations"
+            end
+    ids = lookup_names_by_name(params.dig(:names, :lookup),
+                               related_names_parameters)
+    add_association_condition("#{table}.name_id", ids, *joins)
+
+    if params.dig(:names, :include_all_name_proposals)
+      add_join(:observations, :namings)
+    end
+    return unless params.dig(:names, :exclude_consensus)
+
+    add_not_associated_condition("observations.name_id", ids, *joins)
+  end
+
+  NAMES_EXPANDER_PARAMS = [
+    :include_synonyms, :include_subtaxa, :include_immediate_subtaxa,
+    :exclude_original_names
+  ].freeze
+
+  def related_names_parameters
+    return {} unless params[:names]
+
+    params[:names].dup.slice(*NAMES_EXPANDER_PARAMS).compact
+  end
+
+  def irreconcilable_naming_parameters?
+    params.dig(:names, :exclude_consensus) &&
+      !params.dig(:names, :include_all_name_proposals)
+  end
+
+  # ------------------------------------------------------------------------
+
   def add_needs_naming_condition
     return unless params[:needs_naming]
 
