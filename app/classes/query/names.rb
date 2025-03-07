70b2b32b4b5b5a956715d7b08c11e3c2f5650095
andrew nimmo <andrnimm@fastmail.fm>
2025-03-07 15:23:40 -0800

    Update names.rb

--- app/classes/query/names.rb
+++ app/classes/query/names.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # base class for Query's which return Names
-class Query::Names < Query::Base
+class Query::Names < Query::Base # rubocop:disable Metrics/ClassLength
   include Query::Params::AdvancedSearch
   include Query::Params::Filters
   include Query::Initializers::AdvancedSearch
