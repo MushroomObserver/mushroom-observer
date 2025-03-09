dc0ee5447d9118cecbd69a0c8c47b23b97f7a891
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2025-03-09 11:28:43 -0100

    Revert "Fix line-length offenses"
    This reverts commit 04dab4b4ebb17d855e7ef8b15dfac3a8ea7fcb06.

--- lib/core_extensions/action_view/log_subscriber.rb
+++ lib/core_extensions/action_view/log_subscriber.rb
@@ -9,38 +9,28 @@ module CoreExtensions
 
         info do
           message = +"  Rendered collection of #{from_rails_root(identifier)}"
-          if event.payload[:layout]
-            message << " within #{from_rails_root(event.payload[:layout])}"
-          end
-          message << " #{render_count(event.payload)} " \
-            "(Duration: #{event.duration.round(1)}ms | " \
-            "Allocations: #{event.allocations})"
+          message << " within #{from_rails_root(event.payload[:layout])}" if event.payload[:layout]
+          message << " #{render_count(event.payload)} (Duration: #{event.duration.round(1)}ms | Allocations: #{event.allocations})"
+          message
         end
       end
+      # ::ActionView::LogSubscriber.subscribe_log_level :render_collection, :info
 
       def render_partial(event)
         if event.payload[:cache_hit].present?
           info do
-            message = +"  Rendered " \
-              "#{from_rails_root(event.payload[:identifier])}"
-            if event.payload[:layout]
-              message << " within #{from_rails_root(event.payload[:layout])}"
-            end
-            message << " (Duration: #{event.duration.round(1)}ms | " \
-              "Allocations: #{event.allocations})"
-            return message if event.payload[:cache_hit].nil?
-
-            message << " #{cache_message(event.payload)}"
+            message = +"  Rendered #{from_rails_root(event.payload[:identifier])}"
+            message << " within #{from_rails_root(event.payload[:layout])}" if event.payload[:layout]
+            message << " (Duration: #{event.duration.round(1)}ms | Allocations: #{event.allocations})"
+            message << " #{cache_message(event.payload)}" unless event.payload[:cache_hit].nil?
+            message
           end
         else
           debug do
-            message = +"  Rendered " \
-              "#{from_rails_root(event.payload[:identifier])}"
-            if event.payload[:layout]
-              message << " within #{from_rails_root(event.payload[:layout])}"
-            end
-            message << " (Duration: #{event.duration.round(1)}ms | " \
-              "Allocations: #{event.allocations})"
+            message = +"  Rendered #{from_rails_root(event.payload[:identifier])}"
+            message << " within #{from_rails_root(event.payload[:layout])}" if event.payload[:layout]
+            message << " (Duration: #{event.duration.round(1)}ms | Allocations: #{event.allocations})"
+            message
           end
         end
       end
