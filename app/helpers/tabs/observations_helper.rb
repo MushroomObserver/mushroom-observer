42cf712b117be13edb2e28d75073256868dbd071
andrew nimmo <andrnimm@fastmail.fm>
2025-03-15 23:27:05 -0700

    Update observations_helper.rb

--- app/helpers/tabs/observations_helper.rb
+++ app/helpers/tabs/observations_helper.rb
@@ -42,10 +42,11 @@ module Tabs
     # uses context_nav_links with extra_args { class: "d-block" }
     # the hiccup here is that list_descriptions is already HTML, an inline list
     def name_links_on_mo(name:)
-      tabs = context_nav_links(obs_related_name_tabs(name), { class: "d-block" })
+      tabs = context_nav_links(obs_related_name_tabs(name),
+                               { class: "d-block" })
       tabs += obs_name_description_tabs(name)
       tabs += context_nav_links([occurrence_map_for_name_tab(name)],
-                        { class: "d-block" })
+                                { class: "d-block" })
       tabs.reject(&:empty?)
     end
 
@@ -97,7 +98,8 @@ module Tabs
     # end
 
     def name_links_web(name:)
-      tabs = context_nav_links(observation_web_name_tabs(name), { class: "d-block" })
+      tabs = context_nav_links(observation_web_name_tabs(name),
+                               { class: "d-block" })
       tabs.reject(&:empty?)
     end
 
