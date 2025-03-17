5ed68b08ceb062336a34a8c13f6c60328a903742
andrew nimmo <andrnimm@fastmail.fm>
2025-03-17 04:44:13 -0700

    add methods from main

--- app/classes/query/base.rb
+++ app/classes/query/base.rb
@@ -68,6 +68,30 @@ class Query::Base
     parameter_declarations.keys.except(*subquery_parameters)
   end
 
+  # A "current_or_related_query" may be called for links:
+  # (1) for a new query on a related target model, using the current_query as
+  #     the filtering subquery.
+  # (2) from an index that itself was the result of a subquery.
+  #     For example, if you follow links in the current UI from:
+  #       [target model] of these [filtering model]
+  #       Observations of these names -> (that's a plain obs query)
+  #       Locations of these observations -> (location query with obs_subquery)
+  #       Map of these locations -> (loc, obs_subquery)
+  #       Names at these locations -> (name, obs_subquery, obs have the loc)
+  #       Observations of these names -> (obs query)
+  #     Note that the last index is really the original query, so to prevent
+  #     recursive subquery nesting, we always want check for the currently
+  #     needed (sub)query nested within the params.
+  # (3) from maps to indexes of the same objects. Returns the current_query.
+  #
+  def relatable?(target)
+    self.class.related?(target, model.name.to_sym)
+  end
+
+  def subquery_of(target)
+    self.class.current_or_related_query(target, model.name.to_sym, self)
+  end
+
   def default_order
     self.class.default_order ||
       raise("Didn't supply default order for #{model} query.")
