4d4ff490c17afbdd990a9d4b3822c33d6383360c
andrew nimmo <andrnimm@fastmail.fm>
2025-03-17 18:15:43 -0700

    Rename extensions files to agree with class names

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -5,6 +5,10 @@
 # Rake tasks, depending on their environment, might need to require this file
 # explicitly.
 #
-Rails.root.glob("app/extensions/*_extensions.rb").each do |file|
+Rails.root.glob("app/extensions/*.rb").each do |file|
+  require(file)
+end
+
+Rails.root.glob("app/extensions/active_record/*.rb").each do |file|
   require(file)
 end
