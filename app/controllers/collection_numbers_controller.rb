82ee689372e1ac1a6d3afc75b73a8a6f5b3c4963
andrew nimmo <andrnimm@fastmail.fm>
2025-03-17 22:11:06 -0700

    Switch controller callers of display_opts[:letters] to boolean

--- app/controllers/collection_numbers_controller.rb
+++ app/controllers/collection_numbers_controller.rb
@@ -41,7 +41,7 @@ class CollectionNumbersController < ApplicationController
 
   def index_display_opts(opts, _query)
     {
-      letters: "collection_numbers.name",
+      letters: true,
       num_per_page: 100
     }.merge(opts)
   end
