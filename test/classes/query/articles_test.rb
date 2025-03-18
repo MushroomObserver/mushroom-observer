65b01a936075d965ac7f14898f7095f9020c817e
andrew nimmo <andrnimm@fastmail.fm>
2025-03-17 19:30:32 -0700

    Update articles_test.rb

--- test/classes/query/articles_test.rb
+++ test/classes/query/articles_test.rb
@@ -18,10 +18,10 @@ class Query::ArticlesTest < UnitTestCase
   end
 
   def test_article_id_in_set
-    # art = articles(:premier_article)
-    # expects = [art.id]
-    # scope = Article.id_in_set(art.id)
-    # assert_query_scope(expects, scope, :Article, id_in_set: [art.id])
+    art = articles(:premier_article)
+    expects = [art.id]
+    scope = Article.id_in_set(art.id)
+    assert_query_scope(expects, scope, :Article, id_in_set: [art.id])
     assert_query([], :Article, id_in_set: [])
   end
 
