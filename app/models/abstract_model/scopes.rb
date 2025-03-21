52b5db8bd3d2117dc43bfd59d15dd4451aa9463b
andrew nimmo <andrnimm@fastmail.fm>
2025-03-21 13:26:51 -0700

    Update scopes.rb

--- app/models/abstract_model/scopes.rb
+++ app/models/abstract_model/scopes.rb
@@ -208,14 +208,15 @@ module AbstractModel::Scopes
       send_where_chain(conditions).distinct
     }
 
-    #############################################################################
+    ############################################################################
+    #
     # ADVANCED SEARCH SCOPES
     #
     # Search Content
     # Could do left outer join from observations to comments, but it
     # takes longer.  Instead, break it into two queries, one without
     # comments, and another with inner join on comments.
-    # `klass` refers to the model of an ActiveRecord_Relation
+    # NOTE: `klass` refers to the model of an ActiveRecord_Relation
     scope :search_content, lambda { |phrase|
       if klass == Observation
         obs_joins = nil
