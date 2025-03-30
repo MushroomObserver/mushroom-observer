2f62f2e7a0efac016fa3ab0b359af905e8fd0958
andrew nimmo <andrnimm@fastmail.fm>
2025-03-30 15:31:56 -0700

    Update query.rb

--- app/classes/query.rb
+++ app/classes/query.rb
@@ -213,11 +213,11 @@ class Query
 
   def self.new(model, params = {}, current = nil)
     klass = "Query::#{model.to_s.pluralize}".constantize
-    query = klass.new
-    query.params = params
+    query = klass.new(params)
+    query.params = query.attributes # initialize params for cleaning/validation
     query.subqueries = {}
-    query.validate_params
     query.current = current if current
+    query.valid = query.valid? # reinitializes params after cleaning/validation
     # query.initialize_query # if you want the attributes right away
     query
   end
