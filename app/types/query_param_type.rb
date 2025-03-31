df8330c9b97cf6c350a57ac81295115ef703f12c
andrew nimmo <andrnimm@fastmail.fm>
2025-03-31 15:55:41 -0700

    Fix advanced search query.params error

--- app/types/query_param_type.rb
+++ app/types/query_param_type.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This is a custom attribute type for the Query class parameters.
 # https://stackoverflow.com/a/79417688/3357635
 class QueryParamType < ActiveModel::Type::Value
   # This is required and used if you register the type
