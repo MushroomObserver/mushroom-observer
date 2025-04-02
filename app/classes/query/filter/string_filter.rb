cdbf19e47257f82c1bd1d9c573423c2354a88d31
andrew nimmo <andrnimm@fastmail.fm>
2025-04-01 17:19:16 -0700

    Update string_filter.rb

--- app/classes/query/filter/string_filter.rb
+++ app/classes/query/filter/string_filter.rb
@@ -9,19 +9,5 @@ class Query::Filter
     def on?(val)
       val.present?
     end
-
-    def sql_conditions(query, model, vals)
-      vals.map { |val| sql_condition(query, model, val) }.join(" OR ")
-    end
-
-    def scope_conditions(query, model, vals)
-      return unless vals.length.positive?
-
-      # Start without `or`, chain subsequent conditions
-      conditions = sql_condition(query, model, vals.shift)
-      vals.each do |val|
-        conditions = conditions.or(sql_condition(query, model, val))
-      end
-    end
   end
 end
