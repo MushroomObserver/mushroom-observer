0dc4c5ef99fa5929f48fd7c0fa03b5ed09b50d88
Nathan Wilson <nathan@collectivesource.com>
2025-04-06 09:07:51 -0400

    Add test for sequence creation via Turbo

--- test/controllers/sequences_controller_test.rb
+++ test/controllers/sequences_controller_test.rb
@@ -172,6 +172,17 @@ class SequencesControllerTest < FunctionalTestCase
            "Failed to include Sequence added in RssLog for Observation")
   end
 
+  def test_turbo_create
+    params = {
+      observation_id: observations(:detailed_unknown_obs).id,
+      sequence: { locus: "ITS",
+                  bases: ITS_BASES }
+    }
+    login
+    assert_difference("Sequence.count", 1) { post(:create, params: params,
+                                                  format: :turbo_stream) }
+  end
+
   def test_create_non_repo_sequence
     # Prove user can create non-repository Sequence
     obs = observations(:detailed_unknown_obs)
