456c0f6489834c618843490f03448c55783eaee6
andrew nimmo <andrnimm@fastmail.fm>
2025-04-06 11:58:14 -0700

    add locals, start tests

--- test/controllers/collection_numbers/remove_observations_controller_test.rb
+++ test/controllers/collection_numbers/remove_observations_controller_test.rb
@@ -3,44 +3,71 @@
 require("test_helper")
 
 class CollectionNumbers::RemoveObservationsControllerTest < FunctionalTestCase
-  def test_remove_observation
+  def setup_test_fixtures
     obs1 = observations(:agaricus_campestris_obs)
     obs2 = observations(:coprinus_comatus_obs)
     num1 = collection_numbers(:agaricus_campestris_coll_num)
     num2 = collection_numbers(:coprinus_comatus_coll_num)
+    [obs1, obs2, num1, num2]
+  end
+
+  def test_fixtures_are_correct
+    obs1, obs2, num1, num2 = setup_test_fixtures
     assert_obj_arrays_equal([num1], obs1.collection_numbers)
     assert_obj_arrays_equal([num2], obs2.collection_numbers)
+  end
 
-    # Make sure user must be logged in.
+  # Make sure user must be logged in.
+  def test_must_be_logged_in
+    obs1, _obs2, num1, _num2 = setup_test_fixtures
+
     patch(:update, params: { collection_number_id: num1.id,
                              observation_id: obs1.id })
     assert_obj_arrays_equal([num1], obs1.reload.collection_numbers)
+  end
 
-    # Make sure only owner obs can remove num from it.
-    login("mary")
+  # Make sure only owner obs can remove num from it.
+  def test_only_owner_can_remove
+    obs1, _obs2, num1, _num2 = setup_test_fixtures
+
+    login("mary") # owner is rolf
     patch(:update, params: { collection_number_id: num1.id,
                              observation_id: obs1.id })
     assert_obj_arrays_equal([num1], obs1.reload.collection_numbers)
+  end
 
-    # Make sure badly-formed queries don't crash.
+  # Make sure badly-formed queries don't crash.
+  def test_badly_formed_queries_dont_crash
+    obs1, obs2, num1, num2 = setup_test_fixtures
+
     login("rolf")
-    # patch(:update) not a valid route
     patch(:update, params: { collection_number_id: -1 })
     patch(:update, params: { collection_number_id: num1.id })
     patch(:update, params: { collection_number_id: num1.id,
                              observation_id: "bogus" })
+    # wrong observation for num1
     patch(:update, params: { collection_number_id: num1.id,
                              observation_id: obs2.id })
     assert_obj_arrays_equal([num1], obs1.reload.collection_numbers)
     assert_obj_arrays_equal([num2], obs2.reload.collection_numbers)
+  end
 
-    # Removing num from last obs destroys it.
+  # Removing num from last obs destroys it.
+  def test_removing_actually_destroys_when_no_other_associations
+    obs1, _obs2, num1, _num2 = setup_test_fixtures
+
+    login("rolf")
     patch(:update, params: { collection_number_id: num1.id,
                              observation_id: obs1.id })
     assert_empty(obs1.reload.collection_numbers)
     assert_nil(CollectionNumber.safe_find(num1.id))
+  end
 
-    # Removing num from one of two obs does not destroy it.
+  # Removing num from one of two obs does not destroy it.
+  def test_removing_from_obs_does_not_destroy_when_belongs_to_two_obs
+    obs1, obs2, _num1, num2 = setup_test_fixtures
+
+    login("rolf")
     num2.add_observation(obs1)
     assert_obj_arrays_equal([num2], obs1.reload.collection_numbers)
     assert_obj_arrays_equal([num2], obs2.reload.collection_numbers)
@@ -49,8 +76,12 @@ class CollectionNumbers::RemoveObservationsControllerTest < FunctionalTestCase
     assert_obj_arrays_equal([num2], obs1.reload.collection_numbers)
     assert_empty(obs2.reload.collection_numbers)
     assert_not_nil(CollectionNumber.safe_find(num2.id))
+  end
 
-    # Finally make sure admin has permission.
+  # Finally make sure admin has permission.
+  def test_admin_can_remove
+    obs1, _obs2, _num1, num2 = setup_test_fixtures
+
     make_admin("mary")
     patch(:update, params: { collection_number_id: num2.id,
                              observation_id: obs1.id })
@@ -63,6 +94,7 @@ class CollectionNumbers::RemoveObservationsControllerTest < FunctionalTestCase
     nums  = obs.collection_numbers
     query = Query.lookup_and_save(:CollectionNumber)
     q     = query.id.alphabetize
+
     login(obs.user.login)
     assert_operator(nums.length, :>, 1)
 
@@ -70,5 +102,16 @@ class CollectionNumbers::RemoveObservationsControllerTest < FunctionalTestCase
     patch(:update, params: { collection_number_id: nums[1].id,
                              observation_id: obs.id, q: q })
     assert_redirected_to(observation_path(id: obs.id, q: q))
+  end
+
+  def test_turbo_remove_collection_number
+    obs1, _obs2, num1, _num2 = setup_test_fixtures
+
+    login("rolf")
+    params = { collection_number_id: num1.id,
+               observation_id: obs1.id }
+    patch(:update, params: params,
+                   format: :turbo_stream)
+    assert_obj_arrays_equal([num1], obs1.reload.collection_numbers)
   end
 end
