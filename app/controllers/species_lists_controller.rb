f215ba16a431ffcfa96f6671abd7aee722799fe1
andrew nimmo <andrnimm@fastmail.fm>
2025-04-07 13:17:43 -0700

    Update species_lists_controller.rb

--- app/controllers/species_lists_controller.rb
+++ app/controllers/species_lists_controller.rb
@@ -187,9 +187,11 @@ class SpeciesListsController < ApplicationController
     store_query_in_session(@query) if params[:set_source].present?
 
     @query.need_letters = true
-    @pagination_data = letter_pagination_data(:letter, :page, 100)
-    @objects = @query.paginate(@pagination_data, include:
-                  [:user, :name, :location, { thumb_image: :image_votes }])
+    @pagination_data = number_pagination_data(:page, 100)
+    @objects = @query.paginate(
+      @pagination_data,
+      include: [:user, :name, :location, { thumb_image: :image_votes }]
+    )
     # Save a lookup in comments_for_object
     @comments = @species_list.comments&.sort_by(&:created_at)&.reverse
   end
