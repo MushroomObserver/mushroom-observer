0f43e9d200addb7e10e75038ab63b73b083dd98f
andrew nimmo <andrnimm@fastmail.fm>
2025-04-15 00:40:31 -0700

    Update query_param_type.rb

--- app/types/query_param_type.rb
+++ app/types/query_param_type.rb
@@ -1,41 +1,41 @@
 # frozen_string_literal: true
 
-# Custom attribute type for Query subclass attributes (parameters).
+# MO Custom attribute type for all Query subclass attributes (parameters).
 # The arg `:accepts` describes valid or parseable attribute values,
 # according to the following syntax:
 #
-# = Simple values
+# = Simple value types
 #   :string
 #   :date
 #   :time
 #   :float
 #   :boolean
-#   SpeciesList (any ActiveRecord model instance or id, parsed as an id)
+#   SpeciesList (ActiveRecord model instance or id, parsed as an id)
 #
-# = Array values
+# = Array value types
 #   [:string]
 #   [:date]
 #   [:time]
 #   [Project] (array of ActiveRecord model instances or ids, parsed as ids)
 #
-# = Hash values
+# = Hash value types
 #   { string: [:yes, :no] } (evaluated as an "enum", other values ignored)
 #   { boolean: [true] } (evaluated as an "enum", `false` ignored )
 #   { subquery: :Observation } (evaluated as a subquery, sub-params forwarded)
-#   { north:, south:, east:, west:} (forwarded as a hash of values)
+#   { north:, south:, east:, west: } (forwarded as a hash of values)
 #
-# re: custom types - https://stackoverflow.com/a/79417688/3357635
+# re: custom attribute types - https://stackoverflow.com/a/79417688/3357635
 #
 class QueryParamType < ActiveModel::Type::Value
   attr_reader :accepts
 
-  # We don't want to call `super` here, we want to overwrite it, because it
-  # will not accept our custom arg.
-  def initialize(**args) # rubocop:disable Lint/MissingSuper
-    @accepts = args[:accepts]
+  # Add our custom arg :accepts to the default args.
+  def initialize(precision: nil, limit: nil, scale: nil, accepts: nil)
+    super(precision:, limit:, scale:)
+    @accepts = accepts
   end
 
-  # This is required and used if you register the type
-  # instead of just passing the class
+  # This is required and used if registering the type instead of just passing
+  # the class (registered in config/initializers/active_model_types.rb)
   def type = :query_param
 end
