1678d429767d3c64a06dcbc81506749dce929bd2
andrew nimmo <andrnimm@fastmail.fm>
2025-04-14 18:36:23 -0700

    Update base.rb

--- app/classes/query/base.rb
+++ app/classes/query/base.rb
@@ -76,8 +76,10 @@
 #  Even though `query_param` is a single declared data type, it actually is
 #  the `parameter_declarations` that give the information about the way the
 #  attribute is validated. (My plan was to add that as an arg to each
-#  `query_param` and have a validator automatically parse the declared arg for
-#  all `query_param`s, but i couldn't figure out how to do this.)
+#  `query_param` and have a custom validator parse the declared arg in order
+#  to know how to validate each `query_param`s, but i couldn't figure out
+#  how to do this. This would have allowed simpler `attribute` declarations,
+#  more similar to ActiveRecord models. - AN 2025)
 #
 #  We use a special syntax to declare the data type of each Query parameter /
 #  attribute. The syntax is important because it tells our validation method in
