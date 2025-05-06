b5d1e910d1c8e53b869a9fe022461abbed434917
andrew nimmo <andrnimm@fastmail.fm>
2025-05-05 23:00:58 -0700

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -128,8 +128,7 @@ class Comment < AbstractModel
 
   broadcasts_to(->(comment) { [comment.target, :comments] },
                 inserts_by: :prepend, partial: "comments/comment",
-                locals: { controls: true },
-                target: "comments")
+                locals: { controls: true }, target: "comments")
 
   after_create :notify_users
   after_create :oil_and_water
