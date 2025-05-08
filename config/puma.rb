d600c220543c21c544a3296d8a4d88ad3f07c816
andrew nimmo <andrnimm@fastmail.fm>
2025-05-08 07:02:03 -0700

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -24,8 +24,11 @@ end
 environment rails_env
 pidfile         "#{app_path}/tmp/pids/puma.pid"
 state_path      "#{app_path}/tmp/pids/puma.state"
-activate_control_app
+# To run Solid Queue's supervisor together with Puma and have Puma monitor
+# and manage it. With this you shouldn't have to `bin/rails solid_queue:start`.
+# https://github.com/rails/solid_queue?tab=readme-ov-file#puma-plugin
 plugin :solid_queue
+activate_control_app
 
 if rails_env == "production"
   on_worker_boot do
