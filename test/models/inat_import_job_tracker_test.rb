526bb0ce245357a622075775b223d7600982d1eb
Joe Cohen <jdcohenesq@gmail.com>
2025-05-21 15:17:16 -0700

    Fix failure from prior commit

--- test/models/inat_import_job_tracker_test.rb
+++ test/models/inat_import_job_tracker_test.rb
@@ -9,9 +9,11 @@ class InatImportJobTrackerTest < ActiveSupport::TestCase
   def test_timings
     tracker = inat_import_job_trackers(:timings_tracker)
     inat_import = inat_imports(:timings_import)
+    created = tracker.created_at
 
-    travel((inat_import.imported_count * 10).seconds)
-
-    assert_equal("00:01:20", tracker.remaining_time)
+    # simulate 10 seconds per each obs imported so far
+    travel_to(created + (inat_import.imported_count * 10).seconds) do
+      assert_equal("00:01:20", tracker.remaining_time)
+    end
   end
 end
