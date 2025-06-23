596d212b6f326626ed01fc19977193acfbb8591b
Nathan Wilson <nathan@collectivesource.com>
2025-06-23 14:37:24 -0400

    Rubocop cop and quick cleanup

--- app/controllers/observations_controller/new.rb
+++ app/controllers/observations_controller/new.rb
@@ -65,8 +65,7 @@ module ObservationsController::New
   def init_naming_and_vote
     @naming      = Naming.new
     @vote        = Vote.new
-    trace_tests
-    @given_name  = params[:name] || ""  # can't be nil else rails tries to call @name.name
+    @given_name = params[:name] || ""
     return unless params[:notes] && params[:notes][:Field_Slip_ID]
 
     @given_name = params[:notes][:Field_Slip_ID].tr("_", "")
