f21b50a52805289284d41e255031b1365f7b19f5
andrew nimmo <andrnimm@fastmail.fm>
2025-06-26 02:34:30 -0700

    Update section-update_controller.js
    Clean up. This controller could go away, actually.

--- app/javascript/controllers/section-update_controller.js
+++ app/javascript/controllers/section-update_controller.js
@@ -6,19 +6,15 @@ export default class extends Controller {
   // this is a handler for page elements that get updated
   // on successful form submit, so it "cleans up"
   connect() {
-    this.element.dataset.stimulus = "section-update-connected"
+    this.element.dataset.sectionUpdate = "connected"
 
-    // Note: this is simpler than adding a data-action attribute on every
-    // section replaced by Turbo
+    // Simpler than adding a data-action attribute on every section
+    // replaced by Turbo?
     this.element.addEventListener("turbo:frame-render", this.updated())
   }
 
   updated() {
-    console.log(this.element.id + " turbo:frame-render section updated")
-    // Remove modal which is present for certain updates (but not all)
-    // Must be in jQuery for Boostrap 3 and 4
-    $("#mo_ajax_progress").modal('hide')
-    document.getElementById('mo_ajax_progress_caption').innerHTML = ""
+    // console.log(this.element.id + " turbo:frame-render section updated")
     // broadcast change
     this.dispatch("updated")
   }
