536bdb6befc78bcf83a56445b86ba6d57094c8cc
andrew nimmo <andrnimm@fastmail.fm>
2025-06-26 02:33:57 -0700

    Update modal_controller.js
    Split up modal actions: remove, hide, resetProgress

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -7,17 +7,30 @@ export default class extends Controller {
 
   connect() {
     // console.log("Hello Modal " + this.element.id);
-    this.element.dataset.stimulus = "modal-connected";
+    this.element.dataset.modal = "connected"
   }
 
-  // Modal is only removed in the event that the page section updates.
+  // Modal form is only removed in the event that the page section updates.
   // That event is broadcast from the section-update controller.
-  // We can't fire on form submit response, because unless something's wrong
-  // turbo-stream will send a 200 response.
-  // Must be in jQuery for Boostrap 3 and 4
+  // We can't fire based on submit response, because unless something's wrong
+  // with the request, turbo-stream will send a 200 OK even if it didn't save.
   remove() {
     console.log("Removing modal")
-    $(this.element).modal('hide')
+    this.hide()
     this.element.remove()
+  }
+
+  // The modal_ajax_progress for voting should only be hidden,
+  // since it is printed in the layout and should not be removed from the page.
+  // Must be in jQuery for Boostrap 3 and 4
+  hide() {
+    console.log("Hiding modal")
+    $(this.element).modal('hide')
+    this.resetProgress()
+  }
+
+  // Reset the text within the progress modal, if it exists.
+  resetProgress() {
+    document.getElementById('mo_ajax_progress_caption').innerHTML = ""
   }
 }
