3fbd2614b804b0ed91197e17a10d5ea82a9e3a52
Nathan Wilson <nathan@collectivesource.com>
2025-07-14 21:17:11 -0400

    Add back code index changes

--- app/models/herbarium.rb
+++ app/models/herbarium.rb
@@ -64,7 +64,7 @@ class Herbarium < AbstractModel
   belongs_to :personal_user, class_name: "User"
 
   # Was unable to create an appropriate index that made Trilogy happy.
-  validates :code, uniqueness: true, allow_blank: true # rubocop:disable Rails/UniqueValidationWithoutIndex
+  validates :code, uniqueness: true, allow_blank: true
 
   scope :order_by_default,
         -> { order_by(::Query::Herbaria.default_order) }
