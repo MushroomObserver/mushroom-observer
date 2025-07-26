b7c6f16ba28d4c8726c45d56276846c099fd4b3d
Nathan Wilson <nathan@collectivesource.com>
2025-07-26 14:06:50 -0400

    Work In Progress

---
+++ app/controllers/species_lists/write_in_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module SpeciesLists
+  class WriteInController < ApplicationController
+    before_action :login_required
+
+    def new; end
+
+    def create
+    end
+  end
+end
