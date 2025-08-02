45ddf82aefbc5cb2bf92f478949b39531cbb682c
andrew nimmo <andrnimm@fastmail.fm>
2025-08-02 01:35:15 -0700

    Get user sidebar using tabs

--- app/helpers/tabs/sidebar_helper.rb
+++ app/helpers/tabs/sidebar_helper.rb
@@ -2,6 +2,24 @@
 
 module Tabs
   module SidebarHelper
+    def sidebar_user_tabs(user)
+      [
+        comments_for_user_tab(user, :app_comments_for_you.t),
+        account_show_interests_tab,
+        user_summary_tab(user),
+        account_edit_preferences_tab,
+        join_mailing_list_tab
+      ]
+    end
+
+    def join_mailing_list_tab
+      InternalLink.new(
+        :app_join_mailing_list.t,
+        "https://groups.google.com/forum/?fromgroups=#!forum/mo-general",
+        html_options: { id: "nav_join_mailing_list_link" }
+      ).tab
+    end
+
     def sidebar_observations_tabs(user)
       [
         nav_latest_observations_tab,
