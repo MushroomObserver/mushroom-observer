2229c39305c30f7a821e1763c4c47adb269cdb20
andrew nimmo <andrnimm@fastmail.fm>
2025-08-03 11:38:28 -0700

    Rearrange project tabs and page title for species list

--- app/helpers/species_lists_helper.rb
+++ app/helpers/species_lists_helper.rb
@@ -2,12 +2,12 @@
 
 # View Helpers for Observation Lists
 module SpeciesListsHelper
-  def species_list_title_panel(list)
-    tag.div(class: "species_list_title") do
-      concat(tag.br)
-      concat(panel_block(id: "species_list_title") do
-               tag.span(list.unique_format_name, class: "h3")
-             end)
-    end
-  end
+  # def species_list_title_panel(list)
+  #   tag.div(class: "species_list_title") do
+  #     concat(tag.br)
+  #     concat(panel_block(id: "species_list_title") do
+  #              tag.span(list.unique_format_name, class: "h3")
+  #            end)
+  #   end
+  # end
 end
