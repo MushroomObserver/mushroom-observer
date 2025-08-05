5174085d5d94205ba5229713af8d045ef1f9f22d
andrew nimmo <andrnimm@fastmail.fm>
2025-08-04 23:48:42 -0700

    Delete commented out code

--- app/helpers/species_lists_helper.rb
+++ app/helpers/species_lists_helper.rb
@@ -2,12 +2,4 @@
 
 # View Helpers for Observation Lists
 module SpeciesListsHelper
-  # def species_list_title_panel(list)
-  #   tag.div(class: "species_list_title") do
-  #     concat(tag.br)
-  #     concat(panel_block(id: "species_list_title") do
-  #              tag.span(list.unique_format_name, class: "h3")
-  #            end)
-  #   end
-  # end
 end
