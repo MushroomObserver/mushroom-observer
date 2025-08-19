27b0c53c4ec6664083df641c4f1d968c6f9e2869
andrew nimmo <andrnimm@fastmail.fm>
2025-08-19 12:37:02 -0700

    Get main partial refactored

--- app/helpers/species_lists_helper.rb
+++ app/helpers/species_lists_helper.rb
@@ -2,4 +2,14 @@
 
 # View Helpers for Observation Lists
 module SpeciesListsHelper
+  def species_list_remove_button(species_list:, observation:)
+    put_button(
+      name: :REMOVE.t,
+      path: observation_species_list_path(
+        id: observation.id,
+        species_list_id: species_list.id,
+        commit: "remove"
+      ), data: { confirm: :are_you_sure.l }
+    )
+  end
 end
