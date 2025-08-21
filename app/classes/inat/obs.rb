8eec39c1918f4e44d340f3085ec1e338ab5cda09
Joe Cohen <jdcohenesq@gmail.com>
2025-08-20 17:02:09 -0700

    Map several iNat Obsevation Fields to MO Notes Collector

--- app/classes/inat/obs.rb
+++ app/classes/inat/obs.rb
@@ -190,13 +190,38 @@ class Inat
 
     ########## Other mappings used in MO Observations
 
+    # some iNat Observation Fields used for the collector(s) name
+    # https://www.inaturalist.org/observation_fields?commit=Search&page=1&q=collector&utf8=%E2%9C%93
+    # iNat Observation fields are a mess, with lots of duplication
+    INAT_COLLECTOR_FIELDS = [ # rubocop:disable Lint/UselessConstantScoping
+      "Collector's name", # 2025 Continental MycoBlitz
+      "Collector’s Name", # right single quote
+      "Collector Names",
+      "Names of Collectors",
+      "Collector's Full Name",
+      "Collector",
+      "Name of collector",
+      "Original Collector/Observer",
+      "Collector name",
+      "Collectors Name",
+      "Original Collector/ Observer"
+    ].freeze
     # This will get put into MO Observation's Notes Collector:
     def collector
+=begin
       if inat_obs_field("Collector").present?
         inat_obs_field("Collector")[:value]
       else
         self[:user][:login]
       end
+=end
+      INAT_COLLECTOR_FIELDS.each do |field_name|
+        if inat_obs_field(field_name).present?
+          return inat_obs_field(field_name)[:value]
+        end
+      end
+
+      self[:user][:login]
     end
 
     def dqa
