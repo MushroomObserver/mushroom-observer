13b02785a4f29c80bd2fb386f1c5f97f95e2a1a0
andrew nimmo <andrnimm@fastmail.fm>
2025-08-28 18:32:32 -0700

    Update namings_helper.rb

--- app/helpers/namings_helper.rb
+++ app/helpers/namings_helper.rb
@@ -120,11 +120,9 @@ module NamingsHelper
     percent = "#{naming.vote_percent.round}%"
 
     modal_link_to("naming_votes_#{naming.id}", h(percent),
-                  add_query_param(
-                    observation_naming_votes_path(
-                      observation_id: naming.observation_id,
-                      naming_id: naming.id
-                    )
+                  observation_naming_votes_path(
+                    observation_id: naming.observation_id,
+                    naming_id: naming.id
                   ),
                   class: "vote-percent btn btn-link px-0")
   end
