683a16228c2c706b1ab6ab776be22891726634ee
Nathan Wilson <nathan@collectivesource.com>
2025-09-03 08:44:33 -0400

    Rubocop

--- test/classes/observation_labels/fields_test.rb
+++ test/classes/observation_labels/fields_test.rb
@@ -9,10 +9,13 @@ class FieldsTest < UnitTestCase
     obs_fields = ObservationLabels::Fields.new(obs)
     similar_obs = obs_fields.similar_observations
     assert_includes(similar_obs, observations(:dup_megacollybia_rodmanii_obs))
-    assert_includes(similar_obs, observations(:nearby_megacollybia_rodmanii_obs))
+    assert_includes(similar_obs,
+                    observations(:nearby_megacollybia_rodmanii_obs))
     assert_not_includes(similar_obs,
                         observations(:faraway_megacollybia_rodmanii_obs))
-    assert_not_includes(similar_obs, observations(:older_megacollybia_rodmanii_obs))
-    assert_not_includes(similar_obs, observations(:megacollybia_platyphylla_obs))
+    assert_not_includes(similar_obs,
+                        observations(:older_megacollybia_rodmanii_obs))
+    assert_not_includes(similar_obs,
+                        observations(:megacollybia_platyphylla_obs))
   end
 end
