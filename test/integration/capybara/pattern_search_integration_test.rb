c38d1fcd203362cf367b809539029399a36f60db
andrew nimmo <andrnimm@fastmail.fm>
2025-09-07 15:33:17 -0700

    Delete pattern_search_integration_test.rb

--- test/integration/capybara/pattern_search_integration_test.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require("test_helper")
-
-# Integration tests of the Observations controller and views.
-class ObservationsIntegrationTest < CapybaraIntegrationTestCase
-end
