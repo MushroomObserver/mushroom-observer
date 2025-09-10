bcb384e4749866b644efe2bada32018fa25dd60e
andrew nimmo <andrnimm@fastmail.fm>
2025-09-09 17:11:37 -0700

    Null in_box params if empty

--- test/integration/capybara/observations_integration_test.rb
+++ test/integration/capybara/observations_integration_test.rb
@@ -204,30 +204,6 @@ class ObservationsIntegrationTest < CapybaraIntegrationTestCase
     assert_selector("#title", text: "#{obs.id} #{obs.name.search_name}")
   end
 
-  def test_observation_search_form
-    lookup = "Agaricus campestris"
-    location = locations(:burbank)
-
-    login
-    visit("/observations/search/new")
-    within("#observations_search_form") do |form|
-      assert_selector("#query_observations_names_lookup")
-      form.fill_in("query_observations_names_lookup", with: lookup)
-      # assert_selector("#query_observations_names_lookup", text: lookup)
-      form.select("yes", from: "query_observations_names_include_synonyms")
-      location.bounding_box.each do |key, val|
-        form.fill_in("query_observations_in_box_#{key}", with: val)
-      end
-
-      first(:button, type: "submit").click
-    end
-
-    assert_no_selector("#flash_notices")
-    assert_selector("#filters", text: lookup)
-    assert_selector("#results", text: lookup)
-    assert_selector("#results", text: observations(:agaricus_campestris_obs).id)
-  end
-
   # Tests of show_name_helper module
   # Prove that all these links appear under "Observations of"
   def test_links_to_observations_of
