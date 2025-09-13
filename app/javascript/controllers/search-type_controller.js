0bf07109052b6fc1c015cd695b2fb95bed89c852
andrew nimmo <andrnimm@fastmail.fm>
2025-09-13 15:45:03 -0700

    Sorta workin

--- app/javascript/controllers/search-type_controller.js
+++ app/javascript/controllers/search-type_controller.js
@@ -1,19 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 import { get } from "@rails/request.js" // allows us to call `get` below
+import { delegate, abnegate } from 'jquery-events-to-dom-events'
+
+// https://github.com/leastbad/jquery-events-to-dom-events
+// We use a Stimulus action that listens to `$shown.bs.collapse` on the div
+// (note the `$`). This depends on using `delegate` from the imported library
+// jquery-events-to-dom-events. `delegate` listens for `shown.bs.collapse`, a
+// jQuery event (not a native JS event), and dispatches a native event (with $).
+//
+// If moving to BS 5, can remove.
 
 // Connects to data-controller="search-type"
 export default class extends Controller {
   // targetHelp is the collapse div where help text or form will be rendered
-  static targets = ["select", "help", "helpToggle", "form", "formToggle"]
+  static targets = ["select", "bar", "barToggle", "help", "helpToggle",
+    "form", "formToggle"]
   // helpTypes are PatternSearch classes, formTypes are existing faceted forms.
   static values = { helpTypes: Array, formTypes: Array }
 
   connect() {
-    this.element.dataset.searchType = "connected";
+    this.element.dataset.searchType = "connected"
+    this.delegate = delegate('shown.bs.collapse')
+    this.getHelp()
+    this.getForm()
+  }
+
+  disconnect() {
+    abnegate('shown.bs.collapse', this.delegate)
   }
 
   async getHelp(event) {
-    event.stopPropagation()
+    // event.stopPropagation()
 
     const url = this.helpUrl()
     // console.log(`got url ${url}`);
@@ -32,6 +49,7 @@ export default class extends Controller {
     }
   }
 
+  // Fires when search-type select is changed
   // Both empties the collapse div and hides the toggle, to avoid confusion
   hideHelp() {
     this.helpTarget.innerHTML = ""
@@ -49,7 +67,7 @@ export default class extends Controller {
   }
 
   async getForm(event) {
-    event.stopPropagation()
+    // event.stopPropagation()
 
     const url = this.formUrl()
     // console.log(`got url ${url}`);
@@ -62,12 +80,13 @@ export default class extends Controller {
     const response = await get(url, { responseKind: "turbo-stream" });
     if (response.ok) {
       // Turbo updates the element in the page already
-      this.toggleTarget.classList.remove("d-none")
+      this.formToggleTarget.classList.remove("d-none")
     } else {
       console.log(`got a ${response.status}`);
     }
   }
 
+  // Fires when search-type select is changed
   // Both empties the collapse div and hides the toggle, to avoid confusion
   hideForm() {
     this.formTarget.innerHTML = ""
@@ -82,5 +101,24 @@ export default class extends Controller {
     }
 
     return "/" + controller + "/search/new"
+  }
+
+  // Bootstrap 3 accordions require css panels,
+  // so we have to make our own accordion functionality.
+  closeBar(event) {
+    console.log("closeBar")
+    if (this.hasBarTarget) {
+      $(this.barTarget).collapse("hide")
+    }
+    if (this.hasHelpTarget) {
+      $(this.helpTarget).collapse("hide")
+    }
+  }
+
+  closeForm(event) {
+    console.log("closeForm")
+    if (this.hasFormTarget) {
+      $(this.formTarget).collapse("hide")
+    }
   }
 }
