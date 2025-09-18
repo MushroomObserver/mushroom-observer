719fdd52b5f06fafa277bdc914b9c3a0ea7c4889
andrew nimmo <andrnimm@fastmail.fm>
2025-09-18 13:48:47 -0700

    Fix hidden field default id assignment on autocompleters

--- app/helpers/autocompleter_helper.rb
+++ app/helpers/autocompleter_helper.rb
@@ -144,7 +144,7 @@ module AutocompleterHelper
   # minimum args :form, :type. Send :hidden_name to override default field name,
   # :hidden_value to fill id, :hidden_data to merge with hidden field data
   def autocompleter_hidden_field(**args)
-    return unless args[:form].present? && args[:field].present?
+    return unless args[:form].present? && args[:type].present?
 
     # Default field name is "#{type}_id", so obs.place_name gets obs.location_id
     id = args[:hidden_name] || :"#{args[:type]}_id"
