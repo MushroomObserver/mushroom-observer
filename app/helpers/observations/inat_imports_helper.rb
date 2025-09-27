cdbdab8128841e8b8b19f73ee9638f075a4eeb36
Joseph D. Cohen <JoeCohen@users.noreply.github.com>
2025-09-27 06:10:00 -0400

    Add expected count and link to form

--- app/helpers/observations/inat_imports_helper.rb
+++ app/helpers/observations/inat_imports_helper.rb
@@ -2,5 +2,12 @@
 
 module Observations
   module InatImportsHelper
+    def inat_expected_import_count
+      :inat_import_tbd.l
+    end
+
+    def inat_expected_imports_link
+      :inat_import_tbd.l
+    end
   end
 end
