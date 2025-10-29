422a8ffff03ed74a0031af4ac799ffeda16c03ee
Joe Cohen <jdcohenesq@gmail.com>
2025-10-29 15:43:45 -0700

    Shorten variable name
    This is a thoroughly tested, completely unused class! But I can now revise and use it.

--- app/classes/inat/taxon.rb
+++ app/classes/inat/taxon.rb
@@ -2,6 +2,9 @@
 
 #
 # class to map iNat taxa to MO Names
+# Operates on any iNat :taxon hash, including the :taxon in
+#  - one result of an iNat API observation response,
+#  - one :idenfication of an iNat API observation response
 #
 #  == Class methods
 #
@@ -11,8 +14,8 @@
 #
 class Inat
   class Taxon
-    def initialize(inat_taxon)
-      @inat_taxon = inat_taxon
+    def initialize(taxon)
+      @taxon = taxon
     end
 
     def name
@@ -34,20 +37,20 @@ class Inat
     def matching_mo_names
       return matching_complexes if complex?
 
-      ::Name.where(text_name: @inat_taxon[:name],
-                   rank: @inat_taxon[:rank].titleize).
+      ::Name.where(text_name: @taxon[:name],
+                   rank: @taxon[:rank].titleize).
         # iNat doesn't have taxon names "sensu xxx"
         # so don't map them to MO Names sensu xxx
         where.not(::Name[:author] =~ /^sensu /)
     end
 
     def complex?
-      @inat_taxon[:rank] == "complex"
+      @taxon[:rank] == "complex"
     end
 
     def matching_complexes
       # text_names of MO groups include "group" or the like
-      ::Name.where(::Name[:text_name] =~ /^#{@inat_taxon[:name]}/).
+      ::Name.where(::Name[:text_name] =~ /^#{@taxon[:name]}/).
         where(rank: "Group")
     end
   end
