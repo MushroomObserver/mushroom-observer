b56cfcd30ab362c6322a06393840b668adbbd43d
andrew nimmo <andrnimm@fastmail.fm>
2025-10-30 22:07:56 -0700

    Remove panel subcomponents

--- app/components/panel.rb
+++ app/components/panel.rb
@@ -88,13 +88,71 @@ class Components::Panel < Components::Base
   private
 
   def render_heading
-    PanelHeading(
-      heading: @heading,
-      heading_links: @heading_links,
-      collapse: @collapse,
-      collapse_message: @collapse_message,
-      open: @open
-    )
+    div(class: "panel-heading") do
+      if @collapse.present?
+        render_collapsible_heading
+      else
+        render_static_heading
+      end
+    end
+  end
+
+  def render_static_heading
+    h4(class: "panel-title") do
+      # heading may contain HTML (e.g., submit buttons, formatted text) that
+      # needs to be rendered as HTML, not escaped as text
+      # rubocop:disable Rails/OutputSafety
+      raw(@heading.html_safe)
+      # rubocop:enable Rails/OutputSafety
+      if @heading_links.present?
+        # heading_links contains HTML from link_to helper that needs to be
+        # rendered as HTML, not escaped as text
+        # rubocop:disable Rails/OutputSafety
+        span(class: "float-right") { raw(@heading_links.html_safe) }
+        # rubocop:enable Rails/OutputSafety
+      end
+    end
+  end
+
+  def render_collapsible_heading
+    collapsed_class = @open ? "" : "collapsed"
+
+    h4(class: "panel-title") do
+      link_to(
+        "##{@collapse}",
+        class: class_names("panel-collapse-trigger", collapsed_class),
+        role: "button",
+        data: { toggle: "collapse" },
+        aria: { expanded: @open, controls: @collapse }
+      ) do
+        # heading may contain HTML (e.g., submit buttons, formatted text) that
+        # needs to be rendered as HTML, not escaped as text
+        # rubocop:disable Rails/OutputSafety
+        raw(@heading.html_safe)
+        # rubocop:enable Rails/OutputSafety
+        span(class: "float-right") { render_collapse_icons }
+      end
+    end
+  end
+
+  def render_collapse_icons
+    if @collapse_message.present?
+      span(class: "font-weight-normal mr-2") do
+        plain(@collapse_message)
+      end
+    end
+
+    chevron_down = link_icon(:chevron_down, title: :OPEN.l,
+                                            class: "active-icon")
+    chevron_up = link_icon(:chevron_up, title: :CLOSE.l)
+
+    # link_icon returns HTML strings that need to be rendered as HTML,
+    # not escaped as text. Nil check needed as link_icon may return nil
+    # in test environments where icon assets aren't available
+    # rubocop:disable Rails/OutputSafety
+    raw(chevron_down.html_safe) if chevron_down
+    raw(chevron_up.html_safe) if chevron_up
+    # rubocop:enable Rails/OutputSafety
   end
 
   def render_thumbnail
@@ -146,14 +204,25 @@ class Components::Panel < Components::Base
     # (otherwise it's on outer div)
     body_id = @heading.present? ? nil : @inner_id
 
-    PanelBody(
-      content: content,
-      inner_class: @inner_class,
-      inner_id: body_id
-    )
+    div(
+      class: class_names("panel-body", @inner_class),
+      id: body_id
+    ) do
+      # Content may contain HTML from Rails helpers (e.g., link_to, form
+      # elements) that needs to be rendered as HTML, not escaped as text
+      # rubocop:disable Rails/OutputSafety
+      raw(content.html_safe)
+      # rubocop:enable Rails/OutputSafety
+    end
   end
 
   def render_footer
-    PanelFooter(footer: @footer)
+    div(class: "panel-footer") do
+      # Footer may contain HTML from Rails helpers (e.g., link_to, buttons)
+      # that needs to be rendered as HTML, not escaped as text
+      # rubocop:disable Rails/OutputSafety
+      raw(@footer.html_safe)
+      # rubocop:enable Rails/OutputSafety
+    end
   end
 end
