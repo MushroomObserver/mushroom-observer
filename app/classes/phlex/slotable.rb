8b9e752193b3fb904816fb2b120370fc25af37ca
andrew nimmo <andrnimm@fastmail.fm>
2025-11-04 21:12:22 -0800

    Update slotable.rb

--- app/classes/phlex/slotable.rb
+++ app/classes/phlex/slotable.rb
@@ -44,69 +44,68 @@ module Phlex
         slot_name_with_type = type ? "#{type}_#{slot_name}" : slot_name
         signature = callable.nil? ? "(&block)" : "(*args, **kwargs, &block)"
 
-        setter_method =
-          if collection
-            <<-RUBY
-              def #{slot_name_with_type}#{signature}
-                @#{slot_name}_slots ||= []
-                @#{slot_name}_slots << #{
-                  callable_value(slot_name_with_type, callable)
-                }
-              end
-            RUBY
-          else
-            <<-RUBY
-              def #{slot_name_with_type}#{signature}
-                @#{slot_name}_slot = #{
-                  callable_value(slot_name_with_type, callable)
-                }
-              end
-            RUBY
-          end
+        setter_method = if collection
+                          <<-RUBY
+                            def #{slot_name_with_type}#{signature}
+                              @#{slot_name}_slots ||= []
+                              @#{slot_name}_slots << #{
+                                callable_value(slot_name_with_type, callable)
+                              }
+                            end
+                          RUBY
+                        else
+                          <<-RUBY
+                            def #{slot_name_with_type}#{signature}
+                              @#{slot_name}_slot = #{
+                                callable_value(slot_name_with_type, callable)
+                              }
+                            end
+                          RUBY
+                        end
 
         class_eval(setter_method, __FILE__, __LINE__)
-        if callable.is_a?(Proc)
-          define_lambda_method(slot_name_with_type, callable)
-        end
+        return unless callable.is_a?(Proc)
+
+        define_lambda_method(slot_name_with_type, callable)
       end
 
       def define_lambda_method(slot_name, callable)
         define_method(:"__call_#{slot_name}__", &callable)
-        private :"__call_#{slot_name}__"
+        private(:"__call_#{slot_name}__")
       end
 
       def define_getter_method(slot_name, collection:)
         getter_method = if collection
-          <<-RUBY
+                          <<-RUBY
             def #{slot_name}_slots = @#{slot_name}_slots ||= []
 
             private :#{slot_name}_slots
-          RUBY
-        else
-          <<-RUBY
+                          RUBY
+                        else
+                          <<-RUBY
             def #{slot_name}_slot = @#{slot_name}_slot
 
             private :#{slot_name}_slot
-          RUBY
-        end
+                          RUBY
+                        end
 
         class_eval(getter_method, __FILE__, __LINE__)
       end
 
       def define_predicate_method(slot_name, collection:)
         predicate_method = if collection
-          <<-RUBY
+                             <<-RUBY
             def #{slot_name}_slots? = #{slot_name}_slots.any?
 
             private :#{slot_name}_slots?
-          RUBY
-        else
-          <<-RUBY
+                             RUBY
+                           else
+                             <<-RUBY
             def #{slot_name}_slot? = !#{slot_name}_slot.nil?
 
             private :#{slot_name}_slot?
-          RUBY
-        end
+                             RUBY
+                           end
 
         class_eval(predicate_method, __FILE__, __LINE__)
       end
