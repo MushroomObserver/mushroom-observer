fbcc9f89c405b18dedd3fa74c455c1d024db4098
andrew nimmo <andrnimm@fastmail.fm>
2025-11-06 02:05:24 -0800

    image_show_args to template

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module ImagesHelper
-  # Args for the InteractiveImage component on Images#show
-  def image_show_args
-    { size: :huge,
-      image_link: "#",
-      extra_classes: "huge-image",
-      votes: false }
-  end
-
   # image vote lookup used in show_image
   def find_list_of_votes(image)
     image.image_votes.sort_by do |v|
