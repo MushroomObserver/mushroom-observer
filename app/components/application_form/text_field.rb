f098fba69d49464c9b4674d3a4459b2efabc3ba4
andrew nimmo <andrnimm@fastmail.fm>
2025-12-03 20:57:54 -0800

    Improve

--- app/components/application_form/text_field.rb
+++ app/components/application_form/text_field.rb
@@ -37,51 +37,58 @@ class Components::ApplicationForm < Superform::Rails::Form
 
     private
 
-    # rubocop:disable Metrics/AbcSize
     def render_with_wrapper(&field_input)
-      label_option = wrapper_options[:label]
-      show_label = label_option != false
-      label_text = if label_option.is_a?(String)
-                     label_option
-                   else
-                     field.key.to_s.humanize
-                   end
-      inline = wrapper_options[:inline] || false
-      wrap_class = wrapper_options[:wrap_class]
-      wrap_data = wrapper_options[:wrap_data]
-
-      div(class: form_group_class("form-group", inline, wrap_class),
-          data: wrap_data) do
-        render_label_row(label_text, inline) if show_label
+      div(class: wrapper_class, data: wrapper_options[:wrap_data]) do
+        render_label_row(label_text, inline?) if show_label?
         render_field_input(&field_input)
         render_help_after_field
         render(append_slot) if append_slot
       end
     end
-    # rubocop:enable Metrics/AbcSize
 
-    def render_field_input
-      button = wrapper_options[:button]
-      button_data = wrapper_options[:button_data] || {}
-      addon = wrapper_options[:addon]
+    def show_label?
+      wrapper_options[:label] != false
+    end
 
-      if button.present?
-        div(class: "input-group") do
-          yield
-          span(class: "input-group-btn") do
-            button(type: "button", class: "btn btn-default",
-                   data: button_data) do
-              button
-            end
-          end
-        end
-      elsif addon.present?
-        div(class: "input-group") do
-          yield
-          span(class: "input-group-addon") { addon }
-        end
+    def label_text
+      label_option = wrapper_options[:label]
+      label_option.is_a?(String) ? label_option : field.key.to_s.humanize
+    end
+
+    def inline?
+      wrapper_options[:inline] || false
+    end
+
+    def wrapper_class
+      form_group_class("form-group", inline?, wrapper_options[:wrap_class])
+    end
+
+    def render_field_input(&block)
+      if wrapper_options[:button]
+        render_input_with_button(&block)
+      elsif wrapper_options[:addon]
+        render_input_with_addon(&block)
       else
         yield
+      end
+    end
+
+    def render_input_with_button
+      div(class: "input-group") do
+        yield
+        span(class: "input-group-btn") do
+          button(type: "button", class: "btn btn-default",
+                 data: wrapper_options[:button_data] || {}) do
+            wrapper_options[:button]
+          end
+        end
+      end
+    end
+
+    def render_input_with_addon
+      div(class: "input-group") do
+        yield
+        span(class: "input-group-addon") { wrapper_options[:addon] }
       end
     end
 
