c25e2490a420e2d9073f493e337c637de1f9802a
andrew nimmo <andrnimm@fastmail.fm>
2025-12-06 14:22:58 -0800

    Update queued_email_test.rb

--- test/models/queued_email_test.rb
+++ test/models/queued_email_test.rb
@@ -17,18 +17,6 @@ class QueuedEmailTest < UnitTestCase
     assert_not(email.send_email)
   end
 
-  def test_dump
-    name_tracker = name_trackers(:agaricus_campestris_name_tracker_with_note)
-    naming = namings(:agaricus_campestris_naming)
-    email = QueuedEmail::NameTracking.create_email(name_tracker, naming)
-    email.send_email
-    dump = QueuedEmail.last.dump
-    assert_match(/name_tracker/i, dump)
-    assert_match(/naming/i, dump)
-    assert_match(/rolf/i, dump)
-    assert_match(/mary/i, dump)
-  end
-
   def test_send_email_exception
     raises_exception = -> { raise(RuntimeError.new) }
     email = QueuedEmail::NameChange.new(user: rolf, to_user: mary)
