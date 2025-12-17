74bad56c9c7b8089d802c1b7f77a18bd5ba6011c
Joe Cohen <jdcohenesq@gmail.com>
2025-12-16 19:34:32 -0800

    Translation instead of hardcoded string for unknown user's name

--- app/helpers/object_link_helper.rb
+++ app/helpers/object_link_helper.rb
@@ -190,7 +190,7 @@ module ObjectLinkHelper
   #
   def user_link(user, name = nil, args = {})
     if !user
-      return "?".html_safe
+      return :unknown_user_name.t.html_safe # rubocop:disable Rails/OutputSafety
     elsif user.is_a?(Integer)
       name ||= "#{:USER.t} ##{user}"
       user_id = user
