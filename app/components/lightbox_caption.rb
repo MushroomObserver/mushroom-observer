ef0c55fea5125517965a6a7630bdce227f3e1f18
andrew nimmo <andrnimm@fastmail.fm>
2025-12-30 20:58:06 -0800

    Update lightbox_caption.rb
    Tidy up.

--- app/components/lightbox_caption.rb
+++ app/components/lightbox_caption.rb
@@ -10,31 +10,29 @@
 # Both types include image links (original, EXIF) at the bottom.
 #
 # @example With observation
-#   render LightboxCaption.new(
+#   render(Components::LightboxCaption.new(
 #     user: @user,
 #     image: @image,
 #     image_id: @image.id,
 #     obs: @observation,
 #     identify: true
-#   )
+#   ))
 #
 # @example With observation_view from controller (turbo stream)
-#   render LightboxCaption.new(
+#   render(Components::LightboxCaption.new(
 #     user: @user,
 #     obs: @observation,
 #     identify: true,
 #     observation_view: @observation_view
-#   )
+#   ))
 #
 # @example With image only
-#   render LightboxCaption.new(
+#   render(Components::LightboxCaption.new(
 #     user: @user,
 #     image: @image,
 #     image_id: @image.id
-#   )
+#   ))
 class Components::LightboxCaption < Components::Base
-  include Phlex::Rails::Helpers::LinkTo
-
   prop :user, _Nilable(User)
   prop :image, _Nilable(::Image), default: nil
   prop :image_id, _Nilable(Integer), default: nil
@@ -96,7 +94,7 @@ class Components::LightboxCaption < Components::Base
 
   def render_obs_when
     p(class: "obs-when", id: "observation_when") do
-      plain("#{:WHEN.t}: ")
+      plain("#{:WHEN.l}: ")
       b { @obs.when.web_date }
     end
   end
@@ -142,7 +140,7 @@ class Components::LightboxCaption < Components::Base
 
     p(class: "obs-where-gps", id: "observation_where_gps") do
       render_gps_link if @obs.reveal_location?(@user)
-      i { "(#{:show_observation_gps_hidden.t})" } if @obs.gps_hidden
+      i { "(#{:show_observation_gps_hidden.l})" } if @obs.gps_hidden
     end
   end
 
@@ -159,7 +157,7 @@ class Components::LightboxCaption < Components::Base
     obs_user = @obs.user
 
     p(class: "obs-who", id: "observation_who") do
-      plain("#{:WHO.t}: ")
+      plain("#{:WHO.l}: ")
       render_obs_user(obs_user)
       render_contact_link(obs_user) if show_contact_link?(obs_user)
     end
@@ -203,7 +201,7 @@ class Components::LightboxCaption < Components::Base
 
   def formatted_truncated_notes
     @obs.notes_show_formatted.truncate(150, separator: " ").
-      sub(/\A/, "#{:NOTES.t}: ").wring_out_textile.tpl
+      sub(/\A/, "#{:NOTES.l}: ").wring_out_textile.tpl
   end
 
   def render_image_caption
