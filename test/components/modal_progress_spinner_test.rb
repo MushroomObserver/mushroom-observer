ad41072b26814929224499521f236aa289962c96
andrew nimmo <andrnimm@fastmail.fm>
2026-01-01 12:54:33 -0800

    Update modal_progress_spinner_test.rb

--- test/components/modal_progress_spinner_test.rb
+++ test/components/modal_progress_spinner_test.rb
@@ -2,34 +2,24 @@
 
 require("test_helper")
 
-class ModalProgressSpinnerTest < UnitTestCase
-  include ComponentTestHelper
-
-  def test_renders_modal_structure
+class ModalProgressSpinnerTest < ComponentTestCase
+  def test_renders_non_dismissible_modal_with_spinner
     html = render(Components::ModalProgressSpinner.new)
 
-    assert_includes(html, 'id="modal_progress_spinner"')
-    assert_includes(html, 'class="modal"')
-    assert_includes(html, 'data-controller="modal"')
-  end
+    # Modal structure with non-dismissible settings
+    assert_html(html, "#modal_progress_spinner.modal",
+                attribute: { "data-controller": "modal" })
+    assert_html(html, "#modal_progress_spinner",
+                attribute: { "data-keyboard": "false" })
+    assert_html(html, "#modal_progress_spinner",
+                attribute: { "data-backdrop": "static" })
 
-  def test_renders_non_dismissible_modal
-    html = render(Components::ModalProgressSpinner.new)
+    # Caption and spinner elements
+    assert_html(html, "#modal_progress_spinner_caption")
+    assert_html(html, ".spinner-right.mx-2")
 
-    assert_includes(html, 'data-keyboard="false"')
-    assert_includes(html, 'data-backdrop="static"')
-  end
-
-  def test_renders_caption_and_spinner
-    html = render(Components::ModalProgressSpinner.new)
-
-    assert_includes(html, 'id="modal_progress_spinner_caption"')
-    assert_includes(html, 'class="spinner-right mx-2"')
-  end
-
-  def test_listens_for_section_update_event
-    html = render(Components::ModalProgressSpinner.new)
-
-    assert_includes(html, "section-update:updated@window->modal#hide")
+    # Listens for section update to auto-hide
+    assert_html(html,
+                "[data-action*='section-update:updated@window->modal#hide']")
   end
 end
