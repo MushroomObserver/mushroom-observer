d8fb3e7ec7a662c743d4a78497a49b28a7f1e7ca
Nathan Wilson <nathan@collectivesource.com>
2026-01-05 09:05:30 -0500

    Rubocop

--- app/components/sidebar/languages.rb
+++ app/components/sidebar/languages.rb
@@ -18,9 +18,7 @@ module Components
       register_value_helper :reload_with_args
 
       # Make request available to registered helpers
-      def request
-        @request
-      end
+      attr_reader :request
 
       def view_template
         div(class: "list-group-item pl-3 overflow-visible") do
