1e9637d93f8738f9fd5070908355bef3cc078abe
Joe Cohen <jdcohenesq@gmail.com>
2026-03-10 06:14:41 -0700

    Restore lines inadvertently dropped in merger repairs

--- app/classes/api2/name_api.rb
+++ app/classes/api2/name_api.rb
@@ -71,11 +71,13 @@ class API2
     def build_object
       params = create_params
       parse_name_author_rank_deprecated
+      @log = parse(:boolean, :log, default: true)
       done_parsing_parameters!
       validate_create_parameters!(params)
       parse = make_sure_name_parses!
       make_sure_name_doesnt_exist!(parse)
       name = create_name(parse, params)
+      name.user_log(@user, :log_name_created) if @log
       save_parents(parse)
       after_create(name)
       name
@@ -196,7 +198,12 @@ class API2
       return unless parse.parent_name
 
       parents = Name.find_or_create_name_and_parents(@user, parse.parent_name)
-      parents.each { |n| n.save if n&.new_record? }
+      parents.each do |n|
+        next unless n&.new_record?
+
+        n.save || raise(CreateFailed.new(n))
+        n.user_log(@user, :log_name_created) if @log
+      end
     end
 
     # ----------------------------------------
