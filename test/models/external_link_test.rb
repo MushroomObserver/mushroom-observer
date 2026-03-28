f583f7a5f07b9d83efad53e182f99fdb1c458fe6
Joe Cohen <jdcohenesq@gmail.com>
2026-03-28 06:48:29 -0700

    Add failing test
    also improving assertion failure messages

--- test/models/external_link_test.rb
+++ test/models/external_link_test.rb
@@ -13,16 +13,18 @@ class ExternalLinkTest < UnitTestCase
       external_site: site,
       url: "#{base_url}plus_id"
     )
-    assert_not_nil(link)
-    assert_empty(link.errors)
+    assert_not_nil(link, "ExternalLink should be created")
+    assert_empty(link.errors, "ExternalLink should have no errors")
   end
 
   def test_create_missing_attributes
     link = ExternalLink.create
-    assert_not_empty(link.errors[:user])
-    assert_not_empty(link.errors[:observation])
-    assert_not_empty(link.errors[:external_site])
-    assert_not_empty(link.errors[:url])
+    assert_not_empty(link.errors[:user], "ExternalLink should require a user")
+    assert_not_empty(link.errors[:observation],
+                     "ExternalLink should require an observation")
+    assert_not_empty(link.errors[:external_site],
+                     "ExternalLink should require an external_site")
+    assert_not_empty(link.errors[:url], "ExternalLink should require a url")
   end
 
   def test_create_validate_url
@@ -31,14 +33,16 @@ class ExternalLinkTest < UnitTestCase
 
     link = ExternalLink.create(url: "#{base_url}#{"toolong" * 100}",
                                external_site: site)
-    assert_not_empty(link.errors[:url])
+    assert_not_empty(link.errors[:url],
+                     "URL that is too long should be invalid")
 
     link = ExternalLink.create(url: "#{base_url}invalid url",
                                external_site: site)
-    assert_not_empty(link.errors[:url])
+    assert_not_empty(link.errors[:url],
+                     "URL with spaces should be invalid")
 
     link = ExternalLink.create(url: "#{base_url}url", external_site: site)
-    assert_empty(link.errors[:url])
+    assert_empty(link.errors[:url], "Valid URL should have no url errors")
   end
 
   # iNaturalist's Cloudflare CDN blocks automated HEAD requests with 403,
@@ -53,9 +57,27 @@ class ExternalLinkTest < UnitTestCase
       link = ExternalLink.create!(
         user: dick, observation: obs, external_site: site, url: url
       )
-      assert_empty(link.errors)
-      assert_equal(url, link.url)
+      assert_empty(link.errors, "iNat link should be created without errors")
+      assert_equal(url, link.url, "iNat link URL should be saved unchanged")
     end
+  end
+
+  # iNat URLs must be only the basse url plus an observation numberic id,
+  # e.g. /observations/12345.
+  # A URL like /observations/abc should be invalid.
+  def test_inaturalist_link_requires_numeric_id
+    site = external_sites(:inaturalist)
+    obs = observations(:minimal_unknown_obs)
+    url = "#{site.base_url}notanumber"
+
+    link = ExternalLink.create(
+      user: dick, observation: obs, external_site: site, url: url
+    )
+    assert_not_empty(
+      link.errors[:url],
+      "#{url} is not a valid iNat external link URL. " \
+      "It should be just #{site.base_url} + a numeric ID"
+    )
   end
 
   def test_uniqueness
@@ -64,7 +86,8 @@ class ExternalLinkTest < UnitTestCase
     base_url = site.base_url
 
     another_obs = observations(:minimal_unknown_obs)
-    assert_not_equal(link1.observation.id, another_obs.id)
+    assert_not_equal(link1.observation.id, another_obs.id,
+                     "Fixture observations should be different")
 
     # same observation
     link2 = ExternalLink.create(
@@ -73,7 +96,8 @@ class ExternalLinkTest < UnitTestCase
       external_site: site,
       url: "#{base_url}and_an_id"
     )
-    assert_not_empty(link2.errors)
+    assert_not_empty(link2.errors,
+                     "Duplicate link for same observation should be invalid")
 
     # different observation
     link3 = ExternalLink.create(
@@ -82,6 +106,7 @@ class ExternalLinkTest < UnitTestCase
       external_site: site,
       url: "#{base_url}and_an_id"
     )
-    assert_empty(link3.errors)
+    assert_empty(link3.errors,
+                 "Link for different observation should be valid")
   end
 end
