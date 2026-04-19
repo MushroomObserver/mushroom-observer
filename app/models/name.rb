d11140418a490837261c68e1644e298c64311ded
Joe Cohen <jdcohenesq@gmail.com>
2026-04-19 06:34:46 -0700

    Fix Layout/ExtraSpacing offense

--- app/models/name.rb
+++ app/models/name.rb
@@ -304,6 +304,8 @@ class Name < AbstractModel
     Section: 460,
     Subgenus: 480,
     Genus: 500,
+    # disable cop for readability
+    # rubocop:disable Layout/ExtraSpacing
     Subtribe: 520,  # -inae
     # Tribe: 540,   # -eae
     Subfamily: 560, # -oideae
@@ -314,6 +316,7 @@ class Name < AbstractModel
     Class: 800,     # -mycetes
     Subphylum: 850, # -mycotina
     Phylum: 900,    # -mycota
+    # rubocop:enable Layout/ExtraSpacing
     Kingdom: 1000,
     Domain: 1100
   }
